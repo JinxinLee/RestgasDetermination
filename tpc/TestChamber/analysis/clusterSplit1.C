5d7a095ada0625615a4f13dd2635b84a72d01c85
Christian Hoeppner <christian.hoeppner@cern.ch>
2008-07-22 14:06:01 +0000

    bugfix in cluster split

--- tpc/TestChamber/analysis/clusterSplit1.C
+++ tpc/TestChamber/analysis/clusterSplit1.C
@@ -16,10 +16,8 @@ TCtrack clusterSplit1(TCtrack t){
   std::vector<TCcluster> tpc;//will hold clusters in TPC
   std::vector<TCcluster> others;//will hold clusters NOT in TPC
   std::vector<TCcluster> output;
-  std::cout << "t.Cl(): " << t.nCl() << std::endl;
   for(int i=0;i<t.nCl();++i){
     TCcluster c = t.getCl(i);
-    std::cout << "c.getId(): " << c.getId() << std::endl;
     if(c.getId()<=200&&c.getId()<=299){
       tpc.push_back(c);
     }
@@ -32,8 +30,6 @@ TCtrack clusterSplit1(TCtrack t){
     output.push_back( others.at(i) );
   }
   others.clear();//not needed anymore ->destruct these copies
-
-  std::cout << "tpc.size() : " << tpc.size() << std::endl;
 
 
   //loop over clusters in tpc
@@ -49,39 +45,33 @@ TCtrack clusterSplit1(TCtrack t){
     sort(raw.begin(),raw.end(),TCclusterSortU());
 
     
+
+    std::cout << "@@@@@" << std::endl;
+    for(int i=0;i<raw.size();++i){
+      raw.at(i).print();
+    }
+    std::cout << "@@@@@%%%%%%%" << std::endl;
+
     std::vector<TCcluster> buf;
     buf.push_back( raw.at(0) );
-    std::cout << "raw.size() : " << raw.size() << std::endl;
     if(raw.size()>1){//more than one raw cluster, so sort in padrows
-      std::cout << "## " << buf.at(0).posUVW().X() << std::endl;
       for(int iraw=1;iraw<raw.size();++iraw){
-	std::cout << "##$$ " << raw.at(iraw).posUVW().X() << std::endl;
-	std::cout << "ALPHA" << std::endl;
-	
 	if(fabs(buf.at(0).posUVW().X()-raw.at(iraw).posUVW().X())<1.E-5){//same U pas row
-	  std::cout << "A" << std::endl;
 	  buf.push_back(raw.at(iraw));
-
 	  //if this is the last raw, also push_back onto split
 	  if(iraw==(raw.size()-1)){
-	    std::cout << "A1" << std::endl;
-	    std::cout << "push_back || buf size: " << buf.size() << std::endl;
 	    split.push_back(buf );
 	    buf.clear();
 	  }
 	}
 	else{//next U pad row
-	  std::cout << "B" << std::endl;
 	  //push the buffer on the split vector because this pad row is finished
 	  split.push_back(buf);
-	  std::cout << "push_back || buf size: " << buf.size() << std::endl;
 	  buf.clear();
 	  buf.push_back(raw.at(iraw));
 	  //if this is the last raw, also push_back onto split
 	  if(iraw==(raw.size()-1)){
-	    std::cout << "B1" << std::endl;
-	    std::cout << "push_back || buf size: " << buf.size() << std::endl;
-	    split.push_back(raw);
+	    split.push_back(buf);
 	    buf.clear();
 	  }
 
@@ -90,25 +80,20 @@ TCtrack clusterSplit1(TCtrack t){
     }
     else{//only one raw cluster in this cluster
       split.push_back(buf);
-      std::cout << "C" << std::endl;
-      std::cout << "push_back || buf size: " << buf.size() << std::endl;
+      buf.clear();
     }
-    std::cout << "split.size() : " << split.size() << std::endl;
     for(unsigned int isplit=0;isplit<split.size();++isplit){
-      for(int i=0;i<split.at(isplit).size();++i){
-	std::cout << "@@@@ " << split.at(isplit).at(i).getId() << std::endl;
-      }
       TCcluster arow(split.at(isplit) );
+      std::cout << "####################################" << std::endl;
+      for(int i=0;i<arow.nRaw();++i){
+	arow.getRaw(i).print();
+      }
       arow.setFit(tpc.at(icl).getFit());
       output.push_back(arow);
     }
   }
 
 
-//   for(unsigned int isplit=0;isplit<split.size();++isplit){
-//     TCcluster arow(split.at(isplit) );
-//     output.push_back(arow);
-//   }
   TCtrack returnTrack;
   returnTrack.addClusters(output);
   std::cout << "returnTrack.nCl(): " << returnTrack.nCl() << std::endl;
