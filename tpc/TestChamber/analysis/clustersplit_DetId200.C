5af192206483dbfdb87ed117faef3ef8b73104b5
l woerner <lwoerner>
2008-08-08 04:59:48 +0000

    added gaussian fit to histograms of y and z residuals

--- tpc/TestChamber/analysis/clustersplit_DetId200.C
+++ tpc/TestChamber/analysis/clustersplit_DetId200.C
@@ -38,28 +38,33 @@ void plots(TString files){
 
 
    TH1D *xclust = new TH1D("xclust","",500,-1,1);
-   xclust->SetXTitle("cluster x");
+   xclust->SetXTitle("cluster x [cm]");
    //xclust->SetFillColor(2);
 
    TH1D *xclustw = new TH1D("xclustw","",500,-1,1);
-   xclustw->SetXTitle("cluster x using ClusterSplit1.C");
+   xclustw->SetXTitle("cluster x using ClusterSplit1.C [cm]");
    //xclustw->SetFillColor(2);
 
    TH1D *yclust = new TH1D("yclust","",500,-1,1);
-   yclust->SetXTitle("cluster y");
+   yclust->SetXTitle("cluster y [cm]");
    //yclust->SetFillColor(2);
 
    TH1D *yclustw = new TH1D("yclustw","",500,-1,1);
-   yclustw->SetXTitle("cluster y using ClusterSplit1.C");
+   yclustw->SetXTitle("cluster y using ClusterSplit1.C [cm]");
    //yclustw->SetFillColor(2);
 
    TH1D *zclust = new TH1D("zclust","",500,-1,1);
-   zclust->SetXTitle("cluster z");
+   zclust->SetXTitle("cluster z [cm]");
    //zclust->SetFillColor(2);
 
    TH1D *zclustw = new TH1D("zclustw","",500,-1,1);
-   zclustw->SetXTitle("cluster z using ClusterSplit.C");
+   zclustw->SetXTitle("cluster z using ClusterSplit.C [cm]");
    //zclustw->SetFillColor(2);
+
+  TF1 *fitcurve1 = new TF1("fitcurve1","gaus",-1,1);
+  //  fitcurve->SetLineColor(kRed);
+  TF1 *fitcurve2 = new TF1("fitcurve2","gaus",-1,1);
+  //  fitcurve->SetLineColor(kRed);
 
 
 
@@ -85,6 +90,8 @@ void plots(TString files){
        }
      }
    }
+     yclust->Fit("fitcurve1","R");
+     zclust->Fit("fitcurve2","R");
    
    for (Int_t iev=0;iev<nevent;iev++){
      
@@ -105,8 +112,9 @@ void plots(TString files){
 	 
        }
      }
-
    }
+     yclustw->Fit("fitcurve1","R");
+     zclustw->Fit("fitcurve2","R");
 
   time ( &rawtime );
   std::cout<< "The current time is " << ctime(&rawtime) << std::endl;
