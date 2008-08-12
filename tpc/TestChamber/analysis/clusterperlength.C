49c78f2dced58e911f0fd0f0081e861543cfb099
l woerner <lwoerner>
2008-08-12 12:56:52 +0000

    macro to plot the number of clusters per unit length

---
+++ tpc/TestChamber/analysis/clusterperlength.C
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "TString.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TPad.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include "TPaveText.h"
+#include "TROOT.h"
+
+#include "../src/TCtrack.h"
+#include "../src/TCcluster.h"
+#include "../src/TCclusterSort.h"
+#include <algorithm>
+#include <vector>
+#include <set>
+
+#include "consecCut.C"
+#include "clusterSplit1.C"
+
+
+void plots(TString files){
+
+  //gSystem->Load("libtestChamber"); 
+
+
+  gROOT->Macro("../macro/christian_style.C"); 
+
+  TChain myChain("at");
+
+  cout << "Adding " <<  myChain.Add(files) << " to the chain" << endl;
+
+  Int_t nevent = myChain.GetEntries();
+
+  
+
+  TCtrack *intr=0;
+
+
+   TH1D *nHits = new TH1D("nHits","",500,0,30);
+   nHits->SetXTitle("Distance between two clusters [cm]");
+   //nHits->SetFillColor(2);
+
+   TH1D *nHits1 = new TH1D("nHits1","",500,0,30);
+   nHits1->SetXTitle("Distance between two clusters [cm]");
+   nHits1->SetLineColor(6);
+   //nHits1->SetFillColor(2);
+
+   TH1D *nHits2 = new TH1D("nHits2","",500,0,30);
+   nHits2->SetXTitle("distance between two clusters [cm]");
+   nHits2->SetLineColor(7);
+   //nHits2->SetFillColor(2);
+
+   TH1D *nSelHits = new TH1D("nSelHits","",30,0,30);
+   nSelHits->SetXTitle("Number of Clusters on track");
+   //nSelHits->SetFillColor(6);
+
+   TH1D *clustersperlength = new TH1D("clustersperlength","",250,0,100);
+   clustersperlength->SetXTitle("Number of Clusters per unit length [1/cm]");
+   clustersperlength->SetFillColor(6);
+
+  myChain.SetBranchAddress("track", &intr);
+
+  time_t rawtime;
+  time ( &rawtime );
+  std::cout<< "The current time is " << ctime(&rawtime) << std::endl;
+
+  for (Int_t iev=0;iev<nevent;iev++){
+
+    myChain.GetEntry(iev);
+    TCtrack tr(*intr);
+
+    double n=0;
+
+    for(int i=0;i<tr.nCl();++i){
+	TCcluster c = tr.getCl(i);
+	if(c.getFit()){//was used in fit
+         for(int j=0;j<tr.nCl();++j){
+         TCcluster d = tr.getCl(j);
+	 if(d.getFit()){//was used in fit
+         nHits->Fill(fabs(c.posUVW().X()-d.posUVW().X()));
+         nHits1->Fill(fabs(c.posUVW().Y()-d.posUVW().Y()));
+         nHits2->Fill(fabs(c.posUVW().Z()-d.posUVW().Z()));
+         double a = (fabs(c.posUVW().X()-d.posUVW().X()));
+          for(int m=1;m<tr.nCl()-1;++m){
+          TCcluster k = tr.getCl(m);
+          if(k.getFit()){
+          double l = (fabs(c.posUVW().X()-k.posUVW().X()));
+         if(a<l){
+          n = (fabs(c.posUVW().X()-k.posUVW().X()));
+          //return n;
+          }
+         else{
+          n = (fabs(c.posUVW().X()-d.posUVW().X()));
+          //return n;
+          }
+            }
+           }
+          }
+         }
+ 	}
+      }
+
+    //return n;
+
+    clustersperlength->Fill(tr.nClFit()/n);
+    nSelHits->Fill(tr.nClFit());
+
+  }
+
+  time ( &rawtime );
+  std::cout<< "The current time is " << ctime(&rawtime) << std::endl;
+
+  TCanvas *x = new TCanvas();
+  nHits1->Draw();
+  nHits2->Draw("same");
+  nHits->Draw("same");
+  TCanvas *y = new TCanvas();
+  nSelHits->Draw();
+  TCanvas *z = new TCanvas();
+  clustersperlength->Draw();
+  //x->Update();
+
+}
+
