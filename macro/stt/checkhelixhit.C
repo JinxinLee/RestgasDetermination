30a6853a208d68e48370184a9f1fee74844d679b
Lia Lavezzi <lia.lavezzi@gmail.com>
2008-09-04 14:34:23 +0000

    new checkhelixhits

--- macro/stt/checkhelixhit.C
+++ macro/stt/checkhelixhit.C
@@ -51,9 +51,9 @@
   TCanvas *c = new TCanvas("c", "c", 0, 0, 600, 600);
   c->Divide(1,2);
   TH2F *hxy = new TH2F("hxy","hxy",100,-42,42, 100,-42,42);
-  TH2F *hyz = new TH2F("hyz","hyz",100,-42,42, 100,-40,110);
+  TH2F *hyz = new TH2F("hyz","hyz",100,0,50, 100,-40,110);
    
-  int evt = 0;
+  int evt = 999;
  
   treepnt->GetEntry(evt);
   treedigi->GetEntry(evt);
@@ -64,30 +64,63 @@
   c->cd(2);
   hyz->Draw();
 
+  //  cout << "num tracks " << track->GetEntriesFast() << endl;
+
+  cout << "blue  = Monte Carlo " << endl;
+  cout << "red   = Helix Hit " << endl;
+  cout << "green = Center Of Tubes " << endl;
+
   // tracks loop
   for (Int_t k = 0; k < track->GetEntriesFast(); k++) {
+
     PndSttTrack *stttrack = (PndSttTrack*) track->At(k);
     if(!stttrack) continue;
-   
 
-    for (Int_t j = 0; j < stttrack->GetNofHits() ; j++) {
-      Int_t iHit = stttrack->GetHitIndex(j);
-      PndSttHit *hit = (PndSttHit*) digi->At(iHit);
+    Double_t d0 = stttrack->GetParamLast()->GetX();
+    Double_t phi0 = stttrack->GetParamLast()->GetY();
+    Double_t R = stttrack->GetParamLast()->GetTx();
+    Double_t z0 = stttrack->GetParamLast()->GetZ();
+    Double_t tanl = stttrack->GetParamLast()->GetTy();
+    Double_t h = -(Int_t) stttrack->GetParamLast()->GetQp(); 
+    Double_t ptran = 0.003 * 2 * R;
 
-      // CHECK to be changed
-      PndSttHelixHit *helixhit = NULL;
-      for (Int_t dj = 0; dj < digi->GetEntriesFast(); dj++) {
-	helixhit = (PndSttHelixHit*) hh->At(dj);
-	if(helixhit->GetHitIndex() == iHit) break;
-      }
+    Double_t plong = ptran * tanl;
+    Double_t ptot = sqrt(plong*plong + ptran*ptran);
 
-      // check if hit <-> helix hit association is correct
-      if(helixhit->GetHitIndex() != iHit) cout << "==> " << iHit << " " << helixhit->GetHitIndex() << " " << j << endl;
-      //	else cout << "   " << iHit << " " << helixhit->GetHitIndex()<< endl;
-	
+    Double_t x0 = (d0 + R) * cos(phi0);
+    Double_t y0 = (d0 + R) * sin(phi0);
+
+    cout << "evt" << evt << " track " << k << " num hits " << stttrack->GetNofHelixHits() << " flag " << stttrack->GetFlag() << " ptran " << ptran << " plong "<< plong << " ptot " << ptot << endl;
+
+  c->cd(1);
+      // track
+      TArc *circ = new TArc(x0, y0, R, 0, 360);
+      circ->SetLineColor(k+1);
+      circ->SetFillStyle(0);
+      circ->Draw("SAME");
+   c->cd(2);
+      // track
+      Double_t x1 = d0 * cos(phi0);
+      Double_t y1 = d0 * sin(phi0);
+      PndSttHelixTrackFitter fitter;
+      Double_t scoslT = fitter.CalculateScosl(h, d0, phi0, R, x1, y1);
+     
+      TLine* line = new TLine(scoslT, scoslT*tanl+ z0, 50, 50*tanl + z0);
+      line->SetLineColor(k+1);
+      line->Draw("SAME");
+
+
+    for (Int_t j = 0; j < stttrack->GetNofHelixHits() ; j++) {
+      Int_t iHHit = stttrack->GetHelixHitIndex(j);
+      PndSttHelixHit *helixhit = (PndSttHelixHit*) hh->At(iHHit);
+
+      Int_t hitindex = helixhit->GetHitIndex();
+      PndSttHit* hit = (PndSttHit*) digi->At(hitindex);
       PndSttPoint *mcpoint = (PndSttPoint*) pnt->At(hit->GetRefIndex());
 
       c->cd(1);
+    
+      // points/hits
       TMarker *mrkpnt = new TMarker(mcpoint->GetXtot(), mcpoint->GetYtot(), 2);
       mrkpnt->SetMarkerColor(4);
       mrkpnt->Draw("SAME");
@@ -99,17 +132,45 @@
       mrkhh->Draw("SAME");
 	
       c->cd(2);
-      TMarker *mrkpnt = new TMarker(mcpoint->GetYtot(), mcpoint->GetZtot(), 2);
+    
+      /**
+	 TMarker *mrkpnt = new TMarker(mcpoint->GetYtot(), mcpoint->GetZtot(), 2);
+	 mrkpnt->SetMarkerColor(4);
+	 mrkpnt->Draw("SAME");
+	 TMarker *mrkdigi = new TMarker(hit->GetY(), hit->GetZ(), 6);
+	 mrkdigi->SetMarkerColor(3);
+	 mrkdigi->Draw("SAME");
+	 TMarker *mrkhh = new TMarker(helixhit->GetY(), helixhit->GetZ(), 5);
+	 mrkhh->SetMarkerColor(2);
+	 mrkhh->Draw("SAME");
+      **/
+	
+      //    cout << "MC " <<  mcpoint->GetXtot() 
+      // 	   << " " <<             mcpoint->GetYtot() 
+      // 	   << " " <<             mcpoint->GetZtot() << endl;
+      
+      //   cout << "helixhit " <<  helixhit->GetX() 
+      //        << " " <<             helixhit->GetY() 
+      //        << " " <<             helixhit->GetZ() << endl;
+      
+
+     
+      Double_t scoslMC = fitter.CalculateScosl(h, d0, phi0, R, mcpoint->GetXtot(), mcpoint->GetYtot());
+      TMarker *mrkpnt = new TMarker(scoslMC, mcpoint->GetZtot(), 2);
       mrkpnt->SetMarkerColor(4);
       mrkpnt->Draw("SAME");
-      TMarker *mrkdigi = new TMarker(hit->GetY(), hit->GetZ(), 6);
+
+      Double_t scoslH  = fitter.CalculateScosl(h, d0, phi0, R, hit->GetX(), hit->GetY());
+      TMarker *mrkdigi = new TMarker(scoslH, hit->GetZ(), 6);
       mrkdigi->SetMarkerColor(3);
-      //      mrkdigi->Draw("SAME");
-      TMarker *mrkhh = new TMarker(helixhit->GetY(), helixhit->GetZ(), 5);
+      //  mrkdigi->Draw("SAME");
+
+      Double_t scoslHH = fitter.CalculateScosl(h, d0, phi0, R, helixhit->GetX(), helixhit->GetY());
+      TMarker *mrkhh = new TMarker(scoslHH, helixhit->GetZ(),  5);
       mrkhh->SetMarkerColor(2);
       mrkhh->Draw("SAME");
 	
-
+      //      cout <<  scoslMC<< " " << mcpoint->GetZtot() << endl;
     }
 
   }
