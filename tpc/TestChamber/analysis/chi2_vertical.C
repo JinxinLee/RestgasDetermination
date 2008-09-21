4b77fbdd104a8da25b680351e82c8a73c9387052
l woerner <lwoerner>
2008-09-21 14:36:36 +0000

    small changes

--- tpc/TestChamber/analysis/chi2_vertical.C
+++ tpc/TestChamber/analysis/chi2_vertical.C
@@ -38,12 +38,27 @@ void plots(TString files){
   TH1D *chi2NDF = new TH1D("chi2NDF","",200,-2,10);
   chi2NDF->SetLineColor(6);
 
-  TH1D *chi2 = new TH1D("chi2","",200,-2,10);
+  TH1D *chi2NDF_2clust = new TH1D("chi2NDF_2clust","",200,-2,10);
+  chi2NDF_2clust->SetLineColor(2);
+
+  TH1D *chi2NDF_3clust = new TH1D("chi2NDF_3clust","",200,-2,10);
+  chi2NDF_3clust->SetLineColor(3);
+
+  TH1D *chi2NDF_4clust = new TH1D("chi2NDF_4clust","",200,-2,10);
+  chi2NDF_4clust->SetLineColor(4);
 
   TH1D *chi2NDFw = new TH1D("chi2NDFw","",200,-2,10);
   chi2NDFw->SetLineColor(6);
 
-  TH1D *chi2w = new TH1D("chi2w","",200,-2,10);
+  TH1D *chi2NDF_2clustw = new TH1D("chi2NDF_2clustw","",200,-2,10);
+  chi2NDF_2clustw->SetLineColor(2);
+
+  TH1D *chi2NDF_3clustw = new TH1D("chi2NDF_3clustw","",200,-2,10);
+  chi2NDF_3clustw->SetLineColor(3);
+
+  TH1D *chi2NDF_4clustw = new TH1D("chi2NDF_4clustw","",200,-2,10);
+  chi2NDF_4clustw->SetLineColor(4);
+
 
   myChain.SetBranchAddress("track", &intr);
 
@@ -56,21 +71,66 @@ void plots(TString files){
 
 
     chi2NDF->Fill(tr.getChi2()/tr.getNDF());
-    chi2->Fill(tr.getChi2());
 
     chi2NDFw->Fill(trSplit.getChi2()/trSplit.getNDF());
-    chi2w->Fill(trSplit.getChi2());
 
+  }
+
+  for (Int_t iev=0;iev<nevent;iev++){
+     
+    myChain.GetEntry(iev);
+    TCtrack tr(*intr);
+    TCtrack trSplit = clusterSplit1(tr);
+    trSplit.fit(260);
+    if (tr.nCl()<2) continue;
+
+
+    chi2NDF_2clust->Fill(tr.getChi2()/tr.getNDF());
+
+    chi2NDF_2clustw->Fill(trSplit.getChi2()/trSplit.getNDF());
+
+  }
+
+  for (Int_t iev=0;iev<nevent;iev++){
+     
+    myChain.GetEntry(iev);
+    TCtrack tr(*intr);
+    TCtrack trSplit = clusterSplit1(tr);
+    trSplit.fit(260);
+    if (tr.nCl()<3) continue;
+
+
+    chi2NDF_3clust->Fill(tr.getChi2()/tr.getNDF());
+
+    chi2NDF_3clustw->Fill(trSplit.getChi2()/trSplit.getNDF());
+
+  }
+  for (Int_t iev=0;iev<nevent;iev++){
+     
+    myChain.GetEntry(iev);
+    TCtrack tr(*intr);
+    TCtrack trSplit = clusterSplit1(tr);
+    trSplit.fit(260);
+    if (tr.nCl()<4) continue;
+
+
+    chi2NDF_4clust->Fill(tr.getChi2()/tr.getNDF());
+
+    chi2NDF_4clustw->Fill(trSplit.getChi2()/trSplit.getNDF());
 
   }
 
   TCanvas *chi = new TCanvas("chi","");
   chi2NDF->Draw();
-  chi2->Draw("same");
+  chi2NDF_2clust->Draw("same");
+  chi2NDF_3clust->Draw("same");
+  chi2NDF_4clust->Draw("same");
 
   TCanvas *chiw = new TCanvas("chiw","");
   chi2NDFw->Draw();
-  chi2w->Draw("same");
+  chi2NDF_2clustw->Draw("same");
+  chi2NDF_3clustw->Draw("same");
+  chi2NDF_4clustw->Draw("same");
 
 }
 
