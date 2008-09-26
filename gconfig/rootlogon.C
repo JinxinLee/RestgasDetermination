cb242762f5e4fe504fe83dd6ca60186c231fa733
Stefano Spataro <spataro@to.infn.it>
2008-09-26 13:02:36 +0000

    Added libSttReco

--- gconfig/rootlogon.C
+++ gconfig/rootlogon.C
@@ -13,12 +13,13 @@ rootlogon()
 	gSystem->Load("libPassive");
 	gSystem->Load("libGen");
 	gSystem->Load("libPGen");
-	gSystem->Load("libStt");
 	gSystem->Load("libEmc"); 
         gSystem->Load("libgenfit");
 	gSystem->Load("libtrackrep");
         gSystem->Load("libtpc"); 
         gSystem->Load("libtpcreco");
+        gSystem->Load("libStt");
+        gSystem->Load("libSttReco");
 	gSystem->Load("libTrkBase");
         gSystem->Load("librecotasks");
         gSystem->Load("libMvd");
