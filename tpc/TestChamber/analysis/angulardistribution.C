791fbfd2c58e891cf14eeace715274033b870351
l woerner <lwoerner>
2008-10-08 11:32:42 +0000

    added cuts

--- tpc/TestChamber/analysis/angulardistribution.C
+++ tpc/TestChamber/analysis/angulardistribution.C
@@ -6,6 +6,8 @@
 #include "TPad.h"
 #include "TH1.h"
 #include "TF1.h"
+#include "TH2.h"
+#include "TF2.h"
 #include "TStyle.h"
 #include "TSystem.h"
 #include "TPaveText.h"
@@ -32,42 +34,45 @@ void angulars(TString files){
 
   TCtrack *intr=0;
 
-   TH1D *theta = new TH1D("theta","",500,-100,100);
-   theta->SetXTitle("theta");
-   TH1D *thetax = new TH1D("thetax","",500,-100,100);
-   thetax->SetXTitle("theta x");
-   TH1D *thetay = new TH1D("thetay","",500,-100,100);
-   thetay->SetXTitle("theta y ");
-
-   TH1D *theta_2clust = new TH1D("theta_2clust","",500,-100,100);
-   theta_2clust->SetXTitle("theta");
-   theta_2clust->SetLineColor(2);
-   TH1D *thetax_2clust = new TH1D("thetax_2clust","",500,-100,100);
-   thetax_2clust->SetXTitle("theta x");
-   thetax_2clust->SetLineColor(2);
-   TH1D *thetay_2clust = new TH1D("thetay_2clust","",500,-100,100);
-   thetay_2clust->SetXTitle("theta y ");
-   thetay_2clust->SetLineColor(2);
-
-   TH1D *theta_3clust = new TH1D("theta_3clust","",500,-100,100);
-   theta_3clust->SetXTitle("theta");
-   theta_3clust->SetLineColor(3);
-   TH1D *thetax_3clust = new TH1D("thetax_3clust","",500,-100,100);
-   thetax_3clust->SetXTitle("theta x");
-   thetax_3clust->SetLineColor(3);
-   TH1D *thetay_3clust = new TH1D("thetay_3clust","",500,-100,100);
-   thetay_3clust->SetXTitle("theta y ");
-   thetay_3clust->SetLineColor(3);
+   TH2D *theta_vs_thetaX = new TH2D("theta_vs_thetaX","",500,-100,100,500,-100,100);
+   theta_vs_thetaX->SetXTitle("theta");
+   theta_vs_thetaX->SetYTitle("theta x");
+   TH2D *theta_vs_thetaY = new TH2D("theta_vs_thetaY","",500,-100,100,500,-100,100);
+   theta_vs_thetaY->SetXTitle("theta");
+   theta_vs_thetaY->SetYTitle("theta y");
+   TH2D *thetaY_vs_thetaX = new TH2D("thetaY_vs_thetaX","",500,-100,100,500,-100,100);
+   theta_vs_thetaX->SetXTitle("theta y");
+   theta_vs_thetaX->SetYTitle("theta x");
 
    TH1D *theta_4clust = new TH1D("theta_4clust","",500,-100,100);
    theta_4clust->SetXTitle("theta");
-   theta_4clust->SetLineColor(4);
+   theta_4clust->SetLineColor(2);
    TH1D *thetax_4clust = new TH1D("thetax_4clust","",500,-100,100);
    thetax_4clust->SetXTitle("theta x");
-   thetax_4clust->SetLineColor(4);
+   thetax_4clust->SetLineColor(2);
    TH1D *thetay_4clust = new TH1D("thetay_4clust","",500,-100,100);
    thetay_4clust->SetXTitle("theta y ");
-   thetay_4clust->SetLineColor(4);
+   thetay_4clust->SetLineColor(2);
+
+   TH1D *theta_6clust = new TH1D("theta_6clust","",500,-100,100);
+   theta_6clust->SetXTitle("theta");
+   theta_6clust->SetLineColor(3);
+   TH1D *thetax_6clust = new TH1D("thetax_6clust","",500,-100,100);
+   thetax_6clust->SetXTitle("theta x");
+   thetax_6clust->SetLineColor(3);
+   TH1D *thetay_6clust = new TH1D("thetay_6clust","",500,-100,100);
+   thetay_6clust->SetXTitle("theta y ");
+   thetay_6clust->SetLineColor(3);
+
+   TH1D *theta_8clust = new TH1D("theta_8clust","",500,-100,100);
+   theta_8clust->SetXTitle("theta");
+   theta_8clust->SetLineColor(4);
+   TH1D *thetax_8clust = new TH1D("thetax_8clust","",500,-100,100);
+   thetax_8clust->SetXTitle("theta x");
+   thetax_8clust->SetLineColor(4);
+   TH1D *thetay_8clust = new TH1D("thetay_8clust","",500,-100,100);
+   thetay_8clust->SetXTitle("theta y ");
+   thetay_8clust->SetLineColor(4);
 
   myChain.SetBranchAddress("track", &intr);
 
@@ -79,86 +84,58 @@ void angulars(TString files){
      if (fabs(tr.getAx()) > 1.E3) continue;
      if (fabs(tr.getAy()) > 1.E3) continue;
      //if (tr.getChi2()/tr.getNDF()>4) continue;
-     //if (tr.getChi2()/tr.getNDF()<0.1) continue;
-     //if (tr.nClFit()<2) continue;
-
-	  thetay->Fill(tr.getThY());
-	  thetax->Fill(tr.getThX());
-          theta->Fill(tr.getTh());
-
-  }
-
-  for (Int_t i=0;i<nevent;i++){
-
-    myChain.GetEntry(i);
-    TCtrack tr(*intr);
-
-     if (fabs(tr.getAx()) > 1.E3) continue;
-     if (fabs(tr.getAy()) > 1.E3) continue;
-     //if (tr.getChi2()/tr.getNDF()>4) continue;
-     //if (tr.getChi2()/tr.getNDF()<0.1) continue;
-     if (tr.nClFit()<2) continue;
-
-	  thetay_2clust->Fill(tr.getThY());
-	  thetax_2clust->Fill(tr.getThX());
-          theta_2clust->Fill(tr.getTh());
-
-  }
-
-  for (Int_t i=0;i<nevent;i++){
-
-    myChain.GetEntry(i);
-    TCtrack tr(*intr);
-
-     if (fabs(tr.getAx()) > 1.E3) continue;
-     if (fabs(tr.getAy()) > 1.E3) continue;
-     //if (tr.getChi2()/tr.getNDF()>4) continue;
-     //if (tr.getChi2()/tr.getNDF()<0.1) continue;
-     if (tr.nClFit()<3) continue;
-
-	  thetay_3clust->Fill(tr.getThY());
-	  thetax_3clust->Fill(tr.getThX());
-          theta_3clust->Fill(tr.getTh());
-
-  }
-
-  for (Int_t i=0;i<nevent;i++){
-
-    myChain.GetEntry(i);
-    TCtrack tr(*intr);
-
-     if (fabs(tr.getAx()) > 1.E3) continue;
-     if (fabs(tr.getAy()) > 1.E3) continue;
-     //if (tr.getChi2()/tr.getNDF()>4) continue;
-     //if (tr.getChi2()/tr.getNDF()<0.1) continue;
-     if (tr.nClFit()<4) continue;
+     if (tr.getChi2()/tr.getNDF()<0.1) continue;
+     if (tr.nClFit()<5) continue;
 
 	  thetay_4clust->Fill(tr.getThY());
 	  thetax_4clust->Fill(tr.getThX());
           theta_4clust->Fill(tr.getTh());
+	  theta_vs_thetaX->Fill(tr.getTh(),tr.getThX());
+	  theta_vs_thetaY->Fill(tr.getTh(),tr.getThY());
+	  thetaY_vs_thetaX->Fill(tr.getThY(),tr.getThX());
+
+  }
+
+  for (Int_t i=0;i<nevent;i++){
+
+    myChain.GetEntry(i);
+    TCtrack tr(*intr);
+
+     if (fabs(tr.getAx()) > 1.E3) continue;
+     if (fabs(tr.getAy()) > 1.E3) continue;
+     //if (tr.getChi2()/tr.getNDF()>4) continue;
+     if (tr.getChi2()/tr.getNDF()<0.1) continue;
+     if (tr.nClFit()<7) continue;
+
+	  thetay_6clust->Fill(tr.getThY());
+	  thetax_6clust->Fill(tr.getThX());
+          theta_6clust->Fill(tr.getTh());
+
+  }
+
+  for (Int_t i=0;i<nevent;i++){
+
+    myChain.GetEntry(i);
+    TCtrack tr(*intr);
+
+     if (fabs(tr.getAx()) > 1.E3) continue;
+     if (fabs(tr.getAy()) > 1.E3) continue;
+     //if (tr.getChi2()/tr.getNDF()>4) continue;
+     if (tr.getChi2()/tr.getNDF()<0.1) continue;
+     if (tr.nClFit()<9) continue;
+
+	  thetay_8clust->Fill(tr.getThY());
+	  thetax_8clust->Fill(tr.getThX());
+          theta_8clust->Fill(tr.getTh());
 
   }
 
   TCanvas *th = new TCanvas();
-  theta->Draw();
+  theta_vs_thetaX->Draw("colz");
   TCanvas *thy = new TCanvas();
-  thetay->Draw();
+  theta_vs_thetaY->Draw("colz");
   TCanvas *thx = new TCanvas();
-  thetax->Draw();
-
-  th = new TCanvas();
-  theta_2clust->Draw();
-  thy = new TCanvas();
-  thetay_2clust->Draw();
-  thx = new TCanvas();
-  thetax_2clust->Draw();
-
-  th = new TCanvas();
-  theta_3clust->Draw();
-  thy = new TCanvas();
-  thetay_3clust->Draw();
-  thx = new TCanvas();
-  thetax_3clust->Draw();
+  thetaY_vs_thetaX->Draw("colz");
 
   th = new TCanvas();
   theta_4clust->Draw();
@@ -167,22 +144,33 @@ void angulars(TString files){
   thx = new TCanvas();
   thetax_4clust->Draw();
 
+  th = new TCanvas();
+  theta_6clust->Draw();
+  thy = new TCanvas();
+  thetay_6clust->Draw();
+  thx = new TCanvas();
+  thetax_6clust->Draw();
 
   th = new TCanvas();
-  theta->Draw();
-  theta_2clust->Draw("same");
-  theta_3clust->Draw("same");
-  theta_4clust->Draw("same");
+  theta_8clust->Draw();
   thy = new TCanvas();
-  thetay->Draw();
-  thetay_2clust->Draw("same");
-  thetay_3clust->Draw("same");
-  thetay_4clust->Draw("same");
+  thetay_8clust->Draw();
   thx = new TCanvas();
-  thetax->Draw();
-  thetax_2clust->Draw("same");
-  thetax_3clust->Draw("same");
-  thetax_4clust->Draw("same");
+  thetax_8clust->Draw();
+
+
+  th = new TCanvas();
+  theta_4clust->Draw();
+  theta_6clust->Draw("same");
+  theta_8clust->Draw("same");
+  thy = new TCanvas();
+  thetay_4clust->Draw();
+  thetay_6clust->Draw("same");
+  thetay_8clust->Draw("same");
+  thx = new TCanvas();
+  thetax_4clust->Draw();
+  thetax_6clust->Draw("same");
+  thetax_8clust->Draw("same");
 
 }
 
