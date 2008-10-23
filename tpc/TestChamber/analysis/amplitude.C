05b75e83d53bab8790ca03f32afe3bdf2c244275
l woerner <lwoerner>
2008-10-23 10:08:15 +0000

    added histograms

--- tpc/TestChamber/analysis/amplitude.C
+++ tpc/TestChamber/analysis/amplitude.C
@@ -11,6 +11,7 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TF1.h"
+#include "TProfile.h"
 #include "TStyle.h"
 #include "TSystem.h"
 #include "TPaveText.h"
@@ -38,10 +39,39 @@ void position(TString files){
   //define the histograms
   //without using clusterSplit1.C
 
+  TH2D *amp_cl_u = new TH2D("amp_cl_u","",10,0,10,100,0,5000);
+  TH2D *amp_cl_v = new TH2D("amp_cl_v","",10,0,1,100,0,5000);
+  TH2D *amp_cl_w = new TH2D("amp_cl_w","",100,0,10,100,0,5000);
 
-  TH2D *amp_u = new TH2D("apm_u","",10,0,10,100,0,5000);
+  TProfile *prof_amp_cl_u = new TProfile("prof_amp_cl_u","",100,0,10,0,2000);
+  TProfile *prof_amp_cl_v = new TProfile("prof_amp_cl_v","",500,0,1,0,2000);
+  TProfile *prof_amp_cl_w = new TProfile("prof_amp_cl_w","",100,0,10,0,2000);
+
+  TH1D *amp_cl_v_1 = new TH1D("amp_cl_v_1","",100,0,2000);
+  TH1D *amp_cl_v_2 = new TH1D("amp_cl_v_2","",100,0,2000);
+  TH1D *amp_cl_v_3 = new TH1D("amp_cl_v_3","",100,0,2000);
+  TH1D *amp_cl_v_4 = new TH1D("amp_cl_v_4","",100,0,2000);
+  TH1D *amp_cl_v_5 = new TH1D("amp_cl_v_5","",100,0,2000);
+  TH1D *amp_cl_v_6 = new TH1D("amp_cl_v_6","",100,0,2000);
+  TH1D *amp_cl_v_7 = new TH1D("amp_cl_v_7","",100,0,2000);
+  TH1D *amp_cl_v_8 = new TH1D("amp_cl_v_8","",100,0,2000);
+
+  TH2D *amp_u = new TH2D("amp_u","",10,0,10,100,0,5000);
   TH2D *amp_v = new TH2D("amp_v","",10,0,1,100,0,5000);
   TH2D *amp_w = new TH2D("amp_w","",100,0,10,100,0,5000);
+
+  TProfile *prof_amp_u = new TProfile("prof_amp_u","",100,0,10,0,2000);
+  TProfile *prof_amp_v = new TProfile("prof_amp_v","",100,0,1,0,2000);
+  TProfile *prof_amp_w = new TProfile("prof_amp_w","",100,0,10,0,2000);
+
+  TH1D *amp_v_1 = new TH1D("amp_v_1","",100,0,2000);
+  TH1D *amp_v_2 = new TH1D("amp_v_2","",100,0,2000);
+  TH1D *amp_v_3 = new TH1D("amp_v_3","",100,0,2000);
+  TH1D *amp_v_4 = new TH1D("amp_v_4","",100,0,2000);
+  TH1D *amp_v_5 = new TH1D("amp_v_5","",100,0,2000);
+  TH1D *amp_v_6 = new TH1D("amp_v_6","",100,0,2000);
+  TH1D *amp_v_7 = new TH1D("amp_v_7","",100,0,2000);
+  TH1D *amp_v_8 = new TH1D("amp_v_8","",100,0,2000);
 
   myChain.SetBranchAddress("track", &intr);
 
@@ -54,19 +84,48 @@ void position(TString files){
 	
 	for(int i=0;i<tr.nCl();++i){
 	  TCcluster c = tr.getCl(i);
-	   if(!c.getFit()) continue;
+	  if(!c.getFit()) continue;
 	  //if(c.nPadY()!=2) continue;
 	  //	  if(c.nTime()!=1) continue;
 
-	   // if(c.nRaw()!=2)continue;
+	  // if(c.nRaw()!=2)continue;
+
+	  amp_cl_u->Fill(c.posUVW().X(),c.getAmp());
+	  amp_cl_v->Fill(c.posUVW().Y(),c.getAmp());
+	  amp_cl_w->Fill(c.posUVW().Z(),c.getAmp());
+
+	  prof_amp_cl_u->Fill(c.posUVW().X(),c.getAmp());
+	  prof_amp_cl_v->Fill(c.posUVW().Y(),c.getAmp());
+	  prof_amp_cl_w->Fill(c.posUVW().Z(),c.getAmp());
+
+	  if(c.posUVW().Y()<0.1) amp_cl_v_1->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.1&&c.posUVW().Y()<0.2) amp_cl_v_2->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.2&&c.posUVW().Y()<0.3) amp_cl_v_3->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.3&&c.posUVW().Y()<0.4) amp_cl_v_4->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.4&&c.posUVW().Y()<0.5) amp_cl_v_5->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.5&&c.posUVW().Y()<0.6) amp_cl_v_6->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.6&&c.posUVW().Y()<0.7) amp_cl_v_7->Fill(c.getAmp());
+	  if(c.posUVW().Y()>0.7) amp_cl_v_8->Fill(c.getAmp());
 
 	  for(int j=0;j<c.nRaw();++j){
 
-	  amp_u->Fill(c.getRaw(j).posUVW().X(),c.getRaw(j).getAmp());
-	  amp_v->Fill(c.getRaw(j).posUVW().Y(),c.getRaw(j).getAmp());
-	  amp_w->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).getAmp());
+		amp_u->Fill(c.getRaw(j).posUVW().X(),c.getRaw(j).getAmp());
+		amp_v->Fill(c.getRaw(j).posUVW().Y(),c.getRaw(j).getAmp());
+		amp_w->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).getAmp());
 
-	}
+		prof_amp_u->Fill(c.getRaw(j).posUVW().X(),c.getRaw(j).getAmp());
+		prof_amp_v->Fill(c.getRaw(j).posUVW().Y(),c.getRaw(j).getAmp());
+		prof_amp_w->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).getAmp());
+
+		if(c.getRaw(j).posUVW().Y()<0.1) amp_v_1->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.1&&c.getRaw(j).posUVW().Y()<0.2) amp_v_2->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.2&&c.getRaw(j).posUVW().Y()<0.3) amp_v_3->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.3&&c.getRaw(j).posUVW().Y()<0.4) amp_v_4->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.4&&c.getRaw(j).posUVW().Y()<0.5) amp_v_5->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.5&&c.getRaw(j).posUVW().Y()<0.6) amp_v_6->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.6&&c.getRaw(j).posUVW().Y()<0.7) amp_v_7->Fill(c.getRaw(j).getAmp());
+		if(c.getRaw(j).posUVW().Y()>0.7) amp_v_8->Fill(c.getRaw(j).getAmp());
+	  }
 	}
   }
 
@@ -78,5 +137,78 @@ void position(TString files){
   amp_v->Draw("lego");
   canvas = new TCanvas();
   amp_w->Draw("lego");
+
+  canvas = new TCanvas();
+  prof_amp_u->Draw();
+  canvas = new TCanvas();
+  prof_amp_v->Draw();
+  canvas = new TCanvas();
+  prof_amp_w->Draw();
+
+  canvas = new TCanvas();
+  amp_v_1->Draw();
+  canvas = new TCanvas();
+  amp_v_2->Draw();
+  canvas = new TCanvas();
+  amp_v_3->Draw();
+  canvas = new TCanvas();
+  amp_v_4->Draw();
+  canvas = new TCanvas();
+  amp_v_5->Draw();
+  canvas = new TCanvas();
+  amp_v_6->Draw();
+  canvas = new TCanvas();
+  amp_v_7->Draw();
+  canvas = new TCanvas();
+  amp_v_8->Draw();
+  canvas = new TCanvas();
+  amp_v_1->Draw();
+  amp_v_2->Draw("same");
+  amp_v_3->Draw("same");
+  amp_v_4->Draw("same");
+  amp_v_5->Draw("same");
+  amp_v_6->Draw("same");
+  amp_v_7->Draw("same");
+  amp_v_8->Draw("same");
+
+  canvas = new TCanvas();
+  amp_cl_u->Draw("lego");
+  canvas = new TCanvas();
+  amp_cl_v->Draw("lego");
+  canvas = new TCanvas();
+  amp_cl_w->Draw("lego");
+
+  canvas = new TCanvas();
+  prof_amp_cl_u->Draw();
+  canvas = new TCanvas();
+  prof_amp_cl_v->Draw();
+  canvas = new TCanvas();
+  prof_amp_cl_w->Draw();
+
+  canvas = new TCanvas();
+  amp_cl_v_1->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_2->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_3->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_4->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_5->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_6->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_7->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_8->Draw();
+  canvas = new TCanvas();
+  amp_cl_v_1->Draw();
+  amp_cl_v_2->Draw("same");
+  amp_cl_v_3->Draw("same");
+  amp_cl_v_4->Draw("same");
+  amp_cl_v_5->Draw("same");
+  amp_cl_v_6->Draw("same");
+  amp_cl_v_7->Draw("same");
+  amp_cl_v_8->Draw("same");
 
 }
--- tpc/TestChamber/analysis/position_raw.C
+++ tpc/TestChamber/analysis/position_raw.C
@@ -38,8 +38,11 @@ void position(TString files){
   //define the histograms
   //without using clusterSplit1.C
 
+  TH1D *u = new TH1D("u","",50,0,10);
+  TH1D *v = new TH1D("v","",100,0,1);
+  TH1D *w = new TH1D("w","",100,0,10);
 
-  TH1D *u_raw = new TH1D("u_raw","",10,0,10);
+  TH1D *u_raw = new TH1D("u_raw","",50,0,10);
   TH1D *v_raw = new TH1D("v_raw","",50,0,1);
   TH1D *w_raw = new TH1D("w_raw","",100,0,10);
 
@@ -50,9 +53,21 @@ void position(TString files){
   TH2D *w_vs_v_3 = new TH2D("w_vs_v_3","",100,0,10,10,0,1);
   TH2D *w_vs_v_4 = new TH2D("w_vs_v_4","",100,0,10,10,0,1);
 
-  myChain.SetBranchAddress("track", &intr);
+  TH2D *v_vs_v = new TH2D("v_vs_v","",10,0,1,100,0,1);
 
-  
+  TH2D *v_vs_v_1 = new TH2D("v_vs_v_1","",10,0,1,100,0,1);
+  TH2D *v_vs_v_2 = new TH2D("v_vs_v_2","",10,0,1,100,0,1);
+  TH2D *v_vs_v_3 = new TH2D("v_vs_v_3","",10,0,1,100,0,1);
+  TH2D *v_vs_v_4 = new TH2D("v_vs_v_4","",10,0,1,100,0,1);
+
+  TH2D *u_vs_u = new TH2D("u_vs_u","",10,0,10,10,0,10);
+
+  TH2D *u_vs_u_1 = new TH2D("u_vs_u_1","",10,0,10,10,0,10);
+  TH2D *u_vs_u_2 = new TH2D("u_vs_u_2","",10,0,10,10,0,10);
+  TH2D *u_vs_u_3 = new TH2D("u_vs_u_3","",10,0,10,10,0,10);
+  TH2D *u_vs_u_4 = new TH2D("u_vs_u_4","",10,0,10,10,0,10);
+
+  myChain.SetBranchAddress("track", &intr);
 
   for (Int_t iev=0;iev<nevent;iev++){
      
@@ -63,37 +78,63 @@ void position(TString files){
 	
 	for(int i=0;i<tr.nCl();++i){
 	  TCcluster c = tr.getCl(i);
-	   if(!c.getFit()) continue;
+	  if(!c.getFit()) continue;
 	  //if(c.nPadY()!=2) continue;
 	  //	  if(c.nTime()!=1) continue;
 
-	   // if(c.nRaw()!=2)continue;
+	  // if(c.nRaw()!=2)continue;
+
+	  u->Fill(c.posUVW().X());
+	  v->Fill(c.posUVW().Y());
+	  w->Fill(c.posUVW().Z());
 
 	  for(int j=0;j<c.nRaw();++j){
 
-	  u_raw->Fill(c.getRaw(j).posUVW().X());
-	  v_raw->Fill(c.getRaw(j).posUVW().Y());
-	  w_raw->Fill(c.getRaw(j).posUVW().Z());
+		u_raw->Fill(c.getRaw(j).posUVW().X());
+		v_raw->Fill(c.getRaw(j).posUVW().Y());
+		w_raw->Fill(c.getRaw(j).posUVW().Z());
 
-	  w_vs_v->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
+		w_vs_v->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
 
-	 if(c.nPadY()==1) w_vs_v_1->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
-	 if(c.nPadY()==2) w_vs_v_2->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
-	 if(c.nPadY()==3) w_vs_v_3->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
-	 if(c.nPadY()>3) w_vs_v_4->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
+		if(c.nPadY()==1) w_vs_v_1->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
+		if(c.nPadY()==2) w_vs_v_2->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
+		if(c.nPadY()==3) w_vs_v_3->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
+		if(c.nPadY()>3) w_vs_v_4->Fill(c.getRaw(j).posUVW().Z(),c.getRaw(j).posUVW().Y());
 
-	}
+		v_vs_v->Fill(c.getRaw(j).posUVW().Y(),c.posUVW().Y());
+
+		if(c.nPadY()==1) v_vs_v_1->Fill(c.getRaw(j).posUVW().Y(),c.posUVW().Y());
+		if(c.nPadY()==2) v_vs_v_2->Fill(c.getRaw(j).posUVW().Y(),c.posUVW().Y());
+		if(c.nPadY()==3) v_vs_v_3->Fill(c.getRaw(j).posUVW().Y(),c.posUVW().Y());
+		if(c.nPadY()>3) v_vs_v_4->Fill(c.getRaw(j).posUVW().Y(),c.posUVW().Y());
+
+		u_vs_u->Fill(c.getRaw(j).posUVW().X(),c.posUVW().X());
+
+		if(c.nPadY()==1) u_vs_u_1->Fill(c.getRaw(j).posUVW().X(),c.posUVW().X());
+		if(c.nPadY()==2) u_vs_u_2->Fill(c.getRaw(j).posUVW().X(),c.posUVW().X());
+		if(c.nPadY()==3) u_vs_u_3->Fill(c.getRaw(j).posUVW().X(),c.posUVW().X());
+		if(c.nPadY()>3) u_vs_u_4->Fill(c.getRaw(j).posUVW().X(),c.posUVW().X());
+
+	  }
 	}
   }
 
   //end of event loop
 
   TCanvas *canvas = new TCanvas();
+  u->Draw();
+  canvas = new TCanvas();
+  v->Draw();
+  canvas = new TCanvas();
+  w->Draw();
+
+  canvas = new TCanvas();
   u_raw->Draw();
   canvas = new TCanvas();
   v_raw->Draw();
   canvas = new TCanvas();
   w_raw->Draw();
+
   canvas = new TCanvas();
   w_vs_v->Draw("colz");
   canvas = new TCanvas();
@@ -104,5 +145,27 @@ void position(TString files){
   w_vs_v_3->Draw("lego");
   canvas = new TCanvas();
   w_vs_v_4->Draw("lego");
+
+  canvas = new TCanvas();
+  v_vs_v->Draw("colz");
+  canvas = new TCanvas();
+  v_vs_v_1->Draw("lego");
+  canvas = new TCanvas();
+  v_vs_v_2->Draw("lego");
+  canvas = new TCanvas();
+  v_vs_v_3->Draw("lego");
+  canvas = new TCanvas();
+  v_vs_v_4->Draw("lego");
+
+  canvas = new TCanvas();
+  u_vs_u->Draw("colz");
+  canvas = new TCanvas();
+  u_vs_u_1->Draw("lego");
+  canvas = new TCanvas();
+  u_vs_u_2->Draw("lego");
+  canvas = new TCanvas();
+  u_vs_u_3->Draw("lego");
+  canvas = new TCanvas();
+  u_vs_u_4->Draw("lego");
 
 }
