b1ff6bfac70bba052342b1d38ea345468a4bd1a9
Klaus Goetzen <K.Goetzen@gsi.de>
2008-11-07 10:42:34 +0000

    Added task PndMicroWriter to PndTools/AnalysisTools

---
+++ PndTools/AnalysisTools/AnalysisToolsLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+//#pragma link C++ class  PndAnalysis+;
+#pragma link C++ class  PndMicroWriter+;
+
+//#pragma link C++ class  vector<double>;
+
+#endif
+
