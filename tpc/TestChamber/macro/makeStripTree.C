c3fd7ef5d009a9c6647bf3903915fb39f06e5689
Sverre Dorheim <sverred@gmail.com>
2008-11-25 12:56:24 +0000

    Changed a few bugs considering reading alignmentfile

--- tpc/TestChamber/macro/makeStripTree.C
+++ tpc/TestChamber/macro/makeStripTree.C
@@ -11,15 +11,23 @@ TCcluster makeCluster(TCtrack *t,int id){
   TVector3 trans;
   TMatrixT<double> rot(3,3);
   double pitch,theta,d1,d2,res;
-  TCalign* a = TCalign::getInstance("alignment/simAlign.txt");
+  TCalign* a = TCalign::getInstance("alignment/simRealAlign.txt");
+  a->clear();
+  a->read("alignment/simRealAlign.txt");
   a->getConv(id,trans,rot,pitch,theta,d1,d2,res);
+  //cout<<"x "<<trans[0]<<" y: "<<trans[1]<<" z: "<<trans[2]<<endl;
   double x=t->getAx()*trans.Z()+t->getBx();
   double y=t->getAy()*trans.Z()+t->getBy();
+
   TVector3 globPos(x,y,trans.Z());
   TVector3 pos = a->XYZtoUVW(id,globPos);
+  // cout<<"x glob:"<<globPos[0]<<" y glob: "<<globPos[1]<<" z glob: "<<globPos[2]<<endl;
+  //cout<<"u "<<pos[0]<<" v: "<<pos[1]<<" w: "<<pos[2]<<endl;
   TRandom3 rand2(0);
   double smear = rand2.Gaus(0.,res);
   pos.SetX(pos.X()+smear);
+  pos.SetY(0.);
+  pos.SetZ(0.);
   TVector3 err(res,100.,100.);
   TCcluster c(pos,err,1.,id);
   c.setFit();
@@ -34,7 +42,7 @@ void makeStripTree(){
 
   
 
-  int nTracks = 1;
+  int nTracks = 10000;
 
   TRandom3 rand(0);
 
@@ -42,17 +50,16 @@ void makeStripTree(){
     outTr->clear();
 
     TVector3 pos1,pos2;
-    pos1.SetXYZ(0.,0.,0.);
-    pos2.SetXYZ(1.,2.,100.);
+    //pos1.SetXYZ(0.,0.,0.);
+    //pos2.SetXYZ(1.,2.,100.);
     
-    /* 
-       pos1.SetX(rand.Uniform(-1.,1.));
-       pos1.SetY(rand.Uniform(-1.,1.));
-       pos1.SetZ(rand.Uniform(0.,0.1));
-       pos2.SetX(rand.Uniform(-1.,1.));
-       pos2.SetY(rand.Uniform(-1.,1.));
-       pos2.SetZ(rand.Uniform(100.,100.1));
-    */
+    pos1.SetX(rand.Uniform(-1.,1.));
+    pos1.SetY(rand.Uniform(-1.,1.));
+    pos1.SetZ(rand.Uniform(0.,0.1));
+    pos2.SetX(rand.Uniform(-1.,1.));
+    pos2.SetY(rand.Uniform(-1.,1.));
+    pos2.SetZ(rand.Uniform(100.,100.1));
+       
     TVector3 mom=pos2-pos1;
     mom.SetMag(1.);
     double ax,bx,ay,by;
@@ -60,20 +67,27 @@ void makeStripTree(){
     ax=mom.X()/mom.Z();
     ay=mom.Y()/mom.Z();    
     double t=(0.-pos1.Z())/mom.Z();
-    bx=pos1.X()+t*mom.X();
-    by=pos1.Y()+t*mom.Y();
+    bx=pos1.X()+t*ax;
+    by=pos1.Y()+t*ay;
     
     outTr->setPar(ax,bx,ay,by);
-
-    for(int i=1;i<=4;++i){
-      TCcluster c = makeCluster(outTr,i);
+    //cout<<"hei"<<endl;
+    for(int j=1;j<=8;++j){
+      TCcluster c = makeCluster(outTr,j);
       outTr->addCluster(c);
     }
-    outTr->print();
-    outTr->fit(1,2,3,4);
-    outTr->print();
-    outTree->Fill();    
+    cout<<"Track: "<<i<<" "<<endl;
+    //"************* ---------------------------------------- *************************"<<endl;
+    //    outTr->print();
 
+    TCalign* a = TCalign::getInstance("alignment/simRealAlign.txt");
+    a->clear();
+    a->read("alignment/simAsumedAlign.txt");
+    outTr->fit(1,2,3,4,5,6,7,8);
+    //outTr->print();
+    cout<<endl<<endl;
+    outTree->Fill();    
+    
   }
   rootOutfile->Write();
 }
