cc0bc21c789e4e9776e53570e2d65022771c0c4e
Johan Messchendorp <j.g.messchendorp@rug.nl>
2008-12-06 14:58:43 +0000

    add simulations macros for gg studies

---
+++ macro/SimulationGG/SimulationMacros/sim.C
@@ -0,0 +1,137 @@
+// Macro created 18/09/2006 by S.Spataro
+// It creates a geant simulation file for emc by using an EvtGen outout file
+void sim(TString simdatei, TString input, TString param, Int_t nEvents,  Char_t TransportModel[] = "TGeant4", UInt_t seed=0)
+{
+  gRandom->SetSeed(seed);
+  TStopwatch timer;
+  timer.Start();
+  gDebug=0;
+// Load basic libraries
+      gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+      rootlogon();
+
+  CbmRunSim *fRun = new CbmRunSim();
+  
+  // Set the number of events
+ // Int_t nEvents = n; 
+  
+  // set the MC version used
+  // ------------------------
+  
+  fRun->SetName(TransportModel);
+  
+  fRun->SetOutputFile(simdatei);
+  
+  // Set Material file Name
+  //-----------------------
+  
+  fRun->SetMaterials("media_pnd.geo");
+  
+  // Create and add detectors
+  //-------------------------
+
+  CbmModule *Cave= new PndCave("CAVE");
+  Cave->SetGeometryFileName("pndcave.geo");
+  fRun->AddModule(Cave); 
+  
+  PndEmc *Emc = new PndEmc("EMC",kTRUE);
+  Emc->SetGeometryFileNameDouble("emc_module1245.dat","emc_module3new.root");
+  fRun->AddModule(Emc); 
+
+  CbmModule *Magnet= new PndMagnet("MAGNET");
+  Magnet->SetGeometryFileName("FullSolenoid.root");
+  fRun->AddModule(Magnet);
+
+  CbmModule *Dipole= new PndMagnet("MAGNET");
+  Dipole->SetGeometryFileName("dipole.geo");
+  fRun->AddModule(Dipole);
+
+
+  CbmModule *Pipe= new PndPipe("PIPE");
+  //Pipe->SetGeometryFileName("pipebeamtarget.geo");
+  fRun->AddModule(Pipe);
+
+ 
+/*
+    CbmDetector *Stt = new CbmStt("STT",kFALSE);
+    Stt->SetGeometryFileName("stt24.geo"); // 14 = 1 solo 2layer pablo01.geo 3 layers stt24 
+    fRun->AddModule(Stt);
+*/
+  
+  /*
+    CbmDetector *Stt= new CbmStt("STT", kFALSE); 
+    Stt->SetGeometryFileName("straws_axial.geo");
+    fRun->AddModule(Stt);
+  */
+
+  CbmDetector *Tpc = new PndTpcDetector("TPC", kTRUE);
+  Tpc->SetGeometryFileName("tpc.geo");
+  fRun->AddModule(Tpc);
+
+  CbmDetector *Mvd = new PndMvdDetector("MVD", kTRUE);
+  Mvd->SetGeometryFileName("MVD_v1.0_woPassiveTraps.root");
+  fRun->AddModule(Mvd);
+  
+  CbmDetector *Tof = new PndTof("TOF",kTRUE);
+  Tof->SetGeometryFileName("tofbarrel.geo");
+  fRun->AddModule(Tof);
+
+  CbmDetector *Drc = new PndDrc("DIRC", kTRUE);
+  Drc->SetGeometryFileName("dirc.geo"); 
+  fRun->AddModule(Drc); 
+
+ CbmDetector *Muo = new PndMdt("MDT",kTRUE);
+  Muo->SetGeometryFileName("muopars.root");
+  fRun->AddModule(Muo);
+
+  CbmDetector *Dch = new PndDchDetector("DCH", kTRUE);
+  Dch->SetGeometryFileName("dch.root"); 
+  fRun->AddModule(Dch);
+ 
+
+
+  // Create and Set Event Generator
+  //-------------------------------
+  
+  CbmPrimaryGenerator* primGen = new CbmPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+
+  // EvtGen Generator: 
+
+  CbmEvtGenGenerator* evtGen = new CbmEvtGenGenerator(input);
+  primGen->AddGenerator(evtGen);  
+  
+  fRun->SetStoreTraj(kTRUE); // to store particle trajectories 
+  
+  //magnetic field: no field when commented put
+  //CbmFieldConst *fMagField=new CbmFieldConst();
+  //fMagField->SetField(0.,0.,20.); // values are in kG
+  //fMagField->SetFieldRegion(-50, 50,-50, 50, -100, 100);// values are in cm (xmin,xmax,ymin,ymax,zmin,zmax)
+  //fRun->SetField(fMagField);
+  
+  fRun->Init();
+  
+  // Fill the Parameter containers for this run
+  //-------------------------------------------
+  
+  CbmRuntimeDb *rtdb=fRun->GetRuntimeDb();
+  Bool_t kParameterMerged=kTRUE;
+  CbmParRootFileIo* output=new CbmParRootFileIo(kParameterMerged);
+  output->open(param);
+  rtdb->setOutput(output);
+  rtdb->saveOutput();
+  rtdb->print();
+  
+  // Transport nEvents
+  // -----------------
+  
+  fRun->Run(nEvents);
+  
+  timer.Stop();
+  
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+  
+}  
+ 
