9589515f4accb7a4db7db5314ac3ec5e18729e68
Stefano Spataro <spataro@to.infn.it>
2008-12-18 13:00:50 +0000

    Fix for dch

--- tutorials/lhetrack/rootlogon.C
+++ tutorials/lhetrack/rootlogon.C
@@ -3,46 +3,34 @@ rootlogon()
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();
   
-  // Load this libraries
-  gSystem->Load("libDpmEvtGen.so");
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libParBase");
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libField");
-  gSystem->Load("libPassive");
-  gSystem->Load("libGen");
-  gSystem->Load("libPGen");
-  gSystem->Load("libStt");
-  gSystem->Load("libEmc"); 
-  gSystem->Load("libgenfit");
-  gSystem->Load("libtrackrep");
-  gSystem->Load("libtpc"); 
-  gSystem->Load("libtpcreco");
-  gSystem->Load("libTrkBase");
-  gSystem->Load("librecotasks");
-  gSystem->Load("libMvd");
-  gSystem->Load("libMvdReco");
-  gSystem->Load("libTof");
-  gSystem->Load("libDrcProp");
-  gSystem->Load("libDrc");
-  gSystem->Load("libMdt");
-  gSystem->Load("libDch");
-  gSystem->Load("libLHETrack");
-  gSystem->Load("libGeane");
-
-  // Style options
-  gStyle->SetPalette(1);
-  gStyle->SetCanvasBorderMode(0);
-  gStyle->SetLabelSize(0.05,"X");gStyle->SetLabelSize(0.05,"Y");
-  gStyle->SetTitleSize(0.05,"X");gStyle->SetTitleSize(0.05,"Y");
-  gStyle->SetTitleOffset(.9,"X");gStyle->SetTitleOffset(.9,"Y");
-  gStyle->SetCanvasColor(0);
-  gStyle->SetStatColor(0);
-  gStyle->SetFrameFillColor(0);
-  gStyle->SetLineWidth(2);
-  //gStyle->SetOptTitle(0);
-  gStyle->SetOptStat();
-  //gStyle->SetOptFit(111);
-
+  // Load this example libraries
+        gSystem->Load("libDpmEvtGen.so");
+  	gSystem->Load("libGeoBase");
+	gSystem->Load("libParBase");
+	gSystem->Load("libBase");
+	gSystem->Load("libMCStack");
+	gSystem->Load("libField");
+	gSystem->Load("libPassive");
+	gSystem->Load("libGen");
+	gSystem->Load("libPGen");
+	gSystem->Load("libEmc"); 
+        gSystem->Load("libgenfit");
+	gSystem->Load("libtrackrep");
+        gSystem->Load("libriemann");
+        gSystem->Load("libtpc"); 
+        gSystem->Load("libtpcreco");
+        gSystem->Load("libStt");
+        gSystem->Load("libSttReco");
+	gSystem->Load("libTrkBase");
+        gSystem->Load("librecotasks");
+        gSystem->Load("libMvd");
+        gSystem->Load("libMvdReco");
+        gSystem->Load("libMvdTrk");
+	gSystem->Load("libTof");
+	gSystem->Load("libDrcProp");
+	gSystem->Load("libDrc");
+	gSystem->Load("libMdt");
+	gSystem->Load("libDch");
+        gSystem->Load("libLHETrack");
+	gSystem->Load("libGeane");
 }
