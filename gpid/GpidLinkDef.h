9795c014414c21d1efce78fa7d0fb07ed868f59a
vanni <vanni>
2008-12-25 17:57:46 +0000

    test

--- gpid/GpidLinkDef.h
+++ gpid/GpidLinkDef.h
@@ -4,7 +4,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
  
-#pragma link C++ class  PndGpidKNN;
 #pragma link C++ class  PndGpidTask;
 #pragma link C++ class  PndPidCand+;
 #pragma link C++ class  PndGeaneTrTpcTof+;
