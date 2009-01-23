5edd0d0b8f4226b0aa405e0ea140f53c284b8d52
Alicia Sanchez <A.Sanchez@gsi.de>
2009-01-23 10:44:48 +0000

    SIGABRT removed

--- hyp/PndHypHit.cxx
+++ hyp/PndHypHit.cxx
@@ -1,9 +1,9 @@
 // -------------------------------------------------------------------------
 // -----                    PndHypHit source file                -----
-// -----                  Created 07/04/05  by V. Friese               -----
+// -----                Modified for hyp purpose by A. Sanchez   -----
 // -------------------------------------------------------------------------
 
-
+#include "stdlib.h"
 #include <iostream>
 #include <algorithm>
 #include "PndHypHit.h"
@@ -20,29 +20,16 @@ PndHypHit::PndHypHit() {
 
 
 // -----   Standard constructor   ------------------------------------------
-PndHypHit::PndHypHit(Int_t detID, TString detName, TVector3& pos, TVector3& dpos,Int_t index, Double_t charge, Int_t NPixelHits) 
+PndHypHit::PndHypHit(Int_t detID, TString detName, TVector3& pos, TVector3& dpos,Int_t index, Double_t charge, Int_t NDigiHits) 
   : CbmHit(detID, pos, dpos, index) {
   fDetName = detName;
   //fTrackID = trackId;
   fCharge  = charge;
-  fNPixelHits = NPixelHits;
+  fNDigiHits = NDigiHits;
   fBotIndex = -1;
 }
 
-/*PndHypHit::PndHypHit(PndHypHit& c) 
-: CbmHit()
-{
-	fDetName = c.GetDetName();
-	fTrackID = c.GetTrackID();
-	fCharge  =  c.GetCharge();
-	fNPixelHits = c.GetNPixelHits();
-	SetPosition(c.GetPosition());
-	SetPositionError(c.GetPositionError());
-	SetDetectorID(c.GetDetectorID());
-	SetRefIndex(c.GetRefIndex());
-	
-	
-}*/
+
 // -------------------------------------------------------------------------
 
 
@@ -59,20 +46,7 @@ void PndHypHit::Print(const Option_t* opt) const {
   
 }
 
-Double_t PndHypHit::GetD(Int_t i)
-{
-  if(!gGeoManager){
-    std::cout<<" -E- in PndHypHit::GetDx(): No gGeoManager there. Please use "<<std::endl;
-    abort();
-  }
-  // TODO: Caution! if there is a point reconstructed OuTSIDE its volume,
-  // then the returned stuff is WROMG!
-  ((TGeoNode*)(gGeoManager->FindNode(fX,fY,fZ)))->cd();
-  Double_t local[3]={fDx,fDy,fDz};
-  Double_t master[3];
-  gGeoManager->LocalToMasterVect(local,master);
-  return master[i];
-}
+
 
 
 // -------------------------------------------------------------------------
