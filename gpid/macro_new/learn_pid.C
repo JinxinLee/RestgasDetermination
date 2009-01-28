9b4f1165c3f2fe8a9109a0ae0682d0b710e6659e
vanni <vanni>
2009-01-28 12:23:39 +0000

    new macros

---
+++ gpid/macro_new/learn_pid.C
@@ -0,0 +1,33 @@
+void learn_pid()
+{
+PndMultiClassMVA bdt_train;
+//bdt_train.AddVar("stt");
+bdt_train.AddVar("p");
+
+
+//bdt_train.AddVar("mvd");
+//bdt_train.AddVar("tof");
+
+//bdt_train.AddVar("thetaC");
+bdt_train.AddVar("emc");
+
+bdt_train.AddClass("electron");
+bdt_train.AddClass("pion");
+
+ bdt_train.SetNSigTrain("1500");
+  bdt_train.SetNBkgTrain("1500");
+  bdt_train.SetNSigTest("2");
+  bdt_train.SetNBkgTest("2");
+  bdt_train.SetPruneStrengthBDT("5.0");
+  bdt_train.SetNTreeBDT("50");
+  bdt_train.SetBoostTypeBDT("AdaBoost");
+  bdt_train.SetNCutsBDT("30");
+  bdt_train.SetNKNN("19");
+//  bdt_train.SetNCLASS(3);
+//  bdt_train.SetNVAR(3);
+  bdt_train.SetINFILENAME("test_tree.root");
+//  bdt_train.SetAPPNAME("STT_P_MVD_TOF_THETAC_EMC_1_2_1000NN");        
+  bdt_train.SetAPPNAME("test1");        
+  bdt_train.WriteConfigFile();        
+  bdt_train.TrainTest();
+}
---
+++ gpid/macro_new/run_pid_reco.C
@@ -0,0 +1,84 @@
+void run_pid_reco()
+{
+  // ========================================================================
+  // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
+  Int_t iVerbose = 0;
+
+  // Input file (MC events)
+  //TString inFile = "ele125T_reco.root";
+  TString inFile = "ele_reco.root";
+
+  // Parameter file
+  TString parFile = "params_sttcombi.root";
+
+  // Output file
+  TString outFile = "ele_pid.root";
+
+  // Number of events to process
+  Int_t nEvents = 0;
+ 
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+  TString sysFile = gSystem->Getenv("VMCWORKDIR");
+  // ------------------------------------------------------------------------
+
+
+  // ========================================================================
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+PndEmcMapper *emcMap=PndEmcMapper::Instance(2,"/home/vanni/test/pi_points.root");
+
+  // -----   Digitization run   -------------------------------------------
+  CbmRunAna *fRun= new CbmRunAna();
+  fRun->SetInputFile(inFile);
+  fRun->SetOutputFile(outFile);
+
+
+ TString allDigiFile = sysFile+"/macro/params/all.par";
+
+  CbmRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  CbmParRootFileIo* parInput1 = new CbmParRootFileIo();
+  parInput1->open(parFile.Data());
+	
+  CbmParAsciiFileIo* parIo1 = new CbmParAsciiFileIo();
+  parIo1->open(allDigiFile.Data(),"in");
+        
+  rtdb->setFirstInput(parInput1);
+  rtdb->setSecondInput(parIo1);
+  fRun->LoadGeometry();
+  PndGpidTaskLhe* pid = new PndGpidTaskLhe();
+
+//  pid->SetAPPNAME("e_pi_stt_mvd_tof_p_thetaC_emc_T1000");
+//  pid->SetAPPNAME("STT_P_MVD_TOF_THETAC_EMC_1_2_5NN");
+  pid->SetAPPNAME("test");
+  pid->SetDIR("/home/vanni/work/pid_ana/weights/");
+  pid->SetMVA(PndGpidTask::KNN);
+  fRun->AddTask(pid);
+ fRun->Init();
+ fRun->Run(0, 10);
+// fRun->Run(0, nEvents);
+//  fRun->Run(2500, 2700);
+
+  rtdb->saveOutput();
+  rtdb->print();
+
+  // ------------------------------------------------------------------------
+
+  // -----   Finish   -------------------------------------------------------
+
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Macro finished succesfully." << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+  cout << endl;
+
+}
