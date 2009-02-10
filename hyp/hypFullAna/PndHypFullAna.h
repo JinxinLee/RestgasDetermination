9b41d139c36ba378086cd2b742494edf518afe3a
Alicia Sanchez <A.Sanchez@gsi.de>
2009-02-10 13:46:07 +0000

    full rho analysis

---
+++ hyp/hypFullAna/PndHypFullAna.h
@@ -0,0 +1,112 @@
+#ifndef PNDHYPFULLANA_H
+#define PNDHYPFULLANA_H 1
+
+
+#include "CbmTask.h"
+#include <map>
+#include <string>
+#include "TFile.h"
+#include "TVector3.h"
+#include "TLorentzVector.h"
+
+
+
+class TClonesArray;
+class TObjectArray;
+class TH1F;class TH2F;
+
+class TPidChargedSelector;
+class TPidNeutralSelector;
+class TPidPlusSelector;
+class TPidMinusSelector;
+class TPidMassSelector;
+class TPidSimpleKaonSelector;
+class TPidSimplePionSelector;
+class TPidSimpleProtonSelector;
+
+class PndHypFullAna : public CbmTask
+{
+
+ public:
+	typedef std::map<Int_t, Float_t> mapper;
+
+  /** Default constructor **/  
+  PndHypFullAna();
+
+
+  /** Destructor **/
+  ~PndHypFullAna();
+
+
+  /** Virtual method Init **/
+  virtual InitStatus Init();
+
+
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
+
+  void Finish(TString cat);
+
+  void SetEnergySpectra(int event,int cluster);
+  void SetTotESpectra(int clus);
+  Int_t GetIonCharge(Int_t Z,Int_t &mass,Int_t &str);
+  
+  //void CreateStructure();
+
+ protected:
+
+  int evcount;
+
+  
+
+  // **** some basic selectors
+  //
+  //TPidChargedSelector *chargedSel;
+  TPidNeutralSelector *neutralSel;
+  TPidPlusSelector    *plusSel;
+  TPidMinusSelector   *minusSel;
+  
+  // **** mass selectors for the resonances/composites
+  //
+  TPidMassSelector *phiMSel;
+  TPidMassSelector *pi0MSel;
+  TPidMassSelector *dsMSel;
+  TPidMassSelector *LambMSel;
+  TPidSimpleKaonSelector *kSel;
+  TPidSimplePionSelector *piSel;
+  TPidSimpleProtonSelector *pSel;
+  
+  /**book all the histograms**/
+  TH2F *hvtx2[10];
+  TH1F *spectra[10];
+   
+  TH1F *ds0mass;
+ TH1F *ximass; TH1F *Lamb;
+  TH1F *ppi2mass;
+  TH1F *ppi2; TH1F *e;
+  TH2F *pid;TH2F *pidh;
+
+  TH1F *nmult;
+
+ private: 
+   
+  /** Input array of TpcLheTrack **/
+  
+  TClonesArray* fChargedArray;
+  TClonesArray* fMcTr;
+  TClonesArray* fMicroArray;
+  TClonesArray* fMcCands; 
+  TClonesArray* fMc;
+  TClonesArray* fGe;
+ 
+  /** Geo file to use **/
+
+	/** Get parameter containers **/
+	virtual void SetParContainers();
+
+ 
+  ClassDef(PndHypFullAna,1);
+  
+};
+
+#endif
---
+++ hyp/hypFullAna/PndHypMicroIdealWriter.h
@@ -0,0 +1,101 @@
+/******************************************************
+Class PndHypMicroWriter
+
+Collects Micro infromation from Reconstruction and
+writes out PndMicroCandidates
+
+Author: K.Goetzen, GSI, 06/2008
+
+*******************************************************/ 
+
+
+#ifndef PNDMICROIDEALWRITER_H
+#define PNDMICROIDEALWRITER_H 1
+
+
+#include "CbmTask.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "TVector3.h"
+#include "TLorentzVector.h"
+#include <map>
+#include <string>
+
+class TClonesArray;
+class TObjectArray;
+
+
+class PndHypMicroIdealWriter : public CbmTask
+{
+
+ public:
+	typedef std::map<Int_t, Float_t> mapper;
+
+  /** Default constructor **/  
+  PndHypMicroIdealWriter();
+
+
+  /** Destructor **/
+  ~PndHypMicroIdealWriter();
+
+
+  /** Virtual method Init **/
+  virtual InitStatus Init();
+
+
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
+
+  virtual void Finish();
+  //void CreateStructure();
+
+ protected:
+  
+  void propagate(TLorentzVector &l, TVector3 &p, float charge);
+  
+  TLorentzVector track1; 
+  TLorentzVector track2; 
+
+  /**book all the histograms**/
+
+ private: 
+  int       evtcnt; //event counter for output
+   
+  /** Input array of TpcLheTrack **/
+  TClonesArray* fTrArray;
+  
+  /** Input array of EmcCandidates **/
+  TClonesArray* fHitArray;
+  
+  /** Input array of MCTracks **/
+  TClonesArray* fMCTrack;
+  
+  // Output arrays Candidates
+  TClonesArray* fChargedCandidates;
+  TClonesArray* fNeutralCandidates; 
+  TClonesArray* fMcCandidates;
+  TClonesArray* fMicroIdealCandidates;
+  
+  //output array EventInfo
+  TClonesArray* fEventInfo;
+
+  bool fStoreNeutral;
+  bool fStoreCharged;
+  bool fStoreMC;
+  
+  Int_t mvd_hitidx[1000];
+  Int_t stt_hitidx[1000];
+  Int_t tpc_hitidx[1000];
+  
+ 
+  /** Geo file to use **/
+
+	/** Get parameter containers **/
+	virtual void SetParContainers();
+
+ 
+  ClassDef(PndHypMicroIdealWriter,1);
+  
+};
+
+#endif
