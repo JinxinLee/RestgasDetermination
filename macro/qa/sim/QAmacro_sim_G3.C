0a17b933f9dc10758195afce64c6960e746315a6
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2009-02-16 11:09:54 +0000

    adapt the macro to the new fields

--- macro/qa/sim/QAmacro_sim_G3.C
+++ macro/qa/sim/QAmacro_sim_G3.C
@@ -101,17 +101,27 @@
 
 
   /** Set the field*/
-  PndMultiField *fField= new PndMultiField();
+  fRun->SetBeamEnergy(12);
+   PndMultiField *fField= new PndMultiField();
 
-  PndTransMap *map= new PndTransMap("TransMap", "R");
-  PndDipoleMap *map1= new PndDipoleMap("DipoleMap", "R");
-  PndSolenoidMap *map2= new PndSolenoidMap("SolenoidMap", "R");
-  fField->AddField(map);
-  fField->AddField(map1);
-  fField->AddField(map2);
-  
-  fRun->SetField(fField);
+   PndTransMap *map_t= new PndTransMap("TransMap", "R");
+   PndDipoleMap *map_d1= new PndDipoleMap("DipoleMap1", "R");
+   PndDipoleMap *map_d2= new PndDipoleMap("DipoleMap2", "R");
+   PndSolenoidMap *map_s1= new PndSolenoidMap("SolenoidMap1", "R");
+   PndSolenoidMap *map_s2= new PndSolenoidMap("SolenoidMap2", "R");
+   PndSolenoidMap *map_s3= new PndSolenoidMap("SolenoidMap3", "R");
+   PndSolenoidMap *map_s4= new PndSolenoidMap("SolenoidMap4", "R");
 
+   fField->AddField(map_t);
+   fField->AddField(map_d1);
+   fField->AddField(map_d2);
+   fField->AddField(map_s1);
+   fField->AddField(map_s2);
+   fField->AddField(map_s3);
+   fField->AddField(map_s4);
+
+
+   fRun->SetField(fField);
   fRun->Init();
   // 
   //   // Fill the Parameter containers for this run
