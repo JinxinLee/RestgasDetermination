052f386fc9bc27b750071c23cf8a2bdf18b3daf3
Christian Hoeppner <christian.hoeppner@cern.ch>
2009-03-06 20:15:44 +0000

    added lgpl v3 and copyright statements to all files

--- genfit/RecoHitProducer.h
+++ genfit/RecoHitProducer.h
@@ -1,3 +1,21 @@
+/*  Copyright © 2008, Christian Hoeppner & Sebastian Neubert
+    All rights reserved.
+
+    This file is part of Genfit.
+
+    Genfit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Genfit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser Public License for more details.
+
+    You should have received a copy of the GNU Lesser Public License
+    along with Genfit.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef FITTER_RECOHITPRODUCER_H
 #define FITTER_RECOHITPRODUCER_H
 // Original Authors: Christian Hoeppner (TUM)
