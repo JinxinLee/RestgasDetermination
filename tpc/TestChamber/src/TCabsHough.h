9fd30b8bc8afeafe242d1249afe21aa2ce547d92
Sverre Dorheim <sverred@gmail.com>
2009-03-17 09:38:34 +0000

    made make method virtual

--- tpc/TestChamber/src/TCabsHough.h
+++ tpc/TestChamber/src/TCabsHough.h
@@ -16,7 +16,7 @@ class TCabsHough{
   virtual void convert(std::vector<TCcluster>& _c);
   virtual void doHough()=0;
 
-  void make(std::vector<TCcluster>& _c){
+  virtual void make(std::vector<TCcluster>& _c){
 	convert(_c);
 	doHough();
   }
