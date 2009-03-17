aa3bb7bae4bb8d90c5df87ff5a13350a4291ba05
Sverre Dorheim <sverred@gmail.com>
2009-03-17 09:37:18 +0000

    bugfixes, compiles now

--- tpc/TestChamber/src/TChough2.cxx
+++ tpc/TestChamber/src/TChough2.cxx
@@ -1,47 +1,55 @@
-#include "TChough1.h"
+#include "TChough2.h"
 #include<cmath>
-
 #include"TGraph.h"
+#include"TGraphErrors.h"
 #include"TCanvas.h"
 #include"TApplication.h"
 #include"TSystem.h"
 #include"TRandom.h"
 #include"TEllipse.h"
-
-TChough2::TChough2(const TVector3& _yp,const TVector3& _zp,double _r1,double _r2) : TCabsHough(_yp,_zp){
-  r1Y=_r1;
-  r2Y=_r2;
-  thetaMin=0;
-  thetaMax=3.141592654;
-  
-  nBinsAY = 50;
-  nBinsBY = 50;
+#include <utility>
+//#include "TH2D.h"
+using std::pair;
+TChough2::TChough2(const TVector3& _yp,const TVector3& _zp,double _rR) : TCabsHough(_yp,_zp){
+  cutR=_rR;
+  nBinsR = 100;
+  nBinsTheta = 100;
 
   canvas1 = NULL;
-  houghHistoYZ=NULL;
+  houghHisto=NULL;
   for(int i=0;i<Max_NumberOfHits;i++) {
-	houghLinesYZ[i]=NULL;
+	houghLines[i]=NULL;
   }
-
-
 }
 
 TChough2::~TChough2(){
 }
 
-void TChough2::draw(bool stop,int _x,int _y,int _w,int _h){
-  double x[ypHit.size()];
+void TChough2::draw(bool stop,int _z,int _y,int _w,int _h){
+  double z[ypHit.size()];
   double y[ypHit.size()];
-  for(int i=0;i<ypHit.size();++i){
-    x[i]=zpHit.at(i);
+  double thetaMax_[maxVector.size()];
+  double thetaMaxE_[maxVector.size()];
+  double rMax_[maxVector.size()];
+  double rMaxE_[maxVector.size()];
+  
+  for(unsigned int i=0;i<ypHit.size();++i){
+    z[i]=zpHit.at(i);
     y[i]=ypHit.at(i);
   }
-
+  for(unsigned int i=0;i<maxVector.size();++i){
+    thetaMax_[i]=maxVector.at(i).first;
+    rMax_[i]=maxVector.at(i).second;
+    thetaMaxE_[i]=0;
+    rMaxE_[i]=cutR;
+    
+  }
   static TRandom r(0);
   char buf[10];
   sprintf(buf,"c%5.5f",r.Uniform());
-  TCanvas *c = new TCanvas(buf,"Hough Transformation",_x,_y,_w,_h);
-  TGraph* g = new TGraph(ypHit.size(),x,y);
+  TCanvas *c = new TCanvas(buf,"Hough Transformation",_z,_y,_w,_h);
+  TGraph* g = new TGraph(ypHit.size(),z,y);
+  TGraphErrors* maxPoints = new TGraphErrors(maxVector.size(),thetaMax_,rMax_,thetaMaxE_,rMaxE_);
 
   c->Divide(1,2);
   c->cd(1);
@@ -52,40 +60,34 @@ void TChough2::draw(bool stop,int _x,int _y,int _w,int _h){
   g->Draw("AP");
 
   std::vector<int> selHits;
-  for(int i=0;i<ypHit.size();++i){
-    if(hot(i)){
+  for(unsigned int i=0;i<ypHit.size();++i){
+   if(hot(i)){
       selHits.push_back(i);
-      houghLinesYZ[i]->SetLineStyle(1);
-      houghLinesYZ[i]->SetLineWidth(1);
-      houghLinesYZ[i]->SetLineColor(kGreen);
+      houghLines[i]->SetLineStyle(1);
+      houghLines[i]->SetLineWidth(1);
+      houghLines[i]->SetLineColor(kGreen);
     }
   }
 
-  double xsel[selHits.size()];
+  double zsel[selHits.size()];
   double ysel[selHits.size()];
-  for(int i=0;i<selHits.size();++i){
-    xsel[i]=zpHit.at(selHits.at(i));
+  for(unsigned int i=0;i<selHits.size();++i){
+    zsel[i]=zpHit.at(selHits.at(i));
     ysel[i]=ypHit.at(selHits.at(i));
   }
-  TGraph* gsel = new TGraph(selHits.size(),xsel,ysel);
+  TGraph* gsel = new TGraph(selHits.size(),zsel,ysel);
   gsel->SetMarkerStyle(20);
   gsel->SetMarkerColor(kGreen);
   gsel->Draw("P");
 
 
   c->cd(2);
-  houghHistoYZ->Draw("colz");
-
-  double r1=getScaleA()*r1Y;
-  double r2=getScaleB()*r2Y;
+  houghHisto->Draw("colz");
 
 
-  TEllipse* ell = new TEllipse(aymax,bymax,r1,r2);
-  ell->SetFillStyle(0);
-  ell->SetLineWidth(4);
-  ell->Draw("same");
+  maxPoints->Draw("P");
   for(int i=0;i<NumberOfHits;++i){
-    houghLinesYZ[i]->Draw("same");
+    houghLines[i]->Draw("same");
   }
 
   if(stop){
@@ -101,75 +103,75 @@ void TChough2::doHough(){
   assert(NumberOfHits<=Max_NumberOfHits);
   for(int i=0;i<NumberOfHits;i++) {
 
-    HitCoordinates[i][0] = ypHit.at(i);
-    HitCoordinates[i][1] = zpHit.at(i);
+    HitCoordinates[i][0] = ypHit.at(i)-minY;
+    HitCoordinates[i][1] = zpHit.at(i)-minZ;
 
   }
 
   makeHoughLines();
   makeHoughHisto();
 
-  findMaxInHisto(aymax,bymax);
+  findMaxInHisto(maxVector,nMax);
 
 }
 
-bool TChough2::hot(int index){
-  assert(index<nHits());
-
+bool TChough2::hot(int clIndex,int maxIndex){
+  assert(clIndex<nHits());
+  assert(clIndex>-1);
+  assert(maxIndex<nMax);
   double z,y;
   double discr;
 
-  z = HitCoordinates[index][1];
-  y = HitCoordinates[index][0];
-
-  //b=(-z)*a+x_or_y;
-  double slope = (-z);
-  double offset = y;
-  //geradengleichung so transformieren, dass sie einer ellipse im ursprung entspricht
-  offset -= bymax;
-  offset += slope*aymax;
-  //	cout << a0 << " " << b0 <<  endl;
-
-  double r1=getScaleA()*r1Y;
-  double r2=getScaleB()*r2Y;
-
+  z = HitCoordinates[clIndex][1];
+  y = HitCoordinates[clIndex][0];
   
-  discr = r1*r1*slope*slope + r2*r2 - offset*offset;
-  if(discr>=0) {
-	return true;
+  int maxBinTheta=maxVector.at(maxIndex).first;
+  int maxBinR=maxVector.at(maxIndex).second;
+  
+  double thetaAtMax=maxBinTheta*rangeTheta/nBinsTheta+minTheta;
+  double rAtMax=maxBinR*rangeR/nBinsTheta;
+  double rPoint=z*cos(thetaAtMax) +y*sin(thetaAtMax);
+  discr=cutR-fabs(rPoint-rAtMax);
+  if(discr<0){
+    return false;
   }
-  return false;
+  return true;
+  
 }
 
 
-void TChough2::findMaxInHisto(double& amax,double& bmax) {
+void TChough2::findMaxInHisto(std::vector<std::pair<int,int> > &_maxVector, int _nMax) {
+  TH2D* houghHistoCopy = new TH2D(*houghHisto);
+  for(int i=0;i<nMax;i++){
+    int maxBinTheta;
+    int maxBinR;
+    int zDummy;
+    houghHistoCopy->GetMaximumBin(maxBinTheta,maxBinR,zDummy);
+    for (int k=maxBinTheta-5;k<=maxBinTheta+10;k++){
+      for(int j=maxBinR-5;j<=maxBinR+10;j++){
+        if(k<0){
+          continue;
+        }
+        else if(k>nBinsTheta){
+          continue;
+        }
+        if(j<0){
+          continue;
+        }
+        else if(j>nBinsR){
+          continue;
+        }
+        houghHistoCopy->SetBinContent(k,j,0);
+      }
+    }
+    pair<int,int> max;
+    max.first=maxBinTheta;
+    max.second=maxBinR;
 
-  double minA,maxA,minB,maxB,nBinsA,nBinsB;
-  TH2D* hist;
-
-  hist = houghHistoYZ;
-  minA = minAY;
-  maxA = maxAY;
-  minB = minBY;
-  maxB = maxBY;
-  nBinsA = nBinsAY;
-  nBinsB = nBinsBY;
-
-
-  double binA = (maxA - minA)/nBinsA;
-  double binB = (maxB - minB)/nBinsB;
-
-  double maxEntry = -1;
-
-  for(int ia=0;ia<nBinsA;ia++) {
-	for(int ib=0;ib<nBinsB;ib++) {
-	  if(hist->GetBinContent(ia+1,ib+1) > maxEntry) {
-		maxEntry = hist->GetBinContent(ia+1,ib+1);
-		amax = minA + ia*binA + 0.5*binA;
-		bmax = minB + ib*binB + 0.5*binB;
-	  }
-	}
+    maxVector.push_back(max);
   }
+  delete houghHistoCopy;
+
 }
 
 
@@ -181,10 +183,10 @@ void TChough2::cleanup() {
 
 
 
-  delete houghHistoYZ;
+  delete houghHisto;
 
   for(int i=0;i<Max_NumberOfHits;i++) {
-	delete houghLinesYZ[i];
+	delete houghLines[i];
   }
 
   for(int i=0;i<Max_NumberOfHits;i++) {
@@ -196,10 +198,10 @@ void TChough2::cleanup() {
 
 
 
-  houghHistoYZ=NULL;
+  houghHisto=NULL;
 
   for(int i=0;i<Max_NumberOfHits;i++) {
-	houghLinesYZ[i]=NULL;
+	houghLines[i]=NULL;
   }
   //  std::cout << "done cleaning up..." << std::endl;
 
@@ -208,23 +210,24 @@ void TChough2::cleanup() {
  
  
 
-
+/*
+  r(theta) = z*cos(theta) + y*sin(theta)
+*/
 void TChough2::makeHoughLines(){
   char buf[50];
   char bufName[50];
   for(int i=0;i<NumberOfHits;i++) {
-	sprintf(buf,"%f-%f*x",HitCoordinates[i][0],HitCoordinates[i][1]);
-	sprintf(bufName,"copy%f-%f*x",HitCoordinates[i][0],HitCoordinates[i][1]);
-	houghLinesYZ[i] = new TF1(bufName,buf,minAY,maxAY);
-	//	  houghLinesYZ[i]->SetLineColor(kBlue);
-	houghLinesYZ[i]->SetLineStyle(2);
+	sprintf(buf,"%f*sin(x)+%f*cos(x)",HitCoordinates[i][0],HitCoordinates[i][1]);
+	sprintf(bufName,"copy%f*sin(x)+%f*cos(x)",HitCoordinates[i][0],HitCoordinates[i][1]);
+	houghLines[i] = new TF1(bufName,buf,minTheta,maxTheta);
+	//	  houghLines[i]->SetLineColor(kBlue);
+	houghLines[i]->SetLineStyle(2);
   }
 }
 
 
 void TChough2::findHitsOnTrack(){
-  double z,y;
-  double discr;
+ 
 
   for(int i=0;i<Max_NumberOfHits;i++) {
 	selHits[i] = -1;
@@ -233,133 +236,96 @@ void TChough2::findHitsOnTrack(){
   
 
   for(int ihit=0;ihit<NumberOfHits;ihit++) {
-	z = HitCoordinates[ihit][1];
-	y = HitCoordinates[ihit][0];
-
-	//b=(-z)*a+x_or_y;
-	double slope = (-z);
-	double offset = y;
-	//geradengleichung so transformieren, dass sie einer ellipse im ursprung entspricht
-	offset -= bymax;
-	offset += slope*aymax;
-
-	discr = r1Y*r1Y*slope*slope + r2Y*r2Y - offset*offset;
-	if(discr>=0) {
-	  selHits[nSelHits] = ihit;
-	  nSelHits++;
-	}
-	if(discr>=0) houghLinesYZ[ihit]->SetLineStyle(1);
-	if(discr>=0) houghLinesYZ[ihit]->SetLineWidth(1);
-	if(discr>=0) houghLinesYZ[ihit]->SetLineColor(kGreen);
+    if(hot(ihit)) {
+      selHits[nSelHits] = ihit;
+      nSelHits++;
+      houghLines[ihit]->SetLineStyle(1);
+      houghLines[ihit]->SetLineWidth(1);
+      houghLines[ihit]->SetLineColor(kGreen);
+    }
   }
-
 }
 
 
 void TChough2::makeHoughHisto(){
   char buf[50];
   static TRandom r(0);
-  sprintf(buf,"houghHistoYZ%5.5f",r.Uniform());
-  houghHistoYZ = new TH2D(buf,"",nBinsAY,minAY,maxAY,nBinsBY,minBY,maxBY);
-  houghHistoYZ->SetStats(kFALSE);
-  houghHistoYZ->GetYaxis()->SetTitle("B");
-  houghHistoYZ->GetXaxis()->SetTitle("A");
+  sprintf(buf,"houghHisto%5.5f",r.Uniform());
+  houghHisto = new TH2D(buf,"",nBinsTheta,minTheta,maxTheta,nBinsR,minR,maxR);
+  houghHisto->SetStats(kFALSE);
+  houghHisto->GetYaxis()->SetTitle("R");
+  houghHisto->GetXaxis()->SetTitle("#theta");
   
 
   for(int ihit=0;ihit<NumberOfHits;ihit++) {///NumberOfHits;ihit++) {
-	for(int ia=0;ia<nBinsAY;ia++) {
-	  for(int ib=0;ib<nBinsBY;ib++) {
-		int weight = binWeight(ia,ib,ihit);
-		if(weight > 0) {
-		  houghHistoYZ->SetBinContent(ia+1,ib+1,houghHistoYZ->GetBinContent(ia+1,ib+1) + weight);
-		}
-	  }
-	}
+    for(int iTheta=0;iTheta<nBinsTheta;iTheta++) {
+      double z = HitCoordinates[ihit][1];
+      double y = HitCoordinates[ihit][0];
+      double stepTheta=rangeTheta/nBinsTheta;
+      double stepR=rangeR/nBinsR;
+      double rTmp=z*cos(iTheta*stepTheta-minTheta)+y*sin(iTheta*stepTheta-minTheta);
+      int iR=(int)rTmp/stepR;
+      houghHisto->SetBinContent(iTheta,iR,houghHisto->GetBinContent(iTheta,iR) + 1);
+
+      
+    }
   }
 }
 
 
-int TChough2::binWeight(int ia, int ib, int ihit) {
+int TChough2::binWeight(int iTheta, int iR, int ihit) {
+  return 1;
+}
 
-  double minA,maxA,minB,maxB,nBinsA,nBinsB;
+void TChough2::convert(std::vector<TCcluster>& _c){
+  TVector3 xp=yp.Cross(zp);
+  TMatrixT<double> S(3,3);
+  S[0][0]=xp.X();
+  S[1][0]=xp.Y();
+  S[2][0]=xp.Z();
+  S[0][1]=yp.X();
+  S[1][1]=yp.Y();
+  S[2][1]=yp.Z();
+  S[0][2]=zp.X();
+  S[1][2]=zp.Y();
+  S[2][2]=zp.Z();
 
-  double y;
-  double z = HitCoordinates[ihit][1];
+  //TMatrixT<double> Stransp = S;
+  //Stransp.T();
 
-  y = HitCoordinates[ihit][0];
-  minA = minAY;
-  maxA = maxAY;
-  minB = minBY;
-  maxB = maxBY;
-  nBinsA = nBinsAY;
-  nBinsB = nBinsBY;
-
-  double binA = (maxA - minA)/nBinsA;
-  double binB = (maxB - minB)/nBinsB;
-
+  clear();
+  minY=1.E50;
+  maxY=-1.E50;
+  minZ=1.E50;
+  maxZ=-1.E50;
   
-  double a0 = minA + ia*binA;
-  double b0 = minB + ib*binB;
-  double a1 = a0 + binA;
-  double b1 = b0 + binB;
-  
-  
-  double a_at_b0 = (y-b0)/z;
-  double a_at_b1 = (y-b1)/z;
-  double b_at_a0 = -1.0*a0*z+y;
-  double b_at_a1 = -1.0*a1*z+y;
-
-
-  bool left=false;
-  bool right=false;
-  bool top=false;
-  bool bottom=false;
-  
-  if(b_at_a0>b0 && b_at_a0<b1) left=true;
-  if(b_at_a1>b0 && b_at_a1<b1) right=true;
-  if(a_at_b0>a0 && a_at_b0<a1) bottom=true;
-  if(a_at_b1>a0 && a_at_b1<a1) top=true;
-
-
-  if(top || bottom || left || right) {
-// 	std::cout << ihit << " " << ia << " " << ib << std::endl;
-// 	cout << "top: " << top << endl;
-// 	cout << "bottom: " << bottom << endl;
-// 	cout << "right: " << right << endl;
-// 	cout << "left: " << left << endl;
-//	distance = 1.;
+  for(unsigned int i=0;i<_c.size();++i){
+	TVector3 hitPrime = S * _c.at(i).posXYZ();
+	//xprime isnt needed only doing transform in one plane
+        double y = hitPrime.Y();
+        double z = hitPrime.Z();
+	
+        if(y>maxY) maxY=y;
+	if(y<minY) minY=y;
+	if(z>maxZ) maxZ=z;
+	if(z<minZ) minZ=z;
+        ypHit.push_back(y);
+	zpHit.push_back(z);
   }
-
-
+  minR=0;
   /*
-  if(!top&&bottom&&left&&right) cout << "bottom and left and right" << endl;
-  if(top&&!bottom&&left&&right) cout << "top and left and right" << endl;
-  if(top&&bottom&&!left&&right) cout << "top and bottom and right" << endl;
-  if(top&&bottom&&left&&!right) cout << "top and bottom and left" << endl;
-  if(top&&bottom&&!left&&!right) cout << "top and bottom" << endl;
-  if(top&&!bottom&&left&&!right) cout << "top and left" << endl;
-  if(top&&!bottom&&!left&&right) cout << "top and right" << endl;
-  if(!top&&bottom&&left&&!right) cout << "bottom and left" << endl;
-  if(!top&&bottom&&!left&&right) cout << "bottom and right" << endl;
-  if(!top&&!bottom&&left&&right) cout << "left and right" << endl;
-  if(top&&!bottom&&!left&&!right) cout << "top only" << endl;
-  if(!top&&bottom&&!left&&!right) cout << "bottom only" << endl;
-  if(!top&&!bottom&&left&&!right) cout << "left only" << endl;
-  if(!top&&!bottom&&!left&&right) cout << "right only" << endl;
+    Assuming that a track has to pass all detectors, but not nececarily having hits inn all
+    maxR is the distance from one corner of a rectangle with sides rangeY and rangeZ to the diagonal
+    max/minTheta is calculated using the same asumption
   */
+  double rangeY=maxY-minY;
+  double rangeZ=maxZ-minZ;
 
-  double distance=-1;
-  if(left&&right) distance = sqrt( pow(b_at_a0-b_at_a1,2.) + pow(binA,2.));
-  if(left&&bottom) distance = sqrt( pow(a_at_b0-a0,2.) +pow(b_at_a0-b0,2.) );
-  if(left&&top) distance = sqrt( pow(a_at_b1-a0,2.) +pow(b_at_a0-b1,2.) );
-  if(bottom&&top) distance = sqrt( pow(a_at_b0 - a_at_b1,2.)+pow(binB,2.));
-  if(bottom&&right) distance = sqrt(pow(a1-a_at_b0,2.)+pow(b0-b_at_a1,2.));
-  if(right&&top) distance = sqrt(pow(b1-b_at_a1,2.) + pow(a1-a_at_b1,2.)   );
+  maxR=rangeY*rangeZ / sqrt(pow(rangeY,2) + pow(rangeZ,2));
+  maxTheta=atan(rangeY/rangeZ)+3.141592654/2;
+  minTheta=-maxTheta;
+  if( fabs(maxY-minY)<1.E-10)minY-=1.E-5;
+  if( fabs(maxZ-minZ)<1.E-10)minZ-=1.E-5;
 
-  //habe jetzt distance oder aber -1 fuer nicht getroffen
-  double maxDistance = sqrt(pow(binA,2.)+pow(binB,2.));
-  int weight = (int)((distance * 100) / maxDistance);
-  return weight;
-}
   
-
+}
--- tpc/TestChamber/src/TChough2.h
+++ tpc/TestChamber/src/TChough2.h
@@ -6,52 +6,75 @@
 #include"TF1.h"
 #include"TCanvas.h"
 
-class TChough1 : public TCabsHough{
+class TChough2 : public TCabsHough{
  public:
 
-  virtual ~TChough1();
+  virtual ~TChough2();
+  void convert(std::vector<TCcluster>& _c);
   virtual void doHough();
-  TChough1(const TVector3& _yp,const TVector3& _zp);
-  virtual bool hot(int index);
+  /*
+    nMax beeing the number of straight lines sought
+  */
+  virtual void make(std::vector<TCcluster>& _c, int _nMax=1){
+    nMax=_nMax;
+    convert(_c);
+    doHough();
+  }
+  /*
+    Default values is theta ranging from 0 to pi.Going from 0 - 2pi would alow tracks going both ways, but is not interesting for the beam telescope at elsa. 
+  */
+  TChough2(const TVector3& _yp,const TVector3& _zp,double _rR=0.1);
+  /*
+    Given the index of a cluster, the index of the cluster in the vector handed in by the make method
+    and the index of the maximum wanted, the method returns a boolean true if the cluster belongs to the line found
+  */
+  virtual bool hot(int clIndex, int maxIndex);
+  virtual bool hot(int clIndex){
+    return hot(clIndex,0);
+  }
+
   virtual void draw(bool stop=true,int _x=50,int _y=50,int _w=600,int _h=600);
+  
 
  private:
-  double thetaMin,thetaMax;
-
+  int nMax;
+  /*
+    Vector containing the maximas of the hough image
+  */
+  std::vector< std::pair<int,int> > maxVector;
+  double cutR;
 
   TCanvas *canvas1;
 
-  TH2D* houghHistoYZ;
+  TH2D* houghHisto;
   static const int Max_NumberOfHits = 500;
-
-  TF1* houghLinesYZ[Max_NumberOfHits];
-
-
-  Int_t           NumberOfHits;
-  Double_t        HitCoordinates[Max_NumberOfHits][2];   //[NumberOfHits]
-
-
-  int selHits[Max_NumberOfHits];
-  int nSelHits;
-
-  double r1Y;
-  double r2Y;
-
-  double minAY;
-  double maxAY;
-  double minBY;
-  double maxBY;
   
-  int nBinsAY;
-  int nBinsBY;
+  TF1* houghLines[Max_NumberOfHits];
 
+  Int_t NumberOfHits;
+  Double_t HitCoordinates[Max_NumberOfHits][2];   //[NumberOfHits] 
+  
+  int selHits[Max_NumberOfHits];//for drawing
+  int nSelHits;
+  double rangeR;
+  double minR;
+  double maxR;
 
+  double rangeTheta;
+  double minTheta;
+  double maxTheta;
 
+  double minY;
+  double maxY;
+  double minZ;
+  double maxZ;
+  int nBinsR;
+  int nBinsTheta;
 
   void makeHoughHisto();
   void makeHoughLines();
-  int binWeight(int ia, int ib, int ihit);
-  void findMaxInHisto(double& amax,double& bmax);
+  int binWeight(int iR, int iTheta, int iHit);
+  void findMaxInHisto(std::vector<std::pair<int,int> > &_maxVector, int _nMax);
   void findHitsOnTrack();
 
   void cleanup();
