062ce596afc2057d49d846f7e6ee828472d5b4c6
Christian Hoeppner <christian.hoeppner@cern.ch>
2009-03-18 15:09:42 +0000

    small bug fix

--- trackrep/GeaneTrackRep.h
+++ trackrep/GeaneTrackRep.h
@@ -77,11 +77,13 @@ public:
   void extrapolateToPoca(const TVector3& pos,
 			 TVector3& poca,
 			 TVector3& dirInPoca);
+  /*
   void extrapolateToLine(const TVector3& point1,
 			 const TVector3& point2,
 			 TVector3& poca,
 			 TVector3& dirInPoca);
-  
+  */
+
   TVector3 getPocaOnLine(const TVector3& p1, 
 			 const TVector3& p2, 
 			 bool back=false);
