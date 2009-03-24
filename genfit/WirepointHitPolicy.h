c68c712fe5bd485d8944135089f7d8e7b6b39ac6
Christian Hoeppner <christian.hoeppner@cern.ch>
2009-03-24 00:51:18 +0000

    added author tags

--- genfit/WirepointHitPolicy.h
+++ genfit/WirepointHitPolicy.h
@@ -16,7 +16,7 @@ class AbsTrackRep;
 /** @brief policy class for hits in wire detectors (STT and DCH)
  *  which can measure the coordinate along the wire
  *  @author Christian H&ouml;ppner (Technische Universit&auml;t M&uuml;nchen, original author)
- *  @author Lia Lavezzi (INFN Pavia roiginal author)
+ *  @author Lia Lavezzi (INFN Pavia original author)
  *  @author Sebastian Neubert  (Technische Universit&auml;t M&uuml;nchen, original author)
  * This policy is not valid for any kind of plane orientation
  * choice: to use it you MUST choose a plane described by u 
