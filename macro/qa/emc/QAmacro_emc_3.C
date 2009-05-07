148af68e892f444c5b4b9737ad1946e6d71b4834
Johan Messchendorp <j.g.messchendorp@rug.nl>
2009-05-07 17:17:32 +0000

    emc QA macros call rootlogon.C in /gconfig

--- macro/qa/emc/QAmacro_emc_3.C
+++ macro/qa/emc/QAmacro_emc_3.C
@@ -1,18 +1,10 @@
 {
   // Macro loads a file after reconstruction and plots difference between initial direction of particle and angular position of cluster
   
-	gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-	basiclibs();
-	gSystem->Load("libGeoBase");
-	gSystem->Load("libParBase");
-	gSystem->Load("libBase");
-	gSystem->Load("libPndData");
-	gSystem->Load("libField");
-	gSystem->Load("libGen");
-	gSystem->Load("libPassive");
-	// add the detector libraries you need
-	gSystem->Load("libEmc");	 
-	gSystem->Load("libGeom.so");
+        gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+        gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+        rootlogon();
+        basiclibs();
 	
 	TFile* f = new TFile("full_emc.root"); //file you want to analyse
 	TTree *t=(TTree *) f->Get("cbmsim") ;
