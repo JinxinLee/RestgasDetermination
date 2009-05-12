668aef5badc5708d43959318db2e43d77b630f2a
vanni <vanni>
2009-05-12 16:43:28 +0000

    change

--- gpid/gamma/runpid.sh
+++ gpid/gamma/runpid.sh
@@ -32,8 +32,8 @@ source $PANDAROOTBUILD/config.sh > logfile
 #cp $PANDAROOTHOME/macro/emc/sim_emc.C .
 #cp $PANDAROOTHOME/macro/emc/full_emc.C .
 
-cp $HOME/pandaroot/vanni/gpid/pidmacro/run_sim_alldet.C .
-cp $HOME/pandaroot/vanni/gpid/pidmacro/run_rec_alldet.C .
+cp $HOME/pandaroot/vanni/gpid/gamma/run_sim_alldet.C .
+cp $HOME/pandaroot/vanni/gpid/gamma/run_rec_alldet.C .
 #cp $HOME/pandaroot/vanni/gpid/pidmacro/grep_all.C  .
 #cp $HOME/pandaroot/vanni/gpid/pidmacro/shower.C  .
 #cp $HOME/pandaroot/vanni/gpid/pidmacro/rootlogon.C  .
