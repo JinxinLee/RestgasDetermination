a017288f6100daf37ea541935c3c966e324e8c48
Radoslaw Karabowicz <R.Karabowicz@gsi.de>
2009-05-27 11:38:47 +0000

    Forward tracking first commission, works for GEM and DCH

---
+++ fwd/PndFwdKalmanTask.cxx
@@ -0,0 +1,433 @@
+//-----------------------------------------------------------
+// File and Version Information:
+// $Id$
+//
+// Description:
+//      Implementation of class PndFwdKalmanTask
+//      see PndFwdKalmanTask.hh for details
+//
+// Environment:
+//      Software developed for the PANDA Detector at FAIR.
+//
+// Author List:
+//      Sebastian Neubert    TUM            (original author)
+//      Ralf Kliemt, TU Dresden             (Copied for MVD use)
+//      Radoslaw Karabowicz, GSI            (converted for FWDs)
+//
+//-----------------------------------------------------------
+
+// Panda Headers ----------------------
+
+// This Class' Header ------------------
+#include "PndFwdKalmanTask.h"
+
+// C/C++ Headers ----------------------
+#include <algorithm>
+#include <iostream>
+#include <assert.h>
+
+// Collaborating Class Headers --------
+#include "FairRootManager.h"
+#include "TClonesArray.h"
+#include "Track.h"
+#include "TDatabasePDG.h"
+
+// #include "PndFwdHit.h"
+#include "FairMCPoint.h"
+
+#include "PndGemRecoHit.h"
+#include "PndDchRecoHit2.h"
+
+#include "RecoHitFactory.h"
+#include "Kalman.h"
+#include "FitterExceptions.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TGeoTrack.h"
+#include "TGeoManager.h"
+#include "TLorentzVector.h"
+#include "DetPlane.h"
+#include "FairTrackParH.h"
+
+#include "LSLTrackRep.h"
+#include "GeaneTrackRep.h"
+#include "FairGeanePro.h"
+
+#include "TRandom3.h"
+
+// Class Member definitions -----------
+
+
+PndFwdKalmanTask::PndFwdKalmanTask()
+  : FairTask("Kalman Filter"), fPersistence(kFALSE)
+{
+  fTrackBranchName = "FSTracks";
+
+  fMomentum = 1.;
+  fTheta = 5;
+  fPhi = 0;
+}
+
+
+PndFwdKalmanTask::~PndFwdKalmanTask()
+{
+  if(fPH!=NULL)delete fPH;
+  if(fChi2H!=NULL)delete fChi2H;
+}
+
+InitStatus
+PndFwdKalmanTask::Init()
+{
+  //Get ROOT Manager
+  FairRootManager* ioman= FairRootManager::Instance();
+
+  if(ioman==0)
+    {
+      Error("PndFwdKalmanTask::Init","RootManager not instantiated!");
+      return kERROR;
+    }
+
+  // Get input collection
+  fTrackArray=(TClonesArray*) ioman->GetObject(fTrackBranchName);
+
+  if(fTrackArray==0)
+    {
+      Error("PndFwdKalmanTask::Init","track-array not found!");
+      return kERROR;
+    }
+
+
+  // Build hit factory -----------------------------
+  fTheRecoHitFactory = new RecoHitFactory();
+
+  TClonesArray* gemHitArray=(TClonesArray*) ioman->GetObject("GEMHit");
+  if(gemHitArray==0){ //TODO Convention on detector number needed
+    Error("PndFwdKalmanTask::Init","GEMHit array not found");
+  } else {
+    fTheRecoHitFactory->addProducer
+      (2,new RecoHitProducer<PndGemHit,PndGemRecoHit>(gemHitArray));
+  }
+  TClonesArray* dchCylHitArray=(TClonesArray*) ioman->GetObject("PndDchCylinderHit");
+  if(dchCylHitArray==0){ //TODO Convention on detector number needed
+    Error("PndFwdKalmanTask::Init","PndDchCylinderHit array not found");
+  } else {
+    fTheRecoHitFactory->addProducer
+      (1,new RecoHitProducer<PndDchCylinderHit,PndDchRecoHit2>(dchCylHitArray));
+  }
+
+  fPro = new FairGeanePro();
+
+
+  // setup histograms
+  fPH=new TH1D("pH","p",100,0.4,0.6);
+  fChi2H=new TH1D("chi2H","chi2",100,0,20);
+  fMassV0=new TH1D("massV0","massV0",100,0,5);
+  fMassETAC=new TH1D("massEta","massEta",100,2.5,3.5);
+  fMasses=new TH1D("masses","masses",100,0,5);
+ 
+  fhMomentumX2D = new TH2D("fhMomentumX2D","fhMomentumX2D",1000,-5.,5.,1000,-5.,5.);
+  fhMomentumY2D = new TH2D("fhMomentumY2D","fhMomentumY2D",1000,-5.,5.,1000,-5.,5.);
+  fhMomentumZ2D = new TH2D("fhMomentumZ2D","fhMomentumZ2D",2000,-20.,20.,2000,-20.,20.);
+  fhMomentumM2D = new TH2D("fhMomentumM2D","fhMomentumM2D",2000,  0.,20.,2000,  0.,20.);
+  fhMomentumX1D = new TH1D("fhMomentumX1D","fhMomentumX1D",2000,-10.,10.);
+  fhMomentumY1D = new TH1D("fhMomentumY1D","fhMomentumY1D",2000,-10.,10.);
+  fhMomentumZ1D = new TH1D("fhMomentumZ1D","fhMomentumZ1D",2000,-10.,10.);
+  fhMomentumM1D = new TH1D("fhMomentumM1D","fhMomentumM1D",2000,-10.,10.);
+  fhMomentumRes = new TH2D("fhMomentumRes","fhMomentumRes",40,0.,20.,2000,-20.,20.);
+
+  fhPositionX2D = new TH2D("fhPositionX2D","fhPositionX2D",2000,-100.,100.,2000,-100.,100.);
+  fhPositionY2D = new TH2D("fhPositionY2D","fhPositionY2D",2000,-100.,100.,2000,-100.,100.);
+  fhPositionX1D = new TH1D("fhPositionX1D","fhPositionX1D",2000,-10.,10.);
+  fhPositionY1D = new TH1D("fhPositionY1D","fhPositionY1D",2000,-10.,10.);
+
+
+  return kSUCCESS;
+}
+
+
+void
+PndFwdKalmanTask::Exec(Option_t* opt)
+{
+  std::cout<<"PndFwdKalmanTask::Exec"<<std::endl;
+  // Reset output Array
+  //if(fTrackArray==0) Fatal("Kalman::Exec)","No TrackArray");
+  // fTrackArray->Delete();
+
+  Int_t ntracks=fTrackArray->GetEntriesFast();
+
+  // Detailed output
+  if(fVerbose>1)std::cout<<" -I- PndFwdKalmanTask: contains "<<ntracks<<" Tracks."<<std::endl;
+  if(fVerbose>2){
+    std::cout<< " Detailed Debug info on the tracks:"<<std::endl;
+    unsigned int detid=12345, index=12345;
+    for(Int_t itr=0;itr<ntracks;++itr){
+      Track* trac = (Track*)fTrackArray->At(itr);
+      TrackCand trcnd = (TrackCand)trac->getCand();
+      //      TrackCand* trcnd = (TrackCand*)fTrackArray->At(itr);
+      std::cout<< "TrackCand no. "<<itr<<" has "<<trcnd.getNHits()<<" hits."<<std::endl;
+      std::cout<<"[ ihit | detid | index";
+      for(unsigned int ihit=0;ihit<trcnd.getNHits();ihit++){
+        trcnd.getHit(ihit,  detid,index); //detid and index are written here
+        std::cout<<" ]\n[ "<<ihit<<" | "<<detid<<" | "<<index;
+      }
+      std::cout<<" ]"<<std::endl;
+    }
+  }
+
+  // Cut too busy events TODO
+  if(ntracks>20){
+    std::cout<<"ntracks="<<ntracks<<" Evil Event! skipping"<<std::endl;
+    return;
+  }
+
+  // Fitting ---------------- can go to another task!
+  Kalman fitter;
+  fitter.setNumIterations(fNumIt);
+  //   fitter.setVerbose(fVerbose);
+
+  std::vector<TLorentzVector*> particles;
+  std::vector<Int_t> signs;
+
+  for(Int_t itr=0;itr<ntracks;++itr){
+    std::cout<<"starting track"<<itr<<std::endl;
+    //     AbsTrackRep* rep = new LSLTrackRep();
+
+      Track* trac = (Track*)fTrackArray->At(itr);
+      TrackCand trcnd = (TrackCand)trac->getCand();
+      TVector3 beforePos = trac->getPos();
+      TVector3 beforeMom = trac->getMom();
+
+      beforePos.SetXYZ(0.,0.,0.);
+      beforeMom.SetMagThetaPhi(fMomentum,TMath::DegToRad()*(Double_t)fTheta,TMath::DegToRad()*(Double_t)fPhi);
+
+      Double_t rad = TMath::Sqrt(beforePos.X()*beforePos.X()+beforePos.Y()*beforePos.Y());
+      if ( rad > 20 ) 
+	std::cout << "RADIUS BIGGER THAN 20" << std::endl;
+    
+    // Starting values for guessing
+      Int_t PDGCode= 211;
+//     TVector3 StartPos    = TVector3 (beforePos.X()+gRandom->Gaus(0,0.1),
+// 				     beforePos.Y()+gRandom->Gaus(0,0.1),
+// 				     beforePos.Z()+gRandom->Gaus(0,0.1));
+    TVector3 StartPos    = TVector3 (0.,0.,0.);
+
+				     //1.0,0.0,0.0);//cmn
+    TVector3 StartPosErr = TVector3(0.01,0.01,0.01);
+    TVector3 StartMom    = TVector3 (1.,0.,1.); 
+    std::cout << "setting mag to " << beforeMom.Mag() << std::endl;
+//     StartMom.SetMagThetaPhi(beforeMom.Mag()-0.5  ,//gRandom->Gaus(0,0.1),
+//  			    beforeMom.Theta(),//+gRandom->Gaus(0,0.1),
+//  			    beforeMom.Phi()  );//+gRandom->Gaus(0,0.1));
+    StartMom.SetMagThetaPhi(fMomentum+0.05,TMath::DegToRad()*(Double_t)fTheta,TMath::DegToRad()*(Double_t)fPhi);
+    std::cout << "momenta set to " << StartMom.Mag() << std::endl;
+//    StartMom.SetMagThetaPhi(2.,10.*TMath::Pi()/180.,0.);
+    //5.05 , 30.*TMath::Pi()/360. , 0.);
+//     StartMom.SetXYZ(0.01*TMath::Ceil(100.*beforeMom.X()),
+// 		    0.01*TMath::Ceil(100.*beforeMom.Y()),
+// 		    0.01*TMath::Ceil(100.*beforeMom.Z()));
+    //   StartMom.SetMag(1.1);
+    TVector3 StartMomErr = TVector3(0.1*StartMom);//1.,1.,1.);
+    TDatabasePDG *fdbPDG= TDatabasePDG::Instance();
+    TParticlePDG *fParticle= fdbPDG->GetParticle(PDGCode);
+    Double_t  fCharge= fParticle->Charge()/3.;
+    // what to guess here?
+    TVector3 U(1.,0.,0.);
+    TVector3 V(0.,1.,0.);
+    DetPlane start_pl(StartPos,U,V);
+
+    AbsTrackRep* rep = new GeaneTrackRep(fPro,
+ 					 start_pl,StartMom,
+ 					 StartPosErr,StartMomErr,
+ 					 fCharge,PDGCode);
+    
+    /*    TVector3 dir=StartMom.Unit();
+    Double_t dxdz=dir.X()/dir.Z();
+    Double_t dydz=dir.Y()/dir.Z();
+    Double_t qp=fCharge/StartMom.Mag();
+    std::cout << "2momenta set to " << StartMom.Mag() << " but fcharge = " << fCharge << std::endl;
+    std::cout << "moment moment " << dxdz << " " << dydz << " " << qp << std::endl;
+    AbsTrackRep* rep = new LSLTrackRep(StartPos.Z(),StartPos.X(),StartPos.Y(),dxdz,dydz,qp,
+				       StartPosErr.X(),StartPosErr.Y(),0.1,0.1,0.1,NULL);
+    */
+  
+    Track* trk= new Track(rep);
+
+
+    trk->setCandidate(trcnd);
+    //Track* trk=(Track*)fTrackArray->At(itr);
+    
+    // Load RecoHits
+    try {
+      trk->addHitVector(fTheRecoHitFactory->createMany(trk->getCand()));
+      std::cout<<trk->getNumHits()<<" hits in track "
+	       <<itr<<std::endl;
+    }
+    catch(FitterException& e) {
+      std::cout << e.what() << std::endl;
+      throw e;
+    }
+    
+    // Start Fitter
+    try{
+      std::cout<<"starting fit"<<std::endl;
+      fitter.processTrack(trk);
+    }
+    catch (FitterException e){
+      std::cout<<e.what()<<std::endl;
+    }
+    
+    // Print Track Parameters after fit
+    if(trk->getTrackRep(0)->getStatusFlag()==0){
+      //trk->getTrackRep(0)->Print();
+      DetPlane plane(TVector3(0,0,0.1),TVector3(1,0,0),TVector3(0,1,0));
+      TVector3 p3=trk->getTrackRep(0)->getMom(plane);
+      Double_t p=trk->getMom().Mag();
+      fPH->Fill(p);
+      
+      TLorentzVector* p4=new TLorentzVector();
+      p4->SetXYZM(p3.X(),p3.Y(),p3.Z(),0.493677);
+      particles.push_back(p4);
+      signs.push_back((Int_t)trk->getTrackRep(0)->getCharge());
+      
+      Double_t chi2=trk->getChiSqu();
+      std::cout<<"ChiSq="<<chi2<<std::endl;
+      fChi2H->Fill(chi2);
+    }
+
+    TVector3 resultPos = trk->getPos();
+    TVector3 resultMom = trk->getMom();
+
+    std::cout << "********************************************************" << std::endl;
+    std::cout << "result pos = (" 
+	      << resultPos.X() << ","
+	      << resultPos.Y() << ","
+	      << resultPos.Z() << ")" << std::endl;
+    std::cout << "result mom = (" 
+	      << resultMom.X() << ","
+	      << resultMom.Y() << ","
+	      << resultMom.Z() << ") ----> " << resultMom.Mag() << std::endl;
+
+    GeaneTrackRep* tempRep = (GeaneTrackRep*)trac->getTrackRep(0);
+    std::cout << "BUT IT WAS " << tempRep->getPDG() << std::endl;
+
+    fhMomentumX2D->Fill(beforeMom.X(),resultMom.X());
+    fhMomentumY2D->Fill(beforeMom.Y(),resultMom.Y());
+    fhMomentumZ2D->Fill(beforeMom.Z(),resultMom.Z());
+    fhMomentumM2D->Fill(beforeMom.Mag(),resultMom.Mag());
+    fhMomentumX1D->Fill(beforeMom.X()+resultMom.X());
+    fhMomentumY1D->Fill(beforeMom.Y()+resultMom.Y());
+    fhMomentumZ1D->Fill(beforeMom.Z()+resultMom.Z());
+    fhMomentumM1D->Fill(beforeMom.Mag()-resultMom.Mag());
+    fhMomentumRes->Fill(beforeMom.Mag(),100.*(beforeMom.Mag()-resultMom.Mag())/beforeMom.Mag());
+    
+    fhPositionX2D->Fill(beforePos.X(),resultPos.X());
+    fhPositionY2D->Fill(beforePos.Y(),resultPos.Y());
+    fhPositionX1D->Fill(beforePos.X()-resultPos.X());
+    fhPositionY1D->Fill(beforePos.Y()-resultPos.Y());
+
+  }
+
+  std::cout<<"Fitting done"<<std::endl;
+  
+  // --- ANALYSIS ---
+  if(particles.size()>20)return;
+  std::cout<<"Starting Analysis"<<std::endl;
+  
+  // --- "standard" analysis plots
+  for(Int_t i=0;i<particles.size();++i){
+    fMasses->Fill((particles[i])->M());
+  }
+  
+  
+  signs.clear();
+  particles.clear();
+  
+  return;
+}
+
+void
+PndFwdKalmanTask::WriteHistograms(){
+  std::cout<<"  PndDchKalmanQATask::WriteHistograms() "<<std::endl;
+  TFile* file = FairRootManager::Instance()->GetOutFile();
+  file->cd();
+  file->mkdir("FwdKalmanQA");
+  file->cd("FwdKalmanQA");
+
+  
+  fPH->Write();
+  delete fPH;
+  fPH=NULL;
+  
+  fChi2H->Write();
+  delete fChi2H;
+  fChi2H=NULL;
+
+  fMassV0->Write();
+  delete fMassV0;
+  fMassV0=NULL;
+
+  fMassETAC->Write();
+  delete fMassETAC;
+  fMassETAC=NULL;
+
+  fMasses->Write();
+  delete fMasses;
+  fMasses=NULL;
+
+  fhMomentumX2D->Write();
+  fhMomentumY2D->Write();
+  fhMomentumZ2D->Write();
+  fhMomentumM2D->Write();
+  fhMomentumX1D->Write();
+  fhMomentumY1D->Write();
+  fhMomentumZ1D->Write();
+  fhMomentumM1D->Write();
+  fhMomentumRes->Write();
+
+  fhPositionX2D->Write();
+  fhPositionY2D->Write();
+  fhPositionX1D->Write();
+  fhPositionY1D->Write();
+
+  delete fhMomentumX2D;
+  delete fhMomentumY2D;
+  delete fhMomentumZ2D;
+  delete fhMomentumM2D;
+  delete fhMomentumX1D;
+  delete fhMomentumY1D;
+  delete fhMomentumZ1D;
+  delete fhMomentumM1D;
+  delete fhMomentumRes;
+
+  delete fhPositionX2D;
+  delete fhPositionY2D;
+  delete fhPositionX1D;
+  delete fhPositionY1D;
+
+  fhMomentumX2D = NULL;
+  fhMomentumY2D = NULL;
+  fhMomentumZ2D = NULL;
+  fhMomentumM2D = NULL;
+  fhMomentumX1D = NULL;
+  fhMomentumY1D = NULL;
+  fhMomentumZ1D = NULL;
+  fhMomentumM1D = NULL;
+  fhMomentumRes = NULL;
+
+  fhPositionX2D = NULL;
+  fhPositionY2D = NULL;
+  fhPositionX1D = NULL;
+  fhPositionY1D = NULL;
+
+
+  //  file->Close();
+  //  delete file;
+}
+
+void PndFwdKalmanTask::Finish(){
+  WriteHistograms();
+}
+
+
+ClassImp(PndFwdKalmanTask);
---
+++ fwd/PndFwdPrepareKalmanTracks.cxx
@@ -0,0 +1,257 @@
+// -------------------------------------------------------------------------
+// -----          PndFwdPrepareKalmanTracks  source file               -----
+// -----            Created 30.09.2008  by A. Wronska                  -----
+// -----            Modified 03.04.2009 by R. Karabowicz               -----
+// -----        based on the recotasks/demo code  by S.Neubert         -----
+// -------------------------------------------------------------------------
+
+// Panda Headers ----------------------
+#include "FairRootManager.h"
+#include "Track.h"
+#include "TrackCand.h"
+#include "PndMCTrack.h"
+#include "LSLTrackRep.h"
+#include "GeaneTrackRep.h"
+#include "Kalman.h"
+#include "FitterExceptions.h"
+#include "FairGeanePro.h"
+#include "FairTrackParP.h"
+#include "PndFwdPrepareKalmanTracks.h"
+#include "PndGemMCPoint.h"
+#include "PndGemTrack.h"
+//#include "PndGemTrackMatch.h"
+#include "PndGemHit.h"
+
+#include "PndDchPoint.h"
+#include "PndDchTrack.h"
+#include "PndDchTrackMatch.h"
+#include "PndDchHit.h"
+
+
+// ROOT Class Headers --------
+#include "TClonesArray.h"
+
+// C/C++ Headers ----------------------
+#include <algorithm>
+#include <iostream>
+
+//using namespace::std;
+
+PndFwdPrepareKalmanTracks::PndFwdPrepareKalmanTracks()
+  : FairTask("Translation of PndFwdTracks to Tracks"), fPersistence(kFALSE), fUseGeane(kFALSE), fUseMC(kFALSE), fPDG(11), fMinNofGemHits(6), fMinNofDchHits(30)
+{
+}
+
+
+PndFwdPrepareKalmanTracks::~PndFwdPrepareKalmanTracks()
+{
+  if( 0!= fTrackArray){
+    fTrackArray->Delete();
+    delete fTrackArray;
+  }
+  if( 0!= fGeanePro)
+    delete fGeanePro;
+}
+
+InitStatus
+PndFwdPrepareKalmanTracks::Init()
+{
+  //Get ROOT Manager
+  FairRootManager* ioman= FairRootManager::Instance();
+
+  if(ioman==0)
+    {
+      Error("PndFwdPrepareKalmanTracks::Init","RootManager not instantiated!");
+      return kERROR;
+    }
+ 
+  AddHitBranch(1,"GEMHit");
+
+  // open hit arrays
+  std::map<unsigned int,TString>::iterator iter=fHitBranchNameMap.begin();
+  while(iter!=fHitBranchNameMap.end()){
+    TClonesArray* ar=(TClonesArray*) ioman->GetObject(iter->second);
+    if(ar==0){
+      Error("DemoPRTask::Init","hit-array %s not found!",iter->second.Data());
+    }
+    else{ 
+      fHitBranchMap[iter->first] = ar;
+    }
+    ++iter;
+  }//end loops over hit types
+  fGemHitArray = fHitBranchMap[1];
+  
+  if(fUseMC){
+    // open MCTruth array
+    fMcArray=(TClonesArray*) ioman->GetObject("MCTrack");
+    if(fMcArray==0){
+      Error("PndFwdPrepareKalmanTracks::Init","mctrack-array not found!");
+      return kERROR;
+    } 
+    // open point array
+    fGemPointArray=(TClonesArray*) ioman->GetObject("GEMPoint");
+    if(fGemPointArray==0){
+      Error("PndFwdPrepareKalmanTracks::Init","gempoint-array not found!");
+      return kERROR;
+    } 
+
+    // open point array
+    fDchPointArray=(TClonesArray*) ioman->GetObject("PndDchPoint");
+    if(fDchPointArray==0){
+      Error("PndFwdPrepareKalmanTracks::Init","dchpoint-array not found!");
+      return kERROR;
+    } 
+
+    // open input array of PndGemTrackMatches
+    fDchTrackMatchArray = (TClonesArray*) ioman->GetObject("PndDchTrackMatch"); 
+    if(fDchTrackMatchArray==0){
+      Error("PndFwdPrepareKalmanTracks::Init","PndDchTrackMatch array not found!");
+      return kERROR;
+    }
+  }
+  
+  // open input array of PndGemTracks
+  fGemTrackArray = (TClonesArray*) ioman->GetObject("PndGemTrack"); 
+  if(fGemTrackArray==0){
+    Error("PndFwdPrepareKalmanTracks::Init","PndGemTrack array not found!");
+    return kERROR;
+  }
+
+  // open input array of PndGemTracks
+  fDchTrackArray = (TClonesArray*) ioman->GetObject("PndDchTrack"); 
+  if(fDchTrackArray==0){
+    Error("PndFwdPrepareKalmanTracks::Init","PndDchTrack array not found!");
+    return kERROR;
+  }
+
+  // create and register output array
+  fTrackArray = new TClonesArray("Track"); 
+  ioman->Register("FSTracks","GenFit",fTrackArray,fPersistence);
+  
+  // GeanePro will get Geometry and BField from the Run
+  fGeanePro=new FairGeanePro();
+  
+  return kSUCCESS;
+}
+
+
+void
+PndFwdPrepareKalmanTracks::Exec(Option_t* opt)
+{
+  if(fVerbose>0){
+    std::cout<<"\n\n-------------------------------------------------------"<<std::endl;
+    std::cout<<"      PndFwdPrepareKalmanTracks::Exec                      "<<std::endl;
+    std::cout<<"-------------------------------------------------------"<<std::endl;
+  }
+  if(fTrackArray==0) Fatal("PndFwdPrepareKalmanTracks::Exec)","No TrackArray");
+  fTrackArray->Delete();
+  
+  Int_t nofDchTracks = fDchTrackArray->GetEntriesFast();
+  Int_t nofGemTracks = fGemTrackArray->GetEntriesFast();
+
+  // loop over tracks
+  std::cout << "Looping over " << nofGemTracks << " gem tracks" << endl;
+  for (Int_t igemtr=0; igemtr<nofGemTracks; igemtr++){
+    if(fVerbose>0)
+      std::cout<<"PndFwdPrepareKalmanTracks::Exec(): Processing gem track id= "<<igemtr<<std::endl;
+    PndGemTrack* gemtrack = (PndGemTrack*) fGemTrackArray->At(igemtr);
+    Int_t nofGemHits = gemtrack->GetNofGemHits();
+    if(fVerbose>0)
+      std::cout<<"PndFwdPrepareKalmanTracks::Exec(): I found here "<<nofGemHits<<" hits \n";
+    
+    Int_t gemMCTrackID = (Int_t)gemtrack->GetParamLast()->GetZ()-1e6;
+    std::cout << "!!!!!!!! THIS TRACK HAD MC ID " << gemMCTrackID << " !!!!!!" << std::endl;
+    
+    for (Int_t idchtr=0; idchtr<fDchTrackMatchArray->GetEntries(); idchtr++){
+      PndDchTrackMatch* dchtrmatch = (PndDchTrackMatch*) fDchTrackMatchArray->At(idchtr);
+      if ( dchtrmatch->GetMCTrackID() != gemMCTrackID ) continue;
+      
+      std::cout << "Found matching DCH track, recoId = " << dchtrmatch->GetRecTrackID() << "." << std::endl;
+
+      PndDchTrack* dchtrack = (PndDchTrack*) fDchTrackArray->At(idchtr);
+      Int_t nofDchCylHits = dchtrack->GetNofDchCylinderHits();
+
+      TrackCand* cand = new TrackCand();
+      for(Int_t ihit=0; ihit<nofGemHits; ihit++){
+	Int_t globalHit = gemtrack->GetGemHitIndex(ihit);
+	cand->addHit(2,globalHit);
+      }
+      for(Int_t ihit=0; ihit<nofDchCylHits; ihit++){
+	Int_t globalCHitNu = dchtrack->GetDchCylinderHitIndex(ihit);
+	cand->addHit(1,globalCHitNu);
+      }
+ 
+      for(Int_t ihit=0; ihit<cand->getNHits(); ihit++){
+	unsigned int temp2= 1234, temp3=1234;
+	cand->getHit(ihit, temp2, temp3);
+	cout<<" "<<ihit<<" "<<temp2<<" "<<temp3<<endl;
+      }
+      
+      if(cand->getNHits()<fMinNofGemHits+fMinNofDchHits)
+	continue;
+    
+      Int_t pdg;
+      Double_t q;
+      TVector3 pos, mom;
+    
+      FairTrackParam* param = gemtrack->GetParamFirst();
+      param->Position(pos);
+      param->Momentum(mom);
+      q = (param->GetQp()==0) ? 0 : param->GetQp()/TMath::Abs(param->GetQp());
+      pdg = (Int_t)gemtrack->GetParamLast()->GetQp()-1e6;
+    
+      std::cout<<"pozycje i pedy "<<std::endl;
+      pos.Print();
+      mom.Print();
+      if(mom.Mag()>1e3){
+	Error("PndFwdPrepareKalmanTracks::Exec","Track was incorrectly prefitted - abandoned in Kalman!");
+	continue;
+      }
+      Double_t startPosAccuracy = 0.5;
+      TVector3 poserr(startPosAccuracy,startPosAccuracy,3.*startPosAccuracy);
+      TVector3 startMomAccuracy(0.1,0.1,0.1);
+      TVector3 momerr(mom.X()*startMomAccuracy.X(),
+		      mom.Y()*startMomAccuracy.Y(),
+		      mom.Z()*startMomAccuracy.Z());    
+      TVector3 u(1.,0.,0.);
+      TVector3 v(0.,1.,0.);
+      
+      // create track-representation object and initialize with start values
+      AbsTrackRep* rep=0;
+      if(fUseGeane){
+	DetPlane pl(pos,u,v);
+	GeaneTrackRep *grep=new GeaneTrackRep(fGeanePro,pl,mom,poserr,momerr,q,pdg);
+	grep->setPropDir(1); // propagate in flight direction
+	//  if(fVerbose>0){
+	std::cout<<" ^^^^^^^^^^^^^I prepare the following GeaneTrackRep:"<<std::endl;
+	grep->Print();
+	std::cout<<" ^^^^^^^^^^^^^End of GeaneTrackRep printout"<<std::endl;
+	//}
+	rep = grep;
+      }
+      else {
+	TVector3 dir=mom.Unit();
+	double dxdz=dir.X()/dir.Z();
+	double dydz=dir.Y()/dir.Z();
+	double qp=q/mom.Mag();
+	rep=new LSLTrackRep(pos.Z(),pos.X(),pos.Y(),dxdz,dydz,qp,
+			  poserr.X(),poserr.Y(),0.1,0.1,0.1,NULL);
+	if(fVerbose>0){
+	  std::cout<<" ^^^^^^^^^^^^^I prepare the following LSLTrackRep:"<<std::endl;
+	  rep->Print();
+	  std::cout<<" ^^^^^^^^^^^^^End of LSLTrackRep printout"<<std::endl;
+	}
+      }
+      // create track object
+      Track* trk=new((*fTrackArray)[fTrackArray->GetEntriesFast()]) Track(rep);
+      trk->setCandidate(*cand);
+      cout << "!!!!candidate " <<fTrackArray->GetEntriesFast() <<  " has " << cand->getNHits() << " hits" << endl;
+    }// end loop over gemtracks
+  }
+  if(fVerbose>0)
+    std::cout<<fTrackArray->GetEntriesFast()<<" tracks created"<<std::endl;
+
+  return;
+}
+
+ClassImp(PndFwdPrepareKalmanTracks)
