09618d18abab441f67c7b75e5418901c60a7fc91
Aleksandra Biegun <A.K.Biegun@rug.nl>
2009-06-04 08:21:57 +0000

    file added

---
+++ emc/EmcMC/PndEmcGeoPar.h
@@ -0,0 +1,35 @@
+#ifndef PNDEMCGEOPAR_H
+#define PNDEMCGEOPAR_H
+
+#include "FairParGenericSet.h"
+#include "TH1F.h"
+#include "TObjArray.h"
+
+class PndEmcGeoPar : public FairParGenericSet 
+{
+public:
+  TObjArray            *fGeometryVersion;
+
+  PndEmcGeoPar(const char* name="PndEmcGeoPar",
+	       const char* title="Emc Geometry Version Flag",
+	       const char* context="TestDefaultContext");
+  ~PndEmcGeoPar(void);
+  void clear(void);
+ 
+  void putParams(FairParamList*);
+  Bool_t getParams(FairParamList*);
+ 
+  //TObjArray             *GetGeometryVersion(const Int_t geomVersion){return fGeometryVersion;}
+  //TObjArray         *GetGeometryVersion(){return fGeometryVersion;}
+  Int_t  GetMapperVersion(){return fMapperVersion;}
+  void   SetMapperVersion(Int_t mapperVersion){    //AB
+     fMapperVersion = mapperVersion;
+  }
+
+ private:
+  Int_t fMapperVersion; 
+  
+  ClassDef(PndEmcGeoPar,1)
+};
+
+#endif /* !PNDEMCGEOPAR_H */
