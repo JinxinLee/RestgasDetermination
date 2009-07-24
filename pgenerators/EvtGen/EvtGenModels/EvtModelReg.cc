1b9399573d99add4f7d445c343fde324bb17c63d
Stefano Spataro <spataro@to.infn.it>
2009-07-24 09:29:37 +0000

    pbarp->e+e- model (Gosia)

--- pgenerators/EvtGen/EvtGenModels/EvtModelReg.cc
+++ pgenerators/EvtGen/EvtGenModels/EvtModelReg.cc
@@ -98,6 +98,7 @@
 #include "EvtGenModels/EvtPhiDalitz.hh"
 #include "EvtGenModels/EvtPpbarGammaGamma.hh"
 #include "EvtGenModels/EvtPpbarPi0Gamma.hh"
+#include "EvtGenModels/EvtElectronPositron.hh"
 //#include "EvtGenModels/EvtPpbarPi0Pi0.hh"
 
 #include "EvtGenModels/EvtLNuGamma.hh"
@@ -200,6 +201,7 @@ EvtModelReg::EvtModelReg()
 
     modelist.Register(new EvtPpbarGammaGamma);
     modelist.Register(new EvtPpbarPi0Gamma);
+    modelist.Register(new EvtElectronPositron); // [Gosia Sudol 24/07/09]
 //    modelist.Register(new EvtPpbarPi0Pi0); 
 
     modelist.Register(new EvtBTo4piCP);
