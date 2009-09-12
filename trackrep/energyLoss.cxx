6889808191415bb832533e19bb48feed450f5a03
Christian Hoeppner <christian.hoeppner@cern.ch>
2009-09-12 10:09:06 +0000

    chechin of forgotten files

---
+++ trackrep/energyLoss.cxx
@@ -0,0 +1,13 @@
+#include"energyLoss.h"
+#include"math.h"
+#include"assert.h"
+#include<iostream>
+
+float energyLoss(float beta,float charge,float density,float ZoverA,float MEE){
+  //std::cout << "beta " << beta << " charge " << charge << " density " << density << " ZoverA " << ZoverA<< " " << MEE << std::endl;
+  float retVal = 0.3071*ZoverA*density/(beta*beta)*charge*charge;
+  retVal *= (log(beta*beta*0.510999*2./(1.E-6*MEE*(1-beta*beta)))-beta*beta);
+  assert(retVal>0);
+  //in GeV/cm, hence 1.e-3
+  return 1.E-3*retVal;
+}
---
+++ trackrep/energyLoss.h
@@ -0,0 +1,6 @@
+#ifndef ENERGYLOSS_H
+#define ENERGYLOSS_H
+
+float energyLoss(float beta,float charge,float density,float ZoverA,float MEE);
+
+#endif
