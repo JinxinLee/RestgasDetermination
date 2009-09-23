1c094ce83c942e06414ac60d5cedf3867a19fdf7
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2009-09-23 14:56:41 +0000

    update drawGeo macro

--- macro/run/drawGeo.C
+++ macro/run/drawGeo.C
@@ -1,32 +1,17 @@
 {
+  
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+  
 
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libParBase");
-  gSystem->Load("libBase");
-  gSystem->Load("libField");
-  gSystem->Load("libGen");
-  gSystem->Load("libPassive");
-  gSystem->Load("libPndData");
-  gSystem->Load("libStt"); 
-  gSystem->Load("libMdt");
-  gSystem->Load("libEmc");
-  gSystem->Load("libMvd");
-  gSystem->Load("libDrcProp");
-  gSystem->Load("libDrc");
-
-  gSystem->Load("libGeom.so");
-  TFile* file = new TFile("testsimu10.root");
-
- TGeoManager *geoMan = (TGeoManager*) file->Get("FAIRGeom");
-
+  TFile* file = new TFile("testparams.root");
+  file->Get("FairBaseParSet"); 
+  
 //  TCanvas* c1 = new TCanvas("c1", "", 100, 100, 1100, 1100);
 //  c1->SetFillColor(10);
 
-  geoMan->SetVisLevel(4);
-  geoMan->GetMasterVolume()->Draw("ogl");
+  gGeoManager->SetVisLevel(4);
+  gGeoManager->GetMasterVolume()->Draw("ogl");
 
 
 }
