13296b8de12c0208fe4b68629ef504b57cb56e43
Stefano Spataro <spataro@to.infn.it>
2009-12-02 16:53:56 +0000

    Rejected hits with eloss==0

--- mdt/MdtDigi/PndMdtHitProducerIdeal.cxx
+++ mdt/MdtDigi/PndMdtHitProducerIdeal.cxx
@@ -98,6 +98,7 @@ void PndMdtHitProducerIdeal::Exec(Option_t* opt) {
   
   for (Int_t iPoint=0; iPoint<nPoints; iPoint++) {
     point  = (PndMdtPoint*) fPointArray->At(iPoint);
+    if (point->GetEnergyLoss()==0) continue;
     //if ( (point->GetModule()==1) && (TMath::Odd(point->GetDetectorID())) ) continue;
     point->Position(pos);
     if (fPosResolution>0.)
