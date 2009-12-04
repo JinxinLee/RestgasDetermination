cde30ab7e8d1997afab5b0b58e2130482082555b
Johan Messchendorp <j.g.messchendorp@rug.nl>
2009-12-04 20:49:50 +0000

    added digitization compilation

---
+++ macro/run/compile_example/DigiCompleteTpc.h
@@ -0,0 +1,11 @@
+#ifndef DIGICOMPLETETPC_H
+#define DIGICOMPLETETPC_H
+
+
+#include <TString.h>
+
+void DigiCompleteTpc(TString const &inFile, TString const &parFile, 
+		     TString const &digiFile, TString const &outFile);
+
+
+#endif
