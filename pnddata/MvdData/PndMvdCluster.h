084ff86819bc8d588eb8fe4d08b0336bf5dbdf73
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2009-12-23 08:51:43 +0000

    replacing PndMulitLinkedData with TObject in class inheritance

--- pnddata/MvdData/PndMvdCluster.h
+++ pnddata/MvdData/PndMvdCluster.h
@@ -17,7 +17,7 @@
 //! This class holds the information which Digi belongs to the actual cluster.
 //! The information is stored in a vector<Int_t> which contains the
 //! position of the digi in the TClonesArray where it is stored.
-class PndMvdCluster : public FairMultiLinkedData
+class PndMvdCluster : public TObject //public FairMultiLinkedData
 {
 
 friend std::ostream& operator<< (std::ostream& out, PndMvdCluster& cl){
