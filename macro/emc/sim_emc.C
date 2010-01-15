805138dcb148d8347732064385b29278178257f3
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2010-01-15 13:48:27 +0000

    use new methods to set the field

--- macro/emc/sim_emc.C
+++ macro/emc/sim_emc.C
@@ -113,25 +113,10 @@ sim_emc(Int_t nEvents = 10, Float_t mom = 1.){
   // Create and Set Magnetic Field
   //-------------------------------
   fRun->SetBeamMom(15);
-  PndMultiField *fField= new PndMultiField();
-
-  PndTransMap *map_t= new PndTransMap("TransMap", "R");
-  PndDipoleMap *map_d1= new PndDipoleMap("DipoleMap1", "R");
-  PndDipoleMap *map_d2= new PndDipoleMap("DipoleMap2", "R");
-  PndSolenoidMap *map_s1= new PndSolenoidMap("SolenoidMap1", "R");
-  PndSolenoidMap *map_s2= new PndSolenoidMap("SolenoidMap2", "R");
-  PndSolenoidMap *map_s3= new PndSolenoidMap("SolenoidMap3", "R");
-  PndSolenoidMap *map_s4= new PndSolenoidMap("SolenoidMap4", "R");
-
-  fField->AddField(map_t);
-  fField->AddField(map_d1);
-  fField->AddField(map_d2);
-  fField->AddField(map_s1);
-  fField->AddField(map_s2);
-  fField->AddField(map_s3);
-  fField->AddField(map_s4);
-
+ 	//---------------------Create and Set the Field(s)---------- 
+  PndMultiField *fField= new PndMultiField("FULL");
   fRun->SetField(fField);
+
 
   // EMC Hit producer
   //-------------------------------
