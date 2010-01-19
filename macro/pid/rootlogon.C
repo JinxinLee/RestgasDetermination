b9cb78572533d744437310b411bfbf9df55b1be4
Stefano Spataro <spataro@to.infn.it>
2010-01-19 14:49:25 +0000

    New rootlogon

--- macro/pid/rootlogon.C
+++ macro/pid/rootlogon.C
@@ -3,44 +3,55 @@ rootlogon()
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();
   
-  // Load this example libraries
-        gSystem->Load("libDpmEvtGen.so");
-  	gSystem->Load("libGeoBase");
-	gSystem->Load("libParBase");
-	gSystem->Load("libBase");
-	gSystem->Load("libPndBase");
-	gSystem->Load("libGlobalTasks");
-        gSystem->Load("libTrkBase");
-	gSystem->Load("libPndData");
-	gSystem->Load("libField");
-	gSystem->Load("libPassive");
-	gSystem->Load("libGen");
-	gSystem->Load("libPGen");
-	gSystem->Load("libEmc"); 
-        gSystem->Load("libgenfit");
-	gSystem->Load("libtrackrep");
-	gSystem->Load("libgenfitAdapters");
-        gSystem->Load("libriemann");
-        gSystem->Load("libtpc"); 
-        gSystem->Load("libtpcreco");
-        gSystem->Load("libStt");
-        gSystem->Load("libSttReco");
-        gSystem->Load("libMvd");
-        gSystem->Load("libMvdReco");
-        gSystem->Load("libMvdTrk");
-        gSystem->Load("libGem");
-	gSystem->Load("libTof");
-	gSystem->Load("libDrcProp");
-	gSystem->Load("libDrc");
-	gSystem->Load("libMdt");
-	gSystem->Load("libDch");
-        gSystem->Load("libLheTrack");
-	gSystem->Load("libGeane");
-	gSystem->Load("libRpc");
-	gSystem->Load("libLumi");
-        gSystem->Load("libRho");
-	gSystem->Load("libTMVA.so");
-        gSystem->Load("libPid");
-        gSystem->Load("libAnalysisTools");
-        gSystem->Load("librecotasks");
+  // Load Panda libraries
+        if (isLibrary("libDpmEvtGen"))gSystem->Load("libDpmEvtGen");
+  	if(isLibrary("libGeoBase"))gSystem->Load("libGeoBase");
+	if(isLibrary("libParBase"))gSystem->Load("libParBase");
+	if(isLibrary("libBase"))gSystem->Load("libBase");
+	if(isLibrary("libPndBase"))gSystem->Load("libPndBase");
+	if(isLibrary("libGlobalTasks"))gSystem->Load("libGlobalTasks");
+        if(isLibrary("libTrkBase"))gSystem->Load("libTrkBase");
+	if(isLibrary("libPndData"))gSystem->Load("libPndData");
+	if(isLibrary("libField"))gSystem->Load("libField");
+	if(isLibrary("libPassive"))gSystem->Load("libPassive");
+	if(isLibrary("libGen"))gSystem->Load("libGen");
+	if(isLibrary("libPGen"))gSystem->Load("libPGen");
+	if(isLibrary("libEmc"))gSystem->Load("libEmc"); 
+        if(isLibrary("libgenfit"))gSystem->Load("libgenfit");
+	if(isLibrary("libtrackrep"))gSystem->Load("libtrackrep");
+	if(isLibrary("libgenfitAdapters"))gSystem->Load("libgenfitAdapters");
+        if(isLibrary("libriemann"))gSystem->Load("libriemann");
+        if(isLibrary("libtpc"))gSystem->Load("libtpc"); 
+        if(isLibrary("libtpcreco"))gSystem->Load("libtpcreco");
+        if(isLibrary("libStt"))gSystem->Load("libStt");
+        if(isLibrary("libSttReco"))gSystem->Load("libSttReco");
+        if(isLibrary("libMvd"))gSystem->Load("libMvd");
+        if(isLibrary("libMvdReco"))gSystem->Load("libMvdReco");
+        if(isLibrary("libMvdTrk"))gSystem->Load("libMvdTrk");
+        if(isLibrary("libGem"))gSystem->Load("libGem");
+	if(isLibrary("libTof"))gSystem->Load("libTof");
+	if(isLibrary("libDrcProp"))gSystem->Load("libDrcProp");
+	if(isLibrary("libDrc"))gSystem->Load("libDrc");
+	if(isLibrary("libMdt"))gSystem->Load("libMdt");
+	if(isLibrary("libDch"))gSystem->Load("libDch");
+        if(isLibrary("libLheTrack"))gSystem->Load("libLheTrack");
+	if(isLibrary("libGeane"))gSystem->Load("libGeane");
+	if(isLibrary("libRpc"))gSystem->Load("libRpc");
+	if(isLibrary("libLumi"))gSystem->Load("libLumi");
+        if(isLibrary("libRho"))gSystem->Load("libRho");
+	if(isLibrary("libTMVA"))gSystem->Load("libTMVA.so");
+        if(isLibrary("libPid"))gSystem->Load("libPid");
+        if(isLibrary("libAnalysisTools"))gSystem->Load("libAnalysisTools");
+        if(isLibrary("librecotasks"))gSystem->Load("librecotasks");
+        if(isLibrary("libEnDrc"))gSystem->Load("libEnDrc");
+	if(isLibrary("libDsk"))gSystem->Load("libDsk");
+	
 }
+
+Bool_t isLibrary(const char* libName)
+{
+  if (TString(gSystem->DynamicPathName(libName, kTRUE)) != TString(""))
+    return kTRUE;
+  else  
+    return kFALSE;
+}    
