13f7cc8b0939b9854df1dbdc35257fed605c7b07
Dipanwita Dutta <D.Dutta@gsi.de>
2010-01-21 11:39:40 +0000

    eventDisplay for DIRC added

---
+++ macro/drc/eventDisplay.C
@@ -0,0 +1,40 @@
+eventDisplay()
+{
+   // Load basic libraries
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+  gSystem->Load("libEve");
+  gSystem->Load("libEventDisplay");
+
+                                     
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile("testrun1.root");
+  fRun->SetOutputFile("tst.root");
+  fRun->LoadGeometry();
+
+  
+  
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open("./testparams.root");
+       
+  rtdb->setFirstInput(parInput1);
+ 
+
+  FairEventManager *fMan= new FairEventManager();
+  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
+  FairMCPointDraw *PndBarPoint = new FairMCPointDraw ("DrcBarPoint",kViolet, kFullSquare);
+  FairMCPointDraw *PndPdPoint = new FairMCPointDraw ("DrcPDPoint",kBlue, kFullSquare);
+
+                                                               
+  fMan->AddTask(Track);
+  fMan->AddTask( PndBarPoint);
+  fMan->AddTask( PndPdPoint);
+
+    
+ fRun->Init();
+ fMan->Init();                     
+  
+
+}
