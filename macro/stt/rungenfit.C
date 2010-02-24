21917a7ce1483827527ea90a70a7ed77df6cdf3b
Lia Lavezzi <lia.lavezzi@gmail.com>
2010-02-24 14:00:35 +0000

    detID changed to 9 to work with kSTT

--- macro/stt/rungenfit.C
+++ macro/stt/rungenfit.C
@@ -63,12 +63,12 @@
   if(whichhit == 1){
     // use hit
     PndSttKalmanTask* RecoHitPro = new PndSttKalmanTask();
-    RecoHitPro->AddHitBranch(3,"STTHit");
+    RecoHitPro->AddHitBranch(9, "STTHit");
    }
   else if(whichhit == 2){
     // use helixhit
     PndSttKalmanTask2*RecoHitPro = new PndSttKalmanTask2();
-    RecoHitPro->AddHitBranch(3,"SttHelixHit");
+    RecoHitPro->AddHitBranch(9,"SttHelixHit");
    }
   
   fRun->AddTask(RecoHitPro); 
