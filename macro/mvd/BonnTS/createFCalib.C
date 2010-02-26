0ea90128b81a49cb32e3470a9f028274e6b5a7a1
Simone Bianco <bianco@hiskp.uni-bonn.de>
2010-02-26 10:12:59 +0000

    macro to generato a template for the calib file

---
+++ macro/mvd/BonnTS/createFCalib.C
@@ -0,0 +1,25 @@
+void createFCalib()
+{
+
+  Int_t nFee = 24;
+
+  Int_t nChFe = 128;
+
+  Float_t value = 10.;
+
+  ofstream out("file.calib");
+
+  for (Int_t f = 0 ; f < nFee ; f++)
+    {
+      for (Int_t c = 0 ; c < nChFe ; c++)
+	{
+
+	  out << "0 " << f << " " << c << " " << value << endl; 
+
+	} // end loop on channels
+    }// end loop on fees
+
+  out.close();
+
+
+}
