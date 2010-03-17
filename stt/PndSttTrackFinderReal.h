b22b23cc9edfd9e3192ccf0fb8beb347974f185c
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2010-03-17 10:56:41 +0000

    Gianluigi : fixing some details

--- stt/PndSttTrackFinderReal.h
+++ stt/PndSttTrackFinderReal.h
@@ -159,7 +159,7 @@ class PndSttTrackFinderReal : public PndSttTrackFinder
           N_INTENDED;
 //  Double_t SEMILENGTH_STRAIGHT = 75.;
       static const bool  iplotta = false , ianalizza = true ;
-      static const int istampa = 2,
+      static const int istampa = 0,
             MAXSTTINFO=200000;
       TH1F * hx;
       FILE * HANDLE ;
