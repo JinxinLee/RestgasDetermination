266af7a7cda6d18e7b7f0bf3a6008c48ca2619a0
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2010-04-08 09:16:20 +0000

    FairLink in PndRecoKalmanTask

--- GenfitTools/recotasks/PndRecoKalmanTask.cxx
+++ GenfitTools/recotasks/PndRecoKalmanTask.cxx
@@ -121,7 +121,7 @@ void PndRecoKalmanTask::Exec(Option_t* opt)
       TClonesArray& trkRef = *fFitTrackArray;
       Int_t size = trkRef.GetEntriesFast();
       PndTrack* pndTrack = new(trkRef[size]) PndTrack(fitTrack->GetParamFirst(), fitTrack->GetParamLast(), fitTrack->GetTrackCand(),
-						      fitTrack->GetFlag(), fitTrack->GetChi2(), fitTrack->GetNDF(), fitTrack->GetPidHypo(), itr);
+						      fitTrack->GetFlag(), fitTrack->GetChi2(), fitTrack->GetNDF(), fitTrack->GetPidHypo(), itr, kLheTrack);
     }
   
   if (fVerbose>0) std::cout<<"Fitting done"<<std::endl;
