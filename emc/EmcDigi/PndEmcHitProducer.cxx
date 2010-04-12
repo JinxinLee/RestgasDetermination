4d641c62f50b6e7004d8fd15c64c2e53a17d7a56
Mohammad Babai <m.babai@rug.nl>
2010-04-12 14:02:27 +0000

    Solved programming error (uninitialized param), Wrong usage std::map.

--- emc/EmcDigi/PndEmcHitProducer.cxx
+++ emc/EmcDigi/PndEmcHitProducer.cxx
@@ -33,16 +33,14 @@
 #include "TGeoMatrix.h"
 #include "TVector3.h"
 
-//#include <string>
-
 using std::cout;
 using std::endl;
 using std::map;
 
-		
+
 // -----   Default constructor   -------------------------------------------
 PndEmcHitProducer::PndEmcHitProducer() :
-  FairTask("Ideal EMC hit Producer") { 
+  FairTask("Ideal EMC hit Producer"){
   fStoreHits=kTRUE;
   //eneThr = 0.001; // Energy threshold for emc pad (Now taken from DB)
 }
@@ -60,8 +58,8 @@ PndEmcHitProducer::~PndEmcHitProducer() { delete fEmcStr;}
 
 
 // -----   Public method Init   --------------------------------------------
-InitStatus PndEmcHitProducer::Init() {
- 
+InitStatus PndEmcHitProducer::Init(){
+  
   cout << " INITIALIZATION *********************" << endl;
   
   //FairDetector::Initialize();
@@ -70,7 +68,7 @@ InitStatus PndEmcHitProducer::Init() {
   
   // Get RootManager
   FairRootManager* ioman = FairRootManager::Instance();
-  if ( ! ioman ) {
+  if (! ioman ){
     cout << "-E- PndEmcHitProducer::Init: "
 	 << "RootManager not instantiated!" << endl;
     return kFATAL;
@@ -78,55 +76,53 @@ InitStatus PndEmcHitProducer::Init() {
   
   // Get input array
   fPointArray = (TClonesArray*) ioman->GetObject("EmcPoint");
-  if ( ! fPointArray ) {
+  if (! fPointArray ){
     cout << "-W- PndEmcHitProducer::Init: "
 	 << "No EmcPoint array!" << endl;
     return kERROR;
   }
-
-	// Get input array
-	fMCTrackArray = (TClonesArray*) ioman->GetObject("MCTrack");
-	if ( ! fMCTrackArray ) {
-		cout << "-W- PndEmcMakeCluster::Init: "
-		<< "No MCTrack array! Needed for MC Truth" << endl;
-//		return kERROR;
-	}
-
+  
+  // Get input array
+  fMCTrackArray = (TClonesArray*) ioman->GetObject("MCTrack");
+  if (! fMCTrackArray ){
+    cout << "-W- PndEmcMakeCluster::Init: "
+	 << "No MCTrack array! Needed for MC Truth" << endl;
+    //return kERROR;
+  }
+  
   // Create and register output array
   fDigiArray = new TClonesArray("PndEmcHit");
   
   ioman->Register("EmcHit","Emc",fDigiArray,fStoreHits);
   
-	fGeoPar->InitEmcMapper();
-	fEmcStr=PndEmcStructure::Instance();
+  fGeoPar->InitEmcMapper();
+  fEmcStr=PndEmcStructure::Instance();
   
-	emcX=fEmcStr->GetEmcX();
-	emcY=fEmcStr->GetEmcY();
-	emcZ=fEmcStr->GetEmcZ();;
-
+  emcX=fEmcStr->GetEmcX();
+  emcY=fEmcStr->GetEmcY();
+  emcZ=fEmcStr->GetEmcZ();;
+  
   fEnergyThreshold =fDigiPar->GetEnergyHitThreshold();
   
   cout << "-I- PndEmcHitProducer: Intialization successfull" << endl;
   
   return kSUCCESS;
-
 }
 
-void PndEmcHitProducer::SetParContainers() {
-
+void PndEmcHitProducer::SetParContainers(){
+  
   // Get run and runtime database
   FairRun* run = FairRun::Instance();
-  if ( ! run ) Fatal("SetParContainers", "No analysis run");
-
+  if (! run ) Fatal("SetParContainers", "No analysis run");
+  
   FairRuntimeDb* db = run->GetRuntimeDb();
-  if ( ! db ) Fatal("SetParContainers", "No runtime database");
-
+  if (! db ) Fatal("SetParContainers", "No runtime database");
+  
   // Get Emc geometry parameter container
   fGeoPar = (PndEmcGeoPar*) db->getContainer("PndEmcGeoPar");
   
   // Get Emc digitisation parameter container
   fDigiPar = (PndEmcDigiPar*) db->getContainer("PndEmcDigiPar");
- 
 }
 
 // -------------------------------------------------------------------------
@@ -134,55 +130,57 @@ void PndEmcHitProducer::SetParContainers() {
 // Helper function, does not depend on class, identical to the one in PndEmcMakeCluster
 void PndEmcHitProducer::cleansortmclist( std::vector <Int_t> &newlist,TClonesArray* mcTrackArray)
 {
-	std::vector <Int_t> tmplist;
-	// Sort list...
-	std::sort( newlist.begin(), newlist.end());
-	// and copy every id only once (even so it might be in the list several times)
-	std::unique_copy( newlist.begin(), newlist.end(), std::back_inserter( tmplist ) );
-
-	// Now check if mother or (grand)^x-mother are already in the list
-	// (which means i am a secondary)... if so, remove myself
-	for(Int_t j=tmplist.size()-1; j>=0; j--){
-		bool flag;
-		PndMCTrack *pt;
-		pt=((PndMCTrack*)mcTrackArray->At(tmplist[j]));
-		if(pt->GetMotherID()<0) continue;
-		flag=false;
-		while(!flag){
-			Int_t id;
-			id=pt->GetMotherID();
-			if(id<0) break;
-			pt=(PndMCTrack*)mcTrackArray->At(id);
-
-			for(Int_t k=j-1; k>=0; k--){
-				if(tmplist[k]==id){
-					tmplist.erase(tmplist.begin()+j);
-					flag=true;
-					break;
-				}
-			}
-		}
+  std::vector <Int_t> tmplist;
+  // Sort list...
+  std::sort( newlist.begin(), newlist.end());
+  // and copy every id only once (even so it might be in the list several times)
+  std::unique_copy( newlist.begin(), newlist.end(), std::back_inserter( tmplist ) );
+  
+  // Now check if mother or (grand)^x-mother are already in the list
+  // (which means i am a secondary)... if so, remove myself
+  for(Int_t j=tmplist.size()-1; j>=0; j--){
+    bool flag;
+    PndMCTrack *pt;
+    pt=((PndMCTrack*)mcTrackArray->At(tmplist[j]));
+    if(pt->GetMotherID()<0) continue;
+    flag=false;
+    while(!flag){
+      Int_t id;
+      id=pt->GetMotherID();
+      if(id<0) break;
+      pt=(PndMCTrack*)mcTrackArray->At(id);
+      
+      for(Int_t k=j-1; k>=0; k--){
+	if(tmplist[k]==id){
+	  tmplist.erase(tmplist.begin()+j);
+	  flag=true;
+	  break;
 	}
-	newlist=tmplist;
+      }
+    }
+  }
+  newlist=tmplist;
 }
 
 // -----   Public method Exec   --------------------------------------------
-void PndEmcHitProducer::Exec(Option_t* opt) {
-
+void PndEmcHitProducer::Exec(Option_t* opt)
+{  
   cout << " DIGI EXECUTION *********************" << endl;
   // Reset output array
-  if ( ! fDigiArray ) Fatal("Exec", "No DigiArray");
+  if (! fDigiArray ) Fatal("Exec", "No DigiArray");
   
   fDigiArray->Delete();
   
   // Declare some variables
-  PndEmcPoint* point  = NULL;
+  //PndEmcPoint* point  = NULL;
   map<Int_t, Float_t> fTrackEnergy;
   map<Int_t, Float_t> fTrackTime;  //time of first point
   map<Int_t, std::vector <Int_t> > fTrackMcTruth;  //McTruth
+  
   fTrackEnergy.clear();
   fTrackTime.clear();
   fTrackMcTruth.clear();
+  
   map<Int_t, Float_t>::const_iterator p;
   
   std::vector<PndEmcPoint*> fPointList;// to pass to EmcHit
@@ -190,43 +188,67 @@ void PndEmcHitProducer::Exec(Option_t* opt) {
   // Loop over EmcPoints
   Int_t nPoints = fPointArray->GetEntriesFast();
   
-  Double_t point_time;
-  for (Int_t iPoint=0; iPoint<nPoints; iPoint++)
-  {
-    point  = (PndEmcPoint*) fPointArray->At(iPoint);
-    fTrackEnergy[point->GetDetectorID()] += point->GetEnergyLoss();
-	 point_time=point ->GetTime();
-	 if (point_time<fTrackTime[point->GetDetectorID()]) fTrackTime[point->GetDetectorID()] =point_time;
+  Double_t point_time = 0.00;
+  
+  //------- init containers --- 
 
-	 // Check and save MC truth information
-	 // Eloss==0 tracks are only stored in point, if track is entering detector from outside
-	 // and thats what we are interested in...
-	 if(point->GetEnergyLoss()==0){
-		 fTrackMcTruth[point->GetDetectorID()].push_back(point->GetTrackID());
-//			cout << "ELoss==0 : ID " << point->GetTrackID()<<","<<point->GetDetectorID()<<","<<point->GetXPad()<<","<<point->GetYPad()<<endl;
-	 }
+  for (Int_t iPoint = 0; iPoint < nPoints; iPoint++){
+    PndEmcPoint* point  = (PndEmcPoint*) fPointArray->At(iPoint);
+    fTrackEnergy[point->GetDetectorID()] = 0.00;
+    fTrackTime  [point->GetDetectorID()] = std::numeric_limits<float>::max();
   }
-  // Loop over EmcPoint
 
+  //----------------------------
+  for (Int_t iPoint = 0; iPoint < nPoints; iPoint++){
+    //point  = (PndEmcPoint*) fPointArray->At(iPoint);
+    PndEmcPoint* point  = (PndEmcPoint*) fPointArray->At(iPoint);
+    fTrackEnergy[point->GetDetectorID()] += point->GetEnergyLoss();
+    
+    point_time = point ->GetTime();
+
+    if (point_time < fTrackTime[point->GetDetectorID()]){
+      fTrackTime[point->GetDetectorID()] = point_time;
+    }
+    
+    // Check and save MC truth information
+    // Eloss==0 tracks are only stored in point, if track is entering detector from outside
+    // and thats what we are interested in...
+    if(point->GetEnergyLoss() == 0){
+      (fTrackMcTruth[point->GetDetectorID()]).push_back(point->GetTrackID());
+      //cout << "ELoss==0 : ID " <<
+      //point->GetTrackID()<<","<<point->GetDetectorID()<<","<<point->GetXPad()<<","<<point->GetYPad()<<endl;
+    }
+  }
+
+  // Loop over EmcPoint
+  
   // Loop to register EmcHit
-  for(p=fTrackEnergy.begin(); p!=fTrackEnergy.end(); ++p) {
-	if ((*p).second>fEnergyThreshold){
-	  // Check and save MC truth information B.S.
-	  // remove MC Truth particles which are not needed (eg grand^x-daugherts)
-      if( fMCTrackArray) cleansortmclist(fTrackMcTruth[(*p).first],fMCTrackArray);
+  for( p = fTrackEnergy.begin(); p != fTrackEnergy.end(); ++p){
+    if ((*p).second > fEnergyThreshold){
+      // Check and save MC truth information B.S.
+      // remove MC Truth particles which are not needed (eg grand^x-daugherts)
+      if( fMCTrackArray){
+	cleansortmclist(fTrackMcTruth[(*p).first],fMCTrackArray);
+      }
       AddHit(1, (*p).first, (*p).second, fTrackTime[(*p).first], fTrackMcTruth[(*p).first]);
     }
   }
-  
 }
+
 // -------------------------------------------------------------------------
 // -----   Private method AddDigi   --------------------------------------------
-PndEmcHit* PndEmcHitProducer::AddHit(Int_t trackID,Int_t detID, Float_t energy, Float_t time, std::vector <Int_t> &mctruth){
+PndEmcHit* PndEmcHitProducer::AddHit(Int_t trackID,Int_t detID, Float_t energy,
+				     Float_t time, std::vector <Int_t> &mctruth)
+{
   // It fills the PndEmcHit category
-  //cout << "PndEmcHitProducer: track " << trackID << " evt " << eventID << " sec " << sec << " plane " << pla << " strip " << strip << "box " << box << " tube " << tub << endl;
+  
+  //cout << "PndEmcHitProducer: track " << trackID << " evt " << eventID
+  //<< " sec " << sec << " plane " << pla << " strip " << strip << "box
+  //" << box << " tube " << tub << endl;
   TClonesArray& clref = *fDigiArray;
   Int_t size = clref.GetEntriesFast();
-  return new(clref[size]) PndEmcHit(trackID, detID, energy, time, emcX[detID], emcY[detID], emcZ[detID], mctruth);
+  return new(clref[size]) PndEmcHit(trackID, detID, energy, time, emcX[detID], 
+				    emcY[detID], emcZ[detID], mctruth);
 }
 // ----
 
--- emc/EmcDigi/PndEmcHitProducer.h
+++ emc/EmcDigi/PndEmcHitProducer.h
@@ -2,18 +2,19 @@
 // -----                 PndEmcHitProducer header file                 -----
 // -----                 Created 14/08/06  by S.Spataro                -----
 // -------------------------------------------------------------------------
-#pragma once
 #ifndef PNDEMCHITPRODUCER_H
 #define PNDEMCHITPRODUCER_H
 
+#include <map>
+#include <string>
+#include <limits>
 
 #include "FairTask.h"
 #include "TH1F.h"
 #include "TFile.h"
-#include "PndEmcHit.h"
 #include "TVector3.h"
-#include <map>
-#include <string>
+
+#include "PndEmcHit.h"
 #include "PndEmcStructure.h"
 
 class TClonesArray;
@@ -24,66 +25,65 @@ class PndEmcGeoPar;
 
 class PndEmcHitProducer : public FairTask
 {
-
+  
  public:
-	typedef std::map<Int_t, Float_t> mapper;
-	
-	/** Default constructor **/  
-	PndEmcHitProducer();
+  typedef std::map<Int_t, Float_t> mapper;
+  
+  /** Default constructor **/  
+  PndEmcHitProducer();
+  
+  PndEmcHitProducer(Bool_t val);
+  
+  /** Destructor **/
+  ~PndEmcHitProducer();
+  
+  
+  /** Virtual method Init **/
+  virtual InitStatus Init();
+  
+  
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
+  
+  PndEmcHit* AddHit(Int_t trackID, Int_t detID, Float_t energy, Float_t time,std::vector <Int_t> &mctruth);
 
-	PndEmcHitProducer(Bool_t val);
-	
-	/** Destructor **/
-	~PndEmcHitProducer();
-	
-	
-	/** Virtual method Init **/
-	virtual InitStatus Init();
-
-
-	/** Virtual method Exec **/
-	virtual void Exec(Option_t* opt);
-
-   PndEmcHit* AddHit(Int_t trackID, Int_t detID, Float_t energy, Float_t time,std::vector <Int_t> &mctruth);
-
-    //PndEmcHit* AddHit(Int_t trackID, Int_t detID, Float_t energy, Float_t time,std::vector<PndEmcPoint*> pointList);  // not implemented
+  //PndEmcHit* AddHit(Int_t trackID, Int_t detID, Float_t energy, Float_t time,std::vector<PndEmcPoint*> pointList);
+  // not implemented
   //void CreateStructure();
   
-   void SetStorageOfData(Bool_t val); // Method to specify whether hits are stored or not.
+  void SetStorageOfData(Bool_t val); // Method to specify whether hits are stored or not.
 
  private: 
-   
-	/** Input array of PndEmcPoints **/
-	TClonesArray* fPointArray;
-	TClonesArray* fMCTrackArray;
-	
-	/** Output array of PndEmcHit **/
-	TClonesArray* fDigiArray;  
-	
-	TObjArray *fVolumeArray;
-	
-	/** Geo file to use **/
-	Int_t fMapVersion;
-	Float_t fEnergyThreshold;
   
-	mapper emcX;
-	mapper emcY;
-	mapper emcZ;
-	
-	PndEmcStructure* fEmcStr;
-
-	PndEmcDigiPar*    fDigiPar;      /** Digitisation parameter container **/
-	PndEmcGeoPar*     fGeoPar;       /** Geometry parameter container **/
-
-	/** Get parameter containers **/
-	virtual void SetParContainers();
-
-	Bool_t         fStoreHits; // Flag which specify whether hits are stored or not
-
-	void cleansortmclist( std::vector <Int_t> &newlist,TClonesArray* mcTrackArray);
-
+  /** Input array of PndEmcPoints **/
+  TClonesArray* fPointArray;
+  TClonesArray* fMCTrackArray;
+  
+  /** Output array of PndEmcHit **/
+  TClonesArray* fDigiArray;  
+  
+  TObjArray *fVolumeArray;
+  
+  /** Geo file to use **/
+  Int_t fMapVersion;
+  Float_t fEnergyThreshold;
+  
+  mapper emcX;
+  mapper emcY;
+  mapper emcZ;
+  
+  PndEmcStructure* fEmcStr;
+  
+  PndEmcDigiPar*    fDigiPar;      /** Digitisation parameter container **/
+  PndEmcGeoPar*     fGeoPar;       /** Geometry parameter container **/
+  
+  /** Get parameter containers **/
+  virtual void SetParContainers();
+  
+  Bool_t fStoreHits; // Flag which specify whether hits are stored or not
+  
+  void cleansortmclist( std::vector <Int_t> &newlist,TClonesArray* mcTrackArray);
+  
   ClassDef(PndEmcHitProducer,1);
-  
 };
-
 #endif
