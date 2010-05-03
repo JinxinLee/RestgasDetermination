04274e4a114540c3378582ccc58c374a94c2a08a
Radoslaw Karabowicz <R.Karabowicz@gsi.de>
2010-05-03 17:07:08 +0000

    Changed the macros, QA1 runs the GEM simulation, QA2 runs the GEM reconstruction(digitization, hit finding,track finding, track fitting)

--- macro/qa/gem/QAmacro_gem_1.C
+++ macro/qa/gem/QAmacro_gem_1.C
@@ -1,20 +1,81 @@
 {
+  //FileNames
+  TString OutputFile    = "tstQA.points.root";
+  TString ParOutputFile = "tstQA.param.root";
+  
+  // Load basic libraries---------------------------------------------
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
   rootlogon();
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
+  
+  // Create the Simulation run manager--------------------------------
+  FairRunSim *fRun = new FairRunSim();
+  fRun->SetName("TGeant4");
+  fRun->SetOutputFile(OutputFile.Data());
+  fRun->SetBeamMom(15.0);
+  fRun->SetMaterials("media_pnd.geo");
+  FairRuntimeDb *rtdb=fRun->GetRuntimeDb();
+  
+ //---------------------Set Parameter output      ---------- 
+  Bool_t kParameterMerged=kTRUE;
+  FairParRootFileIo* output=new FairParRootFileIo(kParameterMerged);
+  output->open(ParOutputFile.Data());
+  rtdb->setOutput(output);
 
-  Bool_t fTest=kFALSE;
+  // Create and add detectors
+  //-------------------------
 
-  cout << "<W> No QA module(s) for the GEMC defined ...yet!" << endl;
+  FairModule *Cave= new PndCave("CAVE");
+  Cave->SetGeometryFileName("pndcave.geo");
+  fRun->AddModule(Cave);
 
-  if (fTest){
-    cout << " Test passed" << endl;
-    cout << " All ok " << endl;  
-  }else{
-    cout << " Test Failed" << endl;
-    cout << " Not Ok " << endl;         
-  }
+  FairModule *Pipe= new PndPipe("PIPE");
+  Pipe->SetGeometryFileName("pipebeamtarget.geo");
+  fRun->AddModule(Pipe);
+  
+//  FairModule *Magnet= new PndMagnet("MAGNET");
+//  Magnet->SetGeometryFileName("FullSolenoid.root");
+//  fRun->AddModule(Magnet);
+  
+  FairModule *dipole= new PndMagnet("MAGNET");
+  dipole->SetGeometryFileName("dipole.geo");
+  fRun->AddModule(dipole);
+ 
+  FairDetector *Gem = new PndGemDetector("GEM", kTRUE);
+  Gem->SetGeometryFileName("gem_4Stations.root");
+  Gem->SetVerboseLevel(0);
+  fRun->AddModule(Gem);
+  
+  // Event generator
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+  
+  FairBoxGenerator* boxGen = new FairBoxGenerator(13,1);
+  boxGen->SetThetaRange(12,12);
+  boxGen->SetPhiRange  (0.,360.);
+  boxGen->SetPRange    (2.,2.);
+  primGen->AddGenerator(boxGen);
+  
+  //---------------------Create and Set the Field(s)---------- 
+  PndMultiField *fField= new PndMultiField("FULL");
+  fRun->SetField(fField);
+  //-----------end of Bfield stuff
+
+  // support event display?
+  fRun->SetStoreTraj(kFALSE);
+  fRun->SetRadLenRegister(kFALSE);
+
+  fRun->Init();
+
+  // Transport nEvents
+  // -----------------
+
+  fRun->Run(100);
+
+  rtdb->saveOutput();
+  rtdb->print();
+
+  cout << " Test passed" << endl;
+  cout << " All ok " << endl;  
 
   exit(0); 
 }  
---
+++ macro/qa/gem/QAmacro_gem_2.C
@@ -0,0 +1,131 @@
+{
+  Int_t verboseLevel = 0;
+
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+  TString sysFile = gSystem->Getenv("VMCWORKDIR");
+
+  //FileNames
+  TString MCFile  = "tstQA.points.root";
+  TString parFile = "tstQA.param.root";
+  TString outFile = "tstQA.reco.root";
+
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile(MCFile);
+  fRun->SetOutputFile(outFile);
+
+  // -----  Parameter database   --------------------------------------------
+  TString allDigiFile = sysFile+"/macro/params/gem_4Stations.digi.par";
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(parFile.Data());
+	
+  FairParAsciiFileIo* parIo1 = new FairParAsciiFileIo();
+  parIo1->open(allDigiFile.Data(),"in");
+        
+  rtdb->setFirstInput(parInput1);
+  rtdb->setSecondInput(parIo1);
+  // ------------------------------------------------------------------------
+
+  // -----   Digitizer and Hit Finder   -------------------------------------
+  PndGemDigitize* gemDigitize = new PndGemDigitize("GEM Digitizer", verboseLevel);
+  fRun->AddTask(gemDigitize);
+  
+  PndGemFindHits* gemFindHits = new PndGemFindHits("GEM Hit Finder", verboseLevel);
+  fRun->AddTask(gemFindHits);
+
+  //------ Track finder ------------------------------
+  //Create and add finder task
+  PndGemFindTracks* finderTask = new PndGemFindTracks("PndGemFindTracks");
+  finderTask->SetUseHitOrDigi("hit"); // hit = (default), digi
+  fRun->AddTask(finderTask);
+  
+  //------ Realistic Track finder --------------------
+  PndGemTrackFinderOnHits* mcTrackFinder = new  PndGemTrackFinderOnHits();
+  mcTrackFinder->SetVerbose(verboseLevel);  // verbosity level
+  mcTrackFinder->SetPrimary(0);  // 1 = Only primary tracks are processed, 0 = all (default)
+  finderTask->UseFinder(mcTrackFinder);
+  //--------------------------------------------------
+
+  // ----- Prepare GEANE --------------------------------------------
+  // this will load Geant3 and execute setup macros to initialize geometry:
+  FairGeane *Geane = new FairGeane();
+  fRun->AddTask(Geane);
+  //--------------------------------------------------
+  
+  // -----   Run Kalman fitter   --------------------------------------------
+  PndRecoKalmanTask* recoKalman = new PndRecoKalmanTask();
+  recoKalman->SetTrackInBranchName("GEMTrack");
+  recoKalman->SetTrackOutBranchName("GEMFitTrack");
+  //recoKalman->SetNumIterations(3);
+  fRun->AddTask(recoKalman);
+  // ------------------------------------------------- 
+
+  // -----   Run Track finder QA   --------------------------------------------
+  PndGemTrackFinderQA* trackFinderQA = new PndGemTrackFinderQA();
+  trackFinderQA->SetVerbose(verboseLevel);
+  fRun->AddTask(trackFinderQA);
+  // ------------------------------------------------- 
+
+  // -----   Intialise and run   --------------------------------------------
+  fRun->Init();
+  fRun->Run(0,100);
+
+  //============================================================================
+  cout << endl;
+  cout << "=====================================================================" << endl;
+  cout << "Trying to read histograms..." << flush;
+
+  TH1F* fhMCPrimVsP   = (TH1F*)gDirectory->Get("GemTrackFinderQA/hMCPrimVsP");
+  TH1F* fhRecoPrimVsP = (TH1F*)gDirectory->Get("GemTrackFinderQA/hRecoPrimVsP");
+  TH1F* fhRecoPrimP   = (TH1F*)gDirectory->Get("GemTrackFinderQA/hRecoPrimP");
+
+  Double_t minEfficiency = 80.;
+  Double_t minQuality    =  1./1.2;
+  Double_t maxQuality    =  1./minQuality;
+  Double_t maxResolution =  5.;
+
+  Bool_t fTest = kFALSE;
+
+  if ( fhMCPrimVsP && fhRecoPrimVsP && fhRecoPrimP ) {
+    cout << " DONE" << endl;
+    Int_t fNofMCPrim   = fhMCPrimVsP  ->Integral();
+    Int_t fNofRecoPrim = fhRecoPrimVsP->Integral();
+    Double_t effPrim = 100.*(Double_t)fNofRecoPrim/((Double_t)fNofMCPrim);
+    cout << "             Simulated / Reconstructed:" << endl;
+    cout << " quantity  :       " << fNofMCPrim << " /           " << fNofRecoPrim << "  >>>> " << effPrim << "%" << endl;
+    Double_t mcMomenta   = fhRecoPrimVsP->GetMean();
+    Double_t recoMomenta = fhRecoPrimP  ->GetMean();
+    Double_t momQuality = mcMomenta/recoMomenta;
+    cout << " quality   :     " << mcMomenta << " /         " << recoMomenta << "  >>>> " << momQuality << "" << endl;
+
+    Double_t recoMomRMS    = fhRecoPrimP  ->GetRMS();
+    Double_t momResolution = recoMomRMS/mcMomenta*100.;
+    cout << " resolution:                            >>>> " << momResolution << "%" << endl;
+
+    if ( effPrim       > minEfficiency &&
+	 momQuality    > minQuality &&
+	 momQuality    < maxQuality &&
+	 momResolution < maxResolution )
+      fTest = kTRUE; 
+  }
+  else
+    cout << " FAILED" << endl;
+  cout << "=====================================================================" << endl;
+
+  //============================================================================
+
+  if (fTest){
+    cout << " Test passed" << endl;
+    cout << " All ok " << endl;  
+  }else{
+    cout << " Test Failed" << endl;
+    cout << " Not Ok " << endl;         
+  }
+
+  exit(0); 
+
+}  
+  
