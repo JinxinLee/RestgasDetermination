4f1eb425a43dec9e5fa83dea8a4e2bb801edea20
vandenbm <vandenbm>
2010-05-25 16:44:37 +0000

    Add severals options

--- tpc/TestBench/PndTpcDataReaderTask.h
+++ tpc/TestBench/PndTpcDataReaderTask.h
@@ -51,8 +51,13 @@ class PndTpcDataReaderTask : public FairTask {
   void SetClusterBranchName(const TString& name) {_digiBranchName=name;}
   void SetPersistence(Bool_t opt=kTRUE) {_persistence=opt;}
   void SetDatafile(const TString& name) {_file=name;}
-  void SetCutSmallPad(Bool_t opt=kTRUE) {_smallpad=opt;}
+  void SetCutSmallPad(Bool_t opt=kTRUE) {_cutsmallpad=opt;}  
+  void SetCutBigPad(Bool_t opt=kTRUE) {_cutbigpad=opt;}
   void SetMinDigis(unsigned int md) {fMinDigis=md;}
+  void SetMaxSample(unsigned int md) {fMaxSample=md;}
+  void SetOccMax(unsigned int md) {fCutOcc=md;}
+  void SetNbChip(unsigned int nb) {fNbChip=nb;}
+  void SetOnly800(Bool_t nb=kTRUE) {fOnly800=nb;}
 
  // Operations ----------------------
   virtual InitStatus Init();
@@ -79,11 +84,16 @@ class PndTpcDataReaderTask : public FairTask {
   PndTpcDigiPar* fpar;
 
   int loop;
+  unsigned int fMaxSample;
+  unsigned int fCutOcc;
+  unsigned int fNbChip;
 
   TClonesArray* _sampleOutArray;
   
   Bool_t _persistence;
-  Bool_t _smallpad;
+  Bool_t _cutsmallpad;  
+  Bool_t _cutbigpad;
+  Bool_t fOnly800;
 
 
   // Private Data Members ------------
