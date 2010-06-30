8280a0d31f460e6ca8a0ea1b0ae9971de1c5309b
Huagen Xu <h.xu@fz-juelich.de>
2010-06-30 16:01:06 +0000

    Update Link file due to class change

--- lmd/LmdLinkDef.h
+++ lmd/LmdLinkDef.h
@@ -6,7 +6,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-
 //data
 #pragma link C++ class PndSdsMCPoint+;
 #pragma link C++ class PndSdsDigi+;
@@ -14,51 +13,21 @@
 
 //SDS
 #pragma link C++ class PndSdsDetector+;
-//#pragma link C++ class PndSdsGeoHandling+;
-//#pragma link C++ class PndSdsContFact+;
 
 //mc
 //#pragma link C++ class PndMvdContFact+;
 #pragma link C++ class PndLmdDetector+;
+#pragma link C++ class PndLmdGeo+;
 
 //digi
 //#pragma link C++ class PndLmdHybridHitProducer+;
 #pragma link C++ class PndLmdStripHitProducer+;
-//#pragma link C++ class PndLmdCalcPixel;
-//#pragma link C++ class PndLmdCalcFePixel;
-//#pragma link C++ class PndLmdCalcStrip;
 #pragma link C++ class PndLmdDigiTask+;
-/*#pragma link C++ class PndLmdPixelDigiPar+;
-#pragma link C++ class PndLmdStripDigiPar+;
-#pragma link C++ class PndLmdDigiAna+;*/
+
 #pragma link C++ class PndLmdHitProducerIdeal+;
 #pragma link C++ class PndLmdNoiseProducer+;
+#pragma link C++ class PndLmdContFact+;
 
-//#pragma link C++ class PndLmdGeoHandling+;
-//#pragma link C++ class PndMvdCalcFePixel+;
-
-/*#pragma link C++ class PndMvdMCEventAna+;
-#pragma link C++ class PndMvdEventAna+;
-#pragma link C++ class PndMvdDigiEventAna+;
-#pragma link C++ class PndMvdEventAna+;
-#pragma link C++ class PndMvdMCEventAna+;
-#pragma link C++ class PndMvdDigiEventAna+;
-#pragma link C++ class PndMvdAllDataEventAna+;
-#pragma link C++ class PndMvdFileNameCreator+;
-#pragma link C++ class PndMvdAccessRTDBTask+;
-
-
-#pragma link C++ class PndGeoHitList+;
-#pragma link C++ class PndEventDisplay+;
-#pragma link C++ class PndMvdEventAnaTask+;
-#pragma link C++ class PndMvdEventMerger+;
-#pragma link C++ class PndMvdEventMergerTask+;
-#pragma link C++ class PndMvdMSAnaTask+;*/
-
-//#pragma link C++ class PndStringVector+;
-
-//#pragma link C++ class PndMvdConvertApv+;
-//#pragma link C++ class PndMvdConvertApvTask+;
 
 
 #endif
