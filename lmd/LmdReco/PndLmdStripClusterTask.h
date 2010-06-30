9459f53fc98e859cca1e47ca0de572266b11236a
Huagen Xu <h.xu@fz-juelich.de>
2010-06-30 18:11:29 +0000

    Reload functions and update

--- lmd/LmdReco/PndLmdStripClusterTask.h
+++ lmd/LmdReco/PndLmdStripClusterTask.h
@@ -1,7 +1,10 @@
 // -------------------------------------------------------------------------
 // -----                 PndLmdStripClusterTask header file             -----
 // -------------------------------------------------------------------------
-
+//Notes
+/*Notes for update on Jun30. 2010 by h.xu@fz-juelich.de
+ * Reload the function of SetCalculators() and SetClusterType()
+ */
 
 
 #ifndef PNDLMDSTRIPCLUSTERTASK_H
@@ -24,9 +27,11 @@ class PndLmdStripClusterTask : public PndSdsStripClusterTask
   virtual void SetBranchNames(TString inBranchname, TString outHitBranchname, TString outClustBranchname, TString folderName);
   virtual void SetBranchNames();
   virtual void SetParContainers();
+  virtual void SetClusterType() {fClusterType = kMVDClusterStrip;}
   inline void SetyRotation(const Double_t inrot){fyRotation=inrot;};
   
-  Bool_t Backmap( TVector2 meantopPoint, Double_t toperr , TVector2 meanbotPoint, Double_t boterr, TVector3 &hitpos, TVector3 &hiterr, TString &detname);
+  virtual void SetCalculators();
+//  Bool_t Backmap( TVector2 meantopPoint, Double_t toperr , TVector2 meanbotPoint, Double_t boterr, TVector3 &hitpos, TVector3 &hiterr, TString &detname);
 
   ClassDef(PndLmdStripClusterTask,2);
 
