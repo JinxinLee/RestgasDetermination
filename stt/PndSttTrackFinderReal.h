f36988e03c8363ebf186dbbf29b53139facc9315
Lia Lavezzi <lia.lavezzi@gmail.com>
2010-07-06 15:44:48 +0000

    iplotta set to false

--- stt/PndSttTrackFinderReal.h
+++ stt/PndSttTrackFinderReal.h
@@ -156,7 +156,7 @@ class PndSttTrackFinderReal : public PndSttTrackFinder
       int IVOLTE, ntimes, INTERO, ITRACCIA,
           N_INTENDED;
 //  Double_t SEMILENGTH_STRAIGHT = 75.;
-      static const bool  iplotta = true , ianalizza = true ;
+      static const bool  iplotta = false , ianalizza = true ;
       static const int  nmassimo=10;
       int istampa ;
       TH1F * hx;
