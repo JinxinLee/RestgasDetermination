4ba65d51ae08fe052e440395d2d1ac7bf0c8caa3
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2010-07-09 15:18:13 +0000

    Adding a link to EmcClusterSplitter

--- emc/EmcReco/PndEmcExpClusterSplitter.cxx
+++ emc/EmcReco/PndEmcExpClusterSplitter.cxx
@@ -183,6 +183,7 @@ void PndEmcExpClusterSplitter::Exec(Option_t* opt)
 			
 			PndEmcBump* theNewBump = AddBump(); 
 			theNewBump->MadeFrom(iCluster);
+			theNewBump->SetLink(FairLink("EmcCluster", iCluster));
 
 			for(theDigiIterator = theCluster->MemberDigiMap().begin();
 			theDigiIterator != theCluster->MemberDigiMap().end(); ++theDigiIterator){
@@ -411,6 +412,7 @@ void PndEmcExpClusterSplitter::Exec(Option_t* opt)
 			theBump = theBumpsIterator->second;
 			Int_t size_ba = fBumpArray->GetEntriesFast();
 			PndEmcBump* theNextBump = new((*fBumpArray)[size_ba]) PndEmcBump(*(theBump));
+			std::cout << "Bump Created!" << std::endl;
 			theNextBump->SetLink(FairLink("EmcCluster", iCluster));
 		}
 		
