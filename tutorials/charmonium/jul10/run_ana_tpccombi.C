3a3ab6f90d580d6ca8b4f7633292b625c8cd73c3
Soeren Lange <Soeren.Lange@exp2.physik.uni-giessen.de>
2010-07-14 11:25:05 +0000

    new version of charmonium tutorial now based upon PndEvtgenDirect soeren / 14.07.2010

---
+++ tutorials/charmonium/jul10/run_ana_tpccombi.C
@@ -0,0 +1,87 @@
+//void run_ana_tpccombi(TString fname="output.evt.fast.root",int nevts=0)
+void run_ana_tpccombi(TString fname="pid_tpccombi.root",int nevts=0)
+{
+  	TStopwatch timer;
+  	timer.Start();
+	
+	gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+	
+	TFile *out = TFile::Open("invmass_histo.root","RECREATE");
+
+	//TCanvas *c1=new TCanvas("c1","c1",600,600);
+	
+	// the PndEventReader takes care about file/event handling
+	PndEventReader evr(fname);
+	
+	// to attach more files: evr.Add(fname2);evr.Add(fname3); ...
+
+	TH1F *invmass=new TH1F("invmass","invariant mass",100,2.5,3.5);
+	
+	// the candidates lists we need
+	TCandList p1;
+	TCandList p2;
+	TCandList jpsi;
+
+	TLorentzVector ini(0,0,6.23164,7.24015);
+		
+	if (nevts==0) nevts=evr.GetEntries();
+	// cout << "nevts " << nevts << "\n";
+	int i=0,j=0, k=0, l=0;
+	
+	// *************
+	// this is the loop through the events ... as simple as this...
+	// ****************
+	while (evr.GetEvent() && i++<nevts)
+	{
+
+	    if (!((i+1)%1000)) cout << i << "\n";
+
+	    evr.FillList(p1,"All");
+	    evr.FillList(p2,"All");
+//	    evr.FillList(p1,"Charged");
+//	    evr.FillList(p2,"Charged");
+
+	    // cout << p1.GetLength() << " e+ candidates found" << "\n";
+	    // cout << p2.GetLength() << " e- candidates found" << "\n";
+
+	    for (j=0;j<p1.GetLength();++j) { 
+                if ((p1[j].GetMicroCandidate().GetEmcRawEnergy()/p1[j].GetMicroCandidate().GetMomentum().Mag())>0.0) {
+		    p1[j].SetMass(TRho::Instance()->GetPDG()->GetParticle(11)->Mass());
+		} else {
+		    p1.Remove(p1[j]);
+		    j--;
+		}
+	    }
+	    for (j=0;j<p2.GetLength();++j) { 
+                if ((p2[j].GetMicroCandidate().GetEmcRawEnergy()/p2[j].GetMicroCandidate().GetMomentum().Mag())>0.0) {
+		    p2[j].SetMass(TRho::Instance()->GetPDG()->GetParticle(11)->Mass());
+		} else {
+		    p2.Remove(p2[j]);
+		    j--;
+		}
+
+	    }
+
+	    jpsi.Combine(p1,p2);
+
+	    // cout << jpsi.GetLength() << " J/Psi candidates found" << "\n";
+	    for (l=0;l<jpsi.GetLength();++l) { 
+		invmass->Fill(jpsi[l].M());
+	    }
+
+	}
+	
+	//c1->cd(); 
+
+        invmass->Draw();
+
+	out->cd();
+	invmass->Write();
+	out->Save();
+	
+	timer.Stop();
+	Double_t rtime = timer.RealTime();
+	Double_t ctime = timer.CpuTime();	
+	printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+
+}
---
+++ tutorials/charmonium/jul10/simfast.C
@@ -0,0 +1,84 @@
+// *******
+// Macro for running fast simulation
+// *******
+
+
+void simfast(TString infile="output.evt",int soff=0,int nevts=1000,TString outfile="")
+{
+  TStopwatch timer;
+  timer.Start();
+  gDebug=0;
+
+  // Load basic libraries
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+
+  // Load this libraries
+  gSystem->Load("libRho");
+  gSystem->Load("libfsim");
+ 
+  if (outfile=="") outfile=infile+".fast.root";
+
+  FairRunSim *fRun = new FairRunSim();
+  fRun->SetOutputFile(outfile.Data());
+  fRun->SetName("TGeant3");
+
+  // Create and Set Event Generator
+  //-------------------------------
+
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+
+  //FairEvtGenGenerator* evtGen = new FairEvtGenGenerator(infile.Data());
+  //primGen->AddGenerator(evtGen);
+
+  Float_t mom = 6.991;
+  PndDpmDirect *Dpm= new PndDpmDirect(mom,0);
+  primGen->AddGenerator(Dpm);
+
+  primGen->DoTracking(kFALSE);
+
+  
+  //Setup the Fast Simulation Task
+  //-----------------------------
+  PndFastSim* fastSim = new PndFastSim();
+  
+  //increasing verbosity increases the amount of console output (mainly for debugging)
+  fastSim->SetVerbosity(0);  
+
+  //enable the producting of parametrized neutral (hadronic) split offs
+  if (soff) 
+    fastSim->EnableSplitoffs("splitpars.dat");
+   
+  //EM Calorimeters w/ default parameters (don't have to be set, just to list the available parameters
+  //EM Calorimeters w/ default parameters (don't have to be set, just to list the available parameters
+  fastSim->AddDetector("EmcBarrel","thtMin=22.0 thtMax=140.0 Emin=0.01 barrelRadius=0.5");
+  fastSim->AddDetector("EmcFwCap", "thtMin=5.0 thtMax=22.0 Emin=0.01 dist=2.5");
+  fastSim->AddDetector("EmcBwCap", "thtMin=140.0 thtMax=170.0 Emin=0.01 dist=0.7");
+  fastSim->AddDetector("EmcFS",    "thtMin=0.05 thtMax=5.0 aPar=0.02 bPar=0.0274 Emin=0.01 dist=8.0");
+
+  //Tracking, Vertexing, dE/dx
+  fastSim->AddDetector("Mvd",  "thtMin=0.0 thtMax=180.0 vtxRes=0.0001 pRes=0.1 phiRes=0.01 thetaRes=0.01 dEdxRes=0.25");
+  fastSim->AddDetector("Stt",  "thtMin=7.765 thtMax=159.44 dEdxRes=0.2");
+  fastSim->AddDetector("MdcTS","thtMin=0.5 thtMax=22.0");
+  fastSim->AddDetector("MdcFS","thtMin=0.5 angleXMax=10.0 angleYMax=5.0");  
+  //fastSim->AddDetector("Tpc",  "thtMin=7.765 thtMax=159.44 efficiency=1.0 pmin=0.0 pRes=0.005 phiRes=0.1 thetaRes=0.1 dEdxRes=0.07"); 
+
+  //PID detectors
+  fastSim->AddDetector("DrcBarrel","thtMin=22.0 thtMax=140.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
+  fastSim->AddDetector("DrcDisc","thtMin=5.0 thtMax=22.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
+  fastSim->AddDetector("Rich","angleXMax=5.0 angleYMax=10.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
+  fastSim->AddDetector("Tof","thtMin=22.0 thtMax=140.0 dp=0.01");
+  
+  fRun->AddTask(fastSim);
+  
+  fRun->Init();
+  if (nevts==0) nevts=10;
+  fRun->Run(nevts);		
+  
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+}  
+  
