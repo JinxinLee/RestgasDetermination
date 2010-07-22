d5282193a7d181ff153ddf8527eaca4bb4fd993d
Ralf Kliemt <r.kliemt@gsi.de>
2010-07-22 11:50:41 +0000

    Fixed uninitialized variables

--- fsim/PndFsmCmpDet.cxx
+++ fsim/PndFsmCmpDet.cxx
@@ -137,9 +137,9 @@ PndFsmCmpDet::respond(PndFsmTrack *t) {
   double DrcBarrelThtcErr=0;
   double RichThtcErr=0;
 
-  double dVx;
-  double dVy;
-  double dVz;
+  double dVx=0;
+  double dVy=0;
+  double dVz=0;
   
   double LH_e=1.0;
   double LH_mu=1.0;
