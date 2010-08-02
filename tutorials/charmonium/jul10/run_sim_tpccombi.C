2854ffd08c08aca0db7777316ad6f25828549dcf
Soeren Lange <Soeren.Lange@exp2.physik.uni-giessen.de>
2010-08-02 15:21:29 +0000

    new MVD geometry in run_sim_tpccombi.C

--- tutorials/charmonium/jul10/run_sim_tpccombi.C
+++ tutorials/charmonium/jul10/run_sim_tpccombi.C
@@ -63,7 +63,8 @@ run_sim_tpccombi(Int_t nEvents=10){
   fRun->AddModule(Tpc);
 
   FairDetector *Mvd = new PndMvdDetector("MVD", kTRUE);
-  Mvd->SetGeometryFileName("MVD_v1.0_woPassiveTraps.root");
+  //  Mvd->SetGeometryFileName("MVD_v1.0_woPassiveTraps.root");
+  Mvd->SetGeometryFileName("Mvd-2.1_AddDisks_FullVersion");
   fRun->AddModule(Mvd);
 
   PndEmc *Emc = new PndEmc("EMC",kTRUE);
