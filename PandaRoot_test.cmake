487769cdcc9f15a684de29ecd464ff7434d1f142
Florian Uhlig <f.uhli@gsi.de>
2010-09-23 12:54:56 +0000

    Change CMake scripts. Nightly test are done now with the build type nightly which switch on the effective c++ warnings. Add all effective c++ warnings from external packages (root, pluto, boost, geant3) to !CTestCustom.cmake to be filtered before commiting to the Dashboard. Automatically upload localy changed files to the dashboard during the nightly tests.

--- PandaRoot_test.cmake
+++ PandaRoot_test.cmake
@@ -3,15 +3,40 @@ SET (CTEST_BINARY_DIRECTORY $ENV{BUILDDIR})
 SET (CTEST_SITE $ENV{SITE})
 SET (CTEST_BUILD_NAME $ENV{LABEL})
 SET (CTEST_CMAKE_GENERATOR "Unix Makefiles")
-SET (CTEST_PROJECT_NAME "PANDAROOT")
+SET (CTEST_PROJECT_NAME "PandaRoot")
 
 SET (CTEST_UPDATE_COMMAND "svn")
 SET (CTEST_UPDATE_CHECKOUT  " ${CTEST_UPDATE_COMMAND} update ")
 SET (BUILD_COMMAND "make")
 SET (CTEST_BUILD_COMMAND "${BUILD_COMMAND} -j$ENV{number_of_processors}")
 
+if($ENV{ctest_model} MATCHES Nightly)
 
-CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+  Set (CTEST_CONFIGURE_COMMAND "${CMAKE_EXECUTABLE_NAME} ${CTEST_SOURCE_DIRECTORY} -DCMAKE_BUILD_TYPE=NIGHTLY")
+
+  # get the information about conflicting or localy modified files
+  # from svn, extract the relavant information about the file name
+  # and put the result in the output variable
+  execute_process(COMMAND svn stat -u  
+                  COMMAND grep ^[CM]
+                  COMMAND cut -c21- 
+                  OUTPUT_VARIABLE FILELIST
+                  )
+
+  # create out of the output a cmake list. This step is done to convert the
+  # stream into seperated filenames.
+  # The trick is to exchange an "\n" by an ";" which is the separartor in
+  # a list created by cmake 
+  STRING(REGEX REPLACE "\n" ";" _result "${FILELIST}")
+
+  FOREACH(_file ${_result})
+    STRING(STRIP "${_file}" _file1)
+    SET (CTEST_NOTES_FILES ${CTEST_NOTES_FILES} "${CTEST_SOURCE_DIRECTORY}/${_file1}")
+  ENDFOREACH(_file ${_result})
+
+  CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+
+endif($ENV{ctest_model} MATCHES Nightly)
 
 configure_file(${CTEST_SOURCE_DIRECTORY}/CTestCustom.cmake
                ${CTEST_BINARY_DIRECTORY}/CTestCustom.cmake
