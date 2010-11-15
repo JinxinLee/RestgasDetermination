94585390a8544e758536ff1f9b4ee54288deec96
Radoslaw Karabowicz <R.Karabowicz@gsi.de>
2010-11-15 15:35:24 +0000

    Initial release of MVD-GEM track finder

---
+++ macro/global/mvdgem_reco.C
@@ -0,0 +1,112 @@
+void mvdgem_reco(Int_t nEvents=1000, Int_t pid=13, Double_t theta=5., Int_t nStations = 3) {
+  // ========================================================================
+  // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
+  Int_t iVerbose = 0;
+
+  // Input file (MC events)
+  TString inFile  = Form("MGpoints_%dStations_p%d_theta%.1f_n%d.root",nStations,pid,theta,nEvents);
+
+  // Parameter file
+  TString parFile = Form("MGparams_%dStations_p%d_theta%.1f_n%d.root",nStations,pid,theta,nEvents);
+
+  // Output file
+  TString outFile = Form("MGtracks_%dStations_p%d_theta%.1f_n%d.root",nStations,pid,theta,nEvents);
+
+  // Number of events to process
+  Int_t nEvents = 0;
+ 
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+  TString sysFile = gSystem->Getenv("VMCWORKDIR");
+  // ------------------------------------------------------------------------
+
+  // ---  Now choose concrete engines for the different tasks   -------------
+  // ------------------------------------------------------------------------
+
+  // In general, the following parts need not be touched
+  // ========================================================================
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+  // -----   Digitization run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile(inFile);
+  fRun->SetOutputFile(outFile);
+  
+  // ------------------------------------------------------------------------
+
+  // -----  Parameter database   --------------------------------------------
+  TString allDigiFile = sysFile+"/macro/params/all.par";
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(parFile.Data());
+	
+  FairParAsciiFileIo* parIo1 = new FairParAsciiFileIo();
+  parIo1->open(allDigiFile.Data(),"in");
+        
+  rtdb->setFirstInput(parInput1);
+  rtdb->setSecondInput(parIo1);
+  // ------------------------------------------------------------------------
+
+  // ##################################################################    MVD
+  // -----   MDV digi producer   ----------------------------------------
+  PndMvdDigiTask* mvddigi = new PndMvdDigiTask();
+  mvddigi->SetVerbose(iVerbose);
+  fRun->AddTask(mvddigi);
+    // -----   MDV cluster producer   -----------------------------------
+  Double_t chargecut = 5000., pixelrad=1.8; // one day this will move to the parameter db.
+  PndMvdClusterTask* mvdmccls = new PndMvdClusterTask();
+  mvdmccls->SetVerbose(iVerbose);
+  fRun->AddTask(mvdmccls);
+  // ##################################################################   GEM
+  // -----   GEM digi producer   --------------------------------------
+  Int_t verboseLevel = 0;
+  PndGemDigitize* gemDigitize = new PndGemDigitize("GEM Digitizer", verboseLevel);
+  fRun->AddTask(gemDigitize);
+  // -----   GEM hit producer   ---------------------------------------
+  PndGemFindHits* gemFindHits = new PndGemFindHits("GEM Hit Finder", verboseLevel);
+  fRun->AddTask(gemFindHits);
+  // ##################################################################   MVD-GEM tracking
+  PndMvdGemTrackFinderOnHits* trackFinder = new PndMvdGemTrackFinderOnHits(iVerbose);
+  fRun->AddTask(trackFinder);
+
+  // ----- Prepare GEANE --------------------------------------------
+  // this will load Geant3 and execute setup macros to initialize geometry:
+  FairGeane *Geane = new FairGeane();
+  fRun->AddTask(Geane);
+  //--------------------------------------------------
+
+  // -----   Run Kalman fitter   --------------------------------------------
+  PndRecoKalmanTask* recoKalman = new PndRecoKalmanTask();
+  recoKalman->SetTrackInBranchName("MVDGEMTrack");
+  recoKalman->SetTrackOutBranchName("MVDGEMFitTrack");
+  //recoKalman->SetNumIterations(3);
+  fRun->AddTask(recoKalman);
+
+  // -----   Intialise and run   --------------------------------------------
+  fRun->Init();
+  fRun->Run(0, nEvents);
+
+  rtdb->saveOutput();
+  rtdb->print();
+  // ------------------------------------------------------------------------
+
+  // -----   Finish   -------------------------------------------------------
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Macro finished succesfully." << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+  cout << endl;
+  // ------------------------------------------------------------------------
+
+
+}
---
+++ macro/global/mvdgem_sim.C
@@ -0,0 +1,145 @@
+void mvdgem_sim(Int_t nEvents=1000, Int_t pid=13, Double_t theta=5., Int_t nStations = 3) {
+  
+  if ( nStations != 3 && nStations != 4 ) {
+    cout << "WRONG number of stations, only 3 or 4 allowed." << endl;
+    return;
+  }
+
+  TStopwatch timer;
+  timer.Start();
+  gDebug=0;
+  // Load basic libraries
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+
+  TString parOutput = Form("MGparams_%dStations_p%d_theta%.1f_n%d.root",nStations,pid,theta,nEvents);
+
+  TString outputFile = Form("MGpoints_%dStations_p%d_theta%.1f_n%d.root",nStations,pid,theta,nEvents);
+
+
+
+  FairRunSim *fRun = new FairRunSim();
+  
+  // set the MC version used
+  // ------------------------
+
+  fRun->SetName("TGeant4");
+  
+  fRun->SetOutputFile(outputFile.Data());
+
+  // Set Material file Name
+  //-----------------------
+
+  fRun->SetMaterials("media_pnd.geo");
+  
+  // Create and add detectors
+  //-------------------------
+  
+  FairModule *Cave= new PndCave("CAVE");
+  Cave->SetGeometryFileName("pndcave.geo");
+  fRun->AddModule(Cave); 
+  
+  FairModule *Dipole= new PndMagnet("MAGNET");
+  Dipole->SetGeometryFileName("dipole.geo");
+  fRun->AddModule(Dipole);
+  
+  FairModule *Pipe= new PndPipe("PIPE");
+  fRun->AddModule(Pipe);
+  
+  //-------------------------  MVD       -----------------
+  FairDetector *Mvd = new PndMvdDetector("MVD", kTRUE);
+  Mvd->SetGeometryFileName("MVD_v1.0_woPassiveTraps.root");
+  fRun->AddModule(Mvd);
+  //-------------------------  STT       -----------------
+  FairDetector *Stt= new PndStt("STT", kTRUE);
+  if ( nStations == 3 ) 
+    Stt->SetGeometryFileName("straws_skewed_blocks_35cm_pipe.geo");
+  if ( nStations == 4 ) 
+    Stt->SetGeometryFileName("straws_skewed_blocks_pipe_120cm.geo");
+  fRun->AddModule(Stt);
+  //-------------------------  TPC       -----------------
+//   FairDetector *Tpc = new PndTpcDetector("TPC", kTRUE);
+//   Tpc->SetGeometryFileName("tpc.geo");
+//   fRun->AddModule(Tpc);
+  //-------------------------  GEM       -----------------
+  FairDetector *Gem = new PndGemDetector("GEM", kTRUE);
+  Gem->SetGeometryFileName(Form("gem_%dStations.root",nStations));
+  fRun->AddModule(Gem);
+  //-------------------------  DRC       -----------------
+  PndDrc *Drc = new PndDrc("DIRC", kTRUE);
+  Drc->SetRunCherenkov(kFALSE); // for fast sim Cherenkov -> kFALSE
+  fRun->AddModule(Drc);
+  //-------------------------  DCH       -----------------
+  FairDetector *Dch = new PndDchDetector("DCH", kTRUE);
+  Dch->SetGeometryFileName("dch.root"); 
+  fRun->AddModule(Dch);
+  //-------------------------  EMC       -----------------
+  PndEmc *Emc = new PndEmc("EMC",kTRUE);
+  Emc->SetGeometryFileNameDouble("emc_module1245.dat","emc_module3new.root");
+  fRun->AddModule(Emc);  
+  //-------------------------  TOF       -----------------  
+  FairDetector *Tof = new PndTof("TOF",kTRUE);
+  Tof->SetGeometryFileName("tofbarrel.geo");
+  fRun->AddModule(Tof);
+  //-------------------------  MDT       -----------------
+  PndMdt *Muo = new PndMdt("MDT",kTRUE);
+  Muo->SetBarrel("torino");
+  Muo->SetEndcap("torino");
+  Muo->SetMuonFilter("torino");
+  Muo->SetMdtMagnet(kTRUE);
+  Muo->SetMdtMFIron(kTRUE);
+  fRun->AddModule(Muo);
+  //======================================================
+
+  // Create and Set Event Generator
+  //-------------------------------
+  
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+  
+  FairBoxGenerator *fBox = new FairBoxGenerator(pid, 2);
+  fBox->SetPRange(1.,1.);
+  fBox->SetThetaRange(theta,theta);
+  fBox->SetPhiRange(120.,240.);
+  fBox->SetCosTheta();
+  primGen->AddGenerator(fBox);  
+  
+  fRun->SetBeamMom(15);
+  //---------------------Create and Set the Field(s)---------- 
+  PndMultiField *fField= new PndMultiField("FULL");
+  fRun->SetField(fField);
+  
+  
+  fRun->SetStoreTraj(kTRUE); // toggle this for use with EVE
+  fRun->SetRadLenRegister(kFALSE); // toggle for material budget study
+  
+  fRun->Init();
+
+  // Fill the Parameter containers for this run
+  //-------------------------------------------
+  FairRuntimeDb *rtdb=fRun->GetRuntimeDb();
+  Bool_t kParameterMerged=kTRUE;
+  FairParRootFileIo* output=new FairParRootFileIo(kParameterMerged);
+  output->open(parOutput.Data(),"RECREATE");
+  rtdb->setOutput(output);
+
+  // Transport nEvents
+  // -----------------
+
+  fRun->Run(nEvents);
+
+  rtdb->saveOutput();
+  rtdb->print();
+
+
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+ 
+  cout << " Test passed" << endl;
+  cout << " All ok " << endl;
+  //exit(0);
+   
+}  
+  
