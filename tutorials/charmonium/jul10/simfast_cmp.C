d8fbf658295b9c863ed160ea0ab515d8b3b51c24
Klaus Goetzen <K.Goetzen@gsi.de>
2010-11-16 12:06:02 +0000

    Added simfast_cmp.C

---
+++ tutorials/charmonium/jul10/simfast_cmp.C
@@ -0,0 +1,60 @@
+// *******
+// Macro for running fast simulation
+// *******
+
+
+void simfast_cmp(TString part="psi(2S)", TString decfile="PSI2S.DEC",int nevts=1000,TString outfile="")
+{
+  TStopwatch timer;
+  timer.Start();
+  gDebug=0;
+
+  // Load basic libraries
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+
+  // Load this example libraries
+  //gSystem->Load("libRho");
+  gSystem->Load("libfsim");
+ 
+  if (outfile=="") outfile=decfile+".fast.root";
+
+  FairRunSim *fRun = new FairRunSim();
+  fRun->SetOutputFile(outfile.Data());
+  fRun->SetName("TGeant3");
+
+  // Create and Set Event Generator
+  //-------------------------------
+
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+
+  /// ... generate your signal on the fly
+  PndEvtGenDirect *EvtGen = new PndEvtGenDirect(part,decfile);
+  EvtGen->SetStoreTree();
+  primGen->AddGenerator(EvtGen);
+//  FairEvtGenGenerator* evtGen = new FairEvtGenGenerator(infile.Data());
+//  primGen->AddGenerator(evtGen);
+
+  primGen->DoTracking(kFALSE);
+
+  
+  //Setup the Fast Simulation Task
+  //-----------------------------
+  PndFastSim* fastSim = new PndFastSim();
+  fastSim->SetVerbosity(0);
+  fastSim->AddDetector("CmpDet");
+  fastSim->EnablePropagation();
+  
+  fRun->AddTask(fastSim);
+  
+  fRun->Init();
+  if (nevts==0) nevts=10;
+  fRun->Run(nevts);		
+  
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+}  
+  
