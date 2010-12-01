bf2593c53dc2ab2fd4add2fbe9c233dc45d03f84
Isabella Garzia <garzia@fe.infn.it>
2010-12-01 17:11:25 +0000

    digitalization

--- fts/FtsLinkDef.h
+++ fts/FtsLinkDef.h
@@ -14,6 +14,8 @@
 #pragma link C++ class PndFtsMapCreator+;
 #pragma link C++ class PndFtsTube+;
 #pragma link C++ class PndFtsSingleStraw+;
-//#pragma link C++ class PndFtsHitProducerRealFast+;
+#pragma link C++ class PndFtsHitProducerRealFast+;
+#pragma link C++ class PndFtsHitProducerIdeal+;
+#pragma link C++ class PndFtsHitProducerRealFull+;
 #endif
 
