849b323f1e952a24070f7cd00ca09ad3847f1a06
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2010-12-08 10:46:15 +0000

    Get rid of a debug output

--- pnddata/SdsData/PndSdsDigi.h
+++ pnddata/SdsData/PndSdsDigi.h
@@ -68,15 +68,12 @@ class PndSdsDigi : public FairTimeStamp
 		virtual void AddIndex(int index)
 		{
 			fIndex.push_back(index);
-			std::cout << "MCPointType: " << fMCPointType << std::endl;
 			AddLink(FairLink(fMCPointType, index));
 		}
     
 		virtual void AddIndex(std::vector<Int_t> index)
 		{
 			fIndex = index;
-
-			std::cout << "MCPointType: " << fMCPointType << std::endl;
 			AddLinks(FairMultiLinkedData(fMCPointType, index));
 		}
 		void SetCharge(double charge){fCharge = charge;}
