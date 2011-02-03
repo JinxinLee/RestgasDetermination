a7763997b79f4d7834e8c4a284735442a2040cd4
Felix Boehmer <the.flix@gmx.net>
2011-02-03 17:34:35 +0000

    updates to the macros

--- macro/tpc/TestBench/analyseFOPIReco.py
+++ macro/tpc/TestBench/analyseFOPIReco.py
@@ -24,9 +24,9 @@ files.sort()
 
 
 # -------------------- driftVel correction --------------------------------
-driftVelSim = 0.00055192   #(ArCo2 70/30 @ 250 V/cm)
-driftVelReal = 0.00284     #(ArCo2 90/10 @ 350 V/cm)
-velCorr = driftVelReal / driftVelSim
+#driftVelSim = 0.00055192   #(ArCo2 70/30 @ 250 V/cm)
+#driftVelReal = 0.00284     #(ArCo2 90/10 @ 350 V/cm)
+#velCorr = driftVelReal / driftVelSim
 
 # define cuts in Z
 cuts = (0,10,20,30,40,50,60)
@@ -36,8 +36,12 @@ resUs = dict([(i, ROOT.TH1D("StatsResX"+str(cuts[i]),
 resVIDs = dict([(i, ROOT.TH1D("StatsResXID"+str(cuts[i]), 
                               "Cosmic Residuals X w/o clusters of size 1 edges", 
                               500,-1,1)) for i in range(len(cuts))])
-resVs = dict([(i, ROOT.TH1D("StatsResY"+str(cuts[i]), 
-                            "Cosmic Residuals X'", 500,-1,1)) for i in range(len(cuts))])
+resVs = dict([(i, ROOT.TH1D("StatsResV"+str(cuts[i]), 
+                            "Cosmic Residuals V (X')", 500,-1,1)) for i in range(len(cuts))])
+
+cl2Ds = dict([(i, ROOT.TH1D("cl2Ds"+str(cuts[i]), 
+                            "Cluster Size Distributions as Function of Z", 50,0,50)) 
+              for i in range(len(cuts))])
 
 #resUVs = dict([(i, ROOT.TH1D("StatsResXY"+str(cuts[i]), 
 #                             "Cosmic Residuals X'Y'", 500,-1,1)) for i in range(len(cuts))])
@@ -46,12 +50,26 @@ resVsUs = dict([(i, ROOT.TH2D("StatsResXsYs"+str(cuts[i]),
                               500,-0.5,0.5)) for i in range(len(cuts))])
 
 clSize2D = ROOT.TH1D("clSize2D", "2D Cluster Size Distribution", 100,0,100)
+clSize2D.SetFillColor(ROOT.kBlack)
+clSize = ROOT.TH1D("clSize", "Cluster Size Distribution", 100,0,100)
+clSize.SetLineColor(ROOT.kRed+3)
+
 chi2prob = ROOT.TH1D("chi2prob", "ChiSqu Probability Distribution", 500,0,1)
 chi2raw = ROOT.TH1D("chi2raw", "ChiSqu2 / NDF", 200,0,10)
 
-for f in range(10) :
+diffT = ROOT.TF1("diffT","[0]*TMath::Sqrt(x+[1])",0,75)
+diffT.SetNpx(1000)
+diffT.SetParameter(0,0.0227*10000)
+diffT.SetParameter(1,15.)
+
+chi2func = ROOT.TF1("meh", "[0]*x*TMath::Exp(-2*x)",0,10)
+chi2func.SetParameter(0,2500)
+
+
+for f in range(25) :
     
     file = files[f]
+    #file = "/nfs/hicran/data/tpc/fopi/2010/decoded/runC_1703.reco.root"
     print(file)
     Rfile = ROOT.TFile(file, "read")
     tree = Rfile.Get("cbmsim")
@@ -82,7 +100,7 @@ for f in range(10) :
             v = mom.Cross(u)
             
             for p in range(numHits) :
-                z = tfs.GetHitPositionsZ().at(p) * velCorr
+                z = tfs.GetHitPositionsZ().at(p) 
                 x = tfs.GetHitPositionsX().at(p)
                 y = tfs.GetHitPositionsY().at(p)
                 
@@ -96,11 +114,15 @@ for f in range(10) :
                 resU = res.Dot(u)
                 resV = res.Dot(v)                
                 
+                cl2Dsize = tfs.Get2DClSizes().at(p)
+                clTotSize = tfs.GetClSizes().at(p)
+                clSize2D.Fill(cl2Dsize)
+                clSize.Fill(clTotSize)
+                
                 for i in range(len(cuts)) :
                     if z < cuts[i] :
-                        cl2Dsize = tfs.Get2DClSizes().at(p)
-                        clSize2D.Fill(cl2Dsize)
-                                                
+                        cl2Ds[i-1].Fill(cl2Dsize)
+                        
                         resUs[i-1].Fill(resU)
                         resVs[i-1].Fill(resV)
 #                        resUVs[i-1].Fill(math.sqrt(resX**2 + resY**2)) 
@@ -139,9 +161,14 @@ occZ.Write()
 
 diffV = ROOT.TGraph(6)
 diffV.SetName("diffX")
-diffV.SetTitle("X' Resolution as function of Drift Length")
+diffV.SetTitle("V(X') Resolution as function of Drift Length")
 diffVID = ROOT.TGraph(6)
 diffVID.SetMarkerColor(ROOT.kRed+2)
+bckgrShare = ROOT.TGraph(6)
+bckgrShare.SetName("bckgShare")
+bckgrShare.SetTitle("Share of background (from fits)")
+
+                            
 
 c4 = ROOT.TCanvas()
 c4.Divide(3,2)
@@ -170,9 +197,18 @@ for i in range(6) :
     resVs[i].Fit(fit, "+", "", -1,1)
     diffV.SetPoint(i,cuts[i]+5,fit.GetParameter(2)*10000)
     resVs[i].Write()
+    #calculate ratio of central and background integrals
+    ratio = fit.GetParameter(3)*fit.GetParameter(5)/(fit.GetParameter(0)*fit.GetParameter(2))
+    bckgrShare.SetPoint(i,cuts[i]+5,ratio)
+                             
 
-#c5 = ROOT.TCanvas()
-#c5.Divide(3,2)
+c5 = ROOT.TCanvas()
+c5.Divide(3,2)
+for i in range(6) :
+    c5.cd(i+1)
+    ROOT.gPad.SetLogy(1)
+    cl2Ds[i].Draw()
+
 
 c6 = ROOT.TCanvas()
 c6.Divide(3,2)
@@ -220,44 +256,51 @@ for i in range(6) :
     fit.SetParameter(2,testfit.GetParameter(2))
     fit.SetParLimits(2,testfit.GetParameter(2)*0.1, testfit.GetParameter(2)*20)
     
-    fit.SetParameter(3, 100)
-    fit.SetParLimits(3, 0, testfit.GetParameter(0)/4.)
+    fit.SetParameter(3, 10)
+    fit.SetParLimits(3, 0, testfit.GetParameter(0)/3.)
     fit.SetParameter(4, 0)
-    fit.SetParLimits(4, testfit.GetParameter(1)-0.08, testfit.GetParameter(1)+0.08)
-    fit.SetParameter(5, testfit.GetParameter(2))
-    fit.SetParLimits(5, testfit.GetParameter(2), testfit.GetParameter(2)*30)
+    fit.SetParLimits(4, testfit.GetParameter(1)-0.8, testfit.GetParameter(1)+0.8)
+    fit.SetParameter(5, testfit.GetParameter(2)*2)
+    fit.SetParLimits(5, testfit.GetParameter(2), testfit.GetParameter(2)*50)
+
     resVIDs[i].Fit(fit, "+", "", -1,1)
     diffVID.SetPoint(i,cuts[i]+5,fit.GetParameter(2)*10000)
     resVIDs[i].Write()
 
-
-    
-
-
-#c8 = ROOT.TCanvas()
-#c8.Divide(3,2)
-#for i in range(6) :
-#    c8.cd(i+1)
-#    resXYs[i].SetFillColor(ROOT.kAzure-8)
-#    resXYs[i].Draw()
-#    resXYs[i].Write()
+c8 = ROOT.TCanvas()
+bckgrShare.SetMarkerStyle(21)
+bckgrShare.GetYaxis().SetRangeUser(0,1)
+bckgrShare.GetXaxis().SetTitle("Drift Length Z (cm)")
+bckgrShare.Draw("ALP")
+bckgrShare.Write()
 
 c9 = ROOT.TCanvas()
-diffV.Draw("A*")
+diffV.SetMarkerStyle(21)
+diffV.Draw("ALP")
 diffV.GetXaxis().SetTitle("Drift Length Z (cm)")
 diffV.GetYaxis().SetTitle("X Resolution (#mu m)")
-diffVID.Draw("*")
+diffV.GetYaxis().SetRangeUser(0,2000)
+diffVID.SetMarkerStyle(21)
+diffVID.Draw("LP")
 diffV.Write()
 diffVID.Write()
+diffT.SetLineStyle(4)
+diffT.Draw("same")
+
 
 c10 = ROOT.TCanvas()
 clSize2D.Draw()
+clSize.Draw("same")
 
 c11 = ROOT.TCanvas()
+c11.cd()
+ROOT.gPad.SetLogy(1)
 chi2prob.Draw()
 
 c12 = ROOT.TCanvas()
 chi2raw.Draw()
+chi2func.SetNpx(1000)
+chi2func.Draw("same")
 
 input()
 
