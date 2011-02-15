7896564470d4f3997a72ffa08d6275e0529002d1
Ralf Kliemt <r.kliemt@gsi.de>
2011-02-15 14:21:57 +0000

    update plotting tools collection macro

--- macro/mvd/Tools.C
+++ macro/mvd/Tools.C
@@ -41,22 +41,26 @@
 #include <TStyle.h>
 #include <TROOT.h>
 
-
+void Tools()
+{
+  cout<<"pandaroot/macro/mvd/Tools.C loaded. Enjoy it."<<endl;
+  return;
+}
 
 void DrawText(Double_t posX = 0., Double_t posY = 0., const char* text = "",
-                     Double_t size=0.08, Int_t col=1
-//                      Int_t align=0, Double_t angle=0.,
-//                      Int_t font, Bool_t bNDC=kTRUE
-                     )
+              Double_t size=0.08, Int_t col=1
+              //                      Int_t align=0, Double_t angle=0.,
+              //                      Int_t font, Bool_t bNDC=kTRUE
+              )
 {
   // Drawing some TLatex text in a root canvas, overlaying the content
-
+  
   TLatex* pText=new TLatex(posX,posY,text);
   pText->SetNDC(kTRUE);
-//   pText->SetNDC(bNDC);
+  //   pText->SetNDC(bNDC);
   pText->SetTextColor(col); pText->SetTextSize(size);
-//   pText->SetTextAngle(angle);
-//   pText->SetTextFont(font); pText->SetTextAlign(align);
+  //   pText->SetTextAngle(angle);
+  //   pText->SetTextFont(font); pText->SetTextAlign(align);
   pText->Draw();
 }
 
@@ -64,41 +68,47 @@ void DrawNice2DHisto(TH2* h,const char* opt="",double range = 10.)
 {
   // Draw a 2D histo with the rainbow colors and the palette besides
   TString options = "colz"; options += opt;
-  if(gPad->GetLogz() == 0 && h->GetMaximum()<range) h->SetAxisRange(0.,range,"Z");
+  //if(gPad->GetLogz() == 0 && h->GetMaximum()<range) h->SetAxisRange(0.,range,"Z");
+  //else {
+    // we squeeze here to see better the mean value
+    range = 10*h->Integral()/(h->GetNbinsX()/h->GetNbinsY());
+    cout << "range "<<range<<"   max "<<h->GetMaximum();
+    if( range < h->GetMaximum() ) h->SetAxisRange(0.,range,"Z");
+  //}
   h->SetStats(kFALSE);
   h->SetTitleOffset(0.8,"T");
-  gPad->SetRightMargin(0.16);
+  gPad->SetRightMargin(0.32);
   h->DrawCopy(options.Data());
 }
 
 void set_nicer_2d_plot_style()
 {
-    const Int_t NRGBs = 5;
-    const Int_t NCont = 255;
-
-    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
-    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
-    Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
-    Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
-    TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
-//     gStyle->CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
-    gStyle->SetNumberContours(NCont);
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 99;//255;
+  
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  //     gStyle->CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  gStyle->SetNumberContours(NCont);
 }
 
 void BetterStatBox( TPad* pad ){
-/// Vorgehensweise:
-/// * Histogramme in ein TPad zeichnen (1. Histogramm: Draw(), ntes Histogramm:
-///   Draw("sames"))
-/// * die Update() Funktion vom Canvas aufrufen (die Statistikboxen werden erst
-///   erzeugt, wenn das Histogramm wirklich gezeichnet wird)
-/// * der Funktion das aktuelle TPad uebergeben
-///   (z.B.: TPad* p=(TPad*)gPad; BetterStatBox(p);)
-
+  /// Vorgehensweise:
+  /// * Histogramme in ein TPad zeichnen (1. Histogramm: Draw(), ntes Histogramm:
+  ///   Draw("sames"))
+  /// * die Update() Funktion vom Canvas aufrufen (die Statistikboxen werden erst
+  ///   erzeugt, wenn das Histogramm wirklich gezeichnet wird)
+  /// * der Funktion das aktuelle TPad uebergeben
+  ///   (z.B.: TPad* p=(TPad*)gPad; BetterStatBox(p);)
+  
   int entries  = pad->GetListOfPrimitives()->GetEntries();
   TString name = "";
   TPaveStats *st;
   int obj = 1;
-
+  
   for(int i=0; i<entries; i++){
     name = pad->GetListOfPrimitives()->At(i)->IsA()->GetName();
     if( name.Contains("TH1") ){
@@ -118,57 +128,58 @@ void BetterStatBox( TPad* pad ){
   }
 }
 
- TLegend* BuildLegend_THStack( THStack* stack, float x1, float y1, float x2, float y2 ){
-
-   TLegend* legend = new TLegend(x1,y1,x2,y2);
-   TList*   list = stack->GetHists();
-   TIter    next( list );
-   TH1*     hist;
-
-   while ( hist = (TH1*)next() ) legend->AddEntry(hist,"","F");
-
-   return legend;
- }
+TLegend* BuildLegend_THStack( THStack* stack, float x1, float y1, float x2, float y2 ){
+  
+  TLegend* legend = new TLegend(x1,y1,x2,y2);
+  TList*   list = stack->GetHists();
+  TIter    next( list );
+  TH1*     hist;
+  
+  while ( hist = (TH1*)next() ) legend->AddEntry(hist,"","F");
+  
+  return legend;
+}
 
 void LoadPandaStyle(void)
 {
-
-//--------------------------------------------------------------------------
-// File and Version Information:
-//      $Id: PBase.C,v 1.8 2006/09/22 12:04:13 kliemt Exp $
-//
-// Description:
-//      Initialization code executed at the start of a ROOT session.
-//      Set up the Panda style for approved plots.
-//
-// Environment:
-//      Software developed for the PANDA Detector at GSI, Darmstadt
-//
-// Author List:
-//      Sergey Ganzhur                Original Author
-//      Ralf Kliemt (2008)            Small adjustments for PandaRoot use
-//
-// Copyright Information:
-//     Copyright (C) 2001-2002        Ruhr Universitaet Bochum
-//
-//------------------------------------------------------------------------
-
+  
+  //--------------------------------------------------------------------------
+  // File and Version Information:
+  //      $Id: PBase.C,v 1.8 2006/09/22 12:04:13 kliemt Exp $
+  //
+  // Description:
+  //      Initialization code executed at the start of a ROOT session.
+  //      Set up the Panda style for approved plots.
+  //
+  // Environment:
+  //      Software developed for the PANDA Detector at GSI, Darmstadt
+  //
+  // Author List:
+  //      Sergey Ganzhur                Original Author
+  //      Ralf Kliemt (2008)            Small adjustments for PandaRoot use
+  //
+  // Copyright Information:
+  //     Copyright (C) 2001-2002        Ruhr Universitaet Bochum
+  //
+  //------------------------------------------------------------------------
+  
+  if(gStyle->GetName() == "PANDA") return;
   // use the 'plain' style for plots (white backgrounds, etc)
   //cout << "...using style 'Plain'" << endl;
   gROOT->SetStyle("Plain");
-
+  
   // Create the 'PANDA' style for approved plots. Note that this style may need
   // some fine tuning in your macro depending on what you are plotting, e.g.
   //
   //  gStyle->SetMarkerSize(0.75);  // use smaller markers in a histogram with many bins
   //  gStyle->SetTitleOffset(0.65,"y");  // bring y axis label closer to narrow values
-
-
+  
+  
   // Ralf Kliemt:
-  // Ok I changed a bit for myself here
-
+  // I changed a bit for myself here
+  
   TStyle *pandaStyle= new TStyle("PANDA","PANDA approved plots style");
-
+  
   // use plain black on white colors
   pandaStyle->SetFrameBorderMode(0);
   pandaStyle->SetCanvasBorderMode(0);
@@ -177,21 +188,21 @@ void LoadPandaStyle(void)
   pandaStyle->SetCanvasColor(0);
   pandaStyle->SetTitleColor(1);
   pandaStyle->SetStatColor(0);
-//   pandaStyle->SetFillColor(0);// conflict with 2D plots
-//   pandaStyle->SetPalette(1);
-
+  //   pandaStyle->SetFillColor(0);// conflict with 2D plots
+  //   pandaStyle->SetPalette(1);
+  
   // set the paper & margin sizes
   pandaStyle->SetPaperSize(20,26);
   pandaStyle->SetPadTopMargin(0.05);
   pandaStyle->SetPadRightMargin(0.05);
   pandaStyle->SetPadBottomMargin(0.16);
   pandaStyle->SetPadLeftMargin(0.12);
-
+  
   // use large Times-Roman fonts
-//   pandaStyle->SetTextFont(132);
-//   pandaStyle->SetLabelFont(132,"x");
-//   pandaStyle->SetLabelFont(132,"y");
-//   pandaStyle->SetLabelFont(132,"z");
+  //   pandaStyle->SetTextFont(132);
+  //   pandaStyle->SetLabelFont(132,"x");
+  //   pandaStyle->SetLabelFont(132,"y");
+  //   pandaStyle->SetLabelFont(132,"z");
   pandaStyle->SetTextFont(22);//changed to bold (R.K.)
   pandaStyle->SetTextSize(0.08);
   pandaStyle->SetLabelFont(22,"x");//changed to bold (R.K.)
@@ -203,29 +214,30 @@ void LoadPandaStyle(void)
   pandaStyle->SetTitleSize(0.06,"y");
   pandaStyle->SetLabelSize(0.05,"z");
   pandaStyle->SetTitleSize(0.06,"z");
-
+  
   // use bold lines and markers
   pandaStyle->SetMarkerStyle(8);
-  pandaStyle->SetHistLineWidth(2);//1.85);
+  pandaStyle->SetHistLineWidth(1.85);//1.85);
   pandaStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
-
+  
   // do not display any of the standard histogram decorations
   pandaStyle->SetOptTitle(1);
   pandaStyle->SetOptStat(1);
   pandaStyle->SetOptFit(0);
-
+  
   // put tick marks on top and RHS of plots
   pandaStyle->SetPadTickX(1);
   pandaStyle->SetPadTickY(1);
-
+  
   //R.K. avoid clumsy axis lables
   pandaStyle->SetNdivisions(509); // default root value is 510
-
+  
   //cout <<"    For approved plots use: gROOT->SetStyle(\"PANDA\");"<< endl;
-
+  
   pandaStyle->cd();
   gROOT->ForceStyle();
   set_nicer_2d_plot_style();//[R.K.] use nicer 2D plots
+  return;
 }//void PBase::LoadPandaStyle(void)
 
 
@@ -237,7 +249,7 @@ TH1D TransformHisto(TH2* h2, double min, double max)
    *  Created on: Feb 25, 2009
    *      Author: stockman
    */
-
+  
 	TH1D result("h1","h1", 1000, min, max);
 	int nbins = h2->GetNbinsX() * h2->GetNbinsY();
 	for (int i = 0; i < nbins; i++){
@@ -248,3 +260,66 @@ TH1D TransformHisto(TH2* h2, double min, double max)
 	}
 	return result;
 }
+
+
+plothistosfromfile(TString filename = "histos.root")
+{ // Plot all histograms into a ps file
+  // works with TH1, TH2, & TProfile
+  LoadPandaStyle();
+  TFile* file = new TFile(filename.Data());
+  if (!file) {cout<<"File \""<<filename.Data()<<"\" is not there..."<<endl;return;}
+  TCanvas* can = new TCanvas();
+  TString picname = filename;
+  picname.ReplaceAll(".root",".ps");
+  TString pic = picname + "["; // open empty ps
+  cout << "opening: " << pic.Data()<<endl;
+  can->Print(pic);
+  pic=picname;
+  
+  TList* list = file->GetListOfKeys();
+  if (!list) {cout<<"List not there..."<<endl;return;}
+  int divx=2, divy=2;
+  int padcount = 1;
+  TString keyclass="";
+  can->Divide(divx, divy);
+  for(int i=0;i<list->GetEntries();i++)
+  {
+    if(padcount > divx+divy)
+    {
+      can->Print(pic.Data());
+      can->Clear();
+      can->Divide(divx, divy);
+      padcount=1;
+    }
+    can->cd(padcount);
+    TKey* key = (TKey*)list->At(i);
+    keyclass = key->GetClassName();
+    //cout<<keyclass.Data()<<endl;
+    if(keyclass.Contains("TH1"))
+    {
+      //cout<<"try plotting a TH1"<<endl;
+      TH1* his = (TH1*)key->ReadObj();
+      his->Draw();
+    }else if(keyclass.Contains("TH2"))
+    {
+      //cout<<"try plotting a TH2"<<endl;
+      TH2* his2 = (TH2*)key->ReadObj();
+      //DrawNice2DHisto(his2);
+      his2->Draw("colz");
+    }else if(keyclass.Contains("TProfile"))
+    {
+      //cout<<"try plotting a TH2"<<endl;
+      TProfile* hpro = (TProfile*)key->ReadObj();
+      hpro->Draw();
+    } else continue;
+    
+    padcount++;
+  }
+  
+  can->Print(pic.Data());
+  pic = picname + "]"; // close ps
+  can->Print(pic.Data());
+  cout << "closed: " << pic.Data()<<endl;
+  return;
+}
+
