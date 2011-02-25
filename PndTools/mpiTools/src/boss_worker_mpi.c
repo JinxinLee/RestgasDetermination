f54c1b0827e87d8c05f36e6dc6221206f618ddca
Johan Messchendorp <j.g.messchendorp@rug.nl>
2011-02-25 12:08:17 +0000

    bug fix

--- PndTools/mpiTools/src/boss_worker_mpi.c
+++ PndTools/mpiTools/src/boss_worker_mpi.c
@@ -781,14 +781,14 @@ int DoJob(unsigned int *info, job_description *job, double *time_elapsed, double
 
       if (!(0==mkdir(scratch_dir,0777)))
 	{
-	  fprintf(stderr,"<W:%i> Error creating directory \"%s\": %s\n",rank,command,strerror(errno));
+	  fprintf(stderr,"<W:%i> Error creating directory \"%s\": %s\n",rank,scratch_dir,strerror(errno));
 	  fflush(stderr);
 	  return JOB_INPUT_ERROR;
 	}
       
       if (!(0==chdir(scratch_dir)))
 	{
-	  fprintf(stderr,"<W:%i> Error changing to directory \"%s\": %s\n",rank,command,strerror(errno));
+	  fprintf(stderr,"<W:%i> Error changing to directory \"%s\": %s\n",rank,scratch_dir,strerror(errno));
 	  fflush(stderr);
 	  return JOB_INPUT_ERROR;
 	}
@@ -796,7 +796,7 @@ int DoJob(unsigned int *info, job_description *job, double *time_elapsed, double
 
   if (verbose_mode || dummy_mode)
     {
-      printf("<W:%i> Creating and changing to directory %s\n",rank,command);
+      printf("<W:%i> Creating and changing to directory %s\n",rank,scratch_dir);
       fflush(stdout);
     }
 
