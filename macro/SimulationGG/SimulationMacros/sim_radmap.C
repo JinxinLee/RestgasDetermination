c6f8541b3cdb371ab9c9184db2a211b5a72b570c
Olaf Hartmann <ol.hartmann@GSI.DE>
2011-03-01 10:17:00 +0000

    RadMap simulation macro, updated version

--- macro/SimulationGG/SimulationMacros/sim_radmap.C
+++ macro/SimulationGG/SimulationMacros/sim_radmap.C
@@ -1,26 +1,29 @@
-// Macro 
-// It creates a geant simulation file
-void sim_olaf(TString simdatei, TString input, TString param, Int_t nEvents,  Char_t TransportModel[] = "TGeant4", UInt_t seed=0)
+// Macro sim_radmap
+// It creates a geant simulation file with the RadMap branch
+void sim(TString output, TString input, Int_t nEvents,  Char_t TransportModel[] = "TGeant3", UInt_t seed=0)
 {
+  //   activate TMemStat info file (ROOT >=5.28)
+  //   TMemStat mm("gnubuiltin");
+  
   gRandom->SetSeed(seed);
   TStopwatch timer;
   timer.Start();
   gDebug=0;
-// Load basic libraries
-      gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
-      rootlogon();
-
+  // Load basic libraries
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
+  rootlogon();
+  
   FairRunSim *fRun = new FairRunSim();
   
   // Set the number of events
- // Int_t nEvents = n; 
-  
+  // Int_t nEvents = n; 
+
   // set the MC version used
   // ------------------------
   
   fRun->SetName(TransportModel);
   
-  fRun->SetOutputFile(simdatei);
+  fRun->SetOutputFile(output);
   
   // Set Material file Name
   //-----------------------
@@ -91,67 +94,39 @@ void sim_olaf(TString simdatei, TString input, TString param, Int_t nEvents,  Ch
   
   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
   fRun->SetGenerator(primGen);
-
-  // EvtGen Generator: 
-
+  
   //  FairEvtGenGenerator* evtGen = new FairEvtGenGenerator(input);
   //  primGen->AddGenerator(evtGen);  
-
-//  PndFlukaGenerator* flugen = new PndFlukaGenerator(input);
-//  primGen->AddGenerator(flugen);
-   
-    PndDpmGenerator* dpmGen = new PndDpmGenerator(input);
-    primGen->AddGenerator(dpmGen);
-
-/*
-  FairBoxGenerator* boxGen = new FairBoxGenerator(-211, 1); // 13 = muon; 1 = multipl.
-  boxGen->SetPRange(.4,.4); // GeV/c //setPRange vs setPtRange
-  boxGen->SetPhiRange(0, 360); // Azimuth angle range [degree]
-  boxGen->SetThetaRange(0.001, 180); // Polar angle in lab system range [degree]
-  boxGen->SetCosTheta();//uniform generation on all the solid angle(default)
-  // boxGen->SetXYZ(0., 0.37, 0.); 
-  primGen->AddGenerator(boxGen);
-*/ 
-
-  fRun->SetStoreTraj(kFALSE); // to store particle trajectories 
-  //  fRun->SetRadLenRegister(kTRUE); // radiation length manager
-  fRun->SetRadMapRegister(kTRUE); // radiation map manager
-
-  //magnetic field: no field when commented put
-  //    PndFieldConst *fMagField=new PndFieldConst();
-  //    fMagField->SetField(0.,0.,20.); // values are in kG
-  //    fMagField->SetFieldRegion(-50, 50,-50, 50, -100, 100);// values are in cm (xmin,xmax,ymin,ymax,zmin,zmax)
-  //    fRun->SetField(fMagField);
   
-//  fRun->SetBeamMom(6.2);
+  //  PndFlukaGenerator* flugen = new PndFlukaGenerator(input);
+  //  primGen->AddGenerator(flugen);
+  
+  PndDpmGenerator* dpmGen = new PndDpmGenerator(input);
+  primGen->AddGenerator(dpmGen);
+  
+  /*
+    FairBoxGenerator* boxGen = new FairBoxGenerator(-211, 1); // 13 = muon; 1 = multipl.
+    boxGen->SetPRange(.4,.4); // GeV/c //setPRange vs setPtRange
+    boxGen->SetPhiRange(0, 360); // Azimuth angle range [degree]
+    boxGen->SetThetaRange(0.001, 180); // Polar angle in lab system range [degree]
+    boxGen->SetCosTheta();//uniform generation on all the solid angle(default)
+    // boxGen->SetXYZ(0., 0.37, 0.); 
+    primGen->AddGenerator(boxGen);
+  */ 
+  
+  fRun->SetStoreTraj(kFALSE); // to store particle trajectories 
+  
+  fRun->SetRadMapRegister(kTRUE); // radiation map manager
+  
+  //  fRun->SetBeamMom(6.2);
   fRun->SetBeamMom(15.0);
   PndMultiField *fField= new PndMultiField("FULL");
   fRun->SetField(fField);
 
   fRun->Init();
 
-  // Fill the Parameter containers for this run
-  //-------------------------------------------
-  
-  /*
-  FairRuntimeDb *rtdb=fRun->GetRuntimeDb();
-  Bool_t kParameterMerged=kTRUE;
-  FairParRootFileIo* output=new FairParRootFileIo(kParameterMerged);
-  output->open(param);
-  rtdb->setOutput(output);
-  rtdb->saveOutput();
-  rtdb->print();
-  */
-
-  // Transport nEvents
-  // -----------------
-  
-  gGeoManager->SetVisLevel(10);
+  //  gGeoManager->SetVisLevel(10);
   //  gGeoManager->GetMasterVolume()->Draw("ogl");
-
-  // test
-  //  TH3F* dose1 = new TH3F("dose1","dose1",100,-100.,100.,100,0,.02,100,0.,1000.);
-
 
   fRun->Run(nEvents);
    
