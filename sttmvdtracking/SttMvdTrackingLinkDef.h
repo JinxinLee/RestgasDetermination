c27243284fadc900b7c92a4334a5d43e3ef0f742
Lia Lavezzi <lia.lavezzi@gmail.com>
2011-03-09 15:32:23 +0000

    SttLinkDef and CMakeLists

--- sttmvdtracking/SttMvdTrackingLinkDef.h
+++ sttmvdtracking/SttMvdTrackingLinkDef.h
@@ -4,6 +4,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class PndSttMvdTracking+;           
+// #pragma link C++ class PndSttMvdGemTracking+;           
 
 
 #endif
