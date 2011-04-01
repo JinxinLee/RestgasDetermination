13ded6bc59a0aa8afd785651bdd27d794b6a4a80
Johannes Rauch <Jojo1987>
2011-04-01 12:55:47 +0000

    added ClustVis

--- tpc/tpcLinkDef.h
+++ tpc/tpcLinkDef.h
@@ -13,6 +13,7 @@
 #pragma link C++ class PndTpcDigiPar+;
 #pragma link C++ class PndTpcContFact+;
 #pragma link C++ class PndTpcClusterizerTask+;
+#pragma link C++ class PndTpcClustVis+;
 #pragma link C++ class PndTpcDriftTask+;
 //#pragma link C++ class PndTpcRadialDriftTask+;
 #pragma link C++ class PndTpcGemTask+;
