4304994c20bb2c160429781660be4bb9c0327132
Felix Boehmer <the.flix@gmx.net>
2011-04-05 15:49:05 +0000

    loads independent TpcEve library now

--- macro/tpc/TestBench/runClusterVisualisation.C
+++ macro/tpc/TestBench/runClusterVisualisation.C
@@ -3,6 +3,14 @@ void runClusterVisualisation(TString filename, TString digifile="")
   // ----  Load libraries   -------------------------------------------------
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
   rootlogon();
+
+  // load TpcEve Library
+  if (TString(gSystem->DynamicPathName("libTpcEve", kTRUE)) != TString(""))
+    gSystem->Load("libTpcEve");
+  else{
+    std::cerr<<"Library TpcEve not found. Make sure your ROOT is compiled with EVE"<<std::endl;
+    return;
+  }
   
   TFile* file = new TFile(filename);
   TTree* tree = (TTree*)file->Get("cbmsim");
