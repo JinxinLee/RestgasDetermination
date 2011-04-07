3e925b174f1d341e6b053ad0b24afd699a7d1eba
Sebastian Neubert <sneubert>
2011-04-07 17:59:30 +0000

    added digiMapper intialization

--- macro/tpc/TestBench/runClusterVisualisation.C
+++ macro/tpc/TestBench/runClusterVisualisation.C
@@ -1,4 +1,4 @@
-void runClusterVisualisation(TString filename, double DriftField, TString digifile="") 
+void runClusterVisualisation(TString filename, double DriftField, int paramSet=0, TString digifile="") 
 {
   // ----  Load libraries   -------------------------------------------------
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
@@ -24,7 +24,35 @@ void runClusterVisualisation(TString filename, double DriftField, TString digifi
   PndTpcClustVis* clustVis = PndTpcClustVis::getInstance();
   clustVis->reset();
   clustVis->setTree(tree);
-  clustVis->initDigimapper(DriftField);
+
+  TString gasfile,padplanefile,padshapefile;
+  double gain,spread,zGem,samplingFreq,wallclock;
+  TString basedir = gSystem->Getenv("VMCWORKDIR");
+  if(paramSet==0){ // standard testBench Settings
+    gasfile=basedir+"/tpc/TestBench/ARGON-89.635_CO2-10.365_B0.3_PRES1013.asc";
+    padplanefile=basedir+"/tpc/TestBench/padPlane_FOPI.dat";
+    padshapefile=basedir+"/tpc/TestBench/TBhexa_pads.dat";
+    gain=4000;
+    spread=0.02;
+    zGem=-0.2.;
+    samplingFreq=20;
+    wallclock=-6000;
+  }
+   if(paramSet==1){ // standard PANDA SIM Settings
+    gasfile=basedir+"/tpc/NEON-90_CO2-10_B2_PRES1013.asc";
+    padplanefile=basedir+"/tpc/pndhexplane0.15.dat";
+    padshapefile=basedir+"/tpc/Hexagons0.15.dat";
+    gain=4000;
+    spread=0.02;
+    zGem=-39.5;
+    samplingFreq=40;
+    wallclock=-200000;
+    DriftField=400;
+  }
+
+
+   clustVis->initDigimapper(DriftField,gain,spread,zGem,samplingFreq,wallclock,
+			    gasfile.Data(),padplanefile.Data(),padshapefile.Data());
   clustVis->setOptions("DCR");
   clustVis->gotoEvent(1);
   clustVis->open();
