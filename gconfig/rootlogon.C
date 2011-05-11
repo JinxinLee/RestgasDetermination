605e0585d9081db7ab97b4dc684021c500193068
Ralf Kliemt <r.kliemt@gsi.de>
2011-05-11 13:24:08 +0000

    rootlogon: small order change

--- gconfig/rootlogon.C
+++ gconfig/rootlogon.C
@@ -21,8 +21,8 @@ rootlogon()
   if(isLibrary("libPndBase"))gSystem->Load("libPndBase");
   if(isLibrary("libGlobalTasks"))gSystem->Load("libGlobalTasks");
   if(isLibrary("libTrkBase"))gSystem->Load("libTrkBase");
-  if(isLibrary("libgeneralTools"))gSystem->Load("libgeneralTools");
   if(isLibrary("libPndData"))gSystem->Load("libPndData");
+  if(isLibrary("libgeneralTools"))gSystem->Load("libgeneralTools");
   if(isLibrary("libbuffers"))gSystem->Load("libbuffers");
   if(isLibrary("libField"))gSystem->Load("libField");
   if(isLibrary("libPassive"))gSystem->Load("libPassive");
