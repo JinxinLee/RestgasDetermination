326bcf3821f99840bd572c45309e8d42ed510b8c
Felix Boehmer <the.flix@gmx.net>
2011-05-16 13:21:27 +0000

    New geometry in tpc SIM macro

--- macro/run/tdrct/run_sim_tpccombi_dpm.C
+++ macro/run/tdrct/run_sim_tpccombi_dpm.C
@@ -62,7 +62,8 @@ run_sim_tpccombi_dpm(Int_t nEvents=10, Float_t mom = 5., Int_t mode =1, UInt_t s
   fRun->AddModule(Pipe);
 
   PndTpcDetector *Tpc = new PndTpcDetector("TPC", kTRUE);
-  Tpc->SetGeometryFileName("tpc.geo");
+  Tpc->SetGeometryFileName("TPC_V1.0.root");    //new ROOT geometry
+  //Tpc->SetGeometryFileName("tpc.geo");        //old geometry
   if(mcMode=="TGeant3")  Tpc->SetAliMC();
   fRun->AddModule(Tpc);
 
