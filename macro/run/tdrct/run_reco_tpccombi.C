8e7bad28a9722fde5bbd20638c4653629d30d0da
Felix Boehmer <the.flix@gmx.net>
2011-05-20 15:17:08 +0000

    First running version of TPC reco for testing

--- macro/run/tdrct/run_reco_tpccombi.C
+++ macro/run/tdrct/run_reco_tpccombi.C
@@ -51,27 +51,55 @@
   rtdb->setFirstInput(parInput1);
   rtdb->setSecondInput(parIo1);
   PndGeoHandling* geoH = PndGeoHandling::Instance();
-  // ------------------------------------------------------------------------
-  // -----   LHETRACK  ---------------------------------
-  
-  PndLheHitsMaker* trackMS = new PndLheHitsMaker("Tracking routine");
-  trackMS->SetTpcMode(2);  // 0 OFF, 1 TpcPoint, 2 TpcCluster // TpcPoint smearing [cm], if negative no smearing
-  trackMS->SetMvdMode(2);  // 0 OFF, 1 MVDPoint, 2 MVDHit     // MVDPoint smearing [cm], if negative no smearing
-  trackMS->SetGemMode(2);  // 0 OFF, 1 GEMPoint, 2 GEMHit     // GEMPoint smearing [cm], if negative no smearing
-  fRun->AddTask(trackMS);
-  
-  PndLheTrackFinder* trackFinder    = new PndLheTrackFinder();
-  //PndLheTrackFinderIdeal* trackFinder    = new PndLheTrackFinderIdeal();
-  fRun->AddTask(trackFinder);
-  
-  PndLheTrackFitter* trackFitter    = new PndLheTrackFitter("fitting");
-  fRun->AddTask(trackFitter);
-  
-  PndRecoKalmanTask* recoKalman = new PndRecoKalmanTask();
-  recoKalman->SetTrackInBranchName("LheTrack");
-  recoKalman->SetTrackOutBranchName("LheGenTrack");
-  //recoKalman->SetNumIterations(3);
-  fRun->AddTask(recoKalman);
+
+  bool SimpleClustering=true;
+
+  // ------- RECO procedure ------------------------------------------------
+ 
+  PndTpcClusterFinderTask* tpcCF = new PndTpcClusterFinderTask();
+  //tpcCF->SetDigiPersistence(); // keep reference to digis in clusters
+  tpcCF->SetPersistence(); // keep Clusters
+  tpcCF->timeslice(4); //in samples
+  tpcCF->SetThreshold(1);
+  tpcCF->SetSingleDigiClusterAmpCut(0.);
+  tpcCF->SetClusterAmpCut(0.); // cut on mean digi amplitude
+  tpcCF->SetErrorPars(600.,400.);
+  tpcCF->SetSimpleClustering(); // use PndTpcClusterFinderSimple
+  fRun->AddTask(tpcCF);
+
+  PndTpcRiemannTrackingTask* tpcSPR = new PndTpcRiemannTrackingTask();
+  tpcSPR->SetSortingParameters(
+                   true, // false: sort only according to _sorting (see next argument); true: use internal sorting when adding hits to trackcands
+                   3,    // -1: no sorting, 0: sort Clusters by X, 1: Y, 2: Z, 3: R, 4: distance to origin
+                   0.); // z-position of interaction point (for sorting 4)
+  tpcSPR->SetTrkFinderParameters(
+                   1.9,  // proximity cut in 3D
+                   0.1, // proximity cut on rieman sphere
+                   0.04, // distance to plane cut
+                   0.2,  // szcut
+                   4);   // minimum hits for plane & sz-fit
+  tpcSPR->SetMergeTracks();
+  tpcSPR->SetTrkMergerParameters(
+                   2.2,  // proximity cut
+                   0.33,  // sz cut
+                   0.025);// plane cut (RMS)
+  tpcSPR->SetRiemannScale(); // sets riemannscale for the prototype;
+  tpcSPR->SetPersistence();
+  //tpcSPR->SetStoreHistograms(PROutFile); //
+  //tpcSPR->WriteHistograms(PROutFile);
+  fRun->AddTask(tpcSPR);
+
+
+  KalmanTask* kalman =new KalmanTask();
+  kalman->SetPersistence();
+  kalman->SetNumIterations(3); // number of fitting iterations (back and forth)
+  fRun->AddTask(kalman);
+
+  PndTpcResidualTask* Res = new PndTpcResidualTask();
+  Res->SetPersistence();
+  //SLres->SetClusterBranchName("PndTpcCluster_cut");
+  fRun->AddTask(Res);
+    
  
   // -----   Intialise and run   --------------------------------------------
   PndEmcMapper::Init(6);
