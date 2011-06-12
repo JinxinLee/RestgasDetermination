bd589d61d16f06d69425ff256b16b69a27cc9450
Johannes Rauch <Jojo1987>
2011-06-12 14:40:41 +0000

    took out debug output

--- tpc/tpcreco/PndTpcRiemannTrackingTask.cxx
+++ tpc/tpcreco/PndTpcRiemannTrackingTask.cxx
@@ -329,9 +329,6 @@ PndTpcRiemannTrackingTask::Exec(Option_t* opt)
     int nhits=trk->getNumHits();
     
     // store PndTpcRiemannTracks in output array
-    std::cout<<"DEBUG: "<<std::endl;
-    std::cout<<"trackArray size: "<<_riemannTrackArray->GetEntriesFast()<<std::endl;
-    
     (*_riemannTrackArray)[_riemannTrackArray->GetEntriesFast()] = trk;
     for(unsigned int ih=0;ih<nhits;++ih){
       PndTpcRiemannHit* hit=trk->getHit(ih);
