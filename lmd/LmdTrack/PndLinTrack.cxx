4c10110da43d3a63c0aa1177d7e7099ba0dc5e8d
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2011-06-24 13:10:21 +0000

    Construction was changed to more logical way. Adding of parameters errors in method of class

--- lmd/LmdTrack/PndLinTrack.cxx
+++ lmd/LmdTrack/PndLinTrack.cxx
@@ -10,27 +10,88 @@
 #include "TGeoNode.h"
 #include "TGeoVolume.h"
 #include "TGeoMatrix.h"
-
+#include "TMath.h"
 
 // -----   Default constructor   -------------------------------------------
 PndLinTrack::PndLinTrack() {
+  // std::cout<<"/lmd/LmdTrack/PndLinTrack()"<<std::endl;
+  //fCovMatrix = new TMatrixDSym(6);
 }
 // -------------------------------------------------------------------------
 
 
 // -----   Standard constructor   ------------------------------------------
-PndLinTrack::PndLinTrack(TString detName, Double_t z0, Double_t p0, Double_t p1, Double_t p2, Double_t p3, 
-			 Double_t p0err, Double_t p1err, Double_t p2err, Double_t p3err, 
+PndLinTrack::PndLinTrack(TString detName, Double_t p0, Double_t p1, Double_t p2, Double_t p3, Double_t p4, Double_t p5, 
 			 Double_t chi, Int_t first, Int_t last, Int_t cand){
   fDetName = detName;
-  fP0 = p0; fP1 = p1; fP2 = p2; fP3 = p3;
+  fP0 = p0; fP1 = p1; fP2 = p2; fP3 = p3; fP4 = p4; fP5 = p5;
   fChiSq = chi;
   fFirst = first; fLast = last;
   fCandId = cand;
-  fP0err = p0err; fP1err = p1err; fP2err = p2err; fP3err = p3err;
-  fz0 = z0;
+  // fP0err = p0err; fP1err = p1err; fP2err = p2err; fP3err = p3err;
+  //fz0 = z0;
+  // fCovMatrix = NULL;
+  fCovMatrix.ResizeTo(6,6);
+  // fCovMatrix = TMatrixDSym(6);
 }
 
+TVector3 PndLinTrack::GetDirectionVec(){
+  double l = 1./sqrt(1+fP1*fP1+fP3*fP3);
+  return TVector3(fP1*l,fP3*l,l);
+}
+
+TVector3 PndLinTrack::GetStartErrVec(){
+  return TVector3(TMath::Sqrt((fCovMatrix)(0,0)),TMath::Sqrt((fCovMatrix)(2,2)),TMath::Sqrt((fCovMatrix)(4,4)));
+}
+
+TVector3 PndLinTrack::GetDirectionErrVec(){
+ double l = 1./sqrt(1+fP1*fP1+fP3*fP3);
+ // std::cout<<"l = "<<l<<" pow(l,3) = "<<pow(l,3)<<" p1="<<fP1<<" p3="<<fP3<<std::endl;
+ double ddx_dp1=(1+fP3*fP3);
+ double ddx_dp3=fP1*fP3;
+ double err_dx = pow(l,3)*sqrt(ddx_dp1*ddx_dp1*((fCovMatrix)(1,1))+ddx_dp3*ddx_dp3*((fCovMatrix)(3,3))
+		      +fabs(2*ddx_dp1*ddx_dp3*((fCovMatrix)(1,3))));
+ // std::cout<<"err_dx = "<<ddx_dp1*ddx_dp1*((fCovMatrix)(1,1))<<" + "<<ddx_dp3*ddx_dp3*((fCovMatrix)(3,3))
+ // 	  <<" + "<<2*ddx_dp1*ddx_dp3*((fCovMatrix)(1,3))<<" = "<<err_dx<<std::endl;
+
+ double ddy_dp1=fP1*fP3;
+ double ddy_dp3=(1+fP1*fP1);
+ double err_dy =  pow(l,3)*sqrt(ddy_dp1*ddy_dp1*((fCovMatrix)(1,1))+ddy_dp3*ddy_dp3*((fCovMatrix)(3,3))
+		      +fabs(2*ddy_dp1*ddy_dp3*fabs((fCovMatrix)(1,3))));
+
+ double ddz_dp1=fP1;
+ double ddz_dp3=fP3;
+ double err_dz =pow(l,3)*sqrt(ddz_dp1*ddz_dp1*((fCovMatrix)(1,1))+ddz_dp3*ddz_dp3*((fCovMatrix)(3,3))
+		      +fabs(2*ddz_dp1*ddz_dp3*fabs((fCovMatrix)(1,3))));
+ return TVector3(err_dx,err_dy,err_dz);
+}
+
+void PndLinTrack::GetParErr(Double_t* errpar){ 
+  // errpar[0] = TMath::Sqrt(fabs((fCovMatrix)(0,0))+2*fabs((fCovMatrix)(0,1))+2*fabs((fCovMatrix)(0,2))
+  // 			  +2*fabs((fCovMatrix)(0,3))+2*fabs((fCovMatrix)(0,4))+2*fabs((fCovMatrix)(0,5)));
+
+  // errpar[1] = TMath::Sqrt(fabs((fCovMatrix)(1,1))+2*fabs((fCovMatrix)(0,1))+2*fabs((fCovMatrix)(1,2))
+  // 			  +2*fabs((fCovMatrix)(1,3))+2*fabs((fCovMatrix)(1,4))+2*fabs((fCovMatrix)(1,5)));
+
+  // errpar[2] = TMath::Sqrt(fabs((fCovMatrix)(2,2))+2*fabs((fCovMatrix)(0,2))+2*fabs((fCovMatrix)(1,2))
+  // 			  +2*fabs((fCovMatrix)(2,3))+2*fabs((fCovMatrix)(2,4))+2*fabs((fCovMatrix)(2,5)));
+
+  // errpar[3] = TMath::Sqrt(fabs((fCovMatrix)(3,3))+2*fabs((fCovMatrix)(0,3))+2*fabs((fCovMatrix)(1,3))
+  // 			  +2*fabs((fCovMatrix)(2,3))+2*fabs((fCovMatrix)(3,4))+2*fabs((fCovMatrix)(3,5)));
+  
+  // errpar[4] = TMath::Sqrt(fabs((fCovMatrix)(4,4))+2*fabs((fCovMatrix)(0,4))+2*fabs((fCovMatrix)(1,4))
+  // 			  +2*fabs((fCovMatrix)(2,4))+2*fabs((fCovMatrix)(3,4))+2*fabs((fCovMatrix)(4,5)));
+
+  // errpar[5] = TMath::Sqrt(fabs((fCovMatrix)(5,5))+2*fabs((fCovMatrix)(0,5))+2*fabs((fCovMatrix)(1,5))
+  // 			  +2*fabs((fCovMatrix)(2,5))+2*fabs((fCovMatrix)(3,5))+2*fabs((fCovMatrix)(4,5)));
+
+  errpar[0] = TMath::Sqrt((fCovMatrix)(0,0));
+  errpar[1] = TMath::Sqrt((fCovMatrix)(1,1));
+  errpar[2] = TMath::Sqrt((fCovMatrix)(2,2));
+  errpar[3] = TMath::Sqrt((fCovMatrix)(3,3));
+  errpar[4]=  TMath::Sqrt((fCovMatrix)(4,4));
+  errpar[5]=  TMath::Sqrt((fCovMatrix)(5,5));
+}
 
 // -----   Destructor   ----------------------------------------------------
 PndLinTrack::~PndLinTrack() {}
--- lmd/LmdTrack/PndLinTrack.h
+++ lmd/LmdTrack/PndLinTrack.h
@@ -1,6 +1,7 @@
 // -------------------------------------------------------------------------
 // -----                      PndLinTrack header file                  -----
 // -----                       Created by M. Michel                    -----
+// -----                       Modifided by A.Karavdina                -----
 // -------------------------------------------------------------------------
 
 
@@ -17,6 +18,7 @@
 #include "TVector3.h"
 #include "TString.h"
 #include "TMatrixTSym.h"
+#include "TMatrixDSym.h"
 
 #include <stdio.h>
 #include <iostream>
@@ -29,7 +31,6 @@ class PndLinTrack : public TObject
   /** Default constructor (not for use) **/
   PndLinTrack();
 
-
   /** Standard constructor with all paramneters 
    *@param detName Detector Name
    *@param pX      Parameter of Fit [cm]
@@ -39,8 +40,10 @@ class PndLinTrack : public TObject
    *@param cand    Id of TrackCandidate
    **/
 
-  PndLinTrack(TString detName, Double_t z0, Double_t p0, Double_t p1, Double_t p2, Double_t p3, 
-	      Double_t p0err, Double_t p1err, Double_t p2err, Double_t p3err, 
+  /* PndLinTrack(TString detName, Double_t z0, Double_t p0, Double_t p1, Double_t p2, Double_t p3,  */
+  /* 	      Double_t p0err, Double_t p1err, Double_t p2err, Double_t p3err,  */
+  /* 	      Double_t chi, Int_t first, Int_t last, Int_t cand); */
+  PndLinTrack(TString detName, Double_t p0, Double_t p1, Double_t p2, Double_t p3, Double_t p4,Double_t p5, 
 	      Double_t chi, Int_t first, Int_t last, Int_t cand);
 
   /** Destructor **/
@@ -50,26 +53,38 @@ class PndLinTrack : public TObject
   /** Accessors **/
   
   void SetDetName(TString name)  { fDetName   = name;}
-  
+  void SetCovarianceMatrix(TMatrixDSym covmatrix){
+    for(size_t i=0;i<6;i++){
+      for(size_t j=0;j<6;j++){
+  	fCovMatrix[i][j]= covmatrix[i][j];
+      }
+    }
+  }
+ 
   TString 	GetDetName()		const { return fDetName;}
-  void		GetPar(Double_t* par)	const { par[0]=fP0;par[1]=fP1;par[2]=fP2;par[3]=fP3;}
+  void		GetPar(Double_t* par)	const { par[0]=fP0;par[1]=fP1;par[2]=fP2;par[3]=fP3;par[4]=fP4;par[5]=fP5;}
   /* TVector3	GetStartVec()		const { return TVector3(fP0,fP2,0.);} */
-  TVector3	GetStartVec()		const { return TVector3(fP0,fP2,fz0);}
-  TVector3	GetDirectionVec()	const { return TVector3(fP1,fP3,1.);}
-  TVector3      GetParErr(Double_t* errpar) 
-    const { errpar[0]=fP0err;errpar[1]=fP1err;errpar[2]=fP2err;errpar[3]=fP3err;}
+  TVector3	GetStartVec()		const { return TVector3(fP0,fP2,fP4);}
+  //  TVector3	GetDirectionVec()	const { return TVector3(fP1,fP3,fP5);}
+  TVector3	GetDirectionVec();
+  TVector3	GetStartErrVec();
+  TVector3	GetDirectionErrVec();
+  /* TVector3      GetParErr(Double_t* errpar)  */
+  /*   const { errpar[0]=fP0err;errpar[1]=fP1err;errpar[2]=fP2err;errpar[3]=fP3err;errpar[4]=fP4err;} */
   Double_t 	GetChiSquare()		const { return fChiSq;}
   Int_t		GetFirstHit()		const { return fFirst;}
   Int_t		GetLastHit()		const { return fLast;}
   Int_t		GetTCandID()		const { return fCandId;}
-
+ 
+  void GetParErr(Double_t* errpar);
+  TMatrixDSym GetCovarianceMatrix() {return fCovMatrix;}
  private:
   TString fDetName;  // Detector name
-  Double_t fz0;// z-position of first LMD plane
-  Double_t fP0, fP1, fP2, fP3; // fit-parameter
-  //start (P0, P2, 0), direction (P1, P3, 1) straight line
-
-  Double_t fP0err, fP1err, fP2err, fP3err; // errors of parameters
+  //  Double_t fz0;// z-position of first LMD plane
+  Double_t fP0, fP1, fP2, fP3, fP4, fP5; // fit-parameter
+  //start (P0, P2, P4), direction (P1, P3, P5) straight line
+  TMatrixDSym fCovMatrix;
+  // Double_t fP0err, fP1err, fP2err, fP3err, fP4err; // errors of parameters
   Double_t fChiSq; // Chi-Square of Fit
   Int_t fFirst, fLast;
   Int_t fCandId; //TrackCand id
