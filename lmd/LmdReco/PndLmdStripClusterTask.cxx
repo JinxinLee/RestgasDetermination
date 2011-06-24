1ff418afb53ca8208b02750f9db174637ca777d0
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2011-06-24 13:18:13 +0000

    minor changes

--- lmd/LmdReco/PndLmdStripClusterTask.cxx
+++ lmd/LmdReco/PndLmdStripClusterTask.cxx
@@ -181,30 +181,52 @@ TVector3 PndLmdStripClusterTask::AddMSErr(TVector3 hpos, TVector3 hposerr){
   Double_t Ebeam = TMath::Hypot(fPbeam,fMass);
   TLorentzVector LorMom(0, 0, fPbeam, Ebeam);
   Double_t beta = LorMom.Beta();
-  //  cout<<"beta = "<<beta<<endl;
+  //  cout<<"beta = "<<beta<<" for fPbeam="<<fPbeam<<" GeV/c"<<endl;
   Double_t X = 0.015;
+  //  Double_t X = 0.0012; //TEST
+  // Double_t X = 0.03; //TEST
   Double_t X0 = 9.36;
   Double_t thetaMS = 13.6*1e-3*TMath::Sqrt(X/X0)*(1+0.038*TMath::Log(X/X0))/(beta*fPbeam);
-  // Double_t thetaMS = 13.6*1e-3*TMath::Sqrt(X/X0)/(beta*fPbeam); //TEST!!!
+  //  Double_t thetaMS = 13.6*1e-3*TMath::Power(X/X0,0.555)/(beta*fPbeam);
+  // cout<<"thetaMS [OLD] = "<<thetaMSold<<endl;
+  // Double_t thetaMSnew = 13.6*1e-3*TMath::Sqrt(X/X0)/(beta*fPbeam); //TEST!!!
+  // cout<<"thetaMS [NEW] = "<<thetaMSnew<<endl;
+  // Double_t thetaMS = thetaMSnew;
   //-----------------------------------------------------------
 
   //TO DO: use parameters from geometry info for LUMI
-  // Double_t thetaMS = 5.44e-04/fPbeam;
-  // cout<<"thetaMS = "<<thetaMS<<endl;
   Double_t d = 10; 
+
   double xerr,yerr;
   double zhit = hpos.Z();
-  const double Z0 = 1100.;
-  int num = (zhit-Z0)/10;
+  //  const double Z0 = 1100.;
+  const double Z0 = 1099.;
+  int num = (zhit-Z0)/d;
   //  double numd = (zhit-Z0)/10.;
   //  cout<<"num = "<<num<<" num(double) = "<<numd<<endl;
   xerr = hposerr.X();
   yerr = hposerr.Y();
+  // cout<<"Plane #"<<num<<" before: zhit="<<zhit<<" xerr = "<<xerr<<" yerr = "<<yerr<<endl;
+  Double_t sigmaMSplane = X*thetaMS; //TEST
+  //  cout<<"sigmaMSplane = "<<sigmaMSplane<<" um"<<endl;
+  if(num==0){
+    xerr = TMath::Hypot(xerr,sigmaMSplane);
+    yerr = TMath::Hypot(yerr,sigmaMSplane);
+  }
+  double xhit = hpos.X();
+ 
+  //  Double_t l = 10./cos(2.326*TMath::Pi()/180.);
+  double sigmaMS;
   for(int j=0;j<num;j++){
-    double sigmaMS = (j+1)*d*thetaMS;
+    sigmaMS = (j+1)*d*thetaMS;
+    //    cout<<"sigmaMS = "<<sigmaMS<<" xerr="<<xerr<<" yerr="<<yerr<<endl;
+    //sigmaMS = j*d*thetaMS;
     xerr = TMath::Hypot(xerr,sigmaMS);
     yerr = TMath::Hypot(yerr,sigmaMS);  
+
   }
+ 
+  //  cout<<"Plane #"<<num<<" after: zhit="<<zhit<<" xerr = "<<xerr<<" yerr = "<<yerr<<endl;
 
   // /// only for test -----
   // double sigma_add = 1e-4*(7.083-num*d*0.6238);
@@ -213,7 +235,7 @@ TVector3 PndLmdStripClusterTask::AddMSErr(TVector3 hpos, TVector3 hposerr){
   // ///--------------------
 
   //cout<<"Plane #"<<num<<" zhit="<<zhit<<" xerr = "<<xerr<<" yerr = "<<yerr<<" sigma_add = "<<sigma_add<<endl;
-  //cout<<"Plane #"<<num<<" zhit="<<zhit<<" xerr = "<<xerr<<" yerr = "<<yerr<<endl;
+  //  cout<<"Plane #"<<num<<" after: zhit="<<zhit<<" xerr = "<<xerr<<" yerr = "<<yerr<<endl;
   // if(zhit<Z0){
   //   cout<<"0 zhit="<<zhit<<" xerr = "<<hposerr.X()<<" yerr = "<<hposerr.Y()<<endl;
   //   return hposerr;
@@ -248,21 +270,29 @@ Bool_t PndLmdStripClusterTask::Backmap( TVector2 meantopPoint, Double_t meantope
 {
   // BACKMAPPING
   // get the backmapped point
-  cout<<"PndLmdStripClusterTask::BACKMAP"<<endl;
+  //  cout<<"PndLmdStripClusterTask::BACKMAP"<<endl;
   //Info("Backmap","Sensor ID is %s",sensorID);
   TVector3 localpos;
   TMatrixD locCov(3,3);
   Double_t t, b;
-  cout<<"sensorID = "<<sensorID<<endl;
-  cout<<"fGeoH = "<<fGeoH<<endl;
+  //  cout<<"sensorID = "<<sensorID<<endl;
+  //  cout<<"fGeoH = "<<fGeoH<<endl;
   Double_t errZ = 2.*fGeoH->GetSensorDimensionsShortId(sensorID).Z()/TMath::Sqrt(12.0);
-  cout<<"errZ = "<<errZ<<endl;
+  // cout<<"fGeoH->GetSensorDimensionsShortId(sensorID).Z() = "<<fGeoH->GetSensorDimensionsShortId(sensorID).Z()<<endl;
+  //  Double_t errZ = fGeoH->GetSensorDimensionsShortId(sensorID).Z()/TMath::Sqrt(12.0);//TEST!!!
+  // cout<<"errZ = "<<errZ<<endl;
 
   TVector2 onsensorPoint = 
   CalcLineCross(meantopPoint, fCurrentStripCalcTop->GetStripDirection(), meanbotPoint, fCurrentStripCalcBot->GetStripDirection() );
   // here we assume the sensor system to be in the _Middle_ of the volume
   localpos.SetXYZ( onsensorPoint.X(), onsensorPoint.Y(), 0.);
-  
+  //double zloc = fGeoH->GetSensorDimensionsShortId(sensorID).Z()+onsensorPoint.X()*tan(2.326*TMath::Pi()/180.);//TEST
+  //double zloc = (-1)*fGeoH->GetSensorDimensionsShortId(sensorID).Z();//TEST
+  //  localpos.SetXYZ( onsensorPoint.X(), onsensorPoint.Y(), zloc);//TEST
+  // cout<<"onsensorPoint.X()*tan(2.326*TMath::Pi()/180.) = "<<onsensorPoint.X()*tan(2.326*TMath::Pi()/180.)
+  //     <<" onsensorPoint.X() = "<<onsensorPoint.X()<<endl;
+  // errZ +=fabs(onsensorPoint.X())*tan(2.326*TMath::Pi()/180.); //TEST
+  //  cout<<"onsensorPoint.X() = "<<onsensorPoint.X()<<" errZ = "<<errZ<<endl;
   // let's see if we're still on the sensor (cut combinations with noise off)
   if(fabs(localpos.X()) > fabs(fCurrentDigiPar->GetTopAnchor().X())) return kFALSE;
   if(fabs(localpos.Y()) > fabs(fCurrentDigiPar->GetTopAnchor().Y())) return kFALSE;
@@ -271,25 +301,50 @@ Bool_t PndLmdStripClusterTask::Backmap( TVector2 meantopPoint, Double_t meantope
   hitPos = fGeoH->LocalToMasterShortId(localpos,sensorID);
   
   // calculate the errors corresponding to a skewed system!
+  //  cout<<"PndLmdStripClusterTask::Backmap meantoperr="<<meantoperr<<endl;
   t = meantoperr*fCurrentDigiPar->GetTopPitch()*cos(fCurrentDigiPar->GetOrient());
   b = meanboterr*fCurrentDigiPar->GetBotPitch()*cos(fCurrentDigiPar->GetOrient()+fCurrentDigiPar->GetSkew());
-  locCov[0][0]=t*t+b*b;
+  // t = meantoperr*fCurrentDigiPar->GetTopPitch()*cos(0.5*TMath::Pi()-fCurrentDigiPar->GetOrient());
+  // b = meanboterr*fCurrentDigiPar->GetBotPitch()*cos(0.5*TMath::Pi()-fCurrentDigiPar->GetOrient()+fCurrentDigiPar->GetSkew());//TEST
+    
+  //  cout<<"t = "<<t<<" b = "<<b<<" meantoperr = "<<meantoperr<<" meanboterr = "<<meanboterr<<endl;
+  //  locCov[0][0]=t*t+b*b;
+  locCov[0][0]=t*t+b*b+2*fabs(t*b*cos(fCurrentDigiPar->GetSkew())); //TEST
   t = meantoperr*fCurrentDigiPar->GetTopPitch()*sin(fCurrentDigiPar->GetOrient());
   b = meanboterr*fCurrentDigiPar->GetBotPitch()*sin(fCurrentDigiPar->GetOrient()+fCurrentDigiPar->GetSkew());
-  locCov[1][1]=t*t+b*b;
+  // t = meantoperr*fCurrentDigiPar->GetTopPitch()*sin(0.5*TMath::Pi()-fCurrentDigiPar->GetOrient());
+  // b = meanboterr*fCurrentDigiPar->GetBotPitch()*sin(0.5*TMath::Pi()-fCurrentDigiPar->GetOrient()+fCurrentDigiPar->GetSkew());
+  //  locCov[1][1]= locCov[0][0];
+  locCov[1][1]=t*t+b*b+2*fabs(t*b*cos(fCurrentDigiPar->GetSkew())); //TEST
   locCov[2][2]=errZ*errZ;
-  
+  //  cout<<"meantoperr="<<meantoperr<<" fCurrentDigiPar->GetOrient()="<<fCurrentDigiPar->GetOrient()<<" fCurrentDigiPar->GetSkew()="<<fCurrentDigiPar->GetSkew()<<endl;
+  //  cout<<"t = "<<t<<" b = "<<b<<" meantoperr = "<<meantoperr<<" meanboterr = "<<meanboterr<<endl;
   //do the transformation from sensor to lab frame
-  hitCov = fGeoH->LocalToMasterErrorsShortId(locCov,sensorID);
-  TVector3 hitErr(sqrt(hitCov[0][0]),sqrt(hitCov[1][1]),sqrt(hitCov[2][2]));
-  TVector3 hitErrMSadd = AddMSErr(hitPos, hitErr);
-  hitCov[0][0] = TMath::Power(hitErrMSadd.X(),2);
-  hitCov[1][1] = TMath::Power(hitErrMSadd.Y(),2);
-  hitCov[2][2] = TMath::Power(hitErrMSadd.Z(),2);
 
-  // cout<<" hitCov[0][0] = "<<hitCov[0][0]<<" hitCov[1][1] = "<<hitCov[1][1]<<" hitCov[2][2] = "<<hitCov[2][2]<<endl;
-  // cout<<" sqrt(hitCov[0][0]) = "<<sqrt(hitCov[0][0])<<" sqrt(hitCov[1][1]) = "<<sqrt(hitCov[1][1])
+ 
+  TVector3 hitErr(sqrt(locCov[0][0]),sqrt(locCov[1][1]),sqrt(locCov[2][2]));
+  TVector3 hitErrMSadd = AddMSErr(hitPos, hitErr);
+  locCov[0][0] = TMath::Power(hitErrMSadd.X(),2);
+  locCov[1][1] = TMath::Power(hitErrMSadd.Y(),2);
+  locCov[2][2] = TMath::Power(hitErrMSadd.Z(),2);
+
+  hitCov = fGeoH->LocalToMasterErrorsShortId(locCov,sensorID);
+  // cout<<"sqrt(locCov[0][0]) = "<<sqrt(locCov[0][0])<<" sqrt(locCov[1][1]) = "<<sqrt(locCov[1][1])
+  //     <<" sqrt(locCov[2][2]) = "<<sqrt(locCov[2][2])<<endl;
+  // cout<<"sqrt(hitCov[0][0]) = "<<sqrt(hitCov[0][0])<<" sqrt(hitCov[1][1]) = "<<sqrt(hitCov[1][1])
   //     <<" sqrt(hitCov[2][2]) = "<<sqrt(hitCov[2][2])<<endl;
+
+
+  // TVector3 hitErr(sqrt(hitCov[0][0]),sqrt(hitCov[1][1]),sqrt(hitCov[2][2]));
+  // TVector3 hitErrMSadd = AddMSErr(hitPos, hitErr);
+  // hitCov[0][0] = TMath::Power(hitErrMSadd.X(),2);
+  // hitCov[1][1] = TMath::Power(hitErrMSadd.Y(),2);
+  // hitCov[2][2] = TMath::Power(hitErrMSadd.Z(),2);
+  // // cout<<"GLOBAL hitPos:"<<endl;
+  // // hitPos.Print();
+  
+  // // cout<<" sqrt(hitCov[0][0]) = "<<sqrt(hitCov[0][0])<<" sqrt(hitCov[1][1]) = "<<sqrt(hitCov[1][1])
+  // //     <<" sqrt(hitCov[2][2]) = "<<sqrt(hitCov[2][2])<<endl;
 
   return kTRUE;
 }
