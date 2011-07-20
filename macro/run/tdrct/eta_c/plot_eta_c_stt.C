79de417609acbd204b308c0c147d0271f4e31405
Dima Melnychuk <DimaMelnichuk@mail.ru>
2011-07-20 14:44:07 +0000

    update eta_c analysis macros

--- macro/run/tdrct/eta_c/plot_eta_c_stt.C
+++ macro/run/tdrct/eta_c/plot_eta_c_stt.C
@@ -9,38 +9,30 @@
 	TFile *f = TFile::Open(inFile);
 
 	TH1F *h_etac_nocut=(TH1F *)f->Get("h_etac_nocut");
-	h_etac_nocut->UseCurrentStyle(); 
 	TH1F *h_etac_pid=(TH1F *)f->Get("h_etac_pid");
-	h_etac_pid->UseCurrentStyle(); 
 	TH1F *h_etac_phimass=(TH1F *)f->Get("h_etac_phimass");
-	h_etac_phimass->UseCurrentStyle(); 
 	TH1F *h_etac_vtx=(TH1F *)f->Get("h_etac_vtx");
-	h_etac_vtx->UseCurrentStyle(); 
 	TH1F *h_etac_4c=(TH1F *)f->Get("h_etac_4c");
-	h_etac_4c->UseCurrentStyle(); 
 
 	TH1F *h_mphi_nocuts=(TH1F *)f->Get("h_mphi_nocuts");
-	h_mphi_nocuts->UseCurrentStyle(); 
 	TH1F *h_mphi_pid=(TH1F *)f->Get("h_mphi_pid");
-	h_mphi_pid->UseCurrentStyle(); 
 	TH1F *h_mphi_vtx=(TH1F *)f->Get("h_mphi_vtx");
-	h_mphi_vtx->UseCurrentStyle(); 
 	TH1F *h_mphi_4c=(TH1F *)f->Get("h_mphi_4c");
-	h_mphi_4c->UseCurrentStyle(); 
 	TH1F *h_mphi_final=(TH1F *)f->Get("h_mphi_final");
-	h_mphi_final->UseCurrentStyle();
 	
 	TH1F *nc=(TH1F *)f->Get("nc");
-	nc->UseCurrentStyle(); 
 	 
 	TH1F *h_chi2_4c=(TH1F *)f->Get("h_chi2_4c");
-	h_chi2_4c->UseCurrentStyle(); 
+	TH1F *h_chi2b_4c=(TH1F *)f->Get("h_chi2b_4c");
+	
 	TH1F *h_chi2_vtx=(TH1F *)f->Get("h_chi2_vtx");
-	h_chi2_vtx->UseCurrentStyle(); 
+	TH1F *h_chi2b_vtx=(TH1F *)f->Get("h_chi2b_vtx");
 	TH1F *hvzpos=(TH1F *)f->Get("hvzpos");
-	hvzpos->UseCurrentStyle(); 
 	TH2F *hvpos=(TH2F *)f->Get("hvpos");
-	hvpos->UseCurrentStyle(); 
+	
+	TH1F *hvtxresX=(TH1F *)f->Get("hvtxresX");
+	TH1F *hvtxresY=(TH1F *)f->Get("hvtxresY");
+	TH1F *hvtxresZ=(TH1F *)f->Get("hvtxresZ");
 	
 	TH1F *n_etac=(TH1F *)f->Get("n_etac");
 	TH1F *n_events=(TH1F *)f->Get("n_events");
@@ -54,6 +46,7 @@
 
 	TCanvas *c1=new TCanvas("c1","c1",600,600);
 	nc->Draw();
+	c1->SaveAs("c1_stt.png");
 
 	TCanvas *c2=new TCanvas("c2","c2",600,600);
 	c2->Divide(1,2);
@@ -61,6 +54,7 @@
 	h_mphi_nocuts->Draw();
 	c2->cd(2);
 	h_etac_nocut->Draw();
+	c2->SaveAs("c2_stt.png");
 	
 	TCanvas *c3=new TCanvas("c3","c3",600,600);
 	c3->Divide(1,2);
@@ -68,11 +62,17 @@
 	h_mphi_pid->Draw();
 	c3->cd(2);
 	h_etac_pid->Draw();
+	c3->SaveAs("c3_stt.png");
 	
 	if (!vtxfit)
 	{
 		TCanvas *c6=new TCanvas("c6","c6",600,600);
+		c6->Divide(2,1);
+		c6->cd(1);
 		h_chi2_4c->Draw();
+		c6->cd(2);
+		h_chi2b_4c->Draw();
+		c6->SaveAs("c6_stt.png");
 		
 		TCanvas *c4=new TCanvas("c4","c4",600,600);
 		c4->Divide(1,2);
@@ -80,18 +80,33 @@
 		h_mphi_4c->Draw();
 		c4->cd(2);
 		h_etac_4c->Draw();
+		c4->SaveAs("c4_stt.png");
 	}
 	
 	if (vtxfit)
 	{
 		TCanvas *c7=new TCanvas("c7","c7",600,600);
+		c7->Divide(2,1);
+		c7->cd(1);
 		hvpos->Draw();
-
-		TCanvas *c8=new TCanvas("c8","c8",600,600);
+		c7->cd(2);
 		hvzpos->Draw();
 		
-		TCanvas *c9=new TCanvas("c9","c9",600,600);
+		TCanvas *c8=new TCanvas("c8","c8",600,600);
+		c8->Divide(2,1);
+		c8->cd(1);
 		h_chi2_vtx->Draw();
+		c8->cd(2);
+		h_chi2b_vtx->Draw();
+		
+		TCanvas *c9=new TCanvas("c9","c9",600,600);
+		c9->Divide(3,1);
+		c9->cd(1);
+		hvtxresX->Draw();
+		c9->cd(2);
+		hvtxresY->Draw();
+		c9->cd(3);
+		hvtxresZ->Draw();
 		
 		TCanvas *c10=new TCanvas("c10","c10",600,600);
 		c10->Divide(1,2);
@@ -99,6 +114,7 @@
 		h_mphi_vtx->Draw();
 		c10->cd(2);
 		h_etac_vtx->Draw();
+		c10->SaveAs("c10_stt.png");
 	}
 
 	TCanvas *c5=new TCanvas("c5","c5",600,600);
@@ -106,11 +122,11 @@
 	c5->cd(1);
 	h_mphi_final->Draw();
 	
-	double mean_phi=1.1;
-	double rms_phi=0.1;
+	double mean_phi=1.02;
+	double range_phi=0.02;
 	
 	TF1 *f1_phi = new TF1("f1_phi","gaus",0.9,1.1);
-	h_mphi_final->Fit(f1_phi,"R","",mean_phi-1.6*rms_phi,mean_phi+1.6*rms_phi);
+	h_mphi_final->Fit(f1_phi,"R","",mean_phi-range_phi,mean_phi+range_phi);
 	
 	double sigma1_phi=f1_phi->GetParameter(2);
 	double mean1_phi=f1_phi->GetParameter(1);
@@ -124,11 +140,11 @@
 	c5->cd(2);
 	h_etac_phimass->Draw();
 	// fit eta_c
-	double mean=h_etac_phimass->GetMean();
-	double rms=h_etac_phimass->GetRMS();
+	double mean_etac=2.98;
+	double range_etac=0.1;
 	
 	TF1 *f1 = new TF1("f1","gaus",2.8,3.2);
-	h_etac_phimass->Fit(f1,"R","",mean-1.6*rms,mean+1.6*rms);
+	h_etac_phimass->Fit(f1,"R","",mean_etac-range_etac,mean_etac+range_etac);
 	
 	double sigma1=f1->GetParameter(2);
 	double mean1=f1->GetParameter(1);
@@ -138,5 +154,7 @@
 
 	double sigma2=f2->GetParameter(2);
 	std::cout<<"sigma="<<sigma2<<std::endl;
+	
+	c5->SaveAs("c5_stt.png");
 	
 }
