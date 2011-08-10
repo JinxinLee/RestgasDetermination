18ba6043106b6929050dcb7d831e6da8b91b6738
Sebastian Neubert <sneubert>
2011-08-10 16:59:30 +0000

    added EvtMix Macros

--- macro/tpc/runMCSkim.C
+++ macro/tpc/runMCSkim.C
@@ -7,20 +7,20 @@
   //SET NUMBER OF EVENTS
   // ------------------------------------------------------------------------
 
-  Int_t nEvents=30;
+  Int_t nEvents=3000;
   TString basedir = gSystem->Getenv("VMCWORKDIR");
   
   // Set INPUT DIRECTORY (MC files) and JOBNAME
   // ------------------------------------------------------------------------
   TString inDir="/nfs/nas/data/panda/tpc/SIM/evtmix/";
-  TString jobname="phys_JPsiPiPi";
+  TString jobname="phys_EtaCPhiPhi";
 
   //inDir=(basedir+"/")+inDir;
   TString inFile=(inDir+"/")+jobname;
   inFile+=".mc.root";
  
   TString outFile = inFile;
-  outFile.ReplaceAll(".mc.root", ".skimtest.mc.root");
+  outFile.ReplaceAll(".mc.root", ".skim.mc.root");
   TString paramIn = inFile;
   paramIn.ReplaceAll(".mc.root",".param.root");
   TString paramOut = outFile;
