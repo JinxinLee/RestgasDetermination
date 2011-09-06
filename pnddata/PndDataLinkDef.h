ce53afebfda3e3636cd467dec8ab62a0c7a86e73
Carsten Schwarz <C.Schwarz@gsi.de>
2011-09-06 08:11:30 +0000

    added digi to linkdef

--- pnddata/PndDataLinkDef.h
+++ pnddata/PndDataLinkDef.h
@@ -19,6 +19,7 @@
 #pragma link C++ class PndDrcBarPoint+;
 #pragma link C++ class PndDrcHit+;
 #pragma link C++ class PndDrcPDHit+;
+#pragma link C++ class PndDrcDigi+;
 
 #pragma link C++ class PndDrpcPoint+;
 #pragma link C++ class PndDrpcPointLite+;
