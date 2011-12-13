95d7a074730acb7fa13e5f835b702b0548837393
Florian Uhlig <f.uhli@gsi.de>
2011-12-13 08:59:09 +0000

    Use new DashBoard on cdash.gsi.de. Prepare everything for the coverage analysis.

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -106,3 +106,8 @@ set(CTEST_CUSTOM_WARNING_EXCEPTION
 # -----------------------------------------------------------
 set(CTEST_CUSTOM_WARNING_MATCH	${CTEST_CUSTOM_WARNING_MATCH}
 	)
+
+Set (CTEST_CUSTOM_COVERAGE_EXCLUDE 
+     "Dict.h"
+     "Dict.cxx"
+    )
