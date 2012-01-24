67e6fb57301d930017f59ce162b1dca943f0dc6a
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2012-01-24 08:50:54 +0000

    remove already defined classes (SDS classes) from lmd def file

--- lmd/LmdLinkDef.h
+++ lmd/LmdLinkDef.h
@@ -6,13 +6,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-//data
-#pragma link C++ class PndSdsMCPoint+;
-#pragma link C++ class PndSdsDigi+;
-#pragma link C++ class PndSdsStrip+;
-
-//SDS
-#pragma link C++ class PndSdsDetector+;
 
 //LMD
 #pragma link C++ class PndLmdDetector+;
