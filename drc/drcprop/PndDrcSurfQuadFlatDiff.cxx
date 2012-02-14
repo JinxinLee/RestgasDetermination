818877c60696cf25d962063c76f9f0741065422d
Carsten Schwarz <C.Schwarz@gsi.de>
2012-02-14 10:00:10 +0000

    debugged wrong auxiliary surface

--- drc/drcprop/PndDrcSurfQuadFlatDiff.cxx
+++ drc/drcprop/PndDrcSurfQuadFlatDiff.cxx
@@ -148,8 +148,10 @@ void PndDrcSurfQuadFlatDiff::AddSurface(const PndDrcSurfAbs& surf, XYZPoint p1, 
 		fNormal = (cross1.Mag2() > cross2.Mag2()) ? cross1.Unit() : cross2.Unit();
 		fSurfAux.AddPoint(fS1p1);
 		fSurfAux.AddPoint(fS1p2);
-		fSurfAux.AddPoint(fS2p1);
+		//fSurfAux.AddPoint(fS2p1);
+		//fSurfAux.AddPoint(fS2p2);
 		fSurfAux.AddPoint(fS2p2);
+		fSurfAux.AddPoint(fS2p1);
 	}
 }
 //----------------------------------------------------------------------
@@ -163,25 +165,25 @@ bool PndDrcSurfQuadFlatDiff::SurfaceHit(PndDrcPhoton& ph,
                                         XYZPoint&  pos_new,
                                         double&    path_length) const
 {
-	static const double kEps = 1.0e-9;
+  static const double kEps = 1.0e-9;
 
-	if (Verbosity()>=3) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: name="
-		<<Name()<<endl;
+  if (Verbosity()>=3) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: name="
+			  <<Name()<<endl;
 
 
-	if (!fS1 || !fS2)
-	{
-		cerr<<" *** PndDrcSurfQuadFlatDiff::surfaceHit: at least one of"
-			<<" both surfaces undefined."<<endl;
-		exit(EXIT_FAILURE);
-	}
+  if (!fS1 || !fS2)
+    {
+      cerr<<" *** PndDrcSurfQuadFlatDiff::surfaceHit: at least one of"
+	  <<" both surfaces undefined."<<endl;
+      exit(EXIT_FAILURE);
+    }
 
 
 
   // find first point
 
-	XYZPoint  point = ph.Position();
-	XYZVector dir   = ph.Direction().Unit();
+  XYZPoint  point = ph.Position();
+  XYZVector dir   = ph.Direction().Unit();
 
 
   // calculate hits with area
@@ -193,244 +195,244 @@ bool PndDrcSurfQuadFlatDiff::SurfaceHit(PndDrcPhoton& ph,
   //
   // dir*fNormal > 0 , see condition above
 
-	double h = dir.Dot(fNormal);
-	if (h==0)
-	{
-		if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
-			<<" dir perp norm, no hit. \n";
-		return false;
-	}
-	double   lambda = (fS1p1-point).Dot(fNormal) / dir.Dot(fNormal);
-	pos_new = point + lambda*dir;
+  double h = dir.Dot(fNormal);
+  if (h==0)
+    {
+      if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
+			      <<" dir perp norm, no hit. \n";
+      return false;
+    }
+  double   lambda = (fS1p1-point).Dot(fNormal) / dir.Dot(fNormal);
+  pos_new = point + lambda*dir;
 
-	if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"<<Name()<<endl
-		<<"      lambda pos_new ="<<lambda<<" "<<pos_new<<endl;
+  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"<<Name()<<endl
+			  <<"      lambda pos_new ="<<lambda<<" "<<pos_new<<endl;
 
 
-	if (lambda<-kEps) // flying in wrong direction.
-	{
-		if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: wrong dir "
-			<<" lambda par ="<<lambda<<" no hit."<<endl;
-		return false;
-	}
+  if (lambda<-kEps) // flying in wrong direction.
+    {
+      if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: wrong dir "
+			      <<" lambda par ="<<lambda<<" no hit."<<endl;
+      return false;
+    }
 
-	path_length = fabs(lambda);
+  path_length = fabs(lambda);
 
   // check surfaces between rectangular partial surface and eventual bended
   // surface borders.
-	bool hit_aux_surf = fSurfAux.WithinSurface(pos_new);
+  bool hit_aux_surf = fSurfAux.WithinSurface(pos_new);
 
   // if both are flat, there was not hit.
-	if (fS1->IsFlat() && fS2->IsFlat())
+  if (fS1->IsFlat() && fS2->IsFlat())
+    {
+      if (hit_aux_surf)
 	{
-		if (hit_aux_surf)
-		{
-          if (fPixel){
-	  if (fEffiCathode
-	      ->EffiFlag(ph.Wavelength(),
-			 ph.Direction().Dot(Normal(pos_new))))
-	    { 
-	      ph.SetFate(Drc::kPhotMeasured);
-	      if (fPixelCorr) ph.SetPosition(fPixelPoint);
-	    }
-	  else
-	    { 
-	      ph.SetFate(Drc::kPhotAbsorbed);
-	    }
+	  if (fPixel){
+	    if (fEffiCathode
+		->EffiFlag(ph.Wavelength(),
+			   ph.Direction().Dot(Normal(pos_new))))
+	      { 
+		ph.SetFate(Drc::kPhotMeasured);
+		if (fPixelCorr) ph.SetPosition(fPixelPoint);
+	      }
+	    else
+	      { 
+		ph.SetFate(Drc::kPhotAbsorbed);
+	      }
 	  }
-	  
-		  //if (fPixel) ph.SetFate(Drc::kPhotMeasured);
-
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
-				<<" within rectangular partial surface, hit. "
-				<<endl;
-			return true;
-		}
-		else
-		{
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
-				<<" no hit within reactangular partial surface"
-				<<" of flat borders. "
-				<<endl;
-			return false;
-		}
+		
+	  //if (fPixel) ph.SetFate(Drc::kPhotMeasured);
+		
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
+				  <<" within rectangular partial surface, hit. "
+				  <<endl;
+	  return true;
 	}
-
-
-	bool in_between1 = false;
-	if (!(fS1->IsFlat()))
+      else
 	{
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit:"
+				  <<" no hit within reactangular partial surface"
+				  <<" of flat borders. "
+				  <<endl;
+	  return false;
+	}
+    }
+	
+	
+  bool in_between1 = false;
+  if (!(fS1->IsFlat()))
+    {
       // check if point between curve and line
       // create a fake photon.
-		if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (1) "
-			<<" check point between line and curve of "
-			<<fS1->Name()<<endl;
-		PndDrcPhoton ph1;
-		ph1.SetPosition(pos_new);
-
-		XYZPoint ref(0.5*(XYZVector(fS1p1) + XYZVector(fS1p2)));
+      if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (1) "
+			      <<" check point between line and curve of "
+			      <<fS1->Name()<<endl;
+      PndDrcPhoton ph1;
+      ph1.SetPosition(pos_new);
+	    
+      XYZPoint ref(0.5*(XYZVector(fS1p1) + XYZVector(fS1p2)));
       //                 no addition between XYZPoints possible
-		ref.SetX(1.01*ref.X()); // bend it a little bit outside
-		ref.SetY(1.01*ref.Y()); // so that dir points a little bit inside.
-		dir = (XYZPoint(pos_new)-ref).Unit();
-
-		ph1.SetDirection(dir);
-
-		if (Verbosity()>=4)
-		{
-			cout<<"        with aux. photon "<<endl;
-			cout<<"        ph position "<<ph1.Position()<<endl;
-			cout<<"        & direction "<<ph1.Direction()<<endl;
-		}
-
-		XYZPoint v_dummy;
-		double dummy;
-		if (fS1->SurfaceHit(ph1,v_dummy,dummy))
-		{
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (1) "
-				<<" between line and curve of "
-				<<fS1->Name()<<", hit."<<endl;
-			in_between1 =  true;
-		}
-		else
-		{
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "
-				<<" no hit between line and curve of "
-				<<fS1->Name()<<", check other side."<<endl;
-		}
+      ref.SetX(1.01*ref.X()); // bend it a little bit outside
+      ref.SetY(1.01*ref.Y()); // so that dir points a little bit inside.
+      dir = (XYZPoint(pos_new)-ref).Unit();
+	    
+      ph1.SetDirection(dir);
+	    
+      if (Verbosity()>=4)
+	{
+	  cout<<"        with aux. photon "<<endl;
+	  cout<<"        ph position "<<ph1.Position()<<endl;
+	  cout<<"        & direction "<<ph1.Direction()<<endl;
 	}
-
-
+	    
+      XYZPoint v_dummy;
+      double dummy;
+      if (fS1->SurfaceHit(ph1,v_dummy,dummy))
+	{
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (1) "
+				  <<" between line and curve of "
+				  <<fS1->Name()<<", hit."<<endl;
+	  in_between1 =  true;
+	}
+      else
+	{
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "
+				  <<" no hit between line and curve of "
+				  <<fS1->Name()<<", check other side."<<endl;
+	}
+    }
+	
+	
   //                   convex           concave  cases
-	bool in_between2 = (hit_aux_surf && fS2->IsFlat()); // for later logic
-	if (! fS2->IsFlat())
-	{
+  bool in_between2 = (hit_aux_surf && fS2->IsFlat()); // for later logic
+  if (! fS2->IsFlat())
+    {
       // check if point between curve and line
       // create a fake photon.
-		if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (2) "
-			<<" check point between line and curve of "
-			<<fS2->Name()<<endl;
-		PndDrcPhoton ph1;
-		ph1.SetPosition(pos_new);
-
-		XYZPoint ref(0.5*(XYZVector(fS2p1) + XYZVector(fS2p2)));
+      if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (2) "
+			      <<" check point between line and curve of "
+			      <<fS2->Name()<<endl;
+      PndDrcPhoton ph1;
+      ph1.SetPosition(pos_new);
+	    
+      XYZPoint ref(0.5*(XYZVector(fS2p1) + XYZVector(fS2p2)));
       //                 no addition between XYZPoints possible
-		ref.SetX(1.01*ref.X()); // bend it a little bit outside
-		ref.SetY(1.01*ref.Y()); // so that dir points a little bit inside.
-		dir = (XYZPoint(pos_new)-ref).Unit();
-
-		ph1.SetDirection(dir);
-
-		if (Verbosity()>=4)
-		{
-			cout<<"        with aux. photon "<<endl;
-			cout<<"        ph position "<<ph1.Position()<<endl;
-			cout<<"        & direction "<<ph1.Direction()<<endl;
-		}
-
-		XYZPoint v_dummy;
-		double dummy;
+      ref.SetX(1.01*ref.X()); // bend it a little bit outside
+      ref.SetY(1.01*ref.Y()); // so that dir points a little bit inside.
+      dir = (XYZPoint(pos_new)-ref).Unit();
+	    
+      ph1.SetDirection(dir);
+	    
+      if (Verbosity()>=4)
+	{
+	  cout<<"        with aux. photon "<<endl;
+	  cout<<"        ph position "<<ph1.Position()<<endl;
+	  cout<<"        & direction "<<ph1.Direction()<<endl;
+	}
+	    
+      XYZPoint v_dummy;
+      double dummy;
       // fS2->setVerbosity(4);//###
-		if (fS2->SurfaceHit(ph1,v_dummy,dummy))
-		{
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (2) "
-				<<" between line and curve of "
-				<<fS2->Name()<<", hit."<<endl;
-			in_between2 = true;
-		}
-		else
-		{
-			if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "
-				<<" no hit between line and curve of "
-				<<fS2->Name()<<", no hit possibility left."<<endl;
-		}
-	}
-
-
-	if (Verbosity()>=4)
+      if (fS2->SurfaceHit(ph1,v_dummy,dummy))
 	{
-		cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "<<endl
-			<<"         hit_aux_surf = "<<hit_aux_surf<<endl;
-		if (fS1->IsFlat()) cout<<"         in_between1  = "<<"flat"<<endl;
-		if (!fS1->IsFlat()) cout<<"         in_between1  = "<<in_between1<<endl;
-		if (fS2->IsFlat()) cout<<"         in_between2  = "<<"flat"<<endl;
-		if (!fS2->IsFlat()) cout<<"         in_between2  = "<<in_between2<<endl;
-
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: (2) "
+				  <<" between line and curve of "
+				  <<fS2->Name()<<", hit."<<endl;
+	  in_between2 = true;
 	}
-
-
+      else
+	{
+	  if (Verbosity()>=4) cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "
+				  <<" no hit between line and curve of "
+				  <<fS2->Name()<<", no hit possibility left."<<endl;
+	}
+    }
+	
+	
+  if (Verbosity()>=4)
+    {
+      cout<<"      PndDrcSurfQuadFlatDiff::surfaceHit: "<<endl
+	  <<"         hit_aux_surf = "<<hit_aux_surf<<endl;
+      if (fS1->IsFlat()) cout<<"         in_between1  = "<<"flat"<<endl;
+      if (!fS1->IsFlat()) cout<<"         in_between1  = "<<in_between1<<endl;
+      if (fS2->IsFlat()) cout<<"         in_between2  = "<<"flat"<<endl;
+      if (!fS2->IsFlat()) cout<<"         in_between2  = "<<in_between2<<endl;
+	    
+    }
+	
+	
   // in/exclusion logic
-	if (hit_aux_surf)
-	{
-          if (fPixel){
-	  if (fEffiCathode
-	      ->EffiFlag(ph.Wavelength(),
-			 ph.Direction().Dot(Normal(pos_new))))
-	    { 
-	      ph.SetFate(Drc::kPhotMeasured);
-	      if (fPixelCorr) ph.SetPosition(fPixelPoint);
-	    }
-	  else
-	    { 
-	      ph.SetFate(Drc::kPhotAbsorbed);
-	    }
+  if (hit_aux_surf)
+    {
+      if (fPixel){
+	if (fEffiCathode
+	    ->EffiFlag(ph.Wavelength(),
+		       ph.Direction().Dot(Normal(pos_new))))
+	  { 
+	    ph.SetFate(Drc::kPhotMeasured);
+	    if (fPixelCorr) ph.SetPosition(fPixelPoint);
 	  }
-	  
-	  //if (fPixel) ph.SetFate(Drc::kPhotMeasured); // don' know why this is only implemented for the flat-flat case
-
-      // exclude photons from concave parts
-		if ( !(fS1->IsFlat()) && in_between1) return false;
-		if ( !(fS2->IsFlat()) && in_between2) return false;
-		return true;
-	}
 	else
-	{
-		if (in_between1) return true; // surface convex, photon inside
-		if (in_between2) return true; // surface convex, photon inside
-		return false;
-	}
+	  { 
+	    ph.SetFate(Drc::kPhotAbsorbed);
+	  }
+      }
+	    
+      //if (fPixel) ph.SetFate(Drc::kPhotMeasured); // don' know why this is only implemented for the flat-flat case
+	    
+      // exclude photons from concave parts
+      if ( !(fS1->IsFlat()) && in_between1) return false;
+      if ( !(fS2->IsFlat()) && in_between2) return false;
+      return true;
+    }
+  else
+    {
+      if (in_between1) return true; // surface convex, photon inside
+      if (in_between2) return true; // surface convex, photon inside
+      return false;
+    }
 }
 //----------------------------------------------------------------------
 void PndDrcSurfQuadFlatDiff::Print(fstream& stream) const
 {
-	stream<<" TPolyLine3D *l = new TPolyLine3D(2);"<<endl;
+  stream<<" TPolyLine3D *l = new TPolyLine3D(2);"<<endl;
 
-	stream<<" l->SetPoint("<<0<<","
-		<<fS1p1.X()<<","
-		<<fS1p1.Y()<<","
-		<<fS1p1.Z()<<");"<<endl;
-	stream<<" l->SetPoint("<<1<<","
-		<<fS2p1.X()<<","
-		<<fS2p1.Y()<<","
-		<<fS2p1.Z()<<");"<<endl;
-	stream<<" l->SetLineColor("<<fPrintColor<<");"<<endl;
-	stream<<" l->Draw();"<<endl;
+  stream<<" l->SetPoint("<<0<<","
+	<<fS1p1.X()<<","
+	<<fS1p1.Y()<<","
+	<<fS1p1.Z()<<");"<<endl;
+  stream<<" l->SetPoint("<<1<<","
+	<<fS2p1.X()<<","
+	<<fS2p1.Y()<<","
+	<<fS2p1.Z()<<");"<<endl;
+  stream<<" l->SetLineColor("<<fPrintColor<<");"<<endl;
+  stream<<" l->Draw();"<<endl;
 
-	stream<<" TPolyLine3D *l = new TPolyLine3D(2);"<<endl;
+  stream<<" TPolyLine3D *l = new TPolyLine3D(2);"<<endl;
 
-	stream<<" l->SetPoint("<<0<<","
-		<<fS1p2.X()<<","
-		<<fS1p2.Y()<<","
-		<<fS1p2.Z()<<");"<<endl;
-	stream<<" l->SetPoint("<<1<<","
-		<<fS2p2.X()<<","
-		<<fS2p2.Y()<<","
-		<<fS2p2.Z()<<");"<<endl;
-	stream<<" l->SetLineColor("<<fPrintColor<<");"<<endl;
-	stream<<" l->Draw();"<<endl;
+  stream<<" l->SetPoint("<<0<<","
+	<<fS1p2.X()<<","
+	<<fS1p2.Y()<<","
+	<<fS1p2.Z()<<");"<<endl;
+  stream<<" l->SetPoint("<<1<<","
+	<<fS2p2.X()<<","
+	<<fS2p2.Y()<<","
+	<<fS2p2.Z()<<");"<<endl;
+  stream<<" l->SetLineColor("<<fPrintColor<<");"<<endl;
+  stream<<" l->Draw();"<<endl;
 
-	// for an opened canvas
-	TPolyLine3D *l1 = new TPolyLine3D(2);
-	l1->SetPoint( 0, fS1p1.X(), fS1p1.Y(), fS1p1.Z() );
-	l1->SetPoint( 1, fS2p1.X(), fS2p1.Y(), fS2p1.Z() );
-	l1->SetLineColor(fPrintColor);
-	l1->Draw();
-
-	TPolyLine3D *l2 = new TPolyLine3D(2);
-	l2->SetPoint( 0, fS1p2.X(), fS1p2.Y(), fS1p2.Z() );
-	l2->SetPoint( 1, fS2p2.X(), fS2p2.Y(), fS2p2.Z() );
-	l2->SetLineColor(fPrintColor);
-	l2->Draw();
+  // for an opened canvas
+  TPolyLine3D *l1 = new TPolyLine3D(2);
+  l1->SetPoint( 0, fS1p1.X(), fS1p1.Y(), fS1p1.Z() );
+  l1->SetPoint( 1, fS2p1.X(), fS2p1.Y(), fS2p1.Z() );
+  l1->SetLineColor(fPrintColor);
+  l1->Draw();
+  
+  TPolyLine3D *l2 = new TPolyLine3D(2);
+  l2->SetPoint( 0, fS1p2.X(), fS1p2.Y(), fS1p2.Z() );
+  l2->SetPoint( 1, fS2p2.X(), fS2p2.Y(), fS2p2.Z() );
+  l2->SetLineColor(fPrintColor);
+  l2->Draw();
 }
 //----------------------------------------------------------------------
 void PndDrcSurfQuadFlatDiff::Print() const
