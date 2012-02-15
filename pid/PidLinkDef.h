2dd97d93663c76e4a2d0e4a10864bdf016ae3a5b
Ralf Kliemt <r.kliemt@gsi.de>
2012-02-15 09:04:47 +0000

    activate Ideal PID Fwd_Only

--- pid/PidLinkDef.h
+++ pid/PidLinkDef.h
@@ -14,6 +14,7 @@
 #pragma link C++ class  PndPidMvdPar+;
 #pragma link C++ class  PndPidCorrelator+;
 #pragma link C++ class  PndPidIdealAssociatorTask+;
+#pragma link C++ class  PndPidIdealFwdOnlyAssociatorTask+;
 #pragma link C++ class  PndPidMvdAssociatorTask+;
 #pragma link C++ class  PndPidMdtHCAssociatorTask+;
 #pragma link C++ class  PndPidDrcAssociatorTask+;
