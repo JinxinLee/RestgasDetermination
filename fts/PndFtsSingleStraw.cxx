34264981b74104df442c69d4724248c5c07c3e35
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2012-02-23 17:20:21 +0000

    initialization problems solved partially

--- fts/PndFtsSingleStraw.cxx
+++ fts/PndFtsSingleStraw.cxx
@@ -104,6 +104,7 @@ PndFtsSingleStraw::PndFtsSingleStraw() {
 
   CNumb=0;
   memset(CumClus,0,sizeof(CumClus));
+  memset(CH4Clus,0,sizeof(CH4Clus));  
   memset(PolyaCum,0,sizeof(PolyaCum));
   memset(Xs,0,sizeof(Xs));
 
@@ -120,6 +121,10 @@ PndFtsSingleStraw::PndFtsSingleStraw() {
   Pulse.clear();
   PulseT.clear();
   WDist.clear();
+    
+    
+    
+    
 }
 
 // ----------------------------------------------------------------------
