cf3f2688a8782f73baa0184ab98764e670893862
Stefano Spataro <spataro@to.infn.it>
2012-03-01 10:56:32 +0000

    Fixing not initialized members

--- pnddata/SttData/PndSttPoint.cxx
+++ pnddata/SttData/PndSttPoint.cxx
@@ -9,11 +9,13 @@ using std::cout;
 using std::endl;
 
 // -----   Default constructor   -------------------------------------------
-PndSttPoint::PndSttPoint() : FairMCPoint() {
-  fX_in_local  = fY_in_local   = fZ_in_local   = 0.;
-  fX_out_local = fY_out_local  = fZ_out_local  = 0.;
-  fPx_in       = fPy_in        = fPz_in        = 0.;
-  fPx_out      = fPy_out       = fPz_out       = 0.;
+PndSttPoint::PndSttPoint() : FairMCPoint(),
+			     fX_in_local(0.), fY_in_local(0), fZ_in_local(0),
+			     fX_out_local(0), fY_out_local(0), fZ_out_local(0),
+			     fPx_in(0), fPy_in(0), fPz_in(0),
+			     fPx_out(0), fPy_out(0), fPz_out(0),
+			     fMass(0), fTubeID(0) 
+{
 }
 // -------------------------------------------------------------------------
 
@@ -24,32 +26,18 @@ PndSttPoint::PndSttPoint(Int_t trackID, Int_t detID, TVector3 pos,
 			 TVector3 posInLocal, TVector3 posOutLocal, 
 			 TVector3 momIn, TVector3 momOut,
 			 Double_t tof, Double_t length, Double_t eLoss, Double_t mass)
-  : FairMCPoint(trackID, detID, pos, momIn, tof, length, eLoss) 
+  : FairMCPoint(trackID, detID, pos, momIn, tof, length, eLoss),
+    fX_out_local(posOutLocal.X()), fY_out_local(posOutLocal.Y()), fZ_out_local(posOutLocal.Z()),
+    fX_in_local(posInLocal.X()),  fY_in_local(posInLocal.Y()), fZ_in_local(posInLocal.Z()),
+    fPx_in(momIn.Px()), fPy_in(momIn.Py()), fPz_in(momIn.Pz()),
+    fPx_out(momOut.Px()), fPy_out(momOut.Py()), fPz_out(momOut.Pz()),
+    fMass(mass), fTubeID(0)
 {
-  fX_out_local  = posOutLocal.X();
-  fY_out_local  = posOutLocal.Y();
-  fZ_out_local  = posOutLocal.Z();
-
-  fX_in_local  = posInLocal.X();
-  fY_in_local  = posInLocal.Y();
-  fZ_in_local  = posInLocal.Z();
-
-  fPx_in = momIn.Px();
-  fPy_in = momIn.Py();
-  fPz_in = momIn.Pz();
-  
-  fPx_out = momOut.Px();
-  fPy_out = momOut.Py();
-  fPz_out = momOut.Pz();
-  
   // reset MC monentum
   fPx = (momIn.Px() + momOut.Px()) / 2.;
   fPy = (momIn.Py() + momOut.Py()) / 2.;
   fPz = (momIn.Pz() + momOut.Pz()) / 2.;
-
-  fMass = mass; // stt1
   SetLink(FairLink("MCTrack", trackID));
-
 }
 // -------------------------------------------------------------------------
 
