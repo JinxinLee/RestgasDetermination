250aab97cfb00c89f00cc98acd62a93342ad4a95
Carsten Schwarz <C.Schwarz@gsi.de>
2012-03-09 16:16:13 +0000

    worked on test_simple_lens_sheet

--- drc/drcprop/examples/test_simple_lens_sheet.cc
+++ drc/drcprop/examples/test_simple_lens_sheet.cc
@@ -79,6 +79,21 @@ int main(int argc, char *argv[])
   // Example for a simple sheet with lens and expansion box.
 
 
+  //---------------------------------------------------------
+
+  //int focus_option = 0; // nothing
+  //int focus_option = 1; // lens
+  int focus_option = 2; // focussing downstream spherical mirror
+  
+  
+  int ioption = 1; // 1=cherenkov, 2=testbeam
+
+  //int coating = true;  
+  int coating = false;  
+
+
+  //---------------------------------------------------------
+
   double half_width  = 200.0/2;   //mm
   double half_thick  = 17.0/2;    //mm
   double half_length = 2500.0/2;  //mm
@@ -91,90 +106,113 @@ int main(int argc, char *argv[])
   // of 145.7/1.47 = 99.1
   // or more precise: R = (300/1.47 + 10) *0.47 = 100.6
 
-  double lens_radius = 99999.9;//100.6;     // mm
+  // mirror:
+  // create a focussing mirror at the downstream side
+  // with width like quartz bar, length 40 mm
+  // the focusing distance is 2500+300 mm => radius = 5600 mm
 
-  //lens_radius = 98.6;
-  //lens_radius = 30;
+
+
+  //double lens_radius = 99999.9;     // mm
+  double lens_radius = 100.6;       // mm
+  double mirror_radius = 5600;
   
-
-  int ioption = 2; // 1=cherenkov, 2=testbeam
-  int coating = true;  
   
 
   PndDrcOptDevSys opt_system;
   
   PndDrcOptBrick sheet(half_width,half_thick,half_length);
-  //sheet.Surface("side6")->SetReflectivity(PndDrcOptReflSilver());
-  //sheet.Surface("side6")->SetReflectivity(PndDrcOptReflPerfect());
   sheet.SetOptMaterial(PndDrcOptMatLithotecQ0());
   sheet.SetName("sheet");
   // move sheet such into positive z space such that end of sheet is at z=0
   sheet.AddTransform(Transform3D(XYZVector(0,0,half_length)));
-  //sheet.SetVerbosity(4);
+  //sheet.SetVerbosity(5);
   
+
+  // downstream mirror
+  if (focus_option == 0 || focus_option == 1)
+    {
+      sheet.Surface("side6")->SetReflectivity(PndDrcOptReflSilver());      
+    }
+
   opt_system.AddDevice(sheet);
 
+  if (focus_option == 2)
+    {
+      PndDrcOptLens mirror(half_width,half_thick,10/2,9999,mirror_radius); // spherical mirror
+      mirror.SetOptMaterial(PndDrcOptMatLithotecQ0());
+      mirror.SetName("mirror");
+      mirror.Surface("side6")->SetReflectivity(PndDrcOptReflSilver()); // the mirror
+      mirror.AddTransform(Transform3D(XYZVector(0,0, 2*half_length+10/2)));
+      mirror.SetPrintColor(2);  
+      opt_system.AddDevice(mirror);
+      opt_system.CoupleDevice("mirror","sheet",         "side1","side6");
+    }
+
+  // upstream cylindrical lens... or not
 
   // create two adjacent lenses, one made of quartz and one made of air.
   // and embedd the into sheet.
   // since the curvature is around the y-axis, create it rotated by 90 degree and rotate it back
   double lens_body_hthick = 5.0;
+
+  if (focus_option == 1)
+    {
+      PndDrcOptCylLens lens_quartz(half_thick,half_width,lens_body_hthick,lens_radius,99999.9);
+      lens_quartz.SetOptMaterial(PndDrcOptMatLithotecQ0());
+      lens_quartz.AddTransform(Transform3D(XYZVector(0,0,-lens_body_hthick)));
+      lens_quartz.AddTransform(Transform3D(RotationZ(kPi/2)));
+      lens_quartz.SetName("lens_quartz");
+      lens_quartz.SetPrintColor(2);
+      if (coating) lens_quartz.Surface("side1")->SetReflectivity(PndDrcOptReflGeffcken());
+      //lens_quartz.SetVerbosity(5);
+
+      opt_system.AddDevice(lens_quartz);
+      
+      PndDrcOptCylLens lens_air(half_thick,half_width,lens_body_hthick,99999.9,-lens_radius);
+      lens_air.AddTransform(Transform3D(XYZVector(0,0,-(2+1)*lens_body_hthick)));
+      lens_air.AddTransform(Transform3D(RotationZ(kPi/2)));
+      //lens_air.SetOptMaterial(PndDrcOptMatVacuum());
+      lens_air.SetOptMaterial(PndDrcOptMatVacuum());
+      lens_air.SetName("lens_air");
+      lens_air.SetPrintColor(4);
+      //lens_air.SetVerbosity(5);
+      //if (lens_air.Surface("side6")) lens_air.Surface("side6")->SetVerbosity(5);
+      
+      lens_air.Surface("side56")->SetPrintColor(5);
+      lens_air.Surface("side51")->SetPrintColor(5);
+      
+      opt_system.AddDevice(lens_air);
+      
+      // make the air gap extent larger than the lens
+      PndDrcOptBrick addition_top(2*half_thick,half_width,lens_body_hthick);
+      addition_top.SetOptMaterial(PndDrcOptMatVacuum());
+      addition_top.SetName("addition_top");
+      addition_top.SetPrintColor(4);
+      PndDrcOptBrick addition_bot(addition_top);
+      addition_bot.SetName("addition_bot");
+      
+      addition_top.AddTransform(Transform3D(XYZVector(3*half_thick,0,-(2+1)*lens_body_hthick)));
+      addition_top.AddTransform(Transform3D(RotationZ(kPi/2)));
+      addition_bot.AddTransform(Transform3D(XYZVector(-3*half_thick,0,-(2+1)*lens_body_hthick)));
+      addition_bot.AddTransform(Transform3D(RotationZ(kPi/2)));
+      
+      opt_system.AddDevice(addition_top);
+      opt_system.AddDevice(addition_bot);
+      
+      // couple surface 1 of device 1 with surface 2 of device 2
+      //                       dev1   dev2    surf1     surf2
+      opt_system.CoupleDevice("sheet"        ,"lens_quartz"  ,"side1", "side6");
+      opt_system.CoupleDevice("lens_quartz"  ,"lens_air"     ,"side1", "side6");
+      
+      opt_system.CoupleDevice("addition_top" ,"lens_air",     "side3", "side56");
+      opt_system.CoupleDevice("addition_top" ,"lens_air",     "side3", "side51");
+      
+      opt_system.CoupleDevice("addition_bot" ,"lens_air",     "side5", "side36");
+      opt_system.CoupleDevice("addition_bot" ,"lens_air",     "side5", "side31");
+    }
   
-
-
-  
-
-  PndDrcOptCylLens lens_quartz(half_thick,half_width,lens_body_hthick,lens_radius,99999.9);
-  lens_quartz.SetOptMaterial(PndDrcOptMatLithotecQ0());
-  lens_quartz.AddTransform(Transform3D(XYZVector(0,0,-lens_body_hthick)));
-  lens_quartz.AddTransform(Transform3D(RotationZ(kPi/2)));
-  lens_quartz.SetName("lens_quartz");
-  lens_quartz.SetPrintColor(2);
-  if (coating) lens_quartz.Surface("side1")->SetReflectivity(PndDrcOptReflGeffcken());
-  //lens_quartz.SetVerbosity(5);
-
-  
-
-  //lens_quartz.SetVerbosity(5);
-  
-  opt_system.AddDevice(lens_quartz);
-
-  PndDrcOptCylLens lens_air(half_thick,half_width,lens_body_hthick,99999.9,-lens_radius);
-  lens_air.AddTransform(Transform3D(XYZVector(0,0,-(2+1)*lens_body_hthick)));
-  lens_air.AddTransform(Transform3D(RotationZ(kPi/2)));
-  //lens_air.SetOptMaterial(PndDrcOptMatVacuum());
-  lens_air.SetOptMaterial(PndDrcOptMatVacuum());
-  lens_air.SetName("lens_air");
-  lens_air.SetPrintColor(4);
-  //lens_air.SetVerbosity(5);
-  //if (lens_air.Surface("side6")) lens_air.Surface("side6")->SetVerbosity(5);
-
-  lens_air.Surface("side56")->SetPrintColor(5);
-  lens_air.Surface("side51")->SetPrintColor(5);
-
-
-  opt_system.AddDevice(lens_air);
-
-  // make the air gap extent larger than the lens
-  PndDrcOptBrick addition_top(2*half_thick,half_width,lens_body_hthick);
-  addition_top.SetOptMaterial(PndDrcOptMatVacuum());
-  addition_top.SetName("addition_top");
-  addition_top.SetPrintColor(4);
-  PndDrcOptBrick addition_bot(addition_top);
-  addition_bot.SetName("addition_bot");
-  
-  addition_top.AddTransform(Transform3D(XYZVector(3*half_thick,0,-(2+1)*lens_body_hthick)));
-  addition_top.AddTransform(Transform3D(RotationZ(kPi/2)));
-  addition_bot.AddTransform(Transform3D(XYZVector(-3*half_thick,0,-(2+1)*lens_body_hthick)));
-  addition_bot.AddTransform(Transform3D(RotationZ(kPi/2)));
-
-  opt_system.AddDevice(addition_top);
-  opt_system.AddDevice(addition_bot);
-
-
-
-
-  // expansion box
+// expansion box
   double ex_box_hw = 300;
   double ex_box_ht = 300;
   double ex_box_hl = 150;
@@ -183,33 +221,33 @@ int main(int argc, char *argv[])
   ex_box.SetOptMaterial(PndDrcOptMatMarcol7());
   ex_box.SetName("expansion box");
   //ex_box.Surface("side6")->SetPixel();
-  ex_box.AddTransform(Transform3D(XYZVector(0,0,-ex_box_hl-(2+2)*lens_body_hthick)));
+  if (focus_option==1)
+    {
+      ex_box.AddTransform(Transform3D(XYZVector(0,0,-ex_box_hl-(2+2)*lens_body_hthick)));
+    }
+  else
+    {
+      ex_box.AddTransform(Transform3D(XYZVector(0,0,-ex_box_hl)));
+    }
+  
   ex_box.Surface("side1")->SetPixel();
   if (coating) ex_box.Surface("side6")->SetReflectivity(PndDrcOptReflGeffcken());
- 
-
   opt_system.AddDevice(ex_box);
   
+
+
+  if (focus_option==1)
+    {
+      opt_system.CoupleDevice("lens_air"     ,"expansion box","side1", "side6");
+      opt_system.CoupleDevice("addition_top" ,"expansion box","side1", "side6");
+      opt_system.CoupleDevice("addition_bot" ,"expansion box","side1", "side6");
+    }
+  else
+    {
+      opt_system.CoupleDevice("sheet" ,"expansion box","side1", "side6");
+    }
   
-  // coupling
-  // -z                                                                                                +z
-  // (side1--ex_box--side6)  (side1--lens_air--side6)  (side1--lena_quartz--side6) (side1_sheet--side6)
-
-
-
-  // couple surface 1 of device 1 with surface 2 of device 2
-  //                       dev1   dev2    surf1     surf2
-  opt_system.CoupleDevice("sheet"        ,"lens_quartz"  ,"side1", "side6");
-  opt_system.CoupleDevice("lens_quartz"  ,"lens_air"     ,"side1", "side6");
-  opt_system.CoupleDevice("lens_air"     ,"expansion box","side1", "side6");
   
-  opt_system.CoupleDevice("addition_top" ,"lens_air",     "side3", "side56");
-  opt_system.CoupleDevice("addition_top" ,"lens_air",     "side3", "side51");
-  opt_system.CoupleDevice("addition_top" ,"expansion box","side1", "side6");
-
-  opt_system.CoupleDevice("addition_bot" ,"lens_air",     "side5", "side36");
-  opt_system.CoupleDevice("addition_bot" ,"lens_air",     "side5", "side31");
-  opt_system.CoupleDevice("addition_bot" ,"expansion box","side1", "side6");
 
   // The manager must be created as pointer.		
   // It is created as singleton, that is only 
@@ -240,7 +278,7 @@ int main(int argc, char *argv[])
   // .x Geo.C 
   // .x Screen.C
   // 
-  manager->Print(geo);
+  if (ioption==2) manager->Print(geo);
   //
   // the intention is to play around with routines.
   // there hast to come another geo output after propagation...
@@ -249,45 +287,37 @@ int main(int argc, char *argv[])
   // create a list of photons in sheet
 
   XYZPoint  pos(0,-half_thick-10.0,half_length);
-  XYZVector dir(0,1,-1); 
+  XYZVector dir(0,1,1); 
   double   beta = 0.80;//0.684;
   bool photons_exist = false;
   
   if (ioption==1)
     {
-      photons_exist = manager->Cerenkov(pos,dir,beta,0,1e16,400,405); // generate photons
-      /*
-	list<PndDrcPhoton> list_photon = manager->PhotonList();
-	list<PndDrcPhoton>::const_iterator  kiter=list_photon.begin();
-	kiter++;
-	PndDrcPhoton ph = (*kiter);
-	list_photon.clear();
-	list_photon.push_back(ph);
-	manager->SetPhotonList(list_photon,"sheet");
-      */
-
+      photons_exist = manager->Cerenkov(pos,dir,beta,100000); // generate photons
     }
   else
     {
       PndDrcPhoton ph;
       ph.SetReflectionLimit(200);  
       list<PndDrcPhoton> list_photon;
-      int imax=30;
+      int imax=3; // rays in one dimension
       for (int ix=0; ix<imax; ix++)
 	//  int ix=0;
 	{
 	  for (int iy=0; iy<imax; iy++)	
 	{
 	  
-	  for (double theta=35; theta<=35; theta+=10)
+	  //for (double theta=35; theta<=35; theta+=10)
+	  double theta = 0;
+	  
 	    {    
 	      ph.SetPosition(XYZPoint(
-				      -half_width+1+ix*2*(half_width-1)/imax,
-				      -half_thick+1+iy*2*(half_thick-1)/imax,
-				      1));
+				      -half_width+1+ix*(2*half_width-2)/(imax-1),
+				      -half_thick+1+iy*(2*half_thick-2)/(imax-1),
+				      half_length));
 	      double z = cos(theta*kPi/180);
 	      double y = sin(theta*kPi/180);
-	      ph.SetDirection(XYZVector(0,y,-z));
+	      ph.SetDirection(XYZVector(0,y,z));
 	      ph.SetWavelength(550);
 	      ph.SetDevice(manager->Device("sheet")); 
 	      //ph.SetSeed();
