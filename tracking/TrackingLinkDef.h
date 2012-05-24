72445971afb4748e3cc026b07b787e89de015688
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-05-24 16:49:52 +0000

    new classes added

--- tracking/TrackingLinkDef.h
+++ tracking/TrackingLinkDef.h
@@ -3,7 +3,12 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class PndTracking+;             
+#pragma link C++ class PndTracking+;
+#pragma link C++ class PndSttConformalFilling+;
+#pragma link C++ class PndGlpkFits+;
+#pragma link C++ class PndCTFindTrackInXY+;
+#pragma link C++ class PndCTGeometryCalculations+;
+#pragma link C++ class PndMergeSort+;
 
 
 #endif
