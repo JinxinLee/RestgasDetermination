985cc3c5fdb5ff068898f08da34b776f7a0326a4
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-05-25 18:34:08 +0000

    All crashes connected to HitsinBoxConformal etc. solved.

--- tracking/PndTrkSttConformalFilling.h
+++ tracking/PndTrkSttConformalFilling.h
@@ -1,6 +1,6 @@
 #ifndef PndTrkSttConformalFilling_H
 #define PndTrkSttConformalFilling_H 1
-#include <vector>
+
 // Root includes
 #include "TROOT.h"
 
@@ -23,22 +23,22 @@ class PndTrkSttConformalFilling : public TObject
 
 	UShort_t *FiConformalIndex,
 	void* HitsinBoxConf,
-	std::vector <bool> *InclusionListStt,
+	bool *InclusionListStt,
 	Double_t infoparalConformal[][5],
-	std::vector <UShort_t> *ListSttParHits,
+	UShort_t *ListSttParHits,
 	UShort_t MAXHITSINCELL,
 	void* nBoxConf,
 	UShort_t NFIDIVCONFORMAL,
 	UInt_t Nparal,
 	UShort_t NRDIVCONFORMAL,
-	std::vector <Double_t> *radiaConf,
+	Double_t *radiaConf,
 	UShort_t *RConformalIndex
 	);
 
   void FromXYtoConformal(
 	Double_t trajectory_vertex[2],
 	Double_t info[][7],
-	std::vector <UShort_t> *ListSttParHits,
+	UShort_t *ListSttParHits,
 	UInt_t Nparal,
 	Double_t infoparalConformal[][5],
 	Double_t STRAWRADIUS
