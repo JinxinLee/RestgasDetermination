934953879b761368ac3b5f973557260fcc796e25
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-07-12 10:41:01 +0000

    adding a method

--- tracking/PndTrkPrintouts.h
+++ tracking/PndTrkPrintouts.h
@@ -3,11 +3,13 @@
 
 #include "FairMCPoint.h"
 #include "PndSttTube.h"
+
 // Root includes
+
 #include "TROOT.h"
 
 
-class PndTrkPrintouts : public TObject
+class PndTrkPrintouts
 {
 
 
@@ -18,7 +20,31 @@ class PndTrkPrintouts : public TObject
  /** Destructor **/
  ~PndTrkPrintouts(){};
 
-void stampaSttHits(
+ void stampaMvdHits(
+	char *fMvdPixelBranch,
+	char *fMvdStripBranch,
+	Short_t nMvdPixelHit,
+	Short_t nMvdStripHit,
+	Double_t *refindexMvdPixel,
+	Double_t *refindexMvdStrip,
+	Double_t *sigmaXMvdPixel,
+	Double_t *sigmaXMvdStrip,
+	Double_t *sigmaYMvdPixel,
+	Double_t *sigmaYMvdStrip,
+	Double_t *sigmaZMvdPixel,
+	Double_t *sigmaZMvdStrip,
+	Double_t *XMvdPixel,
+	Double_t *XMvdStrip,
+	Double_t *YMvdPixel,
+	Double_t *YMvdStrip,
+	Double_t *ZMvdPixel,
+	Double_t *ZMvdStrip
+	);
+
+
+
+
+ void stampaSttHits(
 	Short_t iHit,
 	Short_t ipunto,
 	Double_t dradius,
@@ -30,6 +56,28 @@ void stampaSttHits(
 	);
 
 
+ void stampetta(
+	int IVOLTE,
+	bool *keepit,
+	Short_t *ListMvdPixelHitsinTrack,
+	Short_t *ListMvdStripHitsinTrack,
+	Short_t *ListSttParHitsinTrack,
+	Short_t *ListSttSkewHitsinTrack,
+	Short_t *ListSciTilHitsinTrack,
+	Short_t *nMvdPixelHitsinTrack,
+	Short_t *nMvdStripHitsinTrack,
+	Short_t *nSttParHitsinTrack,
+	Short_t *nSttSkewHitsinTrack,
+	Short_t *nSciTilHitsinTrack,
+	Short_t nSttTrackCand,
+	int MAXMVDPIXELHITSINTRACK,
+	int MAXMVDSTRIPHITSINTRACK,
+	int MAXSCITILHITSINTRACK,
+	int MAXSTTHITSINTRACK,
+	Double_t *R,
+	Double_t *Ox,
+	Double_t *Oy
+	);
 
 
 
