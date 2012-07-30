7e5eef04ef99e339c3e9726453d968da6f6c5b84
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2012-07-30 09:03:41 +0000

    add histograms for residuals check

--- macro/lmd/CreateHitsForAlignment.C
+++ macro/lmd/CreateHitsForAlignment.C
@@ -54,6 +54,54 @@
 #include<FairTrackParH.h>
 
 using namespace std;
+
+void combitransToLumiFrame(TVector3& hitPos){
+  //do the transformation from lab frame to LUMI frame (with z-axis perp. to lumi planes)
+  const Double_t  kHalfFoilThickness  = 0.0075; // Thickness of sensitive foil (cm)
+  const Double_t  kTransZ = 1100.; //(cm) //move at z-position
+  const Double_t  kRotUmZ = 476.03; //(cm) //z-point to rotate
+  const Double_t  kTransX = 25; //(cm) //move at x-position
+  const Double_t  kRot =  0.040596358401388; // 2.326 degree  = 4.05963584013881024e-02 rad
+  TVector3 LumiTrans(0,0,kRotUmZ);
+  hitPos -=LumiTrans;
+  hitPos.RotateY(-kRot);
+  LumiTrans = TVector3(0,0,kTransZ-kRotUmZ);
+  hitPos -=LumiTrans;
+  // cout<<"!!! NEW HIT position in LUMI frame!!! "<<endl;
+  //  hitPos.Print();
+}
+
+// void rotateToLumiFrame(TVector3& hitPos){
+//   TMatrixD hitMtx(3,3);
+//   hitMtx[0][0] = hitPos[0];
+//   hitMtx[1][0] = hitPos[1];
+//   hitMtx[2][0] = hitPos[2];
+//   TMatrixD res = rotateToLumiFrame(hitMtx);
+//   hitPos = TVector3(hitMtx(0,0),hitMtx(1,0),hitMtx(2,0));
+// }
+// TMatrixD rotateToLumiFrame(TMatrixD& hitCov){
+//   Double_t theta=2.326;
+//   Double_t degrad = TMath::Pi()/180.;
+//   Double_t sintheta = TMath::Sin(degrad*theta);
+//   Double_t costheta = TMath::Cos(degrad*theta);
+//   TMatrixD rot(3,3);// Rotation around Y axis
+//   rot[0][0]= costheta;
+//   rot[0][1]= 0;
+//   rot[0][2]= sintheta;
+//   rot[1][0]= 0;
+//   rot[1][1]= 1;
+//   rot[1][2]= 0;
+//   rot[2][0]= -sintheta;
+//   rot[2][1]= 0;
+//   rot[2][2]= costheta;
+//   TMatrixD result = rot;
+//   result.T();
+//   result*=hitCov;
+//   hitCov = result;
+//   result*=rot;
+//   return result;
+// }
+
 int main(int __argc,char *__argv[]) {
   //TODO: read this like params!
   //  const int nEvents=500000;
@@ -173,6 +221,12 @@ int main(int __argc,char *__argv[]) {
   recTrack += ".root";
   TChain tTrkRec("cbmsim");
   tTrkRec.Add(recTrack);
+
+  TString simMC=storePath+"/Lumi_MC_";
+  simMC += startEvent;
+  simMC += ".root";
+  TChain tMC("cbmsim");
+  tMC.Add(simMC);
   
   // ---------------------------------------------------------------------------------
 
@@ -199,12 +253,23 @@ int main(int __argc,char *__argv[]) {
   tTrkRec.SetBranchAddress("LMDTrack",&rec_trk);  //Tracks
   //----------------------------------------------------------------------------------  
   
+  //--- MC info -----------------------------------------------------------------
+  TClonesArray* true_tracks=new TClonesArray("PndMCTrack");
+  tMC.SetBranchAddress("MCTrack",&true_tracks);  //True Track to compare
+  
+  TClonesArray* true_points=new TClonesArray("PndSdsMCPoint");
+  tMC.SetBranchAddress("LMDPoint",&true_points);  //True Points to compare
+  //----------------------------------------------------------------------------------
+
   //Histograms with residuals---------------------------------------------------------
  //Create and fill histogramms 
  TObjArray *m_res_x = new TObjArray;
  TObjArray *m_res_y = new TObjArray;
  TObjArray *m_res_r = new TObjArray;
 
+ TObjArray *m_res_mc_x = new TObjArray;
+ TObjArray *m_res_mc_y = new TObjArray;
+ TObjArray *m_res_mc_z = new TObjArray;
 
  double misal_scales[3];
  const double scaleX = 0.05;
@@ -218,13 +283,18 @@ int main(int __argc,char *__argv[]) {
   (scaleY == 0) ? misal_scales[1] = 1. : misal_scales[1] = 1.*scaleY;
   (scaleR == 0) ? misal_scales[2] = 1. : misal_scales[2] = 1.*scaleR;
 
-
+  TH2F *resx = new TH2F("resx","residuals vs. x; x, cm;#delta_{x}, cm",100,-15,15,1000,-1e-1,1e-1);
+  TH2F *resy = new TH2F("resy","residuals vs. y; y, cm;#delta_{y}, cm",100,-15,15,1000,-1e-1,1e-1);
+  TH2F *resxy = new TH2F("resxy","residuals y vs. residuals x; #delta_{x}, cm; #delta_{y}, cm",1000,-1e-1,1e-1,1000,-1e-1,1e-1);
  for (unsigned int histID = 0; histID < 32; ++histID)
  {
     char histoResXName[100];
     char histoResYName[100];
     char histoResRName[100];
    
+    char histoResMCXName[100];
+    char histoResMCYName[100];
+    char histoResMCZName[100];
     sprintf(histoResXName, "residuals_x_%d",histID);
     TH1F* aHisto = new TH1F(histoResXName, histoResXName, 1000, -misal_scales[0], misal_scales[0]);
     m_res_x->Add(aHisto);
@@ -237,6 +307,19 @@ int main(int __argc,char *__argv[]) {
     sprintf(histoResRName, "residuals_r_%d",histID);
     TH1F* aHisto3 = new TH1F(histoResRName, histoResRName, 1000, -misal_scales[2], misal_scales[2]);
     m_res_r->Add(aHisto3);
+
+    sprintf(histoResMCXName, "residuals_mc_x_%d",histID);
+    TH1F* aHistomc = new TH1F(histoResMCXName, histoResMCXName, 1000, -misal_scales[0], misal_scales[0]);
+    m_res_mc_x->Add(aHistomc);
+
+    sprintf(histoResMCYName, "residuals_mc_y_%d",histID);
+    TH1F* aHistomc2 = new TH1F(histoResMCYName, histoResMCYName, 1000, -misal_scales[1], misal_scales[1]);
+
+    m_res_mc_y->Add(aHistomc2);
+
+    sprintf(histoResMCZName, "residuals_mc_z_%d",histID);
+    TH1F* aHistomc3 = new TH1F(histoResMCZName, histoResMCZName, 1000, -misal_scales[2], misal_scales[2]);
+    m_res_mc_z->Add(aHistomc3);
  }
   //----------------------------------------------------------------------------------
 
@@ -246,6 +329,7 @@ int main(int __argc,char *__argv[]) {
     tTrkRec.GetEntry(j);
     tHits.GetEntry(j);
     tdigiHits.GetEntry(j);
+    tMC.GetEntry(j);
     ///-----------------------------------------------------------------------------------------
 
     const int nRecTrks = rec_trk->GetEntriesFast();
@@ -257,12 +341,48 @@ int main(int __argc,char *__argv[]) {
       Int_t candID = trk_lin->GetTCandID();
       PndTrackCand *trkcand = (PndTrackCand*)trkcand_array->At(candID);
       const int Ntrkcandhits= trkcand->GetNHits();
+      if(Ntrkcandhits<4) continue; //!!! TEST with 4 hits tracks only !!!
       double phiMCgl;
 	for (Int_t iHit = 0; iHit < Ntrkcandhits; iHit++){
 	  PndTrackCandHit candhit = (PndTrackCandHit)(trkcand->GetSortedHit(iHit));
 	  Int_t hitID = candhit.GetHitId();
 	  PndSdsHit* myHit = (PndSdsHit*)(rechit_array->At(hitID));
-	  TVector3 HitPos = myHit->GetPosition(); 
+
+	///Top cluster
+	PndSdsClusterStrip* myCluster =  (PndSdsClusterStrip*)(fStripClusterArray->At(myHit->GetClusterIndex()));
+	PndSdsDigiStrip* astripdigi = (PndSdsDigiStrip*)(fStripDigiArray->At(myCluster->GetDigiIndex(0)));
+	if (astripdigi->GetIndex(0) == -1) continue; // sort out noise
+	PndSdsMCPoint* MCPointTop = (PndSdsMCPoint*)(true_points->At(astripdigi->GetIndex(0)));
+	// double zTop = MCPointTop->GetZ();
+	// double xTop = MCPointTop->GetX();
+	// double yTop = MCPointTop->GetY();
+	TVector3 mcTop = MCPointTop->GetPosition(); 
+	TVector3 mcTopOUT = MCPointTop->GetPositionOut();
+	// ///Bottom cluster
+	// Int_t  botIndex = myHit->GetBotIndex();
+	// PndSdsClusterStrip* myClusterBot =  (PndSdsClusterStrip*)(fStripClusterArray->At(botIndex));
+	// PndSdsDigiStrip* astripdigiBot = (PndSdsDigiStrip*)(fStripDigiArray->At(myClusterBot->GetDigiIndex(0)));
+	// if (astripdigiBot->GetIndex(0) == -1) continue; // sort out noise
+	// PndSdsMCPoint* MCPointBot = (PndSdsMCPoint*)(true_points->At(astripdigiBot->GetIndex(0)));
+	// TVector3 mcBot = MCPointBot->GetPosition(); 
+	// mcTop.Print();
+	// cout<<"mid: "<<endl;
+	// mcBot.Print();
+	// // double zBot = MCPointBot->GetZ();
+	// // double xBot = MCPointBot->GetX();
+	// // double yBot = MCPointBot->GetY();
+
+	TVector3 HitPos = myHit->GetPosition(); 
+	combitransToLumiFrame(mcTop);
+	combitransToLumiFrame(mcTopOUT);
+	// combitransToLumiFrame(mcBot);
+	TVector3 mcMid = mcTop+(mcTopOUT-mcTop)*0.5;
+
+	TVector3 ResRecMC = HitPos - mcMid;
+
+	// cout<<"ResRecMC: ";
+	// ResRecMC.Print();
+	// cout<<""<<endl;
 	  double trkX = startlintrk.X()+dirlintrk.X()*(HitPos.Z()-startlintrk.Z());
 	  double trkY = startlintrk.Y()+dirlintrk.Y()*(HitPos.Z()-startlintrk.Z());
 	  double trkR = sqrt(trkX*trkX+trkY*trkY);
@@ -271,6 +391,16 @@ int main(int __argc,char *__argv[]) {
 	  ((TH1F*)m_res_x->At(sensorID))->Fill(HitPos.X()-trkX);
 	  ((TH1F*)m_res_y->At(sensorID))->Fill(HitPos.Y()-trkY);
 	  ((TH1F*)m_res_r->At(sensorID))->Fill(hitR-trkR);
+	  double resx_d = HitPos.X()-trkX;
+	  double resy_d = HitPos.Y()-trkY;
+	  resx->Fill(HitPos.X(),resx_d);
+	  resy->Fill(HitPos.Y(),resy_d);
+	  resxy->Fill(resx_d,resy_d);
+	  if(fabs(resx_d)>0.1 || fabs(resy_d)>0.1)
+	    cout<<"Track #"<<iN<<" from event #"<<j<<" has: resx="<<resx_d<<", resy="<<resy_d<<endl;
+	  ((TH1F*)m_res_mc_x->At(sensorID))->Fill(ResRecMC.X());
+	  ((TH1F*)m_res_mc_y->At(sensorID))->Fill(ResRecMC.Y());
+	  ((TH1F*)m_res_mc_z->At(sensorID))->Fill(ResRecMC.Z());
 	  int endtrk=1;
 	  if((Ntrkcandhits-iHit)>1) endtrk=0;
 	  if(output>0){
@@ -289,6 +419,9 @@ int main(int __argc,char *__argv[]) {
     ((TH1F*)m_res_x->At(histID))->Write();
     ((TH1F*)m_res_y->At(histID))->Write();
     ((TH1F*)m_res_r->At(histID))->Write();
+    ((TH1F*)m_res_mc_x->At(histID))->Write();
+    ((TH1F*)m_res_mc_y->At(histID))->Write();
+    ((TH1F*)m_res_mc_z->At(histID))->Write();
   }
  
 
@@ -311,7 +444,7 @@ int main(int __argc,char *__argv[]) {
   c1->SetBorderMode(0); 
   c1->Divide(4,2);     
   for (unsigned int histID = 8; histID < 16; ++histID){
-    c1->cd(histID+1);
+    c1->cd(histID+1-8);
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitle("(trk-hit)[x], cm");
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_x->At(histID))->SetLineColor(1);
@@ -325,7 +458,7 @@ TCanvas* c2 = new TCanvas("respl3x","residuals, pl#3",200,500,700,800);
   c2->SetBorderMode(0); 
   c2->Divide(4,2);     
   for (unsigned int histID = 16; histID < 24; ++histID){
-    c2->cd(histID+1);
+    c2->cd(histID+1-16);
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitle("(trk-hit)[x], cm");
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_x->At(histID))->SetLineColor(1);
@@ -339,7 +472,7 @@ TCanvas* c3 = new TCanvas("respl4x","residuals, pl#4",200,500,700,800);
   c3->SetBorderMode(0); 
   c3->Divide(4,2);     
   for (unsigned int histID = 24; histID < 32; ++histID){
-    c3->cd(histID+1);
+    c3->cd(histID+1-24);
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitle("(trk-hit)[x], cm");
     ((TH1F*)m_res_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_x->At(histID))->SetLineColor(1);
@@ -367,8 +500,10 @@ TCanvas* c3 = new TCanvas("respl4x","residuals, pl#4",200,500,700,800);
   c11->SetFillColor(0);
   c11->SetBorderMode(0); 
   c11->Divide(4,2);     
+  int i11=1;
   for (unsigned int histID = 8; histID < 16; ++histID){
-    c11->cd(histID+1);
+    c11->cd(i11);
+    i11++;
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitle("(trk-hit)[y], cm");
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_y->At(histID))->SetLineColor(1);
@@ -377,12 +512,14 @@ TCanvas* c3 = new TCanvas("respl4x","residuals, pl#4",200,500,700,800);
  c11->Update();
  c11->Write();
 
+ i11=1;
 TCanvas* c21 = new TCanvas("respl3y","residuals, pl#3",200,500,700,800);
   c21->SetFillColor(0);
   c21->SetBorderMode(0); 
   c21->Divide(4,2);     
   for (unsigned int histID = 16; histID < 24; ++histID){
-    c21->cd(histID+1);
+    c21->cd(i11);
+    i11++;
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitle("(trk-hit)[y], cm");
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_y->At(histID))->SetLineColor(1);
@@ -391,12 +528,14 @@ TCanvas* c21 = new TCanvas("respl3y","residuals, pl#3",200,500,700,800);
  c21->Update();
  c21->Write();
 
+ i11=1;
 TCanvas* c31 = new TCanvas("respl4y","residuals, pl#4",200,500,700,800);
   c31->SetFillColor(0);
   c31->SetBorderMode(0); 
   c31->Divide(4,2);     
   for (unsigned int histID = 24; histID < 32; ++histID){
-    c31->cd(histID+1);
+    c31->cd(i11);
+    i11++;
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitle("(trk-hit)[y], cm");
     ((TH1F*)m_res_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
     ((TH1F*)m_res_y->At(histID))->SetLineColor(1);
@@ -405,6 +544,192 @@ TCanvas* c31 = new TCanvas("respl4y","residuals, pl#4",200,500,700,800);
  c31->Update();
  c31->Write();
 
+ //---------------------------------------------
+
+  TCanvas* c0mc = new TCanvas("resmcpl1x","residuals, pl#1",200,500,700,800);
+  c0mc->SetFillColor(0);
+  c0mc->SetBorderMode(0); 
+  c0mc->Divide(4,2);     
+  for (unsigned int histID = 0; histID < 8; ++histID){
+    c0mc->cd(histID+1);
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitle("(rec-mc)[x], cm");
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_x->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_x->At(histID))->Draw();
+  }
+ c0mc->Update();
+ c0mc->Write();
+
+ TCanvas* c1mc = new TCanvas("resmcpl2x","residuals, pl#2",200,500,700,800);
+  c1mc->SetFillColor(0);
+  c1mc->SetBorderMode(0); 
+  c1mc->Divide(4,2);     
+  for (unsigned int histID = 8; histID < 16; ++histID){
+    c1mc->cd(histID+1-8);
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitle("(rec-mc)[x], cm");
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_x->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_x->At(histID))->Draw();
+  }
+ c1mc->Update();
+ c1mc->Write();
+
+TCanvas* c2mc = new TCanvas("resmcpl3x","residuals, pl#3",200,500,700,800);
+  c2mc->SetFillColor(0);
+  c2mc->SetBorderMode(0); 
+  c2mc->Divide(4,2);     
+  for (unsigned int histID = 16; histID < 24; ++histID){
+    c2mc->cd(histID+1-16);
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitle("(rec-mc)[x], cm");
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_x->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_x->At(histID))->Draw();
+  }
+ c2mc->Update();
+ c2mc->Write();
+
+TCanvas* c3mc = new TCanvas("resmcpl4x","residuals, pl#4",200,500,700,800);
+  c3mc->SetFillColor(0);
+  c3mc->SetBorderMode(0); 
+  c3mc->Divide(4,2);     
+  for (unsigned int histID = 24; histID < 32; ++histID){
+    c3mc->cd(histID+1-24);
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitle("(rec-mc)[x], cm");
+    ((TH1F*)m_res_mc_x->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_x->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_x->At(histID))->Draw();
+  }
+ c3mc->Update();
+ c3mc->Write();
+
+
+ TCanvas* c01mc = new TCanvas("resmcpl1y","residuals, pl#1",200,500,700,800);
+  c01mc->SetFillColor(0);
+  c01mc->SetBorderMode(0); 
+  c01mc->Divide(4,2);     
+  for (unsigned int histID = 0; histID < 8; ++histID){
+    c01mc->cd(histID+1);
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_y->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_y->At(histID))->Draw();
+  }
+ c01mc->Update();
+ c01mc->Write();
+
+ TCanvas* c11mc = new TCanvas("resmcpl2y","residuals, pl#2",200,500,700,800);
+  c11mc->SetFillColor(0);
+  c11mc->SetBorderMode(0); 
+  c11mc->Divide(4,2);     
+  i11=1;
+  for (unsigned int histID = 8; histID < 16; ++histID){
+    c11mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_y->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_y->At(histID))->Draw();
+  }
+ c11mc->Update();
+ c11mc->Write();
+
+ i11=1;
+TCanvas* c21mc = new TCanvas("resmcpl3y","residuals, pl#3",200,500,700,800);
+  c21mc->SetFillColor(0);
+  c21mc->SetBorderMode(0); 
+  c21mc->Divide(4,2);     
+  for (unsigned int histID = 16; histID < 24; ++histID){
+    c21mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_y->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_y->At(histID))->Draw();
+  }
+ c21mc->Update();
+ c21mc->Write();
+
+ i11=1;
+TCanvas* c31mc = new TCanvas("resmcpl4y","residuals, pl#4",200,500,700,800);
+  c31mc->SetFillColor(0);
+  c31mc->SetBorderMode(0); 
+  c31mc->Divide(4,2);     
+  for (unsigned int histID = 24; histID < 32; ++histID){
+    c31mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_y->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_y->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_y->At(histID))->Draw();
+  }
+ c31mc->Update();
+ c31mc->Write();
+
+
+ TCanvas* c02mc = new TCanvas("resmcpl1z","residuals, pl#1",200,500,700,800);
+  c02mc->SetFillColor(0);
+  c02mc->SetBorderMode(0); 
+  c02mc->Divide(4,2);     
+  for (unsigned int histID = 0; histID < 8; ++histID){
+    c02mc->cd(histID+1);
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_z->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_z->At(histID))->Draw();
+  }
+ c02mc->Update();
+ c02mc->Write();
+
+ TCanvas* c12mc = new TCanvas("resmcpl2z","residuals, pl#2",200,500,700,800);
+  c12mc->SetFillColor(0);
+  c12mc->SetBorderMode(0); 
+  c12mc->Divide(4,2);     
+  i11=1;
+  for (unsigned int histID = 8; histID < 16; ++histID){
+    c12mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_z->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_z->At(histID))->Draw();
+  }
+ c12mc->Update();
+ c12mc->Write();
+
+ i11=1;
+TCanvas* c22mc = new TCanvas("resmcpl3z","residuals, pl#3",200,500,700,800);
+  c22mc->SetFillColor(0);
+  c22mc->SetBorderMode(0); 
+  c22mc->Divide(4,2);     
+  for (unsigned int histID = 16; histID < 24; ++histID){
+    c22mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_z->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_z->At(histID))->Draw();
+  }
+ c22mc->Update();
+ c22mc->Write();
+
+ i11=1;
+TCanvas* c32mc = new TCanvas("resmcpl4z","residuals, pl#4",200,500,700,800);
+  c32mc->SetFillColor(0);
+  c32mc->SetBorderMode(0); 
+  c32mc->Divide(4,2);     
+  for (unsigned int histID = 24; histID < 32; ++histID){
+    c32mc->cd(i11);
+    i11++;
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitle("(rec-mc)[y], cm");
+    ((TH1F*)m_res_mc_z->At(histID))->GetXaxis()->SetTitleSize(0.05);
+    ((TH1F*)m_res_mc_z->At(histID))->SetLineColor(1);
+    ((TH1F*)m_res_mc_z->At(histID))->Draw();
+  }
+ c32mc->Update();
+ c32mc->Write();
+ resx->Write();
+ resy->Write();
+ resxy->Write();
  f->Close();
 
 }
