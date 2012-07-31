669f5398d1500669591e4a21acbe792a79826ab4
Stefano Spataro <spataro@to.infn.it>
2012-07-31 15:39:14 +0000

    cm

--- macro/pid/run_sim_sttcombi_pgun.C
+++ macro/pid/run_sim_sttcombi_pgun.C
@@ -122,7 +122,7 @@ run_sim_sttcombi_pgun(Int_t nEvents=10, Int_t pid=13, Float_t p1=1.0, Float_t p2
   boxGen->SetPRange(p1,p2); // GeV/c
   boxGen->SetPhiRange(0., 360.); // Azimuth angle range [degree]
   boxGen->SetThetaRange(0.5, 140.); // Polar angle in lab system range [degree]
-  boxGen->SetXYZ(0., 0., 0.); // mm o cm ??
+  boxGen->SetXYZ(0., 0., 0.); // cm 
   primGen->AddGenerator(boxGen); 
 
   // Create and Set Magnetic Field
