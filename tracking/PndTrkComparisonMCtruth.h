55aeb7994a3d5d079b1c34670786d924862422c9
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-08-01 21:05:58 +0000

    old code style version, 100% efficient with single tracks; starting point for improvements

--- tracking/PndTrkComparisonMCtruth.h
+++ tracking/PndTrkComparisonMCtruth.h
@@ -1,6 +1,7 @@
 #ifndef PndTrkComparisonMCtruth_H
 #define PndTrkComparisonMCtruth_H 1
 
+// #include "FairRootManager.h"
 #include "PndMCTrack.h"
 // Root includes
 #include "TROOT.h"
@@ -12,11 +13,15 @@ struct PndTrkComparisonMCtruth_io_Data{
 	Short_t *Charge;
 	Double_t Cvel;
 	Short_t *daTrackFoundaTrackMC;
+	Double_t DIMENSIONSciTil ;
 	Double_t Errorsqpixel;
 	Double_t Errorsqstrip;
 	Double_t *FI0;
 	TClonesArray *fMCTrackArray;
 	TClonesArray *fMvdMCPointArray;
+	Short_t fSciTilMaxNumber;
+	TClonesArray *fSciTHitArray;
+	TClonesArray *fSciTPointArray;
 	FILE     *HANDLE;
 	FILE     *HANDLE2;
 	Double_t  (*info)[7];
@@ -27,6 +32,7 @@ struct PndTrkComparisonMCtruth_io_Data{
 	bool     *InclusionListStt;
 	Short_t  *ListMvdPixelHitsinTrack;
 	Short_t  *ListMvdStripHitsinTrack;
+	Short_t  *ListSciTilHitsinTrack;
 	Short_t  *ListSttParHitsinTrack;
 	Short_t  *ListSttSkewHitsinTrack;
 	Short_t  *ListTrackCandHit;
@@ -45,12 +51,16 @@ struct PndTrkComparisonMCtruth_io_Data{
 	Short_t  *MCSkewAloneList;
 	Short_t  *MvdPixelCommonList;
 	Short_t  *MvdPixelSpuriList;
+	Short_t  *MCSciTilAloneList; // equivalent to a matrix
+			//  [nTotalCandidates][nSciTilHits];
 	Short_t  *MvdStripCommonList;
 	Short_t  *MvdStripSpuriList;
 	Short_t  *nHitsInMCTrack;
+	Short_t  *nHitsInSciTile;
 	Short_t  *nMCMvdPixelAlone;
 	Short_t  *nMCMvdStripAlone;
 	Short_t  *nMCParalAlone;
+	Short_t  *nMCSciTilAlone;
 	Short_t  *nMCSkewAlone;
 	Short_t  *nMvdPixelCommon;
 	Short_t  *nMvdPixelHitsinTrack;
@@ -61,6 +71,10 @@ struct PndTrkComparisonMCtruth_io_Data{
 	Short_t   nMvdStripHit;
 	Short_t  *nMvdStripSpuriinTrack;
 	Short_t  *nParalCommon;
+	Short_t  *nSciTilCommon;
+	Short_t  nSciTilHits;
+	Short_t  *nSciTilHitsinTrack;
+	Short_t  *nSciTilSpuriinTrack;
 	Short_t  *nSkewCommon;
 	Short_t  *nSkewHitsInMCTrack;
 	Short_t  *nSpuriParinTrack;
@@ -69,6 +83,7 @@ struct PndTrkComparisonMCtruth_io_Data{
 	Short_t  *nSttParHitsinTrack;
 	Short_t  *nSttSkewHitsinTrack;
 	Short_t   nTotalCandidates;
+	Short_t  *OriginalSciTilList;
 	Double_t *Ox;
 	Double_t *Oy;
 	Short_t  *ParalCommonList;
@@ -77,15 +92,22 @@ struct PndTrkComparisonMCtruth_io_Data{
 	Double_t *refindexMvdPixel;
 	Double_t *refindexMvdStrip;
 	Short_t  *resultFitSZagain;
+	Short_t  *SciTilCommonList; // equivalent to a matrix
+			// [nTotalCandidates][MAXSCITILHITSINTRACK];
+	Short_t  *SciTilSpuriList; // equivalent to a matrix
+			//  [nTotalCandidates][MAXSCITILHITSINTRACK];
 	Short_t  *SkewCommonList;
 	Short_t  *SkewSpuriList;
 	bool     *SttSZfit;
 	Double_t *XMvdPixel;
 	Double_t *XMvdStrip;
+	Double_t *XSciTilCenter;
 	Double_t *YMvdPixel;
 	Double_t *YMvdStrip;
+	Double_t *YSciTilCenter;
 	Double_t *ZMvdPixel;
 	Double_t *ZMvdStrip;
+	Double_t *ZSciTilCenter;
 	};
 
 
@@ -103,12 +125,34 @@ class PndTrkComparisonMCtruth
  void AssociateFoundTrackstoMCquater(
 	Double_t BFIELD,
 	Double_t CVEL,
+	Short_t *daTrackFoundaTrackMC,
 	TClonesArray *fMCTrackArray,
+	Int_t   *FromPixeltoMCTrack,
+	Int_t   *FromStriptoMCTrack,
+	int *FromSciTiltoMCTrackList,	// it is an array of
+		//  dimension   nSciTilHits*nMCTracks, equivalent to a matrix
+		//  of dimension  [nSciTilHits][nMCTracks].
 	bool *keepit,
 	Double_t info[][7],
+	Short_t *ListHitsinTrack,
+	Short_t *ListPixelHitsinTrack,
+	Short_t *ListSciTilHitsinTrack,
+	Short_t *ListSkewHitsinTrack,
+	Short_t *ListStripHitsinTrack,
 	int MAXMVDPIXELHITSINTRACK,
 	int MAXMVDSTRIPHITSINTRACK,
+	int MAXSCITILHITSINTRACK,
 	int MAXSTTHITSINTRACK,
+	int *nFromSciTiltoMCTrack,
+	Short_t *nHitsinTrack,
+	int nMCTracks,
+	Short_t *nPixelHitsinTrack,
+	Short_t nSciTilHits,
+	Short_t *nSciTilHitsinTrack, // equivalen to a matrix of
+		// dimension [MAXTRACKSPEREVENT][MAXSCITILHITSINTRACK]
+	Short_t *nSkewHitsinTrack,
+	Short_t *nStripHitsinTrack,
+	Short_t  nTracksFoundSoFar,
 	Double_t *Ox,
 	Double_t *Oy,
 	Double_t *R,
@@ -117,19 +161,7 @@ class PndTrkComparisonMCtruth
 	Double_t *X2,
 	Double_t *Y2,
 	Double_t *X3,
-	Double_t *Y3,
-	Short_t  nTracksFoundSoFar,
-	Short_t *nHitsinTrack,
-	Short_t *ListHitsinTrack,
-	Short_t *nSkewHitsinTrack,
-	Short_t *ListSkewHitsinTrack,
-	Short_t *nPixelHitsinTrack,
-	Short_t *ListPixelHitsinTrack,
-	Int_t   *FromPixeltoMCTrack,
-	Short_t *nStripHitsinTrack,
-	Short_t *ListStripHitsinTrack,
-	Int_t   *FromStriptoMCTrack,
-	Short_t *daTrackFoundaTrackMC
+	Double_t *Y3
 					);
 
  int ComparisonwithMC(
@@ -207,6 +239,51 @@ void MvdMatchtoMC(
 	Int_t *FromPixeltoMCTrack,	// output
 	Int_t *FromStriptoMCTrack	// output
 	);
+
+ void SciTilMatchtoMC(
+	Double_t BFIELD,
+	Double_t CVEL,
+	Double_t DIMENSIONSCITIL,
+	TClonesArray *fMCTrackArray,
+	int *FromSciTiltoMCTrackList,	// output; it is an array of
+	//  dimension   nSciTilHits*nMCTracks, equivalent to a matrix
+	//  of dimension  [nSciTilHits][nMCTracks].
+	TClonesArray *fSciTHitArray,
+	Short_t fSciTilMaxNumber,
+	TClonesArray *fSciTPointArray,
+	int *nFromSciTiltoMCTrack, // output; nFromSciTiltoMCTrack[nSciTilHits].
+	Short_t *nHitsInSciTile,
+	int nMCTracks,
+	Short_t nSciTilHits,
+	Short_t *OriginalSciTilList,
+	Double_t *XSciTilCenter,
+	Double_t *YSciTilCenter,
+	Double_t *ZSciTilCenter
+		);
+
+ void SciTilMatchedSpurioustoTrackCand(
+	Short_t *daTrackFoundaTrackMC,	// input
+	int *FromSciTiltoMCTrackList,	// input; equivalent to a matrix
+					//  of dimension  [nSciTilHits][nMCTracks]
+	bool *keepit,			// input
+	Short_t *ListSciTilHitsinTrack,	// input; equivalent to a matrix of dimension
+					// [MAXTRACKSPEREVENT][MAXSCITILHITSINTRACK]
+	int MAXSCITILHITSINTRACK,	// input
+	Short_t *MCSciTilAloneList,	// output; equivalent to a matrix of dimension
+					// [MAXTRACKSPEREVENT][MAXSCITILHITSINTRACK]
+	int *nFromSciTiltoMCTrack,	// input
+	Short_t *nMCSciTilAlone,	// output
+	int nMCTracks,			// input
+	Short_t nSciTilHits,		// input
+	Short_t  *nSciTilHitsinTrack,	// input
+	Short_t *nSciTilCommon,		// output
+	Short_t *nSciTilSpuriinTrack,	// output
+	Short_t nSttTrackCand,		// input
+	Short_t *SciTilCommonList,	// output; equivalent to a matrix of dimension
+					// [MAXTRACKSPEREVENT][MAXSCITILHITSINTRACK]
+	Short_t *SciTilSpuriList	// output; equivalent to a matrix of dimension
+					// [MAXTRACKSPEREVENT][MAXSCITILHITSINTRACK].
+			);
 
  void stampaMCTracks(
 	Double_t BFIELD,
