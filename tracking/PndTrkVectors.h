5ca88e1f247f9d60cdd63ea4f1f72d7a1fa61ef2
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-08-17 19:19:40 +0000

    adding the check on the dimension and some printouts; now the [] operator call the at() operator.

--- tracking/PndTrkVectors.h
+++ tracking/PndTrkVectors.h
@@ -8,6 +8,26 @@
 
 using namespace std ;
 
+// this class is useful to mimic the []  operator
+// of an array  or the  at() operator of the  std::vector
+// but it contains also the boundary check. If the boundary
+// are violated an error message is printed and a call
+// to the exit  function  with value -1 is performed.
+// The argument for the constructor are :
+//  p   -->  pointer to an EXISTING array(of ANY type) to be mimiced;
+//  dim -->  dimension of the array to be mimiced;
+//  nam -->  string containing the  name of the Vec class
+//	     that mimics the array.
+
+//  The typical statements for producing the Vec mimicing
+//  an existing array of type  MYTYPE is :
+
+/*
+  MYTYPE   array[100];      the array must physically exist in memory;
+  Vec <MYTYPE> myvec(array, 100, "myvec");
+*/
+
+
 template <class T> class Vec{
 
  public:
@@ -16,7 +36,13 @@ template <class T> class Vec{
  TString name;
  T *array;
 
+ // constructor;
  Vec(T *p, int dim, TString nam){
+	if(dim <= 0 || dim > 1000000){
+		cout<<"PndTrkVectors::Vec  the dimension of the array "<<nam<<" is "
+		<<dim<<" and not acceptable; exit(-2) the program.\n";
+		exit(-2);
+	}
 	dimension = dim;
 	array = p;
 	name = nam;
@@ -24,29 +50,26 @@ template <class T> class Vec{
 
  ~Vec(){;};
 
+ // function  at();
  T& at(int index){
 
   if(index>=0 && index<dimension){
    T& alias = array[index];
    return  alias ;
   }
-  cout<< "Array "<<name<<": index = "<<index<<" and it is out of bounds! Exiting the job.\n";
+  cout<< "PndTrkVectors::  array "<<name<<": index = "<<index<<" and it is out of bounds ["<<
+  dimension<<"];exiting the process.\n";
   exit(-1);
  };
 
+ // overloaded operator  [] ; the functionality is identical to the  at() function;
  T& operator[] (int index){
 
-  if(index>=0 && index<dimension){
-   T& alias = array[index];
+   T& alias = at(index);
    return  alias ;
-  }
-  cout<< "Array "<<name<<": index = "<<index<<" and it is out of bounds! Exiting the job.\n";
-  exit(-1);
  };
 
 };
-
-
 
 
 #endif
