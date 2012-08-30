a9717c78f21115552161ce7accdbcfed8c927897
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-08-30 10:23:38 +0000

    cosmetic change

--- sttmvdtracking/PndSttMvdTracking.cxx
+++ sttmvdtracking/PndSttMvdTracking.cxx
@@ -14543,7 +14543,8 @@ if(istampa>1) {cout<<"in BadTrack_ParStt :hit || n. "<<ListHits[ihit]<<", X "<<i
 
 
 	// cut on the minimum (conservative) n. hits that must have fired
-	int nume = length/DiameterStrawTube -islack;
+	int nume ;
+	nume = (int)( length/DiameterStrawTube -islack) ;
 	if( ninside < nume ){
 		if(istampa>1){
 			cout<<"in BadTrack_ParStt, n. Hits inside = "<<ninside
