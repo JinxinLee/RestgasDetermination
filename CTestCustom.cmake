8b40edb438eb3fd41fd90d58956bc9cd82713750
Florian Uhlig <f.uhli@gsi.de>
2012-09-06 04:36:07 +0000

    Filter the terms Warnung and Anmerkung as error execptions. See ticket #89.

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -14,6 +14,12 @@ set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS "500" )
 
 set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_ERRORS   "50" )
 
+set(CTEST_CUSTOM_ERROR_EXCEPTION
+       ${CTEST_CUSTOM_ERROR_EXCEPTION}
+       "Warnung: "
+       "Anmerkung: "
+)
+
 # -----------------------------------------------------------
 # -- Warning execptions
 # -----------------------------------------------------------
