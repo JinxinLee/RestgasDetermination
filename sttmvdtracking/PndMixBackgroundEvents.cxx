6104f5cce025d1897f00380e1bc277245734955a
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2012-10-12 18:01:19 +0000

    cosmetic change.

--- sttmvdtracking/PndMixBackgroundEvents.cxx
+++ sttmvdtracking/PndMixBackgroundEvents.cxx
@@ -456,7 +456,7 @@ return;
 			Double_t *modified
 			)
   {
-	//  STTdriftVEL in cm/nsec (usually 0.002 cm/nsec);
+	//  STTdriftVEL in cm/nsec is  0.0025 cm/nsec;
 	//  isochrone here is in reality a drift radius.
 	*modified = isochrone + time*STTdriftVEL;
 	if( *modified > STRAWRADIUS || *modified < 0.) return false;
