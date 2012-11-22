369b2a400e7cfc881db7abe6daba700ee2efa3d2
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2012-11-22 16:14:31 +0000

    small changes due to file names change

--- macro/lmd/TrksAlgoResults.C
+++ macro/lmd/TrksAlgoResults.C
@@ -12,7 +12,10 @@
 #include "TClonesArray.h"
 #include "TFile.h"
 using namespace std;
-void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/100000events")
+
+void TrksAlgoResults(TString storePath="StripSensors")
+//void TrksAlgoRes(TString storePath="PixelSensors")
+//void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/pixelDesign/100000events")
 {
   // double Ntrks[8]={1,2,3,4,5,10,15,20};
   double Ntrks[7]={1,2,3,4,5,10,15};
@@ -21,7 +24,7 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   double reconstF[7];//number of reconstructed tracks
   double ghostF_I[7]; //percent of ghost tracks
   double missedF_I[7]; //percent of missed tracks
- double ghostF_II[7]; //percent of ghost tracks
+  double ghostF_II[7]; //percent of ghost tracks
   double missedF_II[7]; //percent of missed tracks
 
   double ghostF_I_mean[7]; //average of ghost tracks
@@ -32,9 +35,11 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   double missedF_I_rms[7]; //rms average of missed tracks
   double ghostF_II_rms[7]; //rms average of ghost tracks
   double missedF_II_rms[7]; //rms average of missed tracks
+  double goodF_I_mean[7]; //average of good tracks
+  double goodF_II_mean[7]; //average of good tracks
 
-
-  double timeF[7]={25.3,32.28,41.28,52.47,68.69,334.81,1611.94};//time spent per 1 trk reconstruction , ms
+  //  double timeF[7]={25.3,32.28,41.28,52.47,68.69,334.81,1611.94};//time spent per 1 trk reconstruction , ms
+  double timeF[7]={0,0,0,0,0,0,0};//time spent per 1 trk reconstruction , ms
   //Cellular Automaton
   double reconstCA[7];//number of reconstructed tracks
   double ghostCA_I[7]; //percent of ghost tracks
@@ -50,19 +55,47 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   double missedCA_I_rms[7]; //rms average of missed tracks
   double ghostCA_II_rms[7]; //rms average of ghost tracks
   double missedCA_II_rms[7]; //rms average of missed tracks
+  double goodCA_I_mean[7]; //average of good tracks
+  double goodCA_II_mean[7]; //average of good tracks
 
-  double timeCA[7]={25.99,33.26,41.63,51.25,63.08,155.27,343.77};//time spent per 1 trk reconstruction , ms
+  //  double timeCA[7]={25.99,33.26,41.63,51.25,63.08,155.27,343.77};//time spent per 1 trk reconstruction , ms
+  double timeCA[7]={0,0,0,0,0,0,0};//time spent per 1 trk reconstruction , ms
   for(int iNtrk=0;iNtrk<7;iNtrk++){
+    // ///STRIP -----------------------------------------------------------------
     TString fileCAname=storePath+"/Lumi_out_MC_and_REC_trks_matches_with_IDs0";
-    fileCAname+="_CA";
+    fileCAname+="_CA_";
     TString fileFname=storePath+"/Lumi_out_MC_and_REC_trks_matches_with_IDs0";
-    fileFname+="_Follow";
-    fileCAname+="_new_100000_";
-    fileFname+="_new_100000_";
+    fileFname+="_Follow_";
     fileCAname+=Ntrks[iNtrk];
     fileFname+=Ntrks[iNtrk];
-    fileCAname+="trk_11_91GeV.root";
-    fileFname+="trk_11_91GeV.root";
+    fileCAname+="trks_";
+    fileFname+="trks_";
+     fileCAname+="KalmanFillter_";
+     fileFname+="KalmanFillter_";
+    // fileCAname+="MinuitFit_";
+    // fileFname+="MinuitFit_";
+    fileCAname+="1.5.root";
+    fileFname+="1.5.root";
+    // ///STRIP -----------------------------------------------------------------
+
+    // ///PIXEL -----------------------------------------------------------------
+    // TString fileCAname=storePath+"/Lumi_out_MC_and_REC_trks_matches_with_IDs0";
+    // fileCAname+="_CA_";
+    // TString fileFname=storePath+"/Lumi_out_MC_and_REC_trks_matches_with_IDs0";
+    // fileFname+="_Follow_";
+    // fileCAname+=Ntrks[iNtrk];
+    // fileFname+=Ntrks[iNtrk];
+    // fileCAname+="trks_";
+    // fileFname+="trks_";
+    // fileCAname+="mergedHits_true_";
+    // fileFname+="mergedHits_true_";
+    // //    fileCAname+="addMS_false_KalmanFillter_";
+    // //    fileFname+="addMS_false_KalmanFillter_";
+    // fileCAname+="addMS_true_MinuitFit_";
+    // fileFname+="addMS_true_MinuitFit_";
+    // fileCAname+="15.root";
+    // fileFname+="15.root";
+    // ///PIXEL -----------------------------------------------------------------
 
     TFile *fileCA = new TFile(fileCAname,"READ");
     TFile *fileF = new TFile(fileFname,"READ");
@@ -73,7 +106,10 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
     int nrecF = hResThetaF->GetEntries();
     reconstF[iNtrk]=nrecF;
 
-    //missed\ghost from definition I: compare dPhi & dTheta for MC and REC trks
+    //good and missed\ghost from definition I: compare dPhi & dTheta for MC and REC trks
+    TH1F* hntrkgoodCA_I = (TH1F*)fileCA->Get("hntrkgood_I");
+    goodCA_I_mean[iNtrk] = hntrkgoodCA_I->GetMean();
+
     TH1F* hntrkmissedCA_I = (TH1F*)fileCA->Get("hntrkmissed_I");
     int nmissCA_I = hntrkmissedCA_I->GetEntries();
     int nmissCA_I_under = hntrkmissedCA_I->GetBinContent(0);
@@ -81,6 +117,8 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
     missedCA_I_mean[iNtrk] = hntrkmissedCA_I->GetMean();
     missedCA_I_rms[iNtrk] = hntrkmissedCA_I->GetMeanError();
   
+    TH1F* hntrkgoodF_I = (TH1F*)fileF->Get("hntrkgood_I");
+    goodF_I_mean[iNtrk] = hntrkgoodF_I->GetMean();
     TH1F* hntrkmissedF_I = (TH1F*)fileF->Get("hntrkmissed_I");
     int nmissF_I = hntrkmissedF_I->GetEntries();
     int nmissF_I_under = hntrkmissedF_I->GetBinContent(0);
@@ -103,6 +141,8 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
     ghostF_I_rms[iNtrk] = hntrkghostF_I->GetMeanError();
 
     //missed\ghost from definition II: based on hits info
+    TH1F* hntrkgoodCA_II = (TH1F*)fileCA->Get("hntrkgood_II");
+    goodCA_II_mean[iNtrk] = hntrkgoodCA_II->GetMean();
     TH1F* hntrkmissedCA_II = (TH1F*)fileCA->Get("hntrkmissed_II");
     int nmissCA_II = hntrkmissedCA_II->GetEntries();
     int nmissCA_II_under = hntrkmissedCA_II->GetBinContent(0);
@@ -110,6 +150,8 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
     missedCA_II_mean[iNtrk] = hntrkmissedCA_II->GetMean();
     missedCA_II_rms[iNtrk] = hntrkmissedCA_II->GetMeanError();  
 
+    TH1F* hntrkgoodF_II = (TH1F*)fileF->Get("hntrkgood_II");
+    goodF_II_mean[iNtrk] = hntrkgoodF_II->GetMean();
     TH1F* hntrkmissedF_II = (TH1F*)fileF->Get("hntrkmissed_II");
     int nmissF_II = hntrkmissedF_II->GetEntries();
     int nmissF_II_under = hntrkmissedF_II->GetBinContent(0);
@@ -128,8 +170,8 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
     int nghostF_II = hntrkghostF_II->GetEntries();
     int nghostF_II_under = hntrkghostF_II->GetBinContent(0);
     nghostF_II-=nghostF_II_under;
-    ghostF_II_mean[iNtrk] = hntrkghostF_I->GetMean();
-    ghostF_II_rms[iNtrk] = hntrkghostF_I->GetMeanError();
+    ghostF_II_mean[iNtrk] = hntrkghostF_II->GetMean();
+    ghostF_II_rms[iNtrk] = hntrkghostF_II->GetMeanError();
 
     TH2F* hnRecnMCCA = (TH2F*)fileCA->Get("hnRecnMC");
     TH1D* hnRecnMCCA_rec = hnRecnMCCA->ProjectionY();
@@ -160,44 +202,68 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   TGraph *grSpeedCA = new TGraph(7,Ntrks,timeCA);
   grSpeedCA->SetMarkerStyle(20);
   grSpeedCA->SetMarkerColor(2);
-  grSpeedCA->SetMarkerSize(1.5);
+  grSpeedCA->SetMarkerSize(1.2);
   TGraph *grSpeedF = new TGraph(7,Ntrks,timeF);
   grSpeedF->SetMarkerStyle(20);
   grSpeedF->SetMarkerColor(4);
-  grSpeedF->SetMarkerSize(1.5);
+  grSpeedF->SetMarkerSize(1.2);
   TMultiGraph *mgSpeed = new TMultiGraph("mgSpeed","Speed;N^{trk}_{MC};time per trk, ms");
   mgSpeed->Add(grSpeedCA);
   mgSpeed->Add(grSpeedF);
  
 
-  TGraph *grRecCA = new TGraph(7,Ntrks,reconstCA);
-  grRecCA->SetMarkerStyle(20);
-  grRecCA->SetMarkerColor(2);
-  grRecCA->SetMarkerSize(1.5);
-  TGraph *grRecF = new TGraph(7,Ntrks,reconstF);
-  grRecF->SetMarkerStyle(20);
-  grRecF->SetMarkerColor(4);
-  grRecF->SetMarkerSize(1.5);
-  TMultiGraph *mgTotRec = new TMultiGraph("mgRecTrks","RecTrks;N^{trk}_{MC};N^{trk}_{Rec}");
-  mgTotRec->Add(grRecCA);
-  mgTotRec->Add(grRecF);
+  // TGraph *grRecCA = new TGraph(7,Ntrks,reconstCA);
+  // grRecCA->SetMarkerStyle(20);
+  // grRecCA->SetMarkerColor(2);
+  // grRecCA->SetMarkerSize(1.2);
+  // TGraph *grRecF = new TGraph(7,Ntrks,reconstF);
+  // grRecF->SetMarkerStyle(20);
+  // grRecF->SetMarkerColor(4);
+  // grRecF->SetMarkerSize(1.2);
+  // TMultiGraph *mgTotRec = new TMultiGraph("mgRecTrks","RecTrks;N^{trk}_{MC};N^{trk}_{Rec}");
+  // mgTotRec->Add(grRecCA);
+  // mgTotRec->Add(grRecF);
+
+  TGraph *grRecCA_I = new TGraph(7,Ntrks,goodCA_I_mean);
+  grRecCA_I->SetMarkerStyle(20);
+  grRecCA_I->SetMarkerColor(2);
+  grRecCA_I->SetMarkerSize(1.2);
+  TGraph *grRecF_I = new TGraph(7,Ntrks,goodF_I_mean);
+  grRecF_I->SetMarkerStyle(20);
+  grRecF_I->SetMarkerColor(4);
+  grRecF_I->SetMarkerSize(1.2);
+  
+  TGraph *grRecCA_II = new TGraph(7,Ntrks,goodCA_II_mean);
+  grRecCA_II->SetMarkerStyle(21);
+  grRecCA_II->SetMarkerColor(2);
+  grRecCA_II->SetMarkerSize(1.2);
+  TGraph *grRecF_II = new TGraph(7,Ntrks,goodF_II_mean);
+  grRecF_II->SetMarkerStyle(21);
+  grRecF_II->SetMarkerColor(4);
+  grRecF_II->SetMarkerSize(1.2);
+
+  TMultiGraph *mgTotRec = new TMultiGraph("mgRecTrks","Good RecTrks;N^{trk}_{MC};N^{trk}_{Rec}");
+  mgTotRec->Add(grRecCA_I);
+  mgTotRec->Add(grRecCA_II);
+  mgTotRec->Add(grRecF_I);
+  mgTotRec->Add(grRecF_II);
 
   TGraph *grMissCA_I = new TGraph(7,Ntrks,missedCA_I);
   grMissCA_I->SetMarkerStyle(20);
   grMissCA_I->SetMarkerColor(2);
-  grMissCA_I->SetMarkerSize(1.5);
+  grMissCA_I->SetMarkerSize(1.2);
   TGraph *grMissF_I = new TGraph(7,Ntrks,missedF_I);
   grMissF_I->SetMarkerStyle(20);
   grMissF_I->SetMarkerColor(4);
-  grMissF_I->SetMarkerSize(1.5);
+  grMissF_I->SetMarkerSize(1.2);
   TGraph *grMissCA_II = new TGraph(7,Ntrks,missedCA_II);
   grMissCA_II->SetMarkerStyle(21);
   grMissCA_II->SetMarkerColor(2);
-  grMissCA_II->SetMarkerSize(1.5);
+  grMissCA_II->SetMarkerSize(1.2);
   TGraph *grMissF_II = new TGraph(7,Ntrks,missedF_II);
   grMissF_II->SetMarkerStyle(21);
   grMissF_II->SetMarkerColor(4);
-  grMissF_II->SetMarkerSize(1.5);
+  grMissF_II->SetMarkerSize(1.2);
   TMultiGraph *mgMissed = new TMultiGraph("mgMissed","Missed;N^{trk}_{MC};missed trks, %");
   mgMissed->Add(grMissCA_I);
   mgMissed->Add(grMissF_I);
@@ -208,19 +274,19 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   TGraph *grGhostCA_I = new TGraph(7,Ntrks,ghostCA_I);
   grGhostCA_I->SetMarkerStyle(20);
   grGhostCA_I->SetMarkerColor(2);
-  grGhostCA_I->SetMarkerSize(1.5);
+  grGhostCA_I->SetMarkerSize(1.2);
   TGraph *grGhostF_I = new TGraph(7,Ntrks,ghostF_I);
   grGhostF_I->SetMarkerStyle(20);
   grGhostF_I->SetMarkerColor(4);
-  grGhostF_I->SetMarkerSize(1.5);
+  grGhostF_I->SetMarkerSize(1.2);
   TGraph *grGhostCA_II = new TGraph(7,Ntrks,ghostCA_II);
   grGhostCA_II->SetMarkerStyle(21);
   grGhostCA_II->SetMarkerColor(2);
-  grGhostCA_II->SetMarkerSize(1.5);
+  grGhostCA_II->SetMarkerSize(1.2);
   TGraph *grGhostF_II = new TGraph(7,Ntrks,ghostF_II);
   grGhostF_II->SetMarkerStyle(21);
   grGhostF_II->SetMarkerColor(4);
-  grGhostF_II->SetMarkerSize(1.5);
+  grGhostF_II->SetMarkerSize(1.2);
   TMultiGraph *mgGhost = new TMultiGraph("mgGhost","Ghost;N^{trk}_{MC};ghost trks, %");
   mgGhost->Add(grGhostCA_I);
   mgGhost->Add(grGhostF_I);
@@ -231,36 +297,36 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   TGraphErrors *grMissCA_value_I = new TGraphErrors(7,Ntrks,missedCA_I_mean,eNtrks,missedCA_I_rms);
   grMissCA_value_I->SetMarkerStyle(20);
   grMissCA_value_I->SetMarkerColor(2);
-  grMissCA_value_I->SetMarkerSize(1.5);
+  grMissCA_value_I->SetMarkerSize(1.2);
   TGraphErrors *grMissCA_value_II = new TGraphErrors(7,Ntrks,missedCA_II_mean,eNtrks,missedCA_II_rms);
   grMissCA_value_II->SetMarkerStyle(21);
   grMissCA_value_II->SetMarkerColor(2);
-  grMissCA_value_II->SetMarkerSize(1.5);
+  grMissCA_value_II->SetMarkerSize(1.2);
   TGraphErrors *grGhostCA_value_I = new TGraphErrors(7,Ntrks,ghostCA_I_mean,eNtrks,ghostCA_I_rms);
   grGhostCA_value_I->SetMarkerStyle(20);
   grGhostCA_value_I->SetMarkerColor(2);
-  grGhostCA_value_I->SetMarkerSize(1.5);
+  grGhostCA_value_I->SetMarkerSize(1.2);
   TGraphErrors *grGhostCA_value_II = new TGraphErrors(7,Ntrks,ghostCA_II_mean,eNtrks,ghostCA_II_rms);
   grGhostCA_value_II->SetMarkerStyle(21);
   grGhostCA_value_II->SetMarkerColor(2);
-  grGhostCA_value_II->SetMarkerSize(1.5);
+  grGhostCA_value_II->SetMarkerSize(1.2);
 
   TGraphErrors *grMissF_value_I = new TGraphErrors(7,Ntrks,missedF_I_mean,eNtrks,missedF_I_rms);
   grMissF_value_I->SetMarkerStyle(20);
   grMissF_value_I->SetMarkerColor(4);
-  grMissF_value_I->SetMarkerSize(1.5);
+  grMissF_value_I->SetMarkerSize(1.2);
   TGraphErrors *grMissF_value_II = new TGraphErrors(7,Ntrks,missedF_II_mean,eNtrks,missedF_II_rms);
   grMissF_value_II->SetMarkerStyle(21);
   grMissF_value_II->SetMarkerColor(4);
-  grMissF_value_II->SetMarkerSize(1.5);
+  grMissF_value_II->SetMarkerSize(1.2);
   TGraphErrors *grGhostF_value_I = new TGraphErrors(7,Ntrks,ghostF_I_mean,eNtrks,ghostF_I_rms);
   grGhostF_value_I->SetMarkerStyle(20);
   grGhostF_value_I->SetMarkerColor(4);
-  grGhostF_value_I->SetMarkerSize(1.5);
+  grGhostF_value_I->SetMarkerSize(1.2);
   TGraphErrors *grGhostF_value_II = new TGraphErrors(7,Ntrks,ghostF_II_mean,eNtrks,ghostF_II_rms);
   grGhostF_value_II->SetMarkerStyle(21);
   grGhostF_value_II->SetMarkerColor(4);
-  grGhostF_value_II->SetMarkerSize(1.5);
+  grGhostF_value_II->SetMarkerSize(1.2);
 
   TMultiGraph *mgMissedMean = new TMultiGraph("mgMissedMean","Missed;N^{trk}_{MC};average number of missed trks");
   mgMissedMean->Add(grMissCA_value_I);
@@ -280,7 +346,38 @@ void TrksAlgoResults(TString storePath="/myResults/LUMI_Trk/newCA_oldDesign/1000
   TCanvas *c1 = new TCanvas("Overview");
   c1->Divide(3,2);
   c1->cd(1);
+  // const char *longstring = "K_{S}... K^{*0}... #frac{2s}{#pi#alpha^{2}}
+  //  #frac{d#sigma}{dcos#theta} (e^{+}e^{-} #rightarrow f#bar{f} ) =
+    //  #left| #frac{1}{1 - #Delta#alpha} #right|^{2} (1+cos^{2}#theta)";
+
   mgSpeed->Draw("APL");
+  TLatex latex;
+  latex.SetTextSize(0.035);
+  latex.SetTextAlign(12);  //align at center
+ 
+  latex.DrawLatex(1.8,1.1,"#it{Cellular Automaton (Trks matching)}");
+  latex.DrawLatex(1.8,1.0,"#it{Cellular Automaton (Hits matching)}");
+  latex.DrawLatex(1.8,.8,"#it{Trk-Following (Trks matching)}");
+  latex.DrawLatex(1.8,.7,"#it{Trk-Following (Hits matching)}");
+  TMarker caI(1.2, 0.9, 20);
+  caI.SetMarkerColor(2);
+  caI.SetMarkerSize(1.2); 
+  TMarker caII(1.2, 0.85, 21);
+  caII.SetMarkerColor(2);
+  caII.SetMarkerSize(1.2);
+  caI.DrawMarker(1.2,1.1);
+  caII.DrawMarker(1.2,1.0);
+
+  TMarker fI(1.2, 0.9, 20);
+  fI.SetMarkerColor(4);
+  fI.SetMarkerSize(1.2); 
+  TMarker fII(1.2, 0.85, 21);
+  fII.SetMarkerColor(4);
+  fII.SetMarkerSize(1.2);
+  fI.DrawMarker(1.2,0.8);
+  fII.DrawMarker(1.2,0.7);
+
+  // latex.DrawLatex(.2,.8,longstring);
   c1->cd(4);
   mgTotRec->Draw("APL");
   c1->cd(3);
