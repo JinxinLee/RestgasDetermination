2e631ff22cff1d662ee98a40c9aa4ccd95612a04
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2012-11-22 16:53:12 +0000

    Special class for merged hits with merged hit  ID

---
+++ lmd/LmdReco/PndSdsMergedHit.cxx
@@ -0,0 +1,41 @@
+// -------------------------------------------------------------------------
+// -----                    PndSdsMergedHit source file                -----
+// -----                                 -----
+// -------------------------------------------------------------------------
+
+#include "stdlib.h"
+#include <iostream>
+#include "PndSdsMergedHit.h"
+#include "TGeoManager.h"
+#include "TGeoNode.h"
+#include "TGeoVolume.h"
+#include "TGeoMatrix.h"
+
+
+// -----   Default constructor   -------------------------------------------
+PndSdsMergedHit::PndSdsMergedHit() {
+}
+// -------------------------------------------------------------------------
+
+
+
+// -----   Standard constructor   ------------------------------------------
+PndSdsMergedHit::PndSdsMergedHit(Int_t detID, Int_t sensorID, TVector3& pos, TVector3& dpos,
+                     Int_t clindex, Double_t charge, Int_t NDigiHits, Int_t mcindex, Int_t secMC)
+: PndSdsHit(detID, sensorID, pos, dpos, clindex, charge, NDigiHits, mcindex){
+  fsecMC = secMC;
+}
+// -------------------------------------------------------------------------
+
+PndSdsMergedHit::PndSdsMergedHit(PndSdsHit& c, Int_t secMC):PndSdsHit(c){
+fsecMC = secMC;
+}
+
+
+// -----   Destructor   ----------------------------------------------------
+PndSdsMergedHit::~PndSdsMergedHit() {}
+// -------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------
+ClassImp(PndSdsMergedHit);
