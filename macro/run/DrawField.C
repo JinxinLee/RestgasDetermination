509a9425127ce0adb9e3c5964fcbb9b187a42896
Stefano Spataro <spataro@to.infn.it>
2013-01-21 14:36:22 +0000

    Refreshing macros

--- macro/run/DrawField.C
+++ macro/run/DrawField.C
@@ -1,4 +1,5 @@
 {
+  // Macro to plot the magnetic field
  
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();
@@ -12,22 +13,22 @@
 
    PndMultiField *fField= new PndMultiField();
 
-  // PndTransMap *map_t= new PndTransMap("TransMap.1500", "R");
-   PndDipoleMap *map_d1= new PndDipoleMap("DipoleMap1.1500", "R");
-   PndDipoleMap *map_d2= new PndDipoleMap("DipoleMap2.1500", "R");
- /*  PndSolenoidMap *map_s1= new PndSolenoidMap("SolenoidMap1", "R");
+   PndTransMap *map_t= new PndTransMap("TransMap", "R");
+   PndDipoleMap *map_d1= new PndDipoleMap("DipoleMap1", "R");
+   PndDipoleMap *map_d2= new PndDipoleMap("DipoleMap2", "R");
+   PndSolenoidMap *map_s1= new PndSolenoidMap("SolenoidMap1", "R");
    PndSolenoidMap *map_s2= new PndSolenoidMap("SolenoidMap2", "R");
    PndSolenoidMap *map_s3= new PndSolenoidMap("SolenoidMap3", "R");
    PndSolenoidMap *map_s4= new PndSolenoidMap("SolenoidMap4", "R");
-*/
- //  fField->AddField(map_t);
+
+   fField->AddField(map_t);
    fField->AddField(map_d1);
    fField->AddField(map_d2);
- /*  fField->AddField(map_s1);
+   fField->AddField(map_s1);
    fField->AddField(map_s2);
    fField->AddField(map_s3);
    fField->AddField(map_s4);
-*/
+
    fField->Init();
    
      
