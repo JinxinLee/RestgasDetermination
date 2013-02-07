ad3365b97683d39ca5bf5b65d2eb1b9a761530a0
Stefano Spataro <spataro@to.infn.it>
2013-02-07 09:57:32 +0000

    Fixed SciTil geometry

--- macro/run/sim_complete.C
+++ macro/run/sim_complete.C
@@ -93,7 +93,7 @@ sim_complete(Int_t nEvents = 10, TString  SimEngine ="TGeant3", Float_t mom = 7.
   fRun->AddModule(Emc);
   //-------------------------  SCITIL    -----------------
   FairDetector *SciT = new PndSciT("SCIT",kTRUE);
-  SciT->SetGeometryFileName("barrel-SciTil_18122012.root");
+  SciT->SetGeometryFileName("barrel-SciTil_07022013.root");
   fRun->AddModule(SciT);
   //-------------------------  DRC       -----------------
   PndDrc *Drc = new PndDrc("DIRC", kTRUE);
