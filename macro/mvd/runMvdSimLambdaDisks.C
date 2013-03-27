becf3dcb4a867dff7e3bb76388f8b4782e8813a0
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2013-03-27 13:45:42 +0000

    New macro to create geometry of lambda disks with geometry file and simulation macro

---
+++ macro/mvd/runMvdSimLambdaDisks.C
@@ -0,0 +1,172 @@
+// Macro to simulate the MVD in pandaroot
+// Updated 30.11.2009
+// Ralf Kliemt
+runMvdSimLambdaDisks(Int_t nEvents=10)
+{
+  TStopwatch timer;
+  timer.Start();
+  gDebug=0;
+  int verboseLevel = 0;
+
+  //FileNames
+  TString simOutput="Mvd_Sim_Lambda.root";
+  TString parOutput="Mvd_Params.root";
+
+  // Load basic libraries
+  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+  FairRunSim *fRun = new FairRunSim();
+
+  // set the MC version used
+  // ------------------------
+
+  fRun->SetName("TGeant3");
+  // Choose the Geant Navigation System
+
+  fRun->SetOutputFile(simOutput);
+
+  // Fill the Parameter containers for this run
+  //-------------------------------------------
+  FairRuntimeDb *rtdb=fRun->GetRuntimeDb();
+  Bool_t kParameterMerged=kTRUE;
+  FairParRootFileIo* output=new FairParRootFileIo(kParameterMerged);
+  output->open(parOutput.Data(),"RECREATE");
+  rtdb->setOutput(output);
+
+  // Set Material file Name
+  //-----------------------
+  fRun->SetMaterials("media_pnd.geo");
+
+  // Create and add detectors
+  //-------------------------
+
+  FairModule *Cave= new PndCave("CAVE");
+  Cave->SetGeometryFileName("pndcave.geo");
+  fRun->AddModule(Cave);
+  FairModule *Magnet= new PndMagnet("MAGNET");
+  Magnet->SetGeometryFileName("magnet.geo");
+  fRun->AddModule(Magnet);
+  FairModule *Pipe= new PndPipe("PIPE");
+  //fRun->AddModule(Pipe);
+  //-------------------------  STT       -----------------
+  FairDetector *Stt= new PndStt("STT", kFALSE);
+  Stt->SetGeometryFileName("straws_skewed_blocks_pipe_120cm.geo");
+  fRun->AddModule(Stt);
+
+  FairDetector *Mvd = new PndMvdDetector("MVD", kTRUE);
+  Mvd->SetGeometryFileName("Mvd-2.1_FullVersion.root");
+  Mvd->SetVerboseLevel(verboseLevel);
+  fRun->AddModule(Mvd);
+
+  FairDetector *LambdaDisks = new PndMvdDetector("MVD", kTRUE);
+  LambdaDisks->SetGeometryFileName("LambdaDisksNew.root");
+  LambdaDisks->SetVerboseLevel(verboseLevel);
+  fRun->AddModule(LambdaDisks);
+
+
+  //-------------------------  EMC       -----------------
+  PndEmc *Emc = new PndEmc("EMC",kFALSE);
+  Emc->SetGeometryVersion(15);
+  // See PndEmc::SetGeometryVersion() for available geometries and add there new one if necessary
+  Emc->SetStorageOfData(kFALSE);
+  fRun->AddModule(Emc);
+  //-------------------------  MDT       -----------------
+  PndMdt *Muo = new PndMdt("MDT",kFALSE);
+  Muo->SetBarrel("torino");
+  Muo->SetEndcap("torino");
+  Muo->SetMuonFilter("torino");
+  Muo->SetMdtMagnet(kTRUE);
+  Muo->SetMdtMFIron(kTRUE);
+  //fRun->AddModule(Muo);
+  //-------------------------  DCH       -----------------
+//  FairDetector *Dch = new PndDchDetector("DCH", kFALSE);
+//  Dch->SetGeometryFileName("dch.root");
+  //fRun->AddModule(Dch);
+ 
+  //-------------------------  GEM      -----------------
+  FairDetector *Gem = new PndGemDetector("GEM", kFALSE);
+  Gem->SetGeometryFileName("gem_3Stations.root");
+  //fRun->AddModule(Gem);
+
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  fRun->SetGenerator(primGen);
+
+  primGen->SetEventMeanTime(100);
+
+  // Particle Generator (pdgid,mult, px,py,pz, vx,vy,vz)
+  // single pions for testing
+//      FairParticleGenerator* partGenX   = new FairParticleGenerator(211,1, 1.,0.,0., 0.,0.,0.);
+//      FairParticleGenerator* partGenY   = new FairParticleGenerator(211,1, 0.,1.,0., 0.,0.,0.);
+//      FairParticleGenerator* partGenZ   = new FairParticleGenerator(211,1, 0.,0.1,1., 0.,0.,0.);
+//      FairParticleGenerator* partGenXYZ = new FairParticleGenerator(211,1, 1.,1.,5., 0.,0.,0.);
+//      primGen->AddGenerator(partGenX);
+//      primGen->AddGenerator(partGenY);
+//      primGen->AddGenerator(partGenZ);
+//      primGen->AddGenerator(partGenXYZ);
+
+//      primGen->AddGenerator(partGenXYZ);
+
+ // Ion Generator
+ //    FairIonGenerator *fIongen= new FairIonGenerator(79, 197,79,1, 0.,0., 25, 0.,0.,-1.);
+ //    primGen->AddGenerator(fIongen);
+
+  // Box Generator
+  //Pions
+ FairBoxGenerator *fBox = new FairBoxGenerator(211, 4);
+    fBox->SetPRange(2.5,2.5);
+    fBox->SetThetaRange(5,22);
+    fBox->SetPhiRange(0.,360);
+    fBox->SetCosTheta();
+    primGen->AddGenerator(fBox);
+
+  //EvtGen Generator
+//    FairEvtGenGenerator* evtGen = new
+//      FairEvtGenGenerator("/home/ralfk/Pandaroot/pandaroot/macro/mvd/output.evt");
+//    primGen->AddGenerator(evtGen);
+
+  // Urqmd  Generator
+  //    FairUrqmdGenerator* urqmdGen = new FairUrqmdGenerator("../../input/00-03fm.100ev.f14");
+  //    primGen->AddGenerator(urqmdGen);
+
+  // DPM Generator
+  //PndDpmGenerator* dpmGen = new PndDpmGenerator("/home/kph/stockman/fairroot/cbmroot/pandaroot/pgenerators/DpmEvtGen/Background-micro.root");
+  //primGen->AddGenerator(dpmGen);
+
+  // Field Map Definition
+  // --------------------
+  // 1- Reading the new field map in the old format
+
+	fRun->SetBeamMom(15);
+  //---------------------Create and Set the Field(s)---------- 
+  PndMultiField *fField= new PndMultiField("FULL");
+  fRun->SetField(fField);
+  
+  fRun->SetStoreTraj(kTRUE); // toggle this for use with EVE
+  fRun->SetRadLenRegister(kFALSE); // toggle for material budget study
+
+   fRun->Init();
+
+// Set cuts for storing the trajectpries
+//   FairTrajFilter* trajFilter = FairTrajFilter::Instance();
+//     trajFilter->SetStepSizeCut(0.01); // 1 cm
+//     trajFilter->SetVertexCut(-200., -200., -200, 200., 200., 200.);
+//     trajFilter->SetMomentumCutP(10e-3); // p_lab > 10 MeV
+//     trajFilter->SetEnergyCut(0., 1.02); // 0 < Etot < 1.04 GeV
+//     trajFilter->SetStorePrimaries(kTRUE);
+//     trajFilter->SetStoreSecondaries(kTRUE);
+
+
+
+  // Transport nEvents
+  // -----------------
+
+  fRun->Run(nEvents);
+
+  rtdb->saveOutput();
+  rtdb->print();
+
+
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+}
---
+++ mvd/MvdMC/CreateLambdaGeometry.C
@@ -0,0 +1,177 @@
+/*
+ * CreateLambdaGeometry.C
+ *
+ *  Created on: Mar 21, 2013
+ *      Author: stockman
+ */
+CreateLambdaGeometry()
+{
+	  //-----------------------------
+	Double_t positionOfDisk1inZ = 47.0;		//in cm
+	Double_t positionOfDisk2inZ = 53.0;		//in cm
+
+	  //--------------------------------------------------------------------
+	  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+	  
+	  // Load this libraries
+	  gSystem->Load("libGeoBase");
+	  gSystem->Load("libParBase");
+	  gSystem->Load("libBase");
+	  gSystem->Load("libPndData");
+	  gSystem->Load("libPassive");
+	  
+	  TString outfile= "../../geometry/LambdaDisksNew.root";
+	  TFile* fi = new TFile(outfile,"RECREATE");  
+	  
+	  FairGeoLoader* geoLoad = new FairGeoLoader("TGeo","FairGeoLoader");
+	  FairGeoInterface *geoFace = geoLoad->getGeoInterface();
+	  geoFace->setMediaFile("../../geometry/media_pnd.geo");
+	  geoFace->readMedia();
+	  geoFace->print();
+
+	  FairGeoMedia *Media =  geoFace->getMedia();
+	  FairGeoBuilder *geobuild=geoLoad->getGeoBuilder();
+
+	  FairGeoMedium *CbmMediumSilicon  = Media->getMedium("silicon");
+	  FairGeoMedium *CbmMediumCarbon  = Media->getMedium("carbon");
+	  FairGeoMedium *CbmMediumCarbonFoam  = Media->getMedium("carbonfoam");
+	  FairGeoMedium *CbmMediumAluminium = Media->getMedium("aluminium");
+	  
+	  Int_t nmed=geobuild->createMedium(CbmMediumSilicon);
+	  nmed=geobuild->createMedium(CbmMediumCarbon);
+	  nmed=geobuild->createMedium(CbmMediumCarbonFoam);
+	  nmed=geobuild->createMedium(CbmMediumAluminium);
+
+	  TGeoManager* gGeoMan = (TGeoManager*)gROOT->FindObject("FAIRGeom");
+
+	  TGeoVolume *top = new TGeoVolumeAssembly("top");
+	                    
+	  gGeoMan->SetTopVolume(top);
+	  
+	TGeoVolume *combinedLambdaDisks = new TGeoVolumeAssembly("CombinedLambdaDisks");
+	TGeoVolumeAssembly* lambdaDisk = new TGeoVolumeAssembly("LambdaDisk");
+	TGeoVolumeAssembly* largeRing = new TGeoVolumeAssembly("LargeRing");
+	TGeoVolumeAssembly* smallRing = new TGeoVolumeAssembly("SmallRing");
+
+//	lambdaDisk->AddNode(largeSensorVolume,0,trc1);
+	TGeoShape* largeSensorShape  = new TGeoTrd1("StripSensorActiveLargeShape", 3.4593/2, 1.9936/2,0.03/2, 5.5665/2);
+	TGeoVolume* largeSensorVolume = new TGeoVolume("StripSensorActiveLarge",largeSensorShape,gGeoMan->GetMedium("silicon"));
+
+	TGeoRotation rotSensor("rotSensor",0,90,0);
+	TGeoTranslation transLargeSensor(0,(14.8669 + 5.5665)/2,0);
+	TGeoCombiTrans combined1(transLargeSensor, rotSensor);
+
+	for (int i = 0; i < 12; i++){
+		TString rotName("rotLargeSens");
+		rotName.Append(i+1);
+		TGeoRotation rotSens(rotName.Data(), 0,0, i*2 * 360/24);
+		TGeoHMatrix sens = rotSens * combined1;
+		largeRing->AddNode(largeSensorVolume, i, new TGeoHMatrix(sens));
+	}
+
+	TGeoShape* smallSensorShape = new TGeoTrd1("StripSensorActiveSmallShape", 1.9579/2, 0.5529/2, 0.03/2, 5.3358/2);
+	TGeoVolume* smallSensorVolume = new TGeoVolume("StripSensorActiveSmall", smallSensorShape, gGeoMan->GetMedium("silicon"));
+
+	TGeoTranslation transSmallSensor(0,5.3358/2+2.1,0);
+	TGeoCombiTrans combined2(transSmallSensor, rotSensor);
+
+	for (int i = 0; i < 12; i++){
+		TString rotName("rotSmallSens");
+		rotName.Append(i+1);
+		TGeoRotation rotSens(rotName.Data(), 0,0, i*2 * 360/24);
+		TGeoHMatrix sens = rotSens * combined2;
+		smallRing->AddNode(smallSensorVolume, i+12, new TGeoHMatrix(sens));
+	}
+
+	lambdaDisk->AddNode(largeRing, 0, new TGeoTranslation(0,0,-0.5));
+	lambdaDisk->AddNode(largeRing, 1, new TGeoRotation("largeRingRot",0,0,360/24));
+
+	TGeoCombiTrans combiSmallRing2(TGeoTranslation(0,0,-1.5), TGeoRotation("smallRingRot",0,0,360/24));
+	lambdaDisk->AddNode(smallRing, 0, new TGeoTranslation(0,0,-1.0));
+	lambdaDisk->AddNode(smallRing, 1, new TGeoHMatrix(combiSmallRing2));
+
+	TGeoVolumeAssembly* supportDisk = new TGeoVolumeAssembly("SupportDisk");
+
+	TGeoVolumeAssembly* supportLarge = new TGeoVolumeAssembly("SupportLarge");
+		TGeoShape* carbonFoamShape = new TGeoBBox(3.4/2, 0.2/2, 3.5/2);
+		TGeoVolume* carbonFoam = new TGeoVolume("CarbonFoam", carbonFoamShape, gGeoMan->GetMedium("carbonfoam"));
+		TGeoShape* carbonFiberShape = new TGeoBBox(3.4/2, 0.02/2, 3.5/2);
+		TGeoVolume* carbonFiber = new TGeoVolume("CarbonFiber", carbonFiberShape, gGeoMan->GetMedium("carbon"));
+		TGeoShape* siliconReadoutShape = new TGeoBBox(3.4/2, 0.02/2, 3.5/2);
+		TGeoVolume* siliconReadout = new TGeoVolume("SiliconReadout", siliconReadoutShape, gGeoMan->GetMedium("silicon"));
+		TGeoShape* aluCablesShape = new TGeoBBox(3.4/2, 0.4/2, 3.5/2);
+		TGeoVolume* aluCables = new TGeoVolume("AluCables", aluCablesShape, gGeoMan->GetMedium("aluminium"));
+
+		supportLarge->AddNode(carbonFoam, 1);
+		supportLarge->AddNode(carbonFiber, 1, new TGeoTranslation(0,(0.2 + 0.02)/2, 0));
+		supportLarge->AddNode(carbonFiber, 2, new TGeoTranslation(0,-(0.2 + 0.02)/2, 0));
+		supportLarge->AddNode(siliconReadout, 1, new TGeoTranslation(0,(0.2 + 0.02 + 0.02)/2, 0));
+		supportLarge->AddNode(siliconReadout, 2, new TGeoTranslation(0,-(0.2 + 0.02 + 0.02)/2, 0));
+		supportLarge->AddNode(aluCables, 1, new TGeoTranslation(0,(0.2 + 0.02 + 0.02 + 0.4)/2, 0));
+		supportLarge->AddNode(aluCables, 2, new TGeoTranslation(0,-(0.2 + 0.02 + 0.02 + 0.4)/2, 0));
+
+		TGeoTranslation transSupportLarge(0,(14.8669)/2 + 5.5665 + 1,0);
+
+		for (int i = 0; i < 24; i++){
+			TString rotName("rotSupportLarge");
+			rotName.Append(i+1);
+			TGeoRotation rotSens(rotName.Data(), 0,0, i * 360/24);
+			TGeoHMatrix sens = rotSens * transSupportLarge;
+			supportDisk->AddNode(supportLarge, i+1, new TGeoHMatrix(sens));
+		}
+
+		TGeoVolumeAssembly* supportSmall = new TGeoVolumeAssembly("SupportSmall");
+		TGeoShape* carbonFoamShapeSmall = new TGeoBBox(1.8/2, 0.2/2, 3.5/2);
+		TGeoVolume* carbonFoamSmall = new TGeoVolume("CarbonFoamSmall", carbonFoamShapeSmall, gGeoMan->GetMedium("carbonfoam"));
+		TGeoShape* carbonFiberShapeSmall = new TGeoBBox(1.8/2, 0.02/2, 3.5/2);
+		TGeoVolume* carbonFiberSmall = new TGeoVolume("CarbonFiberSmall", carbonFiberShapeSmall, gGeoMan->GetMedium("carbon"));
+		TGeoShape* siliconReadoutShapeSmall = new TGeoBBox(1.8/2, 0.02/2, 3.5/2);
+		TGeoVolume* siliconReadoutSmall = new TGeoVolume("SiliconReadoutSmall", siliconReadoutShapeSmall, gGeoMan->GetMedium("silicon"));
+		TGeoShape* aluCablesShapeSmall = new TGeoBBox(1.8/2, 0.4/2, 3.5/2);
+		TGeoVolume* aluCablesSmall = new TGeoVolume("AluCablesSmall", aluCablesShapeSmall, gGeoMan->GetMedium("aluminium"));
+
+		supportSmall->AddNode(carbonFoamSmall, 1);
+		supportSmall->AddNode(carbonFiberSmall, 1, new TGeoTranslation(0,(0.2 + 0.02)/2, 0));
+		supportSmall->AddNode(carbonFiberSmall, 2, new TGeoTranslation(0,-(0.2 + 0.02)/2, 0));
+		supportSmall->AddNode(siliconReadoutSmall, 1, new TGeoTranslation(0,(0.2 + 0.02 + 0.02)/2, 0));
+		supportSmall->AddNode(siliconReadoutSmall, 2, new TGeoTranslation(0,-(0.2 + 0.02 + 0.02)/2, 0));
+		supportSmall->AddNode(aluCablesSmall, 1, new TGeoTranslation(0,(0.2 + 0.02 + 0.02 + 0.4)/2, 0));
+		supportSmall->AddNode(aluCablesSmall, 2, new TGeoTranslation(0,-(0.2 + 0.02 + 0.02 + 0.4)/2, 0));
+
+		TGeoTranslation transSupportSmall(0,2.1 + 5.3358 + 0.5,0);
+
+		for (int i = 0; i < 24; i++){
+			TString rotName("rotSupportSmall");
+			rotName.Append(i+1);
+			TGeoRotation rotSens(rotName.Data(), 0,0, i * 360/24);
+			TGeoHMatrix sens = rotSens * transSupportSmall;
+			supportDisk->AddNode(supportSmall, i+1, new TGeoHMatrix(sens));
+		}
+
+	combinedLambdaDisks->AddNode(lambdaDisk,1,new TGeoTranslation(0,0,positionOfDisk1inZ));
+	combinedLambdaDisks->AddNode(lambdaDisk,2,new TGeoTranslation(0,0,positionOfDisk2inZ));
+	combinedLambdaDisks->AddNode(supportDisk,1, new TGeoTranslation(0,0,(positionOfDisk1inZ + positionOfDisk2inZ)/2));
+
+	top->AddNode(combinedLambdaDisks,0);
+
+	  gGeoMan->CloseGeometry();
+	  top->Write();
+	  fi->Close();
+	//   gGeoManager->Export(outfile);
+	  top->Draw("ogl"); 
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
