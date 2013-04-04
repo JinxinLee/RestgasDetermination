b10a15482a72a07f504c9d4dabc09f496630e54c
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2013-04-04 13:59:31 +0000

    cosmetic changes.

--- tracking/PndTrkSttAdjacencies.cxx
+++ tracking/PndTrkSttAdjacencies.cxx
@@ -53,7 +53,7 @@ using namespace std;
 		nParContiguous[i+1]= 0;
 		for(j=0;j<NUMBER_STRAWS; j++){
 			if(j==i) continue;
-			if( fabs(wiredirection[j].Z() -1.) > 1.e-5) continue; // for now I don't consider the skews;
+			if( fabs((wiredirection[j]).Z() -1.) > 1.e-5) continue; // for now I don't consider the skews;
 			dis2 = ( x[i]-x[j])*( x[i]-x[j]) +
 				(y[i]-y[j])*(y[i]-y[j]);
 			if(dis2>1.1) continue;
