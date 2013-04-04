cf4f35ad63ebe81877aebd921f608916ac43fcc1
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2013-04-04 14:30:41 +0000

    cosmetic changes necessary for the MAC compiler.

--- tracking/PndTrkSttAdjacencies.cxx
+++ tracking/PndTrkSttAdjacencies.cxx
@@ -33,8 +33,13 @@ using namespace std;
 
 //	FILE *HAND = fopen("ListaSttStraw.lis","w");
 
-	double dis2, x[NUMBER_STRAWS], y[NUMBER_STRAWS], z[NUMBER_STRAWS];
-	TVector3 center[NUMBER_STRAWS], wiredirection[NUMBER_STRAWS];
+	double
+		dis2,
+		x[NUMBER_STRAWS],
+		y[NUMBER_STRAWS],
+		z[NUMBER_STRAWS],
+		Zwiredirection[NUMBER_STRAWS];
+
 	PndSttTube *pSttTube;
 
 	// the total n. of STT straws is NUMBER_STRAWS;
@@ -43,17 +48,17 @@ using namespace std;
 		x[i-1] = pSttTube->GetPosition().X();
 		y[i-1] = pSttTube->GetPosition().Y();
 		z[i-1] = pSttTube->GetPosition().Z();
-		wiredirection[i-1] = pSttTube->GetWireDirection();
+		Zwiredirection[i-1] = pSttTube->GetWireDirection().Z();
 	}
 
 
 	for (i=0;i< NUMBER_STRAWS; i++){
-		if( fabs(wiredirection[i].Z() -1.) > 1.e-5) continue; // for now I don't consider the skews;
+		if( fabs(Zwiredirection[i] -1.) > 1.e-5) continue; // for now I don't consider the skews;
 
 		nParContiguous[i+1]= 0;
 		for(j=0;j<NUMBER_STRAWS; j++){
 			if(j==i) continue;
-			if( fabs((wiredirection[j]).Z() -1.) > 1.e-5) continue; // for now I don't consider the skews;
+			if( fabs(Zwiredirection[j] -1.) > 1.e-5) continue; // for now I don't consider the skews;
 			dis2 = ( x[i]-x[j])*( x[i]-x[j]) +
 				(y[i]-y[j])*(y[i]-y[j]);
 			if(dis2>1.1) continue;
@@ -63,32 +68,6 @@ using namespace std;
 		}
 
 
-/*
-		fprintf(HAND,"Stt straw n. %d ",i+1);
-		fprintf(HAND,", X %g, Y %g, Z %g ",
-		x[i],y[i],z[i]);
-
-		if(wiredirection[i].Z() <0.) {
-			fprintf(HAND," dirX %g , dirY %g , dirZ %g ; straw contigue = %d ;",
-			-wiredirection[i].X(),-wiredirection[i].Y(),-wiredirection[i].Z(),
-			nParContiguous[i+1]);
-		}else{
-			fprintf(HAND," dirX %g , dirY %g , dirZ %g ; straw contigue = %d ;",
-			wiredirection[i].X(),wiredirection[i].Y(),wiredirection[i].Z(),
-			nParContiguous[i+1]);
-		}
-
-
-		if( nParContiguous[i+1]>0 ) {
-			fprintf(HAND," lista :");
-			for(n=0;n<nParContiguous[i+1];n++){
-				fprintf(HAND," %d",ListParContiguous[i+1][n]);
-			}
-			fprintf(HAND,";\n");
-		} else {
-			fprintf(HAND,"\n");
-		}
-*/
 	}
 
 //	fclose (HAND);
