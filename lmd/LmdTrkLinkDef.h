54d13bd325c786376ec764d256d40bbc3f73e9e2
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-04-11 20:07:37 +0000

    comment Runge-Kutta task compilation because chenges in GenFit are needed

--- lmd/LmdTrkLinkDef.h
+++ lmd/LmdTrkLinkDef.h
@@ -19,7 +19,7 @@
 //#pragma link C++ class PndLmdTrackCandToGFTrackCandConvertorTask+;
 #pragma link C++ class PndLmdGeaneTask+;
 #pragma link C++ class PndLmdKalmanTask+;
-#pragma link C++ class PndLmdBPRungeKuttaTask+;
+//#pragma link C++ class PndLmdBPRungeKuttaTask+;
 //#pragma link C++ class FairGeaneTrLmd+;
 
 
