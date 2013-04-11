d044f2a568ae758883ce9b39e1f49ceea503823b
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-04-11 17:26:21 +0000

    set usage of FULL mag. fields for a while instead of *_v1

--- macro/lmd/runLumiPixel0SimBox.C
+++ macro/lmd/runLumiPixel0SimBox.C
@@ -143,23 +143,24 @@ void runLumiPixel0SimBox(const int nEvents=10, const int startEv=0, const double
   //reading the new field map in the old format
   fRun->SetBeamMom(mom);
 
-  PndMultiField *fField= new PndMultiField();
-  
-  PndTransMap *map_t = new PndTransMap("TransMap_v1", "R");
-  PndDipoleMap *map_d1 = new PndDipoleMap("DipoleMap1_v1", "R");
-  PndDipoleMap *map_d2 = new PndDipoleMap("DipoleMap2_v1", "R");
-  PndSolenoidMap *map_s1 = new PndSolenoidMap("SolenoidMap1", "R");
-  PndSolenoidMap *map_s2 = new PndSolenoidMap("SolenoidMap2", "R");
-  PndSolenoidMap *map_s3 = new PndSolenoidMap("SolenoidMap3", "R");
-  PndSolenoidMap *map_s4 = new PndSolenoidMap("SolenoidMap4", "R");
-  
-  fField->AddField(map_t);
-  fField->AddField(map_d1);
-  fField->AddField(map_d2);
-  fField->AddField(map_s1);
-  fField->AddField(map_s2);
-  fField->AddField(map_s3);
-  fField->AddField(map_s4);
+
+  PndMultiField *fField= new PndMultiField("FULL"); 
+  //TODO: change FULL to *_v1 maps
+  //  PndMultiField *fField= new PndMultiField();
+  // PndTransMap *map_t = new PndTransMap("TransMap_v1", "R");
+  // PndDipoleMap *map_d1 = new PndDipoleMap("DipoleMap1_v1", "R");
+  // PndDipoleMap *map_d2 = new PndDipoleMap("DipoleMap2_v1", "R");
+  // PndSolenoidMap *map_s1 = new PndSolenoidMap("SolenoidMap1", "R");
+  // PndSolenoidMap *map_s2 = new PndSolenoidMap("SolenoidMap2", "R");
+  // PndSolenoidMap *map_s3 = new PndSolenoidMap("SolenoidMap3", "R");
+  // PndSolenoidMap *map_s4 = new PndSolenoidMap("SolenoidMap4", "R");
+  // fField->AddField(map_t);
+  // fField->AddField(map_d1);
+  // fField->AddField(map_d2);
+  // fField->AddField(map_s1);
+  // fField->AddField(map_s2);
+  // fField->AddField(map_s3);
+  // fField->AddField(map_s4);
   
   fRun->SetField(fField);
   
