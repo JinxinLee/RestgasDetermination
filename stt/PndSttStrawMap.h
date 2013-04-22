2a93e1fe2ac378299dea7246f35e864ddaa48e35
Marius Mertens <mertens>
2013-04-22 13:34:04 +0000

    + Added class to convert tube IDs to sector and row numbers.

---
+++ stt/PndSttStrawMap.h
@@ -0,0 +1,51 @@
+/*
+ * PndSTTStrawMap.h
+ *
+ *  Created on: Apr 16, 2013
+ *      Author: mertens
+ */
+
+#ifndef PNDSTTSTRAWMAP_H_
+#define PNDSTTSTRAWMAP_H_
+
+#include <map>
+#include <vector>
+
+class TClonesArray;
+
+using std::map;
+using std::vector;
+
+class PndSttStrawMap
+{
+public:
+
+	PndSttStrawMap();
+
+	const vector<int>& GetStrawRow(int sector, int row) const {return (fStrawIndex.find(sector))->second.at(row);}
+	const vector<vector<int> >& GetStrawSector(int sector) const {return (fStrawIndex.find(sector))->second;}
+	int GetSector(int strawindex) const {return fSectorOfStraw.at(strawindex);}
+	int FindPhiSector(double phi) const;
+	int GetRow(int strawindex) const {return fRowOfStraw.at(strawindex);}
+	int GetLeftSector(int sector) const {return ((sector +1) % 6);}
+	int GetRightSector(int sector) const {return ((sector +5) % 6);}
+
+	bool IsEdgeStraw(int strawindex) const;
+	int IsSectorBorderStraw(int strawindex) const;
+
+	void GenerateStrawMap(const TClonesArray* const stt_tube_array);
+
+private:
+
+	map<int, vector< vector<int> > > fStrawIndex;
+	vector<int> fSectorOfStraw;
+	vector<int> fRowOfStraw;
+	bool fStrawMapInitialized;
+
+	vector<double> fSectorStart;
+	vector<double> fSectorEnd;
+
+};
+
+
+#endif /* PNDSTTSTRAWMAP_H_ */
