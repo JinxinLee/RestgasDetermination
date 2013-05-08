6c3476ad659e08c662991f44d5039e113f40999c
Stefan Pflueger <s.pflueger@gsi.de>
2013-05-08 15:55:46 +0000

    small update on lmd macro makeLumiFitPlots.C. added 2d acceptance to overview canvas and fully documented this macro as well

--- macro/lmd/LMD_fit/makeLumiFitPlots.C
+++ macro/lmd/LMD_fit/makeLumiFitPlots.C
@@ -1,3 +1,24 @@
+/*
+ * This is the macro that produces plots from the luminosity fitting procedure
+ * General information about the individual classes of the LmdFit framework can be
+ * found in the doxygen manual
+ * Run it like this:
+ * 
+ * root -l -b -q 'makeLumiFitPlots.C("dir_to_elastic_scattering_data")'
+ *
+ * note: the dir_to_elastic_scattering_data should contain the fit_result.root root
+ * file that contains the data and fit result on which fit were performed...
+ *
+ * The usage of this macro is straight forward and uses primarily the
+ * PndLmdResultPlotter class (which is a helper class for creating plots on luminosity
+ * fits).
+ * -First you read in a PndLmdData object that was saved by the runLumi6Fit.C macro
+ * -Then you create so graph bundles that contain all the information of the fit
+ *  results and data in form of ROOT objects.
+ * -Finally you can pass these bundles to functions of the plotter and generate
+ *  different type of plots
+ */
+
 void makeLumiFitPlots(TString path) {
   std::cout << "Generating lumi plots for fit results....\n";
 
@@ -9,16 +30,24 @@ void makeLumiFitPlots(TString path) {
   gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
   gSystem->Load("libLmdFit");
 
+
+  // create an instance of PndLmdResultPlotter the plotting helper class
   PndLmdResultPlotter plotter;
 
+  // ================================ BEGIN CONFIG ================================ //
   // PndLmdResultPlotter sets default pad margins etc that should be fine for most cases
-  // you can fine tune it by changing the bottom stuff
-  /*gStyle->SetPadTopMargin(0.06);
-   gStyle->SetPadBottomMargin(0.12);
-   gStyle->SetPadLeftMargin(0.14);
-   gStyle->SetPadRightMargin(0.1);*/
+  // you can fine tune it and overwrite the default values 
+  //gStyle->SetPadTopMargin(0.06);
+  //gStyle->SetPadBottomMargin(0.12);
+  //gStyle->SetPadLeftMargin(0.14);
+  //gStyle->SetPadRightMargin(0.1);
 
+  // overwrite the default theta plot range if possible (if its larger than the max
+  // plot range then it has no effect)
   plotter.setThetaPlotRange(1.0, 18.0);
+
+  // The plotter has more options for text positioning and tex sizes for which you can
+  // overwrite the default values here
   //plotter.setTextLeftPos(0.20);
   //plotter.setTextTopPos(0.98);
   //plotter.setTextSpacing(0.08);
@@ -29,49 +58,33 @@ void makeLumiFitPlots(TString path) {
   //plotter.setLabelOffsetY(0.007);
   //plotter.setTitleOffsetX(1.0);
   //plotter.setTitleOffsetY(1.5);
+  // ================================= END CONFIG ================================= //
+  
 
+  // read in data from a root file which will return a vector of pointers to PndLmdData objects
   std::vector<PndLmdData*> data_vec = plotter.getDataFromPath(path);
 
+  // =============================== BEGIN PLOTTING =============================== //
   // if you only have a single data object (mostly the case)
   if (data_vec.size() > 0) {
     //std::map<PndLmdAcceptance*, std::vector<PndLmdLumiFitResult*> > &fit_map = data_vec[0]->getFitMap();
     std::vector<PndLmdAcceptance*> accs = data_vec[0]->getListOfAcceptances();
+    // in case there exists only a single acceptance (with which fit were performed...
+    // usually the case)
     if (accs.size() > 0) {
+      // create a vector of graph bundles (one entry for each fit option)
       std::vector<PndLmdResultPlotter::graph_bundle_1D> graph_bundles =
           plotter.makeGraphBundles1D(data_vec[0], accs[0]);
 
-      PndLmdResultPlotter::acceptance_bundle_1D acceptance_bundle =
-          plotter.makeAcceptanceBundle1D(accs[0]);
+      // create an acceptance bundle for this acceptance
+      PndLmdResultPlotter::acceptance_bundle acc_bundle =
+          plotter.makeAcceptanceBundle(accs[0]);
 
-      TCanvas *c = plotter.makeOverviewCanvas(graph_bundles, acceptance_bundle);
+      // make an overview canvas for these fit specs
+      TCanvas *c = plotter.makeOverviewCanvas(graph_bundles, acc_bundle);
+      // create a pdf
       c->SaveAs("fitresults_overview.pdf");
     }
   }
+  // ================================ END PLOTTING ================================ //
 }
-
-/*
- for (int i = 0; i < fit_res.size(); i++) {
- if (fit_res[i]->getLumiFitOptions()->getBinaryOptions()
- == fitop_normal.getBinaryOptions()) {
- c->cd(k + 1);
- plotter.fillSinglePad(c, vec_lmd_data[0], iter->first, fit_res[i],
- "fit_reco", lumi_ref, rangel, rangeh, 0, 0, 1, 0.45, 0.98);
- double lumi = fit_res[i]->getLuminosity();
- double lumi_err = fit_res[i]->getLuminosityError();
- x[k] = k + 1;
- y[k] = plotter.calulateLumiRelDiff(lumi, lumi_err, lumi_ref).first;
- y_errs[k] =
- plotter.calulateLumiRelDiff(lumi, lumi_err, lumi_ref).second;
- proj->Fill(y[k]);
- }*/
-
-/*int main(int argc, char *argv[]) {
- if (argc == 2) {
- makeComparisonPlots(argv[1]);
- } else if (argc > 2) {
- std::vector<std::string> paths;
- for (int i = 1; i < argc; i++)
- paths.push_back(std::string(argv[i]));
- makeComparisonCanvas(paths);
- }
- }*/
