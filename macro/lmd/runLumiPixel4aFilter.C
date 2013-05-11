65f0b051d39d1666387f6f688d671d842cc1d2e1
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-05-11 15:16:16 +0000

    fillter for fitted trks based on chi2 & hit info

---
+++ macro/lmd/runLumiPixel4aFilter.C
@@ -0,0 +1,112 @@
+void runLumiPixel4aFilter(const int nEvents=100000, const int startEvent=0, TString storePath="tmpOutput", const int verboseLevel=0, const bool mergedHits=true)
+{
+  // ========================================================================
+  // Input file (MC events)
+  TString MCFile = storePath+"/Lumi_MC_";
+  MCFile += startEvent;
+  MCFile += ".root";
+  
+
+  TString RecoFile = storePath;
+  if(mergedHits) RecoFile +="/Lumi_recoMerged_";
+  else  RecoFile +="/Lumi_reco_";
+  RecoFile += startEvent;
+  RecoFile += ".root";
+  // TCand file
+  TString CandFile = storePath+"/Lumi_TCand_";
+  CandFile += startEvent;
+  CandFile += ".root";
+  // Track file
+  TString TrkFile = storePath+"/Lumi_Track_";
+  TrkFile += startEvent;
+  TrkFile += ".root";
+
+  // Parameter file
+  TString parFile = storePath+"/Lumi_Params_";
+  parFile += startEvent;
+  parFile += ".root";
+
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+  gSystem->Load("libSds");
+  gSystem->Load("libSdsReco");
+  gSystem->Load("libLmd");
+  gSystem->Load("libLmdReco");
+  gSystem->Load("libLmdTrk");
+  // ------------------------------------------------------------------------
+  // Output file
+    TString outFile = storePath+"/Lumi_TrackFiltered_";
+    outFile += startEvent;
+    outFile += ".root";
+    
+    std::cout << "MCFile  : " << MCFile.Data()<< std::endl;
+    std::cout << "RecoFile: " << RecoFile.Data()<< std::endl;
+    std::cout << "TCandFile: " << CandFile.Data()<< std::endl;
+    std::cout << "TrackInFile: " << TrkFile.Data()<< std::endl;
+    std::cout << "TrackOutFile: " << outFile.Data()<< std::endl;
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile(MCFile);
+  fRun->AddFriend(RecoFile);
+  fRun->AddFriend(CandFile);
+  fRun->AddFriend(TrkFile);
+  fRun->SetOutputFile(outFile);
+  // ------------------------------------------------------------------------
+
+
+  // -----  Parameter database   --------------------------------------------
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo(kTRUE);
+  parInput1->open(parFile.Data(),"UPDATE");
+  rtdb->setFirstInput(parInput1);
+
+
+  // ======================================
+  // ======                       Track Filter                          ======
+  // ======================================
+  
+  // -----  LMD collections names & importain parameters --------------------------------------------
+  TString inHits = "LMDHitsPixel";
+  if(mergedHits){
+    inHits = "LMDHitsMerged";
+  }
+  PndLmdTrksFilterTask* lmdfilt = new PndLmdTrksFilterTask();
+  lmdfilt->SetVerbose(verboseLevel);
+  fRun->AddTask(lmdfilt);
+
+  rtdb->setOutput(parInput1);
+  rtdb->print();
+  // =====                 End of Track Filter                          =====
+  // ======================================
+
+  
+  // -----   Intialise and run   --------------------------------------------
+  fRun->Init();
+
+  fRun->Run(0,nEvents);
+  cout<<"fRun->Run(0, "<<nEvents<<") was done!"<<endl;
+  // ------------------------------------------------------------------------
+
+ rtdb->saveOutput();
+ rtdb->print();
+  // -----   Finish   -------------------------------------------------------
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Macro finished succesfully." << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+  cout << endl;
+  // ------------------------------------------------------------------------
+
+
+}
