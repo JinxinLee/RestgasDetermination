089064507213045eab3d02f383935feb1acb2088
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2013-05-17 09:41:23 +0000

    Including new track finders into compilation

--- mvd/MvdTrkLinkDef.h
+++ mvd/MvdTrkLinkDef.h
@@ -22,8 +22,10 @@
 #pragma link C++ class PndMvdKalmanTask+;
 #pragma link C++ class PndMvdTrackFinderAnaTask+;
 
-#pragma link C++ class PndMvdRiemannTrackFinder+;
+//#pragma link C++ class PndMvdRiemannTrackFinder+;
+#pragma link C++ class PndMvdGemRiemannTrackFinder+;
 #pragma link C++ class PndMvdRiemannTrackFinderTask+;
+#pragma link C++ class PndGemRiemannTrackFinderTask+;
 
 //#pragma link C++ class PndMvdTPCRiemannTrackFinderTask+;
 //#pragma link C++ class PndMvdTPCRiemannTrackFinderTaskCutPar+;
