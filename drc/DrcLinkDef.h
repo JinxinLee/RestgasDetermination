fa97a49f7cf5ed0f1c616c2709bc60c5b2a992b1
Maria Patsyuk <mpatsyuk>
2013-05-23 08:38:42 +0000

    new class for digitization was added - PndDrcHitFinder

--- drc/DrcLinkDef.h
+++ drc/DrcLinkDef.h
@@ -12,9 +12,16 @@
 #pragma link C++ class  PndGeoDrcPar+;
 #pragma link C++ class  PndDrcHitProducerIdeal+;
 #pragma link C++ class  PndDrcHitProducerReal+;
-#pragma link C++ class  DrawHits+;
-#pragma link C++ class  PndDrcDigiPar+;
 
+#pragma link C++ class  DrawHits+;
+#pragma link C++ class  FullReco+;
+#pragma link C++ class  PndDrcDigiPar+;
+#pragma link C++ class  PndDrcDigiTask+;
+#pragma link C++ class  PndDrcHitFinder+;
+#pragma link C++ class  PndDrcRecoLookup+;
+#pragma link C++ class  PndDrcRecoLookupMap+;
+#pragma link C++ class  PndPhoGunT+;
+#pragma link C++ class  PndKaleidoscop+;  
 
 
 
