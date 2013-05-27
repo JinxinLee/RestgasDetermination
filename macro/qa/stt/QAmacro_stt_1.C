02bb8baf8258ac15bb2ff80d916a0c4d87ad3373
Lia Lavezzi <lia.lavezzi@gmail.com>
2013-05-27 14:06:21 +0000

    field fixed (for real now)

--- macro/qa/stt/QAmacro_stt_1.C
+++ macro/qa/stt/QAmacro_stt_1.C
@@ -24,10 +24,8 @@ void QAmacro_stt_1()
  
   // -----   Magnetic field   -------------------------------------------
   // Constant Field
-  PndConstField *fMagField = new PndConstField();
-  fMagField->SetField(0., 0. ,20. ); // values are in kG
-  fMagField->SetFieldRegion(-50, 50,-50, 50, -50, 120);// values are in cm (xmin,xmax,ymin,ymax,zmin,zmax)
-  fRun->SetField(fMagField);
+  PndMultiField *fField= new PndMultiField("FULL");
+  fRun->SetField(fField);
   // --------------------------------------------------------------------
 
   // Set Material file Name
