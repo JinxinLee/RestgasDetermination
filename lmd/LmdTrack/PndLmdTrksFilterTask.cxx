ee6ff20ac7c79942d50a81d1c857a1d9e18ef65c
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-06-05 16:56:20 +0000

    moved here cut based on theta & phi of trk

--- lmd/LmdTrack/PndLmdTrksFilterTask.cxx
+++ lmd/LmdTrack/PndLmdTrksFilterTask.cxx
@@ -159,9 +159,8 @@ void PndLmdTrksFilterTask::Exec(Option_t* opt)
     FairTrackParP fFittedTrkP = trkpnd->GetParamFirst();
     TVector3 MomRecLMD(fFittedTrkP.GetPx(),fFittedTrkP.GetPy(),fFittedTrkP.GetPz());
     MomRecLMD *=1./MomRecLMD.Mag();
-    double thetaCent=MomRecLMD.Theta()-0.0402;
-    htthetatphiTrkFit->Fill(MomRecLMD.Theta(),MomRecLMD.Phi());
-    //    if(abs(thetaCent)>0.010 || abs(MomRecLMD.Phi())>0.22) dirOK=false;
+    double thetaCent=MomRecLMD.Theta()-0.040;
+    if(abs(thetaCent)>0.011 || abs(MomRecLMD.Phi())>0.25) dirOK=false;
     // if(abs(thetaCent)>0.020 || abs(MomRecLMD.Phi())>0.3) dirOK=false; //wide enought, but still should cut smth
     // //--------------------------
 
@@ -305,6 +304,12 @@ void PndLmdTrksFilterTask::Exec(Option_t* opt)
 	    <<mcidtop[0][i]<<", "<<mcidtop[1][i]<<", "<<mcidtop[2][i]<<", "<<mcidtop[3][i]<<")"<<endl;
       }
       PndTrack* trkpnd = (PndTrack*)(fTrkArray->At(i));
+      // // //check theta&phi-----
+      // FairTrackParP fFittedTrkP = trkpnd->GetParamFirst();
+      // TVector3 MomRecLMD(fFittedTrkP.GetPx(),fFittedTrkP.GetPy(),fFittedTrkP.GetPz());
+      // MomRecLMD *=1./MomRecLMD.Mag();
+      // double thetaCent=MomRecLMD.Theta()-0.0402;
+      // htthetatphiTrkFit->Fill(MomRecLMD.Theta(),MomRecLMD.Phi());
       new((*fTrkOutArray)[rec_trk]) PndTrack(*(trkpnd)); //save Track
       rec_trk++;
     }
