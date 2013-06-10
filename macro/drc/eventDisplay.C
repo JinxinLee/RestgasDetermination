c638909f17d399a7e4b67965a71aff753b9e2ded
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2013-06-10 16:43:28 +0000

    updated Event Display macro

--- macro/drc/eventDisplay.C
+++ macro/drc/eventDisplay.C
@@ -1,39 +1,36 @@
 eventDisplay()
 {
-   // Load basic libraries
+  // Load libraries
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
   rootlogon();
   gSystem->Load("libEve");
   gSystem->Load("libEventDisplay");
-
+  gSystem->Load("libPndEventDisplay");
                                      
   // -----   Reconstruction run   -------------------------------------------
   FairRunAna *fRun= new FairRunAna();
-  fRun->SetInputFile(" testrun1.root");
+  fRun->SetInputFile("sim.root");
+  TString digifile = "digi.root";  
+  //TString digifile = "digiG410k.root"; 
+  fRun->AddFriend(digifile);
   fRun->SetOutputFile("tst.root");
- 
-  
   
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
   FairParRootFileIo* parInput1 = new FairParRootFileIo();
-  parInput1->open("./params_testrun1.root");
-       
+  parInput1->open("par.root");      
   rtdb->setFirstInput(parInput1);
  
-
   FairEventManager *fMan= new FairEventManager();
   FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
   FairMCPointDraw *PndBarPoint = new FairMCPointDraw ("DrcBarPoint",kViolet, kFullSquare);
   FairMCPointDraw *PndPdPoint = new FairMCPointDraw ("DrcPDPoint",kBlue, kFullSquare);
-
+  PndDrcAccuDigiPixelDraw *PndAccuDigi = new PndDrcAccuDigiPixelDraw ("DrcPDHit",digifile,10.); //0.05
                                                                
   fMan->AddTask(Track);
-  fMan->AddTask( PndBarPoint);
-  fMan->AddTask( PndPdPoint);
-
-    
-// fRun->Init();
- fMan->Init();                     
-  
-
+  fMan->AddTask(PndBarPoint);
+  fMan->AddTask(PndPdPoint);
+  fMan->AddTask(PndAccuDigi);
+   
+  fMan->Init();                     
+ 
 }
