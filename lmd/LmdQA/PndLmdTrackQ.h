a0e67b40ff876e8a31d76701f8563cd1522fcc5d
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-06-19 11:27:56 +0000

    Container and Task for track quality assignment (good/ghost/missed)

---
+++ lmd/LmdQA/PndLmdTrackQ.h
@@ -0,0 +1,86 @@
+// -------------------------------------------------------------------------
+// -----               PndLmdTrackQ header file                  -----
+// -----       Created 18/06/2013 by A.Karavdina          -----
+// -------------------------------------------------------------------------
+
+
+/** PndLmdTrackQ.h
+ *@author A.Karavdina <karavdin@kph.uni-mainz.de>
+ **
+ ** Qualified reconstructed track in Luminosity detector
+ **/
+
+
+#ifndef PNDLMDTRACKQ_H
+#define PNDLMDTRACKQ_H
+
+
+#include "TVector3.h"
+#include "TString.h"
+#include "TObject.h"
+#include "TMatrixTSym.h"
+#include "TMatrixDSym.h"
+
+#include <stdio.h>
+#include <iostream>
+
+//#include "PndTrack.h"
+
+class PndLmdTrackQ: public TObject
+{
+public:
+  /** Default constructor **/
+  PndLmdTrackQ();
+
+  PndLmdTrackQ(double Plab);
+
+  /** Destructor **/
+  ~PndLmdTrackQ();
+
+
+  /** Accessors **/
+
+  void SetTrkRecStatus(int st){fTrkRecStatus = st;}
+  int GetTrkRecStatus() const{return fTrkRecStatus;}
+  void SetPDGcode(int pdg){fPDGcode = pdg;}
+  int GetPDGcode() const{return fPDGcode;}
+
+  void SetLMDpoint(double x, double y, double z){fXrecLMD=x;fYrecLMD=y;fZrecLMD=z;}
+  void GetLMDpoint(TVector3 lmdp){lmdp = TVector3(fXrecLMD,fYrecLMD,fZrecLMD);}
+  void SetLMDdir(double theta, double phi){fThetarecLMD = theta; fPhirecLMD = phi;}
+  double GetLMDtheta() const{return fThetarecLMD;}
+  double GetLMDphi() const{return fPhirecLMD;}
+  void SetLMDchi2(double chi2){fchi2LMD = chi2;}
+  double GetLMDchi2() const{return fchi2LMD;}
+  void SetIPpoint(double x, double y, double z){fXrec=x;fYrec=y;fZrec=z;}
+  void GetIPpoint(TVector3 lmdp){lmdp = TVector3(fXrec,fYrec,fZrec);}
+  void SetIPmom(double theta, double phi,double mom){fThetarec = theta; fPhirec = phi; fMomrec = mom;}
+  double GetIPtheta() const{return fThetarec;}
+  double GetIPphi() const{return fPhirec;}
+  double GetIPmom() const{return fMomrec;}
+
+  void SetMCpoint(double x, double y, double z){fXmc=x;fYmc=y;fZmc=z;}
+  void GetMCpoint(TVector3 lmdp){lmdp = TVector3(fXmc,fYmc,fZmc);}
+  void SetMCmom(double theta, double phi,double mom){fThetamc = theta; fPhimc = phi; fMommc = mom;}
+  double GetMCtheta() const{return fThetamc;}
+  double GetMCphi() const{return fPhimc;}
+  double GetMCmom() const{return fMommc;}
+  void SetSecondary(int sec){fSecondary = sec;}
+  int GetSecondary() const{return fSecondary;}
+
+ private:
+  Double_t fXrecLMD,fYrecLMD,fZrecLMD,fThetarecLMD,fPhirecLMD;
+  Double_t fXrec,fYrec,fZrec,fThetarec,fPhirec, fMomrec;
+  Double_t fXmc,fYmc,fZmc,fThetamc,fPhimc, fMommc;
+  Double_t fchi2LMD;
+  int fTrkRecStatus;// 0 = good,
+                              // >0 = ghost: 1 = hit mixture, 2 = doubled trk
+                              // <0 = missed: -1=trk-search losses,-2=little amount of hits,-3=no MC hits, -10=trk wasn't back-propag
+  int fSecondary;//0=primary, 1=secondary
+  int fPDGcode;// PDG code particle made the main contribution (65 % of hits) to the rec.track, -9999=n/a
+
+
+  ClassDef(PndLmdTrackQ,10);
+};
+
+#endif
---
+++ lmd/LmdQA/PndLmdTrkQTask.h
@@ -0,0 +1,87 @@
+// -------------------------------------------------------------------------
+// -----              PndLmdTrkQTask header file            -----
+// -----            Created 18/06/13  by A.Karavdina          -----
+// -------------------------------------------------------------------------
+
+/** PndLmdTrkQTask.h
+ *@author A.Karavdina <karavdin@kph.uni-mainz.de>
+ **
+ ** Assigns reconstructed track to simulated one, checks good/ghost/missed
+ **/
+
+
+#ifndef PNDLMDTRKQTASK_H
+#define PNDLMDTRKQTASK_H
+
+
+// framework includes
+#include "FairTask.h"
+#include "TH1.h"
+#include "TMatrixD.h"
+#include "TVector3.h"
+#include "PndGeoHandling.h"
+#include <vector>
+#include <map>
+
+#include "LmdQA/PndLmdTrackQ.h"
+
+class TClonesArray;
+class PndSdsCluster;
+
+
+class PndLmdTrkQTask : public FairTask
+{
+ public:
+
+  /** Default constructor **/  
+  PndLmdTrkQTask();
+
+  ///Set up beam momuntum value
+  PndLmdTrkQTask(Double_t pBeam);
+
+  /** Destructor **/
+  ~PndLmdTrkQTask();
+
+
+  /** Virtual method Init **/
+  //  virtual void SetParContainers();
+  virtual InitStatus Init();
+
+
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
+  
+  virtual void Finish();
+  void SetWriteMC(bool wr){fWriteAllMC = wr;}
+ private:
+
+  // Input Data ----------
+  TClonesArray* fMCHits;
+  TClonesArray* fMCTracks;
+  TClonesArray* fDigis;
+  TClonesArray* fClusters;
+  TClonesArray* fRecHits;
+  TClonesArray* fRecCandTracks;
+  TClonesArray* fRecTracks;
+  TClonesArray* fRecBPTracks;
+
+  // Output Data -------
+  TClonesArray*  fTrackQ;		
+ 
+  int fEventNr;
+// bool fUseMVDPoint;
+  Double_t fPbeam;
+  // Int_t fPDGid;
+  // TVector3 vtx;
+  bool fWriteAllMC;
+
+  
+
+
+  
+
+  ClassDef(PndLmdTrkQTask,1);
+
+};
+
+#endif
