79ba814eda39be7821af5cdc0f0e1ca2e5c69ec2
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-06-27 19:18:07 +0000

    fix bug due to changed name of mtx file

--- macro/lmd/runLumiPixel2Reco.C
+++ macro/lmd/runLumiPixel2Reco.C
@@ -99,7 +99,8 @@ void runLumiPixel2Reco(const int nEvents=10, const int startEvent=0, TString sto
   //  PndLmdIdealClusterTask* lmdmccls = new PndLmdIdealClusterTask();
   lmdmccls->SetVerbose(verboseLevel);
   lmdmccls->SetMSflag(useMSerr);//switch on\off error calculation due to multiple scaterring
-  lmdmccls->SetMtxPath(storePath);
+  //  lmdmccls->SetMtxPath(storePath);
+  lmdmccls->SetMtxPath("../../../pandaroot/input/");
   fRun->AddTask(lmdmccls);
   rtdb->setOutput(parInput1);
   rtdb->print();
