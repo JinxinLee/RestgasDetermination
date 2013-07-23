820bd47718e235cb54f4d05a591eb880147736cc
Stefano Spataro <spataro@to.infn.it>
2013-07-23 14:50:51 +0000

    RKTrackRep for FTS

--- GenfitTools/recohits/PndFtsRecoHit.cxx
+++ GenfitTools/recohits/PndFtsRecoHit.cxx
@@ -21,6 +21,7 @@
 
 // Collaborating Class Headers --------
 #include "GeaneTrackRep.h"
+#include "RKTrackRep.h"
 #include "PndFtsHit.h"
 #include "PndFtsTube.h"
 #include "PndFtsMapCreator.h"
@@ -129,6 +130,17 @@ PndFtsRecoHit::getHMatrix(const GFAbsTrackRep* stateVector)
     HMatrix[0][4] = 0.;
 
     return HMatrix;
+  }
+ else if (dynamic_cast<const RKTrackRep*>(stateVector) != NULL) { // CHECK this has been added but not tested: if you are not going to use it for testing purposes, please use GeaneTrackRep
+   TMatrixT<double> HMatrix(1,5);
+
+   HMatrix[0][0] = 0.;
+   HMatrix[0][1] = 0.;
+   HMatrix[0][2] = 0.;
+   HMatrix[0][3] = 1.;
+   HMatrix[0][4] = 0.;
+   
+   return HMatrix;
   }
   else {
     std::cerr << "PndFtsRecoHit can only handle state"
