da4b0ae4d13492f7461bda91a7bd0785e1ddcc04
Marius Mertens <mertens>
2013-07-30 15:06:55 +0000

    + Added FairLink to EventHeader.

--- stt/PndSttHitProducerRealFull.cxx
+++ stt/PndSttHitProducerRealFull.cxx
@@ -249,6 +249,7 @@ PndSttHit* PndSttHitProducerRealFull::AddHit(Int_t detID, Int_t tubeID, Int_t iP
 	  hitnew->ResetLinks();
 	  FairEventHeader* evtHeader = (FairEventHeader*)FairRootManager::Instance()->GetObject("EventHeader.");
 	  hitnew->AddLink(FairLink(evtHeader->GetInputFileId(), evtHeader->GetMCEntryNumber(),  "STTPoint", iPoint));
+	  hitnew->AddLink(FairLink(-1, FairRootManager::Instance()->GetEntryNr(), "EventHeader.", -1));
   }
   fDataBuffer->FillNewData(hitnew, p+EventTime+timeOfFlight, timeOfFlight+EventTime);
   return hitnew;
