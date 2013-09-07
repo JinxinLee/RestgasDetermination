b488c95fd344c8e57c4e656b80d82bd9c509bb94
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2013-09-07 11:21:44 +0000

    minor changes

--- macro/lmd/Anastasia/tests/DPM_sep2013/runLumiPixel0SimDPMDirect.C
+++ macro/lmd/Anastasia/tests/DPM_sep2013/runLumiPixel0SimDPMDirect.C
@@ -67,6 +67,26 @@ void runLumiPixel0SimDPMDirect(const int nEvents=10, const int startEvent=0, con
 
   //reading the new field map in the old format
   fRun->SetBeamMom(mom);
+
+ //reading the new field map in the old format
+  fRun->SetBeamMom(mom);
+  // PndMultiField *fField= new PndMultiField();
+  // PndTransMap *map_t = new PndTransMap("TransMap", "R");
+  // PndDipoleMap *map_d1 = new PndDipoleMap("DipoleMap1", "R");
+  // PndDipoleMap *map_d2 = new PndDipoleMap("DipoleMap2", "R");
+  // PndSolenoidMap *map_s1 = new PndSolenoidMap("SolenoidMap1", "R");
+  // PndSolenoidMap *map_s2 = new PndSolenoidMap("SolenoidMap2", "R");
+  // PndSolenoidMap *map_s3 = new PndSolenoidMap("SolenoidMap3", "R");
+  // PndSolenoidMap *map_s4 = new PndSolenoidMap("SolenoidMap4", "R");
+  // fField->AddField(map_t);
+  // fField->AddField(map_d1);
+  // fField->AddField(map_d2);
+  // fField->AddField(map_s1);
+  // fField->AddField(map_s2);
+  // fField->AddField(map_s3);
+  // fField->AddField(map_s4);
+  
+
   PndMultiField *fField= new PndMultiField("FULL");
   fRun->SetField(fField);
   
