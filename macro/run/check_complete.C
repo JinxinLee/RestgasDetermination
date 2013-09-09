676db110ad455703be674cf98531ab6d3257ef5c
Stefano Spataro <spataro@to.infn.it>
2013-09-09 14:12:07 +0000

    we need also this macro

---
+++ macro/run/check_complete.C
@@ -0,0 +1,66 @@
+#include "TString.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TKey.h"
+#include <iostream>
+#include "TH1F.h"
+#include "TRandom.h"
+
+using std::cout;
+using std::endl;
+
+bool check_complete(TString fn="output_ana.root", TString fn2="ana_target.root")
+{
+	bool fTest=kFALSE;
+	
+	TFile *f=new TFile(fn,"READ");
+	if (!f->IsZombie())
+	{
+		TFile *f2=new TFile(fn2,"READ");
+			
+		TKey *key;
+		TIter next(f->GetListOfKeys());
+		
+		int failcount = 0;
+		
+		while ( (key = (TKey*)next()) )
+		{
+			TObject *obj = key->ReadObj();
+			
+			// only check TH1Fs
+			if (!obj->InheritsFrom("TH1F")) continue;
+			
+			TString name = obj->GetName();
+			TH1F* h  = (TH1F*) obj; 
+			TH1F* h2 = (TH1F*) f2->Get(name);
+			
+			if ( h->GetEntries()<3 ) 
+			{
+				cout << "Histogram (almost) empty : " << name << " \"" << h2->GetTitle() << "\":  N = " <<  h->GetEntries() << endl;
+				failcount++;
+			}
+			else  
+			{
+				double P = h2->KolmogorovTest(h);
+				if ( P<0.1 )
+				{
+					cout << "Incompatible distribution: " << name << " \"" << h2->GetTitle() << "\":  P = " << P << endl;
+					failcount++;
+				}
+			}
+		
+		}
+		
+		if (failcount==0) fTest = kTRUE;
+	}
+	
+	if (fTest){
+		cout << " Test passed" << endl;
+		cout << " All ok " << endl;  
+	}else{
+		cout << " Test Failed" << endl;
+		cout << " Not Ok " << endl;         
+	}
+    
+    exit(fTest);
+}
