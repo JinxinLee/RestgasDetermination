b6f94f9d3f7a204a40b440a330fcd0462c1f5096
Dima Melnychuk <DimaMelnichuk@mail.ru>
2013-09-13 11:03:28 +0000

    bug fix in PndEmcHitProducer, attempt No 2

--- emc/EmcDigi/PndEmcHitProducer.cxx
+++ emc/EmcDigi/PndEmcHitProducer.cxx
@@ -42,7 +42,7 @@ static Int_t HowManyHitsAboveThreshold = 0;
 // -----   Default constructor   -------------------------------------------
 PndEmcHitProducer::PndEmcHitProducer() :
 	FairTask("Ideal EMC hit Producer"),
-	fUse_nonuniformity(0), fNonuniformityFile(""), fPointArray(new TClonesArray()), fMCTrackArray(new TClonesArray()), fHitArray(new TClonesArray()), fVolumeArray(new TObjArray()), fMapVersion(0), emcX(), emcY(), emcZ(), fEmcStr(), fMapper(), fDigiPar(new PndEmcDigiPar()), fGeoPar(new PndEmcGeoPar()), fNonuniformityPar(new PndEmcDigiNonuniformityPar()), fStoreHits(kTRUE), fEnergyThreshold(0)
+	fUse_nonuniformity(0), fNonuniformityFile(""), fPointArray(), fMCTrackArray(), fHitArray(), fVolumeArray(), fMapVersion(0), emcX(), emcY(), emcZ(), fEmcStr(), fMapper(), fDigiPar(), fGeoPar(), fNonuniformityPar(), fStoreHits(kTRUE), fEnergyThreshold(0)
 {
 	fNonuniformityFile=gSystem->Getenv("VMCWORKDIR");
 	fNonuniformityFile+="/input/EmcDigiNoniformityPars.root";
@@ -51,7 +51,7 @@ PndEmcHitProducer::PndEmcHitProducer() :
 
 PndEmcHitProducer::PndEmcHitProducer(Bool_t val) :
 	FairTask("Ideal EMC hit Producer"),
-	fUse_nonuniformity(0), fNonuniformityFile(""), fPointArray(new TClonesArray()), fMCTrackArray(new TClonesArray()), fHitArray(new TClonesArray()), fVolumeArray(new TObjArray()), fMapVersion(0), emcX(), emcY(), emcZ(), fEmcStr(), fMapper(), fDigiPar(new PndEmcDigiPar()), fGeoPar(new PndEmcGeoPar()), fNonuniformityPar(new PndEmcDigiNonuniformityPar()), fStoreHits(val), fEnergyThreshold(0)
+	fUse_nonuniformity(0), fNonuniformityFile(""), fPointArray(), fMCTrackArray(), fHitArray(), fVolumeArray(), fMapVersion(0), emcX(), emcY(), emcZ(), fEmcStr(), fMapper(), fDigiPar(), fGeoPar(), fNonuniformityPar(), fStoreHits(val), fEnergyThreshold(0)
 { 
 	fNonuniformityFile=gSystem->Getenv("VMCWORKDIR");
 	fNonuniformityFile+="/input/EmcDigiNoniformitypars.root";
