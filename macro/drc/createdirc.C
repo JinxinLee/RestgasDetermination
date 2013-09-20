a510f7cdec44a11d79f4273dbacb53dedf93139f
Maria Patsyuk <mpatsyuk>
2013-09-20 08:30:58 +0000

    input parameters for easier check of geometry options

--- macro/drc/createdirc.C
+++ macro/drc/createdirc.C
@@ -16,11 +16,17 @@
 // fFocusingSystem = 4 - two component spherical lens is used
 // fFocusingSystem = 6 - three component spherical lens is used
 
-
+// par1 and par2 - lens radii
+// par3 - EVdrop [cm]
+// par4 - EVoffset [cm]
+// par5 - bottom tilt of the EV [degrees]
+// par6 - depth of the EV [cm]
+// par7 - EV back angle [degrees]
+// par8 - free parameter
 // B E F O R E   R U N N I N G   T H E   P R O G R A M   C H E C K   T H E   N A M E   O F   T H E   O U T P U T   F I L E ! ! ! 
 const Double_t pi =  4.*atan(1.);
 
-void createdirc(Int_t fGeomType = 1, Int_t fFocusingSystem = 6, Int_t iter=0, TString geomPath=".", Double_t par1=-100, Double_t par2=-100, Double_t par3=-100, Double_t par4=-100, Double_t par5=2.5, Double_t par6=-100, Double_t par7=-100, Double_t par8=-100){ 
+void createdirc(Int_t fGeomType = 1, Int_t fFocusingSystem = 6, Int_t iter=0, TString geomPath=".", Double_t par1=-100, Double_t par2=-100, Double_t par3=-100, Double_t par4=-100, Double_t par5=-100, Double_t par6=-100, Double_t par7=-100, Double_t par8=-100){ 
 
   { // initialization 
     gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
