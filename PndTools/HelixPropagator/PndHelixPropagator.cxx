6f38229f42854264230dc56a053af3b1af72c5e8
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2013-09-30 08:25:51 +0000

    Simple Helix propagator for event display

---
+++ PndTools/HelixPropagator/PndHelixPropagator.cxx
@@ -0,0 +1,103 @@
+/*
+ * PndHelixPropagator.cxx
+ *
+ *  Created on: Sep 23, 2013
+ *      Author: stockman
+ */
+
+#include "PndHelixPropagator.h"
+#include "TMath.h"
+
+#include <iostream>
+
+  ClassImp(PndHelixPropagator)
+
+PndHelixPropagator::PndHelixPropagator() :
+	fFieldStrength(0), fOrigin(), fMomentum(), fCharge(0)
+{
+}
+
+PndHelixPropagator::~PndHelixPropagator() {
+	// TODO Auto-generated destructor stub
+}
+
+FairTrackPar PndHelixPropagator::PropagateToZ(Double_t zPos)
+{
+	TVector3 magField(0,0,fFieldStrength);
+	TVector3 dirRadius = fMomentum.Cross(magField).Unit();
+	TVector3 circleCenter = fOrigin + (fCharge * Radius() * dirRadius);
+
+	Double_t ratioPtPl = fMomentum.Pt() / fMomentum.z();
+
+	TVector3 zeroCircle = fOrigin - circleCenter;
+
+	Double_t zLength = zPos - fOrigin.z();
+	Double_t arcLength = ratioPtPl * zLength;
+	Double_t deltaPhi = -arcLength / Radius();
+	Double_t newPhi = deltaPhi + zeroCircle.Phi();
+
+	TVector3 newOrigin = zeroCircle;
+	newOrigin.SetPhi(newPhi);
+	newOrigin += circleCenter;
+	newOrigin.SetZ(zPos);
+	TVector3 newMomentum = fMomentum;
+
+	newMomentum.RotateZ(deltaPhi);
+
+	std::cout << "Origin: "; fOrigin.Print(); std::cout << std::endl;
+	std::cout << "Momentum: "; fMomentum.Print(); std::cout << " Ratio Pt/Pl: " << ratioPtPl << " Radius: " << Radius() << " "; dirRadius.Print(); std::cout << std::endl;
+	std::cout << "circleCenter: "; circleCenter.Print(); std::cout << std::endl;
+	std::cout << "zeroCircle: "; zeroCircle.Print(); std::cout << std::endl;
+	std::cout << "Phi: " << zeroCircle.Phi() << " deltaPhi: " << deltaPhi << " newPhi: " << newPhi << std::endl;
+	std::cout << "NewOrigin: "; newOrigin.Print(); std::cout << std::endl;
+	std::cout << "NewMomentum: "; newMomentum.Print(); std::cout << std::endl;
+
+
+	FairTrackPar result(newOrigin.x(), newOrigin.y(), newOrigin.z(), newMomentum.Px(), newMomentum.Py(), newMomentum.Pz(), fCharge);
+
+	return result;
+}
+
+FairTrackPar PndHelixPropagator::PropagateByAngle(Double_t step)
+{
+	TVector3 magField(0,0,fFieldStrength);
+	TVector3 dirRadius = fMomentum.Cross(magField).Unit();
+	TVector3 circleCenter = fOrigin + (fCharge * Radius() * dirRadius);
+	Double_t stepInRad = step / 180 * TMath::Pi();
+
+	Double_t ratioPtPl = 0;
+	if (fMomentum.Pz() != 0)
+		ratioPtPl = fMomentum.Pt() / fMomentum.Pz();
+
+	TVector3 zeroCircle = fOrigin - circleCenter;
+
+	Double_t newPhi = zeroCircle.Phi() + stepInRad;
+	TVector3 newOrigin = zeroCircle;
+	newOrigin.SetPhi(newPhi);
+	newOrigin += circleCenter;
+	if (ratioPtPl != 0)
+		newOrigin.SetZ(stepInRad * Radius() / ratioPtPl);
+	else
+		newOrigin.SetZ(fOrigin.Z());
+
+	TVector3 newMomentum = fMomentum;
+
+	newMomentum.RotateZ(stepInRad);
+
+	std::cout << "Origin: "; fOrigin.Print(); std::cout << std::endl;
+	std::cout << "Momentum: "; fMomentum.Print(); std::cout << " Ratio Pt/Pl: " << ratioPtPl << " Radius: " << Radius() << " "; dirRadius.Print(); std::cout << std::endl;
+	std::cout << "circleCenter: "; circleCenter.Print(); std::cout << std::endl;
+	std::cout << "zeroCircle: "; zeroCircle.Print(); std::cout << std::endl;
+	std::cout << "Phi: " << zeroCircle.Phi() << " deltaPhi: " << stepInRad << " newPhi: " << newPhi << std::endl;
+	std::cout << "NewOrigin: "; newOrigin.Print(); std::cout << std::endl;
+	std::cout << "NewMomentum: "; newMomentum.Print(); std::cout << std::endl;
+
+
+	FairTrackPar result(newOrigin.x(), newOrigin.y(), newOrigin.z(), newMomentum.Px(), newMomentum.Py(), newMomentum.Pz(), fCharge);
+	return result;
+}
+
+
+Double_t PndHelixPropagator::Radius(){
+	return fMomentum.Perp() / (fFieldStrength * 0.3)*100;
+}
---
+++ PndTools/HelixPropagator/PndHelixPropagator.h
@@ -0,0 +1,38 @@
+/*
+ * PndHelixPropagator.h
+ *
+ *  Created on: Sep 23, 2013
+ *      Author: stockman
+ */
+
+#ifndef PNDHELIXPROPAGATOR_H_
+#define PNDHELIXPROPAGATOR_H_
+
+#include "TObject.h"
+#include "TVector3.h"
+#include "FairTrackPar.h"
+#include <utility>
+
+class PndHelixPropagator : public TObject {
+public:
+	PndHelixPropagator();
+	PndHelixPropagator(Double_t fieldStrength, TVector3 origin, TVector3 momentum, Double_t charge):
+		fFieldStrength(fieldStrength), fOrigin(origin), fMomentum(momentum), fCharge(charge)
+	{};
+	virtual ~PndHelixPropagator();
+	FairTrackPar PropagateToZ(Double_t zPos);		//< returns position and momentum at requested z-Value [cm]
+	FairTrackPar PropagateToS(Double_t arcLength){};
+	FairTrackPar PropagateByAngle(Double_t step); //< angle step in degrees from start position/momentum
+
+private:
+	Double_t Radius();
+
+	Double_t fFieldStrength; //< magentic field strength in z-Direction in Tesla
+	TVector3 fOrigin;		 //< Origin of track in cm
+	TVector3 fMomentum;		 //< Momentum of track in GeV
+	Double_t fCharge;		 //< Sign of charge
+public:
+  ClassDef(PndHelixPropagator,1)
+};
+
+#endif /* PNDHELIXPROPAGATOR_H_ */
