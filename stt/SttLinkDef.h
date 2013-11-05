c274362f9be6de7272ec0f95b209cc2ddcdb22d9
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2013-11-05 16:26:23 +0000

    removed PndTrackFinderReal.cxx from compilation since they are obsolete and also contain reference to glpk functions;

--- stt/SttLinkDef.h
+++ stt/SttLinkDef.h
@@ -24,7 +24,7 @@
 #pragma link C++ class PndSttHitProducerRealFast+;
 #pragma link C++ class PndSttHitProducerRealFull+;
 #pragma link C++ class PndSttHelixHitProducer+;
-#pragma link C++ class PndSttTrackFinderReal+;
+//   #pragma link C++ class PndSttTrackFinderReal+;
 #pragma link C++ class PndSttTrackFitterQATask+;
 #pragma link C++ class PndSttMapCreator+;
 #pragma link C++ class PndSttTube+;
