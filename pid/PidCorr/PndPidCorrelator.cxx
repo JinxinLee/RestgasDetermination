76e9ad47a21dc8cb148c5ddf82aa2832d0054b6c
Stefano Spataro <spataro@to.infn.it>
2013-11-25 12:33:26 +0000

    Fix emc calibrator

--- pid/PidCorr/PndPidCorrelator.cxx
+++ pid/PidCorr/PndPidCorrelator.cxx
@@ -622,7 +622,7 @@ InitStatus PndPidCorrelator::Init() {
         fEmcErrorMatrixPar->SetErrorMatrixObject(fEmcErrorMatrix->GetParObject());
         //std::cout<<"PndPidCorrelator: Emc error matrix is read from file"<<std::endl;
       }
-    fEmcCalibrator= PndEmcClusterCalibrator::MakeEmcClusterCalibrator(2, 3);	
+    fEmcCalibrator= PndEmcClusterCalibrator::MakeEmcClusterCalibrator(1, 3);	
   }
 
   if (fFast)  cout << "-W- PndPidCorrelator::Init: Using fast correlator!!" << endl;
