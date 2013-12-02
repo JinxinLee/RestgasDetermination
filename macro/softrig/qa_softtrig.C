96a52c9f0712a9fcf8ae22642ff42a1079929645
Klaus Goetzen <K.Goetzen@gsi.de>
2013-12-02 14:35:32 +0000

    small modifications

--- macro/softrig/qa_softtrig.C
+++ macro/softrig/qa_softtrig.C
@@ -1,49 +1,49 @@
+// Check if file exists and readable
 bool checkfile(TString fn)
 {
 	bool fileok=true;
-	TFile fff(fn); 
+	TFile fff(fn, "READ"); 
 	if (fff.IsZombie()) fileok=false;
 	TTree *t=(TTree*)fff.Get("cbmsim");
 	if (t==0x0) fileok=false;
 	
 	if (!fileok) cout <<"Skipping broken file '"<<fn<<"'"<<endl;
+	fff.Close();
+	
 	return fileok;
 }
 
-void qa_softtrig(TString outpre="M9999", int mode, double pmom, int from=1, int to=1, int nEvents=0)
+// Add many input files to FairRunAna
+void attachFiles(FairRunAna* fRun, TString pref, int min, int max)
 {
-	gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
-	gSystem->Load("libSofTrig");
-	
-  	TString OutFile   = outpre+"_st_qa.root"; 
-	TString inParFile = TString::Format("%s_%d_par.root",outpre.Data(),from);
-	
-  	bool finefile[1000];
-  	for (int i=0;i<1000;++i) finefile[i]=false;
-  
-  	FairRunAna *fRun= new FairRunAna();
-  
-   	bool firstfile=true;
+	bool firstfile=true;
+	for (int i=min;i<=max;++i) {
+		TString fname = TString::Format("%s_%d_pid.root",pref.Data(),i);
 
-  	// add pid files
-  	for (int i=from;i<=to;++i)
-  	{
-		TString fname = outpre+"_"; fname+=i; fname+="_pid.root";
-		//TString fname2 = outpre+"_"; fname2+=i; fname2+="_sim.root";
-
-		if (checkfile(fname) )//&& checkfile(fname2))
-		{
-			finefile[i]=true;
+		if (checkfile(fname) ) {
 			if (firstfile) fRun->SetInputFile(fname);
 			else fRun->AddFile(fname);
 			firstfile=false;
 		}
+	}
+}
+
+void qa_softtrig(TString pref="M9999", int mode, double pmom, int from=1, int to=1, int nEvents=0)
+{
+  	TString OutFile   = TString::Format("%s_%d_%d_sof.root",pref.Data(),from, to); 
 	
-  	}
+  	FairRunAna *fRun= new FairRunAna();
+	
+	// attach input files according to parameters
+	attachFiles(fRun, pref, from, to);
+	
+	// turn off FairLogger output
 	FairLogger::GetLogger()->SetLogToFile(kFALSE);
+	
 	
 	// *** initialization
 /*	FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+	TString inParFile = TString::Format("%s_%d_par.root",pref.Data(),from);
 	
 	FairParRootFileIo* parIO = new FairParRootFileIo();
 	parIO->open(inParFile);
@@ -62,8 +62,12 @@ void qa_softtrig(TString outpre="M9999", int mode, double pmom, int from=1, int 
 	// *** HERE OUR TASK GOES!
 	PndSoftTriggerTask *stTask = new PndSoftTriggerTask(pmom, mode);
 	
-	stTask->SetGammaMinE(0.10);
-	stTask->SetTrackMinP(0.10);
+	stTask->SetTag_All(true);		// tag all modes
+	stTask->SetQA_All(true);		// ntuple output for all modes
+	
+	stTask->SetGammaMinE(0.10);		// global energy pre-cut for neutrals 
+	stTask->SetTrackMinP(0.10);		// global momentum pre-cut for charged 	
+	stTask->SetInitialPidCut(0.1);	// global PID pre-cut for charged 	
 	
 	fRun->AddTask(stTask);
 	
