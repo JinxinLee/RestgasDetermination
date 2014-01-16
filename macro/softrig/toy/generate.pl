ec50e2d95ae4cfc59f7a7dacf1ed92159fbc985b
Klaus Goetzen <K.Goetzen@gsi.de>
2014-01-16 13:01:44 +0000

    development

--- macro/softrig/toy/generate.pl
+++ macro/softrig/toy/generate.pl
@@ -7,8 +7,9 @@ if ( @ARGV!=2 )
     print "\nUSAGE:\n";
     print "generate.pl <mode> <nevts>\n\n";
     print "  mode  : mode code for soft trigger, e.g. 38201: J/psi pi+ pi-, J/psi -> e+ e- at E_cm = 3.77 GeV\n";
-    print "          mode='all' generates all (caution! takes long)\n";
-    print "          mode='core' generates 10 core modes (caution! takes also long)\n";
+    print "          mode='all' generates all Evt modes (caution! takes long)\n";
+    print "          mode='core' generates 10 Evt core modes (caution! takes also long)\n";
+    print "          mode='dpm' generates all DPM data\n";
     print "  nevts : number of events to be generated\n\n";
     exit(0);
 }
@@ -24,10 +25,13 @@ my %moms = (
    "50" => 12.348596,
    "55" => 15.152765 
 );
-my @core24 = qw(24000 24300 24400 24900);
-my @core38 = qw(38000 38100 38120 38200 38201 38221 38300 38400 38900);
-my @core45 = qw(45000 45100 45120 45140 45200 45201 45221 45300 45400 45900);
-my @core55 = qw(55000 55100 55120 55140 55200 55201 55221 55300 55400 55420 55900);
+
+my @dpm    = qw(24900 38900 45900 55900);
+
+my @core24 = qw(24000 24300 24400);
+my @core38 = qw(38000 38100 38120 38200 38201 38221 38300 38400);
+my @core45 = qw(45000 45100 45120 45140 45200 45201 45221 45300 45400);
+my @core55 = qw(55000 55100 55120 55140 55200 55201 55221 55300 55400 55420);
 
 my @modes24 = qw(24000 
                  24300 24320 24340 
@@ -82,35 +86,49 @@ elsif ($enmode eq "core")
 	push (@modelist,@core45);
 	push (@modelist,@core55);
 }
+elsif ($enmode eq "dpm")
+{
+	push (@modelist,@dpm);
+}
 else
 {
 	push (@modelist,$enmode);
 }
 
+print "Generating modes: ".join(", ", @modelist)."\n";
+
 my $res  = "pbarpSystem0";
 
 foreach (@modelist)
 {
-	my $en   = substr($enmode,0,2);
+	my $en   = substr($_,0,2);
 	my $pmom = $moms{$en};
-	my $mode = substr($enmode,2,3);
+	my $mode = substr($_,2,3);
 	my $decfile = `ls ../decfiles/M$mode*dec 2> /dev/null`;
 	chomp($decfile);
 
 	if ($nevts<=0) {print "invalid number of events: $nevts\n";exit(0);}
-	if ($decfile eq "" && $mode!="900") {print "no decay file found for mode $mode\n";exit(0);}
+	if ($decfile eq "" && $mode ne "900") {print "no decay file found for mode $mode\n";exit(0);}
 	if ($pmom<1) { print "invalid pmom\n"; exit(0);}
 
 	my $nevtsk   = $nevts/1000;
 
 	if ($mode!="900")
 	{
-		my $outfile = "EVT_".$enmode."_".$nevtsk."k.root";
-		system("./simpleEvtGenRO $res $decfile $nevts $pmom; mv evtOutput.root data/$outfile");
+		my $outfile = "EVT_".$_."_".$nevtsk."k.root";
+        print "Generating EvtGen mode $_(p=$pmom), $nevts evts -> data/$outfile\n";
+        if (-e "evtOutput.root") {`rm evtOutput.root`;}
+		`./simpleEvtGenRO $res $decfile $nevts $pmom > /dev/null`;
+        if (-e "evtOutput.root") {
+          `mv evtOutput.root data/$outfile > /dev/null`;
+		}
+		else {print "Failed to generate mode $_\n";}
 	}
 	else
 	{
-		my $outfile = "EVT_".$enmode."_".$nevtsk."k.root";
-		print "DPMGen\n";
+		my $outfile = "DPM_".$_."_".$nevtsk."k.root";
+        print "Generating DPMGen mode $_(p=$pmom), $nevts evts -> data/$outfile\n";
+		`echo 0 $pmom 0 $nevts | ./DPMGen 2> /dev/null`;
+        `mv Background-micro.root data/$outfile`;
 	}
 }
