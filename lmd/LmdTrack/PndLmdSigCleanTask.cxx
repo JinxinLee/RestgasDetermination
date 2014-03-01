5b724ddbbaa570988bfb17ab065f5a4792fa5839
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2014-03-01 19:48:55 +0000

    fix BDT cut

--- lmd/LmdTrack/PndLmdSigCleanTask.cxx
+++ lmd/LmdTrack/PndLmdSigCleanTask.cxx
@@ -213,8 +213,8 @@ bool  PndLmdSigCleanTask::CheckMVA(FairTrackParH* fTrk){
   aphrec = fTrk->GetPhi();
   double mva_response =  reader->EvaluateMVA(fmethodName);
   // if(mva_response>-0.0599) res=true; //BDT 
-  //  if(mva_response>0) res=true; //BDT //TEST
-  if(mva_response>-0.065) res=true; //BDT //TEST
+  if(mva_response>0) res=true; //BDT //TEST
+  //  if(mva_response>-0.065) res=true; //BDT //TEST
   else res=false;
   return res;
 }
