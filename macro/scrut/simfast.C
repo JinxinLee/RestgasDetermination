0402e21d967b293f150f9bfdb660022a184796ac
Klaus Goetzen <K.Goetzen@gsi.de>
2014-03-27 10:28:17 +0000

    updated simfast.C with MdtPid and EmcPid

--- macro/scrut/simfast.C
+++ macro/scrut/simfast.C
@@ -103,12 +103,12 @@ void simfast(TString Prefix, TString Decfile, Float_t Mom, Int_t nEvents = 1000,
   fastSim->AddDetector("ScMvdGemFts", "thtMin=5.    thtMax=7.8   ptmin=0.1 pmin=0.0 pRes=0.03  thtRes=0.001 phiRes=0.001 efficiency=0.80");
   fastSim->AddDetector("ScFts",       "thtMin=0.    thtMax=5.    ptmin=0.0 pmin=0.5 pRes=0.05  thtRes=0.002 phiRes=0.002 efficiency=0.80");
   // - other options:
-  //fastSim->AddDetector("ScMvdFts",    "thtMin=5.    thtMax=10    ptmin=0.0 pRes=0.05  thtRes=0.002 phiRes=0.002 efficiency=0.80");
-  //fastSim->AddDetector("ScGemFts",    "thtMin=3.    thtMax=10    ptmin=0.0 pRes=0.05  thtRes=0.002 phiRes=0.002 efficiency=0.80");
+  //fastSim->AddDetector("ScMvdFts",    "thtMin=5.    thtMax=10    ptmin=0.0 pmin=0.0 pRes=0.05  thtRes=0.002 phiRes=0.002 efficiency=0.80");
+  //fastSim->AddDetector("ScGemFts",    "thtMin=3.    thtMax=10    ptmin=0.0 pmin=0.0 pRes=0.05  thtRes=0.002 phiRes=0.002 efficiency=0.80");
   // - STT alone:
-  //fastSim->AddDetector("ScSttAlone",  "thtMin=133.6 thtMax=159.5 ptmin=0.1 pRes=0.030 thtRes=0.06 phiRes=0.1 efficiency=0.25");
-  //fastSim->AddDetector("ScSttAlone",  "thtMin=20.9  thtMax=133.6 ptmin=0.1 pRes=0.026 thtRes=0.06 phiRes=0.1 efficiency=0.95");
-  //fastSim->AddDetector("ScSttAlone",  "thtMin=7.8   thtMax=20.9  ptmin=0.1 pRes=0.026 thtRes=0.06 phiRes=0.1 efficiency=0.25");
+  //fastSim->AddDetector("ScSttAlone",  "thtMin=133.6 thtMax=159.5 ptmin=0.1 pmin=0.0 pRes=0.030 thtRes=0.06 phiRes=0.1 efficiency=0.25");
+  //fastSim->AddDetector("ScSttAlone",  "thtMin=20.9  thtMax=133.6 ptmin=0.1 pmin=0.0 pRes=0.026 thtRes=0.06 phiRes=0.1 efficiency=0.95");
+  //fastSim->AddDetector("ScSttAlone",  "thtMin=7.8   thtMax=20.9  ptmin=0.1 pmin=0.0 pRes=0.026 thtRes=0.06 phiRes=0.1 efficiency=0.25");
 
   // -----------------------------------------------------------------------------------
   // Vertexing
@@ -123,9 +123,9 @@ void simfast(TString Prefix, TString Decfile, Float_t Mom, Int_t nEvents = 1000,
   // EM Calorimeters w/ default parameters
   // (don't have to be set, just to list the available parameters
   // -----------------------------------------------------------------------------------
-  fastSim->AddDetector("EmcBarrel","thtMin=22.0 thtMax=140.0 Emin=0.01 barrelRadius=0.5");
+  fastSim->AddDetector("EmcBarrel","thtMin=22.0 thtMax=142.0 Emin=0.01 barrelRadius=0.5");
   fastSim->AddDetector("EmcFwCap", "thtMin=5.0 thtMax=22.0 Emin=0.01 dist=2.5");
-  fastSim->AddDetector("EmcBwCap", "thtMin=140.0 thtMax=170.0 Emin=0.01 dist=0.7");
+  fastSim->AddDetector("EmcBwCap", "thtMin=142.0 thtMax=160.0 Emin=0.01 dist=0.7");
   fastSim->AddDetector("EmcFS",    "thtMin=0.05 thtMax=5.0 aPar=0.02 bPar=0.0274 Emin=0.01 dist=8.0");
 
   // -----------------------------------------------------------------------------------
@@ -134,11 +134,17 @@ void simfast(TString Prefix, TString Decfile, Float_t Mom, Int_t nEvents = 1000,
   // -----------------------------------------------------------------------------------
   fastSim->AddDetector("MvdPid","thtMin=5.  thtMax=133.6 ptmin=0.1  dEdxResMulti=1. efficiency=1."); //Note: A Bethe-Bloch-Landau-Gauss Prametrization from 2008
   fastSim->AddDetector("SttPid","thtMin=7.8 thtMax=159.5 ptmin=0.1 dEdxRes=1. efficiency=1."); //Note: A dEdX parametrization from 2008
+  
   fastSim->AddDetector("DrcBarrel","thtMin=22.0 thtMax=140.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
   fastSim->AddDetector("DrcDisc","thtMin=5.0 thtMax=22.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
   fastSim->AddDetector("Rich","angleXMax=5.0 angleYMax=10.0 dthtc=0.01 nPhotMin=5 effNPhotons=0.075");
-  fastSim->AddDetector("MdtBarrel", "thtMin=10.0 thtMax=130.0 pMin=1.0 efficiency=0.95 misId=0.01");
-  fastSim->AddDetector("MdtForward","thtMin=0.0  thtMax=10.0  pMin=1.5 efficiency=0.95 misId=0.01");
+  
+  fastSim->AddDetector("ScMdtPidBarrel", "thtMin=10.0 thtMax=130.0 pMin=1.0 efficiency=0.95 misId=0.01");
+  fastSim->AddDetector("ScMdtPidForward","thtMin=0.0  thtMax=10.0  pMin=1.5 efficiency=0.95 misId=0.01");
+  
+  fastSim->AddDetector("ScEmcPidFwCap",  "thtMin=10.0  thtMax=22.0  ptmin=0.0 pmin=0.0 efficiency=1.0");
+  fastSim->AddDetector("ScEmcPidBarrel", "thtMin=22.0  thtMax=142.0 ptmin=0.2 pmin=0.0 efficiency=1.0");
+  fastSim->AddDetector("ScEmcPidBwCap",  "thtMin=142.0 thtMax=160.0  ptmin=0.0 pmin=0.0 efficiency=1.0");
 
   fRun->AddTask(fastSim);
   //-------------------------  Initialize the RUN  -----------------
