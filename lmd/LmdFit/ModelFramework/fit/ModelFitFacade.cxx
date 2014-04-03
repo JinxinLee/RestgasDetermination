2c8d153b6600cf9c1893c27210ff3a95efde26d2
Stefan Pflueger <s.pflueger@gsi.de>
2014-04-03 09:21:40 +0000

    changed shared_ptr null check to work on older compilers (lmdfit)

--- lmd/LmdFit/ModelFramework/fit/ModelFitFacade.cxx
+++ lmd/LmdFit/ModelFramework/fit/ModelFitFacade.cxx
@@ -65,7 +65,7 @@ ModelFitResult ModelFitFacade::Fit() {
 	ModelFitResult fit_result_dummy;
 
 	// check that estimator is set
-	if (estimator == nullptr) {
+	if (!estimator) {
 		fit_result_dummy.setFitStatus(-1);
 		return fit_result_dummy;
 	}
@@ -78,7 +78,7 @@ ModelFitResult ModelFitFacade::Fit() {
 	estimator->applyEstimatorOptions(estimator_options);
 
 	// check that minimizer exists
-	if (minimizer == nullptr) {
+	if (!minimizer) {
 		fit_result_dummy.setFitStatus(-2);
 		return fit_result_dummy;
 	}
