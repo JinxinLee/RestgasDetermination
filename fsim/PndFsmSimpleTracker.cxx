84e27c41f89cee87a4ab42ffcf69d913d9e72e2c
Klaus Goetzen <K.Goetzen@gsi.de>
2014-04-09 09:34:05 +0000

    Added optional parametrization for dp, dtht, dphi, eff

--- fsim/PndFsmSimpleTracker.cxx
+++ fsim/PndFsmSimpleTracker.cxx
@@ -114,7 +114,16 @@ PndFsmSimpleTracker::detected(PndFsmTrack *t) const
     double p=t->p4().Vect().Mag();
     double charge=t->charge();
 
-    return ( charge!=0.0 && theta>=_thtMin && theta<=_thtMax && p_t>_ptmin && p>_pmin && _rand->Rndm()<=_efficiency);
+	double eff = _efficiency;
+	// use parametrization for eff(p) for p<0.6 as multiplier for _efficiency from 
+    // https://panda-wiki.gsi.de/foswiki/pub/Computing/Minutes07April2014/Costanza_EVO_April07.pdf
+	if (eff<0)
+	{
+		eff = -eff;
+		if (p<0.6) eff = eff * (0.32602 + 1.91679*p -1.58341*p*p + 0.305179*p*p*p);
+	}
+	
+    return ( charge!=0.0 && theta>=_thtMin && theta<=_thtMax && p_t>_ptmin && p>_pmin && _rand->Rndm()<=eff);
 }
 
 
@@ -122,20 +131,48 @@ double
 PndFsmSimpleTracker::dp(PndFsmTrack *t) const
 {
   double p=t->p4().Vect().Mag();
-
-  return (_pRes * p );
+  
+  double res;
+  
+  // use parametrization dp/p(p) from 
+  // https://panda-wiki.gsi.de/foswiki/pub/Computing/Minutes07April2014/Costanza_EVO_April07.pdf
+  if (_pRes<0) 
+	  res = 0.012 + p*0.0034;
+  else // use fixed resolution
+	  res = _pRes * p;
+  
+  return res;
 }
 
 double
 PndFsmSimpleTracker::dphi(PndFsmTrack *t) const
 {
-  return _phiRes;
+  double res = _phiRes;
+  
+  // use parametrization dphi(p) from 
+  // https://panda-wiki.gsi.de/foswiki/pub/Computing/Minutes07April2014/Costanza_EVO_April07.pdf
+  if (_phiRes<0)
+  {
+	  double p=t->p4().Vect().Mag();
+	  res = 2.497e-3/pow(p,1.307) + 8.77e-4;
+  }
+  
+  return res;
 }
 
 double
 PndFsmSimpleTracker::dtheta(PndFsmTrack *t) const
 {
-  return _thtRes;
+  double res = _thtRes;
+  
+  // use parametrization dtheta(p) from 
+  // https://panda-wiki.gsi.de/foswiki/pub/Computing/Minutes07April2014/Costanza_EVO_April07.pdf
+  if (_thtRes<0)
+  {
+	  double p=t->p4().Vect().Mag();
+	  res = 1.81e-3/pow(p,1.131) + 3.85e-4;
+  }
+  return res;
 }
 
 void
@@ -147,9 +184,13 @@ PndFsmSimpleTracker::print(ostream &o)
   o  <<"  _ptmin      = " <<  _ptmin  << endl;
   o  <<"  _pmin       = " <<  _pmin   << endl;
   o  <<"  _pRes       = " <<  _pRes   << " (rel)"<< endl;
+  if (_pRes<0) o<<"     ... using parametrisation function for dp/p."<<endl;
   o  <<"  _thtRes     = " <<  _thtRes << endl;
+  if (_thtRes<0) o<<"     ... using parametrisation function for dtht."<<endl;
   o  <<"  _phiRes     = " <<  _phiRes << endl;
+  if (_phiRes<0) o<<"     ... using parametrisation function for dphi."<<endl;
   o  <<"  _efficiency = " << _efficiency<<endl;
+  if (_efficiency<0) o<<"     ... using parametrisation function for p<0.6."<<endl;
 }
 
 void
@@ -160,10 +201,14 @@ PndFsmSimpleTracker::initParameters()
   _thtMax          = 159.44;
   _ptmin           = 0.1;
   _pmin            = 0.0;
-  _pRes            = 0.02;   // 2%
-  _thtRes          = 0.005;
-  _phiRes          = 0.005;
-  _efficiency	   = 1.0;
+  // if the resolutions are <0, than the parametrisations according to
+  // https://panda-wiki.gsi.de/foswiki/pub/Computing/Minutes07April2014/Costanza_EVO_April07.pdf
+  // p.6, p.9, p.10 are used
+  _pRes            = -1.;//0.02;   // 2%
+  _thtRes          = -1.;//0.005;
+  _phiRes          = -1.;//0.005;
+  // if the efficiency<0 and p<0.6GeV/c, parametrisation as multiplier to _efficiency according to p.8; else flat _efficiency 
+  _efficiency	   = -1.0;//
 
 }
 
