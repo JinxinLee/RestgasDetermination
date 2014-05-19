21ed962d5cbdf86bb59b68252b23462e3fe66177
Mohammad Al-Turany <M.Al-Turany@gsi.de>
2014-05-19 14:24:31 +0000

    revert last accidental commit

--- macro/stt/run.C
+++ macro/stt/run.C
@@ -36,7 +36,7 @@
   fRun->AddModule(Cave);
 
   FairDetector *Stt= new PndStt("STT", kTRUE);
-  Stt->SetGeometryFileName("straws.geo");
+  Stt->SetGeometryFileName("straws_skewed_blocks_35cm_pipe.geo");
   fRun->AddModule(Stt);
 
   // Create and Set Event Generator
