5816673b8797707467645f8e4e86a21934166a6d
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2014-06-11 15:49:50 +0000

    time-based sim for the barrel DIRC

---
+++ drc/PndDrcDigiWriteoutBuffer.h
@@ -0,0 +1,42 @@
+/*
+ * PndDrcDigiWriteoutBuffer.h
+ *
+ */
+// -------------------------------------------------------------------------
+// -----                   PndDrcDigiWriteoutBuffer header file        -----
+// -----         HARPHOOL KUMAWAT h.kumawat@gsi.de                     -----
+// -----                                                               -----
+// -------------------------------------------------------------------------
+
+#ifndef PNDDRCDIGIWRITEOUTBUFFER_H_
+#define PNDDRCDIGIWRITEOUTBUFFER_H_
+
+#include "FairWriteoutBuffer.h"
+#include "PndDrcDigi.h"
+
+class FairTimeStamp;
+
+class PndDrcDigiWriteoutBuffer : public FairWriteoutBuffer{
+
+public:
+	PndDrcDigiWriteoutBuffer();
+	PndDrcDigiWriteoutBuffer(TString branchName, TString folderName, Bool_t persistance);
+
+	virtual ~PndDrcDigiWriteoutBuffer();
+
+	void AddNewDataToTClonesArray(FairTimeStamp*);
+
+	virtual double FindTimeForData(FairTimeStamp* data) ;
+	virtual void FillDataMap(FairTimeStamp* data, double activeTime) ;
+	virtual void EraseDataFromDataMap(FairTimeStamp* data);
+	virtual std::vector<std::pair<double, FairTimeStamp*> > Modify(std::pair<double, FairTimeStamp*> oldData, std::pair<double, FairTimeStamp*> newData);
+        virtual void FillNewData(FairTimeStamp* data, double startTime, double activeTime);
+    virtual void FillDataToDeadTimeMap(FairTimeStamp* data, double activeTime);
+protected:	
+
+	std::map<PndDrcDigi, double> fData_map;
+	
+	ClassDef(PndDrcDigiWriteoutBuffer, 1);
+};
+
+#endif /* PNDDrcDigiWRITEOUTBUFFER_H_ */
