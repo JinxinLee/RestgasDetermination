3449da52971fbb1813967717d45eb2bec5d5faf5
Stefano Spataro <spataro@to.infn.it>
2014-06-13 09:37:14 +0000

    Now geo manager inside fairgeoparset

--- macro/run/drawGeo.C
+++ macro/run/drawGeo.C
@@ -1,17 +1,21 @@
-
-
 drawGeo()
 {
-  
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
-  rootlogon();
-  
   TFile* file = new TFile("simparams.root");
-  file->Get("FairBaseParSet"); 
   
-  gGeoManager->SetVisLevel(3);
-  gGeoManager->GetMasterVolume()->Draw("ogl");
-
+  if (!gGeoManager) {
+    file->Get("FairBaseParSet");
+    TGeoManager *geoMan = gGeoManager;
+    if(!geoMan) {
+      file->Get("FairGeoParSet");
+      geoMan = gGeoManager;
+      if(!geoMan) {
+        std::cout<<"Could not find valid GeoManager. Abort now!"<<std::endl;
+        exit(1);
+      }
+    }
+    geoMan->SetVisLevel(3);
+    geoMan->GetMasterVolume()->Draw("ogl");
+  }
 
 }
 
