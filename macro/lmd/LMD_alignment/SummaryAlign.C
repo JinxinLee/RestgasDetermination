adfba93c02dce65395e0b00b501ae74fb3610ce8
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2014-07-30 12:58:41 +0000

    make flexible plots limits

--- macro/lmd/LMD_alignment/SummaryAlign.C
+++ macro/lmd/LMD_alignment/SummaryAlign.C
@@ -9,6 +9,8 @@ using namespace std;
 void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_1000000/mom_15/", double tr_sc=0, double rt_sc=0)
 //void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10000/mom_15//", double tr_sc=0, double rt_sc=0)
 {
+  const int colors[4] = {28,kAzure+2,46,8};
+  const int colors_a[4] = {kViolet-6,kOrange+7,kGreen+3,kRed+1};
   const int ntrksSample = 1e4;
   const double i_TrksSimi = 100./ntrksSample; //relative to simulated in %
   // const int nParDt=8;
@@ -17,6 +19,8 @@ void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10
   // double v_Da[nParDa]={0,1,3,6,9};
   const int nParDt=10;
   double v_Dt[nParDt]={0, 50, 100, 200, 300, 400, 500, 600, 800, 1000};
+  // const int nParDt=1;
+  // double v_Dt[nParDt]={0};
  const int nParDa=4;
  double v_Da[nParDa]={0,1,3,5};
  // const int nParDa=1;
@@ -263,15 +267,54 @@ void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10
     mis_v_a[ipart][3][i] = 1e3*(hmis_a_3->ProjectionY()->GetMean());
     mis_v_a[ipart][4][i] = 1e3*(hmis_a_4->ProjectionY()->GetMean());
     mis_v_a[ipart][5][i] = 1e3*(hmis_a_5->ProjectionY()->GetMean());
+    cout<<"mis_v_a["<<ipart<<"][5]["<<i<<"] = "<<mis_v_a[ipart][5][i]<<endl;
+    cout<<"mis_a["<<ipart<<"][5]["<<i<<"] = "<<mis_a[ipart][5][i]<<endl;
     /// END (Misalignment constants) -----------------------------------------------------------
     fmisc->Close();
     }// trans
  }// rotation
  
  // Draw results --------------------------------
+ double min_theta_mean_b = theta_mean_b[0][0], min_theta_rms_b = theta_rms_b[0][0];
+ double min_theta_mean_a = theta_mean_a[0][0], min_theta_rms_a = theta_rms_a[0][0];
+ double max_theta_mean_b = theta_mean_b[0][0], max_theta_rms_b = theta_rms_b[0][0];
+ double max_theta_mean_a = theta_mean_a[0][0], max_theta_rms_a = theta_rms_a[0][0];
+ for(int ipart=0;ipart<nParDa;ipart++){
+   for(int i=0;i<nParDt;i++){
+     if(min_theta_mean_b>theta_mean_b[ipart][i]) min_theta_mean_b = theta_mean_b[ipart][i];
+     if(max_theta_mean_b<theta_mean_b[ipart][i]) max_theta_mean_b = theta_mean_b[ipart][i];
+     if(min_theta_mean_a>theta_mean_a[ipart][i]) min_theta_mean_a = theta_mean_a[ipart][i];
+     if(max_theta_mean_a<theta_mean_a[ipart][i]) max_theta_mean_a = theta_mean_a[ipart][i];
+
+     if(min_theta_rms_b>theta_rms_b[ipart][i]) min_theta_rms_b = theta_rms_b[ipart][i];
+     if(max_theta_rms_b<theta_rms_b[ipart][i]) max_theta_rms_b = theta_rms_b[ipart][i];
+     if(min_theta_rms_a>theta_rms_a[ipart][i]) min_theta_rms_a = theta_rms_a[ipart][i];
+     if(max_theta_rms_a<theta_rms_a[ipart][i]) max_theta_rms_a = theta_rms_a[ipart][i];
+   }
+ }
+
+ for(int ipart=0;ipart<nParDa;ipart++){
+   for(int i=0;i<nParDt;i++){
+
+     if(min_theta_mean_b>theta_mean_r[ipart][i]) min_theta_mean_b = theta_mean_r[ipart][i];
+     if(max_theta_mean_b<theta_mean_r[ipart][i]) max_theta_mean_b = theta_mean_r[ipart][i];
+     if(min_theta_mean_a>theta_mean_r[ipart][i]) min_theta_mean_a = theta_mean_r[ipart][i];
+     if(max_theta_mean_a<theta_mean_r[ipart][i]) max_theta_mean_a = theta_mean_r[ipart][i];
+
+     if(min_theta_rms_b>theta_rms_r[ipart][i]) min_theta_rms_b = theta_rms_r[ipart][i];
+     if(max_theta_rms_b<theta_rms_r[ipart][i]) max_theta_rms_b = theta_rms_r[ipart][i];
+     if(min_theta_rms_a>theta_rms_r[ipart][i]) min_theta_rms_a = theta_rms_r[ipart][i];
+     if(max_theta_rms_a<theta_rms_r[ipart][i]) max_theta_rms_a = theta_rms_r[ipart][i];
+   }
+ }
+
  TCanvas c1;
- TLegend *leg = new TLegend(0.78,0.51,0.98,0.98);
- leg->SetFillColor(0);
+ TLegend *leg_b = new TLegend(0.83,0.65,0.97,0.97);
+ leg_b->SetFillColor(0);
+ TLegend *leg_a = new TLegend(0.83,0.65,0.97,0.97);
+ leg_a->SetFillColor(0);
+ // TLegend *leg_b = new TLegend(0.78,0.51,0.98,0.98);
+ // leg_b->SetFillColor(0);
 
  TMultiGraph *mgr_theta = new TMultiGraph();  
  TMultiGraph *mgr_theta_b = new TMultiGraph();  
@@ -281,48 +324,60 @@ void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10
  //  double [nParDt], theta_rms_a[nParDa][nParDt], theta_rms_r[nParDa][nParDt];
   gr_theta_b[ipart] = new TGraphErrors(nParDt,v_Dt,theta_mean_b[ipart],0,0);
   gr_theta_b[ipart]->SetMarkerStyle(20+ipart); 
-  gr_theta_b[ipart]->SetMarkerColor(kGreen-3); 
+  //  gr_theta_b[ipart]->SetMarkerColor(kGreen-3); 
+  //  gr_theta_b[ipart]->SetMarkerColor(30*ipart+8);
+  gr_theta_b[ipart]->SetMarkerColor(colors[ipart]);
+  gr_theta_b[ipart]->SetLineColor(colors[ipart]);
+  // gr_theta_b[ipart]->SetLineColor(kGreen-3+ipart);
   gr_theta_b[ipart]->SetMarkerSize(1.7+ipart*0.3); 
   gr_theta_a[ipart] = new TGraphErrors(nParDt,v_Dt,theta_mean_a[ipart],0,0);
   gr_theta_a[ipart]->SetMarkerStyle(20+ipart); 
-  gr_theta_a[ipart]->SetMarkerColor(kOrange+7); 
+  //gr_theta_a[ipart]->SetMarkerColor(kOrange+7); 
+  gr_theta_a[ipart]->SetMarkerColor(colors_a[ipart]); 
+  gr_theta_a[ipart]->SetLineColor(colors_a[ipart]);
   gr_theta_a[ipart]->SetMarkerSize(1.7+ipart*0.3); 
   mgr_theta_b->Add(gr_theta_b[ipart]);
   mgr_theta->Add(gr_theta_a[ipart]);
   if(ipart==0){
   gr_theta_r[ipart] = new TGraphErrors(nParDt,v_Dt,theta_mean_r[ipart],0,0);
   gr_theta_r[ipart]->SetMarkerStyle(20+ipart); 
-  gr_theta_r[ipart]->SetMarkerColor(15); 
-  gr_theta_r[ipart]->SetMarkerSize(3.); 
+  gr_theta_r[ipart]->SetMarkerColor(kGray+3); 
+  gr_theta_r[ipart]->SetMarkerSize(2.7); 
   mgr_theta_b->Add(gr_theta_r[ipart]);
   mgr_theta->Add(gr_theta_r[ipart]);
-  leg->AddEntry(gr_theta_r[ipart],"IDEAL","lep");
+  // leg->AddEntry(gr_theta_r[ipart],"ideal","lep");
+  leg_b->AddEntry(gr_theta_r[ipart],"ideal","lep");
+  leg_a->AddEntry(gr_theta_r[ipart],"ideal","lep");
   }
   TString dRName = "#Delta_{r} = ";
   dRName +=v_Da[ipart];
-  TString dRName_b = dRName+ " (BEFORE)";  
-  TString dRName_a = dRName+ " (AFTER)";  
-  leg->AddEntry(gr_theta_b[ipart],dRName_b,"lep");
-  leg->AddEntry(gr_theta_a[ipart],dRName_a,"lep");
-
+  //  TString dRName_b = dRName+ " (BEFORE)";  
+  // TString dRName_a = dRName+ " (AFTER)";  
+  // leg->AddEntry(gr_theta_b[ipart],dRName_b,"lep");
+  // leg->AddEntry(gr_theta_a[ipart],dRName_a,"lep");
+  TString dRName_b = dRName;  
+  leg_b->AddEntry(gr_theta_b[ipart],dRName_b,"lep");
+  leg_a->AddEntry(gr_theta_a[ipart],dRName_b,"lep");
  }// END loop over rotaion scale
   
  mgr_theta_b->Draw("AP");
- leg->Draw();
+ leg_b->Draw();
+ mgr_theta_b->SetTitle("Before");
  mgr_theta_b->GetXaxis()->SetTitle("#Delta_{t}, #mum");
  mgr_theta_b->GetYaxis()->SetTitle("#theta_{mean}, #murad");
- mgr_theta_b->GetHistogram()->SetMaximum(200.);
- mgr_theta_b->GetHistogram()->SetMinimum(-200.);
+ mgr_theta_b->GetHistogram()->SetMaximum(1.15*max_theta_mean_b);
+ mgr_theta_b->GetHistogram()->SetMinimum(0.85*min_theta_mean_b);
  c1.Print(resname_pdf_o); //write canvas and keep the ps file open
  c1.Clear();
  
  
  mgr_theta->Draw("AP");
- leg->Draw();
+ leg_a->Draw();
+ mgr_theta->SetTitle("After");
  mgr_theta->GetXaxis()->SetTitle("#Delta_{t}, #mum");
  mgr_theta->GetYaxis()->SetTitle("#theta_{mean}, #murad");
- mgr_theta->GetHistogram()->SetMaximum(10.);
- mgr_theta->GetHistogram()->SetMinimum(-10.);
+ mgr_theta->GetHistogram()->SetMaximum(1.15*max_theta_mean_a);
+ mgr_theta->GetHistogram()->SetMinimum(0.85*min_theta_mean_a);
 
  c1.Print(resname_pdf_o); //write canvas and keep the ps file open
  c1.Clear();
@@ -332,75 +387,105 @@ void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10
  for(int ipart=0;ipart<nParDa;ipart++){// loop over rotaion scale
    gr_theta_b[ipart] = new TGraphErrors(nParDt,v_Dt,theta_rms_b[ipart],0,0);
    gr_theta_b[ipart]->SetMarkerStyle(20+ipart); 
-   gr_theta_b[ipart]->SetMarkerColor(kGreen-3); 
+   //  gr_theta_b[ipart]->SetMarkerColor(kGreen-3); 
+   gr_theta_b[ipart]->SetMarkerColor(colors[ipart]); 
+   gr_theta_b[ipart]->SetLineColor(colors[ipart]);
    gr_theta_b[ipart]->SetMarkerSize(1.7+ipart*0.3); 
    gr_theta_a[ipart] = new TGraphErrors(nParDt,v_Dt,theta_rms_a[ipart],0,0);
   gr_theta_a[ipart]->SetMarkerStyle(20+ipart); 
-  gr_theta_a[ipart]->SetMarkerColor(kOrange+7); 
+  //  gr_theta_a[ipart]->SetMarkerColor(kOrange+7); 
+  gr_theta_a[ipart]->SetMarkerColor(colors_a[ipart]); 
+  gr_theta_a[ipart]->SetLineColor(colors_a[ipart]);
   gr_theta_a[ipart]->SetMarkerSize(1.7+ipart*0.3); 
   mgr_theta_b_rms->Add(gr_theta_b[ipart]);
   mgr_theta_rms->Add(gr_theta_a[ipart]);
   if(ipart==0){
     gr_theta_r[ipart] = new TGraphErrors(nParDt,v_Dt,theta_rms_r[ipart],0,0);
     gr_theta_r[ipart]->SetMarkerStyle(20+ipart); 
-    gr_theta_r[ipart]->SetMarkerColor(15); 
-    gr_theta_r[ipart]->SetMarkerSize(3.);
+    gr_theta_r[ipart]->SetMarkerColor(kGray+3); 
+    gr_theta_r[ipart]->SetMarkerSize(2.7);
     mgr_theta_b_rms->Add(gr_theta_r[ipart]);
     mgr_theta_rms->Add(gr_theta_r[ipart]);
   }
  }// END loop over rotaion scale
   
  mgr_theta_b_rms->Draw("AP");
- leg->Draw();
+ leg_b->Draw();
  mgr_theta_b_rms->GetXaxis()->SetTitle("#Delta_{t}, #mum");
  mgr_theta_b_rms->GetYaxis()->SetTitle("#theta_{rms}, #murad");
- mgr_theta_b_rms->GetHistogram()->SetMaximum(1500.);
- mgr_theta_b_rms->GetHistogram()->SetMinimum(90.);
-
+ mgr_theta_b_rms->GetHistogram()->SetMaximum(1.15*max_theta_rms_b);
+ mgr_theta_b_rms->GetHistogram()->SetMinimum(0.85*min_theta_rms_b);
+ mgr_theta_b_rms->SetTitle("Before");
  c1.Print(resname_pdf_o); //write canvas and keep the ps file open
  c1.Clear();
  
  
  mgr_theta_rms->Draw("AP");
- leg->Draw();
+ leg_a->Draw();
  mgr_theta_rms->GetXaxis()->SetTitle("#Delta_{t}, #mum");
  mgr_theta_rms->GetYaxis()->SetTitle("#theta_{rms}, #murad");
- mgr_theta_rms->GetHistogram()->SetMaximum(200.);
- mgr_theta_rms->GetHistogram()->SetMinimum(90.);
-
+ mgr_theta_rms->GetHistogram()->SetMaximum(1.15*max_theta_rms_a);
+ mgr_theta_rms->GetHistogram()->SetMinimum(0.85*min_theta_rms_a);
+ mgr_theta_rms->SetTitle("After");
  c1.Print(resname_pdf_o); //write canvas and keep the ps file open
  c1.Clear();
  
  //put together missalignment consts results
   TGraphErrors *gr_mis_b[6][nParDa];
   TGraphErrors *gr_mis_a[6][nParDa];
-  TMultiGraph *mgr_mis[6];
+  TMultiGraph *mgr_mis[6]; 
+  TMultiGraph *mgr_mis_b[6];
   for(int ialc=0;ialc<6;ialc++){
     mgr_mis[ialc] = new TMultiGraph();
+    mgr_mis_b[ialc] = new TMultiGraph();
   }
   for(int ipart=0;ipart<nParDa;ipart++){// loop over rotaion scale
     for(int ialc=0;ialc<6;ialc++){
       gr_mis_b[ialc][ipart] = new TGraphErrors(nParDt,v_Dt,mis_v_b[ipart][ialc],0,mis_b[ipart][ialc]);
       gr_mis_b[ialc][ipart]->SetMarkerStyle(20+ipart);
-      gr_mis_b[ialc][ipart]->SetMarkerColor(kGreen-3);
+      gr_mis_b[ialc][ipart]->SetMarkerColor(colors[ipart]);
+      gr_mis_b[ialc][ipart]->SetLineColor(colors[ipart]);
       gr_mis_b[ialc][ipart]->SetMarkerSize(1.7+ipart*0.3);
       gr_mis_a[ialc][ipart] = new TGraphErrors(nParDt,v_Dt,mis_v_a[ipart][ialc],0,mis_a[ipart][ialc]);
       gr_mis_a[ialc][ipart]->SetMarkerStyle(20+ipart);
-      gr_mis_a[ialc][ipart]->SetMarkerColor(kOrange+7);
+      gr_mis_a[ialc][ipart]->SetMarkerColor(colors_a[ipart]);
       gr_mis_a[ialc][ipart]->SetMarkerSize(1.7+ipart*0.3);
-     
-      //  mgr_mis[ialc]->Add(gr_mis_b[ialc][ipart]);
+      gr_mis_a[ialc][ipart]->SetLineColor(colors_a[ipart]);
+      mgr_mis_b[ialc]->Add(gr_mis_b[ialc][ipart]);
       mgr_mis[ialc]->Add(gr_mis_a[ialc][ipart]);
     }
   }// END loop over rotaion scale
  for(int ialc=0;ialc<6;ialc++){
    if(ialc<2 || ialc==5){
+     mgr_mis_b[ialc]->Draw("AP");
+     leg_b->Draw();
+     mgr_mis_b[ialc]->GetXaxis()->SetTitle("#Delta_{t}, #mum");
+     mgr_mis_b[ialc]->GetHistogram()->SetMaximum(1200.);
+     mgr_mis_b[ialc]->GetHistogram()->SetMinimum(-1200.);
+     if(ialc==5){
+       mgr_mis_b[ialc]->GetHistogram()->SetMaximum(10.);
+       mgr_mis_b[ialc]->GetHistogram()->SetMinimum(-10.);
+     }
+     mgr_mis_b[ialc]->SetTitle("Before");
+     if(ialc==0) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{x}, #mum");
+     if(ialc==1) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{y}, #mum");
+     if(ialc==2) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{z}, #mum");
+     if(ialc==3) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{#alpha}, mrad");
+     if(ialc==4) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{#beta}, mrad");
+     if(ialc==5) mgr_mis_b[ialc]->GetYaxis()->SetTitle("#Delta_{#gamma}, mrad");
+     c1.Print(resname_pdf_o); //write canvas and keep the file open
+     c1.Clear();
+
      mgr_mis[ialc]->Draw("AP");
-     leg->Draw();
+     leg_a->Draw();
      mgr_mis[ialc]->GetXaxis()->SetTitle("#Delta_{t}, #mum");
      mgr_mis[ialc]->GetHistogram()->SetMaximum(10.);
      mgr_mis[ialc]->GetHistogram()->SetMinimum(-10.);
-
+     if(ialc==5){
+       mgr_mis[ialc]->GetHistogram()->SetMaximum(5.);
+       mgr_mis[ialc]->GetHistogram()->SetMinimum(-5.);
+     }
+     mgr_mis[ialc]->SetTitle("After");
      if(ialc==0) mgr_mis[ialc]->GetYaxis()->SetTitle("#Delta_{x}, #mum");
      if(ialc==1) mgr_mis[ialc]->GetYaxis()->SetTitle("#Delta_{y}, #mum");
      if(ialc==2) mgr_mis[ialc]->GetYaxis()->SetTitle("#Delta_{z}, #mum");
@@ -409,6 +494,8 @@ void SummaryAlign(TString pathG="/panda/pandaroot/macro/lmd/testPixelAlignSIM_10
      if(ialc==5) mgr_mis[ialc]->GetYaxis()->SetTitle("#Delta_{#gamma}, mrad");
      c1.Print(resname_pdf_o); //write canvas and keep the file open
      c1.Clear();
+
+
    }
  }
 
@@ -424,18 +511,19 @@ TLegend *leg2 = new TLegend(0.78,0.58,0.98,0.98);
  for(int ipart=0;ipart<nParDa;ipart++){// loop over rotaion scale
    grstatBef[ipart] = new TGraphErrors(nParDt,v_Dt,TrksBef[ipart],0,0);
    grstatBef[ipart]->SetMarkerStyle(20+ipart);
-   grstatBef[ipart]->SetMarkerColor(kGreen-3);
+   grstatBef[ipart]->SetMarkerColor(colors[ipart]);
    grstatBef[ipart]->SetMarkerSize(1.7+ipart*0.3);
 
-  
    grstatKnossos0[ipart] = new TGraphErrors(nParDt,v_Dt,TrksKnossos0[ipart],0,errTrksKnossos0[ipart]);
    grstatKnossos0[ipart]->SetMarkerStyle(20+ipart);
-   grstatKnossos0[ipart]->SetMarkerColor(kGreen-3);
+   grstatKnossos0[ipart]->SetMarkerColor(colors[ipart]);
+   grstatKnossos0[ipart]->SetLineColor(colors[ipart]);
    grstatKnossos0[ipart]->SetMarkerSize(1.7+ipart*0.3);
    mgr_stat_kn->Add(grstatKnossos0[ipart]);
    grstatKnossos[ipart] = new TGraphErrors(nParDt,v_Dt,TrksKnossos[ipart],0,errTrksKnossos[ipart]);
    grstatKnossos[ipart]->SetMarkerStyle(20+ipart);
-   grstatKnossos[ipart]->SetMarkerColor(kOrange+7);
+   grstatKnossos[ipart]->SetMarkerColor(colors_a[ipart]);
+   grstatKnossos[ipart]->SetLineColor(colors_a[ipart]);
    grstatKnossos[ipart]->SetMarkerSize(1.7+ipart*0.3);
    TString dRName = "#Delta_{r} = ";
    dRName +=v_Da[ipart];
@@ -452,13 +540,13 @@ TLegend *leg2 = new TLegend(0.78,0.58,0.98,0.98);
    mgr_stat->Add(grstatBef[ipart]);
    mgr_stat->Add(grstatAft[ipart]);
  }// END loop over rotaion scale
- mgr_stat->Draw("AP");
- mgr_stat->GetXaxis()->SetTitle("#Delta_{t}, #mum");
- mgr_stat->GetYaxis()->SetTitle("N_{REC}");
- mgr_stat->GetHistogram()->SetMaximum(1e5);
- mgr_stat->GetHistogram()->SetMinimum(0);
- leg->Draw();
- c1.Print(resname_pdf_o); //write canvas and keep the file open
+ // mgr_stat->Draw("AP");
+ // mgr_stat->GetXaxis()->SetTitle("#Delta_{t}, #mum");
+ // mgr_stat->GetYaxis()->SetTitle("N_{REC}");
+ // mgr_stat->GetHistogram()->SetMaximum(1e5);
+ // mgr_stat->GetHistogram()->SetMinimum(0);
+ // leg_b->Draw();
+ // c1.Print(resname_pdf_o); //write canvas and keep the file open
  c1.Clear();
  mgr_stat_kn->Draw("AP");
  mgr_stat_kn->SetTitle("#trks used in Millepede (av. per sector)");
