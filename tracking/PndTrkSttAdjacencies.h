f919969b909c113b26d24033912ada1fc11b2449
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2014-08-19 21:56:22 +0000

    modifications

--- tracking/PndTrkSttAdjacencies.h
+++ tracking/PndTrkSttAdjacencies.h
@@ -15,8 +15,6 @@ class PndTrkSttAdjacencies
  /** Destructor **/
  ~PndTrkSttAdjacencies();
 
-
-
  void CalculateAdjacentStt(
 	// in this geametry there are 4542 Stt Straws (axial and skew);
 		const Short_t NUMBER_STRAWS,  // number of Stt Straws in total;
@@ -25,6 +23,23 @@ class PndTrkSttAdjacencies
 				// NUMBER_STRAWS+1 because the numbering scheme for the Stt straws goes
 				//  from 1 to NUMBER_STRAWS included;
 		Short_t ListParContiguous[][6]  // output list (axial Stt);
+			);
+
+
+
+
+ void CalculateAdjacentStt2(
+	// in this geametry there are 4542 Stt Straws (axial and skew);
+		const Short_t NUMBER_STRAWS,  // number of Stt Straws in total;
+		TClonesArray *SttTubeArray, // input; array of the Stt tubes;
+		Short_t *nParContiguous,  // output; number of contiguous straws (axial Stt);
+				// NUMBER_STRAWS+1 because the numbering scheme for the Stt straws goes
+				//  from 1 to NUMBER_STRAWS included;
+		Short_t ListParContiguous[][6],  // output list (axial Stt);
+		Double_t *x,	// X position center of tube;
+		Double_t *y,	// Y position center of tube;
+		Double_t *z,	// Z position center of tube;
+		Double_t *rxy2	// X*X+Y*Y position center of tube;
 			);
 
   ClassDef(PndTrkSttAdjacencies,1);
