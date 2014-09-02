f02587c8e665526eba38bfc502d59fc883c6d8d6
Klaus Goetzen <K.Goetzen@gsi.de>
2014-09-02 08:37:31 +0000

    minor variable fix

--- softrig/PndSoftTriggerTask.cxx
+++ softrig/PndSoftTriggerTask.cxx
@@ -81,7 +81,7 @@ const int fSTPidIndex[14] = {-11, 11, -13, 13, 211, -211, 321, -321, 2212, -2212
 
 // *** mapped variable names for selection
 //                      0         1            2            3         4         5       6        7      8       9         10           11        12   13  14     15    16     17      18       19       20
-TString fSTnames[] = {"eslnpide","eslnpidmu","eslnpidpi","eslnpidk","eslnpidp","esthr","esapl","esfw1","npart","esptmax","detemcsum","detemcmax","p","pt","pcm","tht","d0pt","d1pt","d0pidk","d1tht","mmiss",
+TString fSTnames[] = {"eslnpide","eslnpidmu","eslnpidpi","eslnpidk","eslnpidp","esthr","esapl","esfw1","esnpart","esptmax","detemcsum","detemcmax","p","pt","pcm","tht","d0pt","d1pt","d0pidk","d1tht","mmiss",
 //                      21         22           23       24       25     26   
 				      "essumpt",  "essumptcl", "d0pcm", "d1p", "thtcm", "ecm"};
 
