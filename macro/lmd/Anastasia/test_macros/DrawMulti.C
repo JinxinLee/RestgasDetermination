10e4d7dd09fd1ba908fc5edc3f089ba1131dec62
Anastasia Karavdina <karavdin@KPH.UNI-MAINZ.DE>
2014-09-30 05:29:16 +0000

    correct event time

--- macro/lmd/Anastasia/test_macros/DrawMulti.C
+++ macro/lmd/Anastasia/test_macros/DrawMulti.C
@@ -2,13 +2,14 @@
 
 void DrawMulti(){
   //	gROOT->Macro("$VMCWORKDIR/macro/lmd/Anastasia/test_macros/rootlogon.C");
-	TFile *f = new TFile("/home/karavdina/soft/pandaroot/macro/lmd/DPM_el_inel_1_5/Lumi_digi_0.root","READ");
+  //	TFile *f = new TFile("/home/karavdina/soft/pandaroot/macro/lmd/DPM_el_inel_15/Lumi_digi_0.root","READ");
+	TFile *f = new TFile("/home/karavdina/soft/pandaroot/macro/lmd/DPMelinel_125000ev/mom_15/Lumi_DigisQA_0.root","READ");
 	TTree *trec = (TTree*)f->Get("cbmsim");
 	TH2D *hmlt = new TH2D("hmlt","; time stamp, ns; multiplicity, trks/ev",60,0,1500,25,0,25);
 	TH1D *hml = new TH1D("hml","; multiplicity, trks/ev",25,0,25);
-       	double nEv =trec->Project("hmlt","LMDPixelDigis.GetNIndices():LMDPixelDigis.fTimeStamp");
+       	double nEv =trec->Project("hmlt","LMDPixelDigisQ.GetNIndices():LMDPixelDigisQ.fTimeStamp");
 	//	trec->Project("hml","LMDPixelDigis.GetNIndices()","LMDPixelDigis.fTimeStamp<39.","");
-	trec->Project("hml","LMDPixelDigis.GetNIndices()","LMDPixelDigis.fTimeStamp<39.");
+	trec->Project("hml","LMDPixelDigisQ.GetNIndices()","LMDPixelDigisQ.fTimeStamp<39.");
 	//	nEv *=1e-2; 
 	hmlt->SetZTitle("Number of events, %");
 	double scl = 100/hmlt->Integral();
