1f870f4d2b94c2a4ec3454c3400ec8133c1eb13b
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2014-10-28 12:38:56 +0000

    Calculation of charge from LE and TE

--- pnddata/SdsData/PndSdsDigiTopix4.cxx
+++ pnddata/SdsData/PndSdsDigiTopix4.cxx
@@ -7,6 +7,13 @@ PndSdsDigiTopix4::PndSdsDigiTopix4() : PndSdsDigiPixel(), fLeadingEdge(0), fTrai
 PndSdsDigiTopix4::PndSdsDigiTopix4(std::vector<Int_t> index, Int_t detID, Int_t sensorID, Int_t fe, Int_t col, Int_t row, UInt_t leadingEdge, UInt_t trailingEdge, UInt_t frameCount, Double_t timeStamp):
 PndSdsDigiPixel(index, detID, sensorID, fe, col, row, 0, timeStamp), fLeadingEdge(leadingEdge), fTrailingEdge(trailingEdge), fFrameCount(frameCount)
 {
+	Double_t charge = 0;
+	if(leadingEdge < trailingEdge){
+		charge = trailingEdge - leadingEdge;
+	} else {
+		charge = (trailingEdge + 4096) - leadingEdge;
+	}
+	SetCharge(charge);
 }
 
 ClassImp(PndSdsDigiTopix4);
