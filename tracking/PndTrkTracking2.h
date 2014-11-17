c382733aa2414bc865fe656680be5578ae5e5ca2
Gianluigi Boca <Gianluigi.Boca@pv.infn.it>
2014-11-17 20:44:52 +0000

    put the assignments of the static constants in the .cxx file;

--- tracking/PndTrkTracking2.h
+++ tracking/PndTrkTracking2.h
@@ -126,8 +126,8 @@ class PndTrkTracking2 : public FairTask
 				// start at 1 and goes up to 4542 included;
 
   static const Double_t
-	THETAMIN		= 0.,
-	THETAMAX		= 2.*3.141592654;
+	THETAMIN,
+	THETAMAX;
 
 
   bool
