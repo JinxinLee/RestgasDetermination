c06b2d8aa239c6286493c7f5e7b318b88ce2ebc6
Ralf Kliemt <r.kliemt@gsi.de>
2014-12-10 07:53:42 +0000

    Convenient name for vertex fitter addded (with a simple typedef)

---
+++ PndTools/AnalysisTools/Fitter/PndKalmanVtxFitter.h
@@ -0,0 +1,12 @@
+// A typedef for convenience
+// Author R.Kliemt 2014 GSI
+//
+#ifndef PNDKALMANVTXFITTER_H
+#define PNDKALMANVTXFITTER_H
+
+
+#include "PndVtxPRG.h"
+
+typedef PndVtxPRG PndKalmanVtxFitter;
+
+#endif
