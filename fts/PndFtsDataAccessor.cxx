977afb0d8ba1793173f7ce591cf1e0d8f66a5d73
Martin J. Galuska <MartinJGaluska>
2014-12-10 17:06:00 +0000

    Initialization of variable for B field access.

--- fts/PndFtsDataAccessor.cxx
+++ fts/PndFtsDataAccessor.cxx
@@ -39,7 +39,7 @@
 using namespace std;
   //________________________________________________________________
 PndFtsDataAccessor::PndFtsDataAccessor():
-  FairTask("FTSDataAccessor"), fMCTracks(0), pdg(0), fPersistence(kTRUE)
+  FairTask("FTSDataAccessor"), fMCTracks(0), pdg(0), fPersistence(kTRUE), By(0)
 {
     //---
   fVerbose = 0;
