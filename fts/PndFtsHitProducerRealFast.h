0d1a675ff87a2cdefcedb8ac625f2c727ba40c46
Martin J. Galuska <MartinJGaluska>
2014-12-19 10:49:38 +0000

    Reverted introduction of flag to switch between using MC point coordinates and centers of straws as hit coordinates (for FTS PR debugging and parameter optimization). I will add a copy of the modified task with a different name in the tracking subfolder.

--- fts/PndFtsHitProducerRealFast.h
+++ fts/PndFtsHitProducerRealFast.h
@@ -16,67 +16,65 @@ class TObjectArray;
 class PndFtsHitProducerRealFast : public FairTask
 {
 
-public:
+ public:
 
-	/** Default constructor **/
-	PndFtsHitProducerRealFast(Bool_t useMcTruthPositionForHits=kFALSE);
-
-
-	/** Destructor **/
-	~PndFtsHitProducerRealFast();
+  /** Default constructor **/  
+  PndFtsHitProducerRealFast();
 
 
-	/** Virtual method Init **/
-	virtual InitStatus Init();
+  /** Destructor **/
+  ~PndFtsHitProducerRealFast();
 
 
-	/** Virtual method Exec **/
-	virtual void Exec(Option_t* opt);
-
-	PndFtsHit* AddHit(Int_t detID, Int_t tubeID, Int_t chamberID, Int_t layerID, Int_t skew, Int_t iPoint, TVector3& pos, TVector3& dpos, Double_t p, Double_t rsim, Double_t closestDistanceError, Double_t depcharge);
-
-	PndFtsHitInfo* AddHitInfo(Int_t fileNumber, Int_t eventNumber, Int_t trackID, Int_t pointID, Int_t nMerged, Bool_t isFake);
-
-	void FoldZPosWithResolution(Double_t &zpos, Double_t &zposError, TVector3 localInPos, TVector3 localOutPos);
-
-	Double_t GetError(Double_t);
+  /** Virtual method Init **/
+  virtual InitStatus Init();
 
 
-	/** set persistence flag **/
-	void SetPersistence(Bool_t persistence) { fPersistence = persistence; }
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
 
-private:
+  PndFtsHit* AddHit(Int_t detID, Int_t tubeID, Int_t chamberID, Int_t layerID, Int_t skew, Int_t iPoint, TVector3& pos, TVector3& dpos, Double_t p, Double_t rsim, Double_t closestDistanceError, Double_t depcharge);
 
-	void SetParContainers();
+  PndFtsHitInfo* AddHitInfo(Int_t fileNumber, Int_t eventNumber, Int_t trackID, Int_t pointID, Int_t nMerged, Bool_t isFake);
 
+  void FoldZPosWithResolution(Double_t &zpos, Double_t &zposError, TVector3 localInPos, TVector3 localOutPos);
+  
+  Double_t GetError(Double_t);
 
-	/** Input array of PndFtsPoints **/
-	TClonesArray* fPointArray;
+ 
+  /** set persistence flag **/
+  void SetPersistence(Bool_t persistence) { fPersistence = persistence; }
 
-	/** Output array of PndFtsHits **/
-	TClonesArray* fHitArray;
+  private: 
 
-	TObjArray *fVolumeArray;
-
-	/** Output array of PndFtsHitInfo **/
-	TClonesArray* fHitInfoArray;
-	Int_t fevtn;
-
-	PndGeoFtsPar*  fFtsParameters;
-
-	/** object persistence **/
-	Bool_t  fPersistence; //!
-
-	/** if kTRUE then hits have same coordinates as MC points. If kFALSE hits have center of straw as position. **/
-	const Bool_t fUseMcTruthPositionForHits;
-
-	TClonesArray* fTubeArray;
+  void SetParContainers();
 
 
-	PndFtsHitProducerRealFast(const  PndFtsHitProducerRealFast& L);
-	PndFtsHitProducerRealFast& operator= (const  PndFtsHitProducerRealFast&) {return *this;}
+  /** Input array of PndFtsPoints **/
+  TClonesArray* fPointArray; 
 
-	ClassDef(PndFtsHitProducerRealFast,1);
+  /** Output array of PndFtsHits **/
+  TClonesArray* fHitArray;   
+
+  TObjArray *fVolumeArray;   
+
+  /** Output array of PndFtsHitInfo **/
+  TClonesArray* fHitInfoArray;
+  Int_t fevtn;
+
+  PndGeoFtsPar*  fFtsParameters;
+  
+  /** object persistence **/
+  Bool_t  fPersistence; //!
+
+
+  TClonesArray* fTubeArray;
+
+
+  PndFtsHitProducerRealFast(const  PndFtsHitProducerRealFast& L);
+  PndFtsHitProducerRealFast& operator= (const  PndFtsHitProducerRealFast&) {return *this;}
+  
+  ClassDef(PndFtsHitProducerRealFast,1);
 
 };
 
