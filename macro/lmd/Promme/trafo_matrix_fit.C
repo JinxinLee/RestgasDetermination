68af4d50b11ec6d3fed3dfdf5a2359da0974411e
Prometeusz Jasinski <jasinski@kph.uni-mainz.de>
2015-02-27 14:05:02 +0000

    saving todays changes to trafo matrix fits

--- macro/lmd/Promme/trafo_matrix_fit.C
+++ macro/lmd/Promme/trafo_matrix_fit.C
@@ -25,6 +25,9 @@
 #include "Math/Factory.h"
 #include "Math/Functor.h"
 #include "TError.h"
+#include "TLine.h"
+
+#include <iomanip>
 
 using namespace std;
 
@@ -38,7 +41,9 @@ void DrawProgressBar(int len, double percent);
 // the application
 // Assuming to find an mc file called
 // ./Lumi_MC_0.root
-void trafo_matrix_fit();
+// if invert, then the solution for a transformation
+// from lmd to ip is searched
+void trafo_matrix_fit(bool invert = false);
 
 // calling all vectors to minimize the parameters xx
 // (Minimizer function)
@@ -47,7 +52,9 @@ double Chi2(const double *xx);
 // load vectors from a given MC file
 // vectors from hits at the first LMD plane
 // with corresponding hits at the IP will be loaded
-void load_vectors(string filename);
+// if invert, then the solution for a transformation
+// from lmd to ip is searched
+void load_vectors(string filename, bool invert = false);
 
 // test loaded vectors on a transformation matrix
 // see code of Chi2() for the structure explanation of the matrix xx
@@ -77,32 +84,118 @@ public:
 		return result;
 	}
 	double& operator[] (int i) {return entry[i];}
+	TRepVect operator- (const TRepVect vect){
+		TRepVect result;
+		for (int i = 0; i < 7; i++){
+			result.entry[i] = this->entry[i]-vect.entry[i];
+		}
+		return result;
+	}
 };
+
 class TRepVectPair{
 public:
 	TRepVect IP;
 	TRepVect LMD;
 	bool valid;
+	void invert(){
+		TRepVect _temp = IP;
+		IP = LMD;
+		LMD = _temp;
+	}
+};
+
+// containing a 7 x 7 transformation matrix for TRepVect
+/* Assuming the following structure of the matrix
+ * which is used to transform homogeneous coordinates
+ * as well as momenta directions
+ * (x,y,z,1,px/pz,py/pz,1)^T = xi
+ */
+// The corresponding transformation would be
+// xi' = Mij * xj
+// where xi' are the vector parameters at the LMD
+// and xj the vector parameters at the IP
+// xi' will be written as xi
+// Mij * xj - xi' should be ideally 0
+// what will be used as an minimization criterion
+class TRepMat{
+public:
+	double M[7][7];
+	TRepMat(){
+		for (int i = 0; i < 7; i++){
+			for (int j = 0; j < 7; j++){
+				M[i][j] = 0;
+			}
+		}
+	}
+	// constructor for 49 entries in a row
+	// to be matched on the matrix
+	TRepMat(const double* mat){
+		for (int i = 0; i < 7*7; i++){
+			(*this)[i] = mat[i];
+		}
+	}
+	double& operator[](int const ij) {
+		return M[ij/7][ij%7];
+	}
+	double& GetSet(int col, int row){
+		return M[col][row];
+	}
+	double* Get(){
+		double* result = new double[7*7];
+		for (int i = 0; i < 7*7; i++){
+			result[i] = (*this)[i];
+		}
+		return result;
+	}
+	TRepVect operator*(const TRepVect vect){
+		TRepVect result;
+		for (int i = 0; i < 7; i++){
+			for (int j = 0; j < 7; j++){
+				result.entry[i] += this->M[i][j] * vect.entry[j];
+			}
+		}
+		return result;
+	}
+	TRepMat operator*(const TRepMat mat){
+		TRepMat result;
+		for (int i = 0; i < 7; i++){
+			for (int j = 0; j < 7; j++){
+				for (int k = 0; k < 7; k++){
+					result.M[i][j] += this->M[i][k] * mat.M[k][j];
+				}
+			}
+		}
+		return result;
+	}
+	void Print(){
+		cout << " The numbers are " << endl;
+		for (int i = 0; i < 7; i++){
+			for (int j = 0; j < 7; j++){
+				cout << std::setprecision(4) << "\t" << M[i][j] << ", ";
+			}
+			cout << endl;
+		}
+	}
 };
 
 vector<TRepVectPair> repvectpairs;
 
 // see what is going on during minimization
 TGraph convergence_graph;
+TH1D Chi2_dist("Chi2_dist", "chi2 distribution", 100, 0, 0.002);
+TLine meanline;
 int ncalls(0);
 
 int main() {
-	trafo_matrix_fit();
+	trafo_matrix_fit(false);
 	return 0;
 }
 
 double Chi2(const double *xx)
 {
-	/* Assuming the following structure of the matrix
-	 * which is used to transform homogeneous coordinates
-	 * as well as momenta directions
-	 * (x,y,z,1,px/pz,py/pz,1)^T = xi
-	 */
+	Chi2_dist.Reset();
+/*
 	double Mij[7][7] = {
 			{xx[0],xx[1],xx[2],xx[3],xx[4],xx[5],xx[6]},
 			{xx[7],xx[8],xx[9],xx[10],xx[11],xx[12],xx[13]},
@@ -112,18 +205,13 @@ double Chi2(const double *xx)
 			{xx[35],xx[36],xx[37],xx[38],xx[39],xx[40],xx[41]},
 			{xx[42],xx[43],xx[44],xx[45],xx[46],xx[47],xx[48]}
 	};
-	// The corresponding transformation would be
-	// xi' = Mij * xj
-	// where xi' are the vector parameters at the LMD
-	// and xj the vector parameters at the IP
-	// xi' will be written as xi
-	// Mij * xj - xi' should be ideally 0
-	// what will be used as an minimization criterion
+	*/
+	TRepMat Mij(xx);
 	double mean_diff = 0;
-
 	int nvectors = repvectpairs.size();
 	for (int ivector = 0; ivector < nvectors; ivector++){
-		TRepVect x0;
+		TRepVect x0 = Mij*repvectpairs[ivector].IP;
+		x0 = x0 - repvectpairs[ivector].LMD;
 		//cout << endl;
 		//for (int i = 0; i < 7; i++){
 		//	cout << "                                            " << repvectpairs[ivector].IP[i] << endl;
@@ -134,6 +222,7 @@ double Chi2(const double *xx)
 		//	}
 		//	cout << endl;
 		//}
+		/*
 		for (int i = 0; i < 7; i++){
 			//cout << endl << i << " : " << repvectpairs[ivector].IP[i] << " becomes ";
 			for (int j = 0; j < 7; j++){
@@ -142,17 +231,22 @@ double Chi2(const double *xx)
 			//cout << x0[i] << " and should be ";
 			x0[i] -= repvectpairs[ivector].LMD[i];
 			//cout << endl << repvectpairs[ivector].LMD[i] << endl;
-		}
+		}*/
 		//for (int i = 0; i < 7; i++){
 		//	cout << " " << x0[i] << "\t" << repvectpairs[ivector].LMD[i] << endl;
 		//}
 		// calculate the length of the vector
-		mean_diff += x0.Mag2();
+		Chi2_dist.Fill(x0.Mag2());
+		//cout << x0.Mag2() << endl;
+		mean_diff += x0.Mag2()/(double) nvectors;
+
 		//break;
 	}
 	// normalize
 	//cout << " the difference before normalization is " << mean_diff << endl;
-	mean_diff /= (double) repvectpairs.size();
+	//cout << mean_diff << " " << endl;
+	//mean_diff /= (double) repvectpairs.size();
+	//cout << " " << mean_diff << endl;
 	/*cout << " The numbers are " << endl;
 	for (int i = 0; i < 7; i++){
 		for (int j = 0; j < 7; j++){
@@ -165,26 +259,44 @@ double Chi2(const double *xx)
 	ncalls++;
 	if (ncalls > convergence_graph.GetN()){
 		convergence_graph.Set(ncalls+100);
-		convergence_graph.Draw("ALP");
-		gPad->Update();
+		for (int i = ncalls; i < ncalls + 100; i++)
+			convergence_graph.SetPoint(i, 0, convergence_graph.GetY()[0]);
 	}
 	convergence_graph.SetPoint(ncalls-1, ncalls, mean_diff);
+	if (ncalls%100 == 0){
+		Chi2_dist.Draw();
+		//convergence_graph.Draw("A*P");
+		gPad->SetLogy();
+		meanline.SetX1(mean_diff);
+		meanline.SetX2(mean_diff);
+		meanline.SetY1(1e-4);
+		meanline.SetY2(1000);
+		meanline.SetLineColor(2);
+		meanline.SetLineWidth(2);
+		meanline.Draw();
+		gPad->Update();
+		//sleep(1);
+	}
+	//mean_diff /= (double) repvectpairs.size();
 
-	return mean_diff;
+	return Chi2_dist.GetMean();
+	return mean_diff; // the mean diff seems not a good measure without a cut such as the histogram is performing
 }
 
 void trafo_matrix_test(const double *xx, string filename){
 	TCanvas canvas("canvas", "canvas", 600, 600);
-	TH1F hist_res_x("hist_res_x", "resolution x", 500, -0.02, 0.02);
-	TH1F hist_res_y("hist_res_y", "resolution y", 500, -0.02, 0.02);
-	TH1F hist_res_z("hist_res_z", "resolution z", 500, -0.02, 0.02);
-	TH1F hist_res_w("hist_res_w", "resolution w", 500, -0.02, 0.02);
-	TH1F hist_res_pxpz("hist_res_pxpz", "resolution px/pz", 500, -0.02, 0.02);
-	TH1F hist_res_pypz("hist_res_pypz", "resolution py/pz", 500, -0.02, 0.02);
-	TH1F hist_res_pw("hist_res_pw", "resolution pw", 500, -0.02, 0.02);
+	TH1F hist_res_x("hist_res_x", "resolution x", 500, -0.005, 0.005);
+	TH1F hist_res_y("hist_res_y", "resolution y", 500, -0.005, 0.005);
+	TH1F hist_res_z("hist_res_z", "resolution z", 500, -0.005, 0.005);
+	TH1F hist_res_w("hist_res_w", "resolution w", 500, -0.005, 0.005);
+	TH1F hist_res_pxpz("hist_res_pxpz", "resolution px/pz", 500, -0.005, 0.005);
+	TH1F hist_res_pypz("hist_res_pypz", "resolution py/pz", 500, -0.005, 0.005);
+	TH1F hist_res_pw("hist_res_pw", "resolution pw", 500, -0.005, 0.005);
 	//TH1F hist_res_theta("hist_res_theta", "resolution #theta", 1000, -0.1, 0.1);
 	//TH1F hist_res_phi("hist_res_phi", "resolution #phi", 1000, -0.1, 0.1);
 	TH1F* hists[7] = {&hist_res_x, &hist_res_y, &hist_res_z, &hist_res_w, &hist_res_pxpz, &hist_res_pypz, &hist_res_pw};
+	TRepMat Mij(xx);
+	/*
 	double Mij[7][7] = {
 			{xx[0],xx[1],xx[2],xx[3],xx[4],xx[5],xx[6]},
 			{xx[7],xx[8],xx[9],xx[10],xx[11],xx[12],xx[13]},
@@ -193,20 +305,22 @@ void trafo_matrix_test(const double *xx, string filename){
 			{xx[28],xx[29],xx[30],xx[31],xx[32],xx[33],xx[34]},
 			{xx[35],xx[36],xx[37],xx[38],xx[39],xx[40],xx[41]},
 			{xx[42],xx[43],xx[44],xx[45],xx[46],xx[47],xx[48]}
-	};
+	};*/
 	int nvectors = repvectpairs.size();
 	for (int ivector = 0; ivector < nvectors; ivector++){
-		TRepVect x0;
+		//TRepVect x0;
 		//TVector3 dir_lmd(repvectpairs[ivector].LMD[4], repvectpairs[ivector].LMD[5], repvectpairs[ivector].LMD[6]);
 		//TVector3 dir_after;
+		TRepVect x0 = Mij*repvectpairs[ivector].IP;
+		x0 = x0 - repvectpairs[ivector].LMD;
 		for (int i = 0; i < 7; i++){
-			for (int j = 0; j < 7; j++){
-				x0[i] += Mij[i][j] * repvectpairs[ivector].IP[j];
-			}
+			//for (int j = 0; j < 7; j++){
+			//	x0[i] += Mij[i][j] * repvectpairs[ivector].IP[j];
+			//}
 			//if (i == 4) dir_after.SetX(x0[i]);
 			//if (i == 5) dir_after.SetY(x0[i]);
 			//if (i == 6) dir_after.SetZ(x0[i]);
-			x0[i] -= repvectpairs[ivector].LMD[i];
+			//x0[i] -= repvectpairs[ivector].LMD[i];
 			hists[i]->Fill(x0[i]);
 		}
 		//hist_res_theta.Fill(dir_after.Theta()-dir_lmd.Theta());
@@ -224,7 +338,7 @@ void trafo_matrix_test(const double *xx, string filename){
 //	canvas.Print((filename.str()+"]").c_str());
 }
 
-void load_vectors(string filename){
+void load_vectors(string filename, bool invert){
 	PndLmdDim& lmddim = PndLmdDim::Get_instance();
 	TChain tMC("cbmsim");
 	tMC.Add(filename.c_str());
@@ -304,6 +418,7 @@ void load_vectors(string filename){
 			//nhitstotal++;
 		}
 		if (repvectpair.valid){
+			if (invert) repvectpair.invert();
 			repvectpairs.push_back(repvectpair);
 			/*
 			cout << endl;
@@ -317,11 +432,34 @@ void load_vectors(string filename){
 	cout << endl << " found " << repvectpairs.size() << " vector pairs for analysis " << endl;
 }
 
-void trafo_matrix_fit(){
+void trafo_matrix_fit(bool invert){
 	TApplication myapp("myapp", 0, 0);
-	//load_vectors("./plab_1.5GeV/dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.1_0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root");
-	//load_vectors("./plab_15.0GeV/dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root");
-	load_vectors("./plab_1.5GeV/dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root");
+	// all 1.5 GeV/c files by stefan
+	/*
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.1_0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.5_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.001_0.001_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.001_0.001/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.5_0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.1_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.1_-0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.1_-0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.1_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.5_-0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.5_0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_-0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.1_0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_-0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.1_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_-0.5_-0.5_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.5_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+*/
+	load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
+
+	// 15 GeV/c files by stefan
+	//load_vectors("./dpm_elastic_thmin_0.06deg/ip_offset_XYZDXDYDZ_0.0_0.0_0.0_0.08_0.08_0.35/beam_grad_XYDXDY_0.0_0.0_0.0_0.0/200000/mc_data/Lumi_MC*.root", invert);
 
 	ROOT::Math::Minimizer* min =
 			ROOT::Math::Factory::CreateMinimizer("Minuit2", "");
@@ -331,17 +469,101 @@ void trafo_matrix_fit(){
 	min->SetPrintLevel(1);
 
 	ROOT::Math::Functor f(&Chi2, 49);
-	// starting point is
-	double Mij[49] = {
-			1		,0	,40e-3	,6.5*40e-3	,1.124	,0		,0		, // angles are scaled by 10
-			0		,1	,0		,0			,0		,1.124	,0		, // 1.124 equals 11.24m
-			-40e-3	,0	,1		,11.24		,0		,0		,0		,
+	/*
+	double Mtranssol[49] = {
+			1		,0	,0		,0			,0  	,0		,0		, // angles are scaled by 10
+			0		,1	,0		,0			,0		,0  	,0		, // 2 equals 2m
+			0	    ,0	,1		,6.5		,0		,0		,0		,
+			0		,0	,0		,1			,0		,0		,0		,
+			0		,0	,0		,0			,1		,0		,0		, // 40 mrad times 10
+			0		,0	,0		,0			,0		,1		,0		,
+			0		,0	,0		,0			,0		,0		,1
+	};
+	double Mrotsol[49] = {
+			cos(.21)	,-sin(.21)	,0		,0			,0		,0		,0		, // angles are scaled by 10
+			sin(.21)	,cos(.21)	,0		,0			,0		,0		,0		, // 1.124 equals 11.24m
+			0			,0			,1		,0			,0		,0		,0		,
+			0			,0			,0		,1			,0		,0		,0		,
+			0			,0			,0		,0			,1		,0		,0		, // 40 mrad times 10
+			0			,0			,0		,0			,0		,1		,0		,
+			0			,0			,0		,0			,0		,0		,1
+	};
+	double Mrotdip[49] = {
+			1		,0	,40e-3	,0			,0		,0		,0		, // angles are scaled by 10
+			0		,1	,0		,0			,0		,0		,0		, // 1.124 equals 11.24m
+			-40e-3	,0	,1		,0			,0		,0		,0		,
 			0		,0	,0		,1			,0		,0		,0		,
 			0		,0	,0		,0			,1		,0		,400e-3	, // 40 mrad times 10
 			0		,0	,0		,0			,0		,1		,0		,
 			0		,0	,0		,0			,0		,0		,1
 	};
+	double Mtransdiplmd[49] = {
+			1		,0	,0		,0			,1.124  ,0		,0		, // angles are scaled by 10
+			0		,1	,0		,0			,0		,1.124  ,0		, // 2 equals 2m
+			0	    ,0	,1		,4.74		,0		,0		,0		,
+			0		,0	,0		,1			,0		,0		,0		,
+			0		,0	,0		,0			,1		,0		,0		, // 40 mrad times 10
+			0		,0	,0		,0			,0		,1		,0		,
+			0		,0	,0		,0			,0		,0		,1
+	};*/
+
+	//TRepMat M1(Mtranssol), M2(Mrotsol), M3(Mrotdip), M4(Mtransdiplmd);
+	//TRepMat MSum = M4* M3 * M2 * M1;
+	//M4.Print();
+	//M3.Print();
+	//M2.Print();
+	//M1.Print();
+	// hacking
+	//MSum.M[0][6] = 0;
+	//MSum.M[1][3] = 0;
+	//MSum.Print();
+	//double* Mij = MSum.Get();
+	//double phi = 0.21;
 	/*
+	double Mij[49] = {
+			1		,0		,0.04	,6.5*0.04	,1.0992	,-0.234	,0		, // angles are scaled by 10
+			0		,1		,0		,0			,+0.234	,1.0992	,0		, // 1.124 equals 11.24m
+			-0.04	,0		,1		,11.24		,0		,0		,0		,
+			0		,0		,0		,1			,0		,0		,0		,
+			0		,0		,0		,0			,0.978	,-0.208	,0.4	, // 40 mrad times 10
+			0		,0		,0		,0			,0.208	,0.978	,0.0  ,
+			0		,0		,0		,0			,0		,0		,1
+	};*/
+	double Mij[49] = {
+			1		,0	,40e-3	,6.5*40e-3	,1.124	,0.			,0		, // angles are scaled by 10
+			0		,1	,0		,0			,0		,1.124		,0		, // 1.124 equals 11.24m
+			-40e-3	,0	,1		,11.24		,0		,0			,0		,
+			0		,0	,0		,1			,0		,0			,0		,
+			0		,0	,0		,0			,1		,0			,400e-3	, // 40 mrad times 10
+			0		,0	,0		,0			,0		,1			,0		,
+			0		,0	,0		,0			,0		,0			,1
+	};
+	if (invert){
+		double Mij_inverted[49] = {
+		/*		1		,0	,-0.04	,4.75*0.04	,-1.124	,0			,0.4*1.124	, // angles are scaled by 10
+				0		,1	,0		,0			,0		,-1.124		,0			, // 1.124 equals 11.24m
+				0.04	,0	,1		,-11.24		,0		,0			,0			,
+				0		,0	,0		,1			,0		,0			,0			,
+				0		,0	,0		,0			,1		,0			,-400e-3	, // 40 mrad times 10
+				0		,0	,0		,0			,0		,1			,0			,
+				0		,0	,0		,0			,0		,0			,1 */
+				0.979707, 0.00855083, -0.957085, 10.5033, -1.14336, -0.00568183,
+				0.45782, 0.0379627, 0.955083, 0.999504, -11.2443, 0.00606571,
+				-1.07678, -0.00239097, 9.70718, -4.38641, 244.35, -2749.01,
+				-0.0322177, 4.76301, 0.0134341, 0.0000190301, 0.000359596,
+				0.000503436, 0.994336, 0.0000179324, -0.00038629, -6.77702e-6,
+				0.147446, -0.229086, 3.19516, -35.9521, 0.974497, 0.28175,
+				-0.389989, 0.105476, 0.0543932, 0.267799, -3.03747, -0.167528,
+				0.943272, 0.0670344, 0.0000190301, 0.000359596, 0.000503436,
+				-0.00566317, 0.0000179324, -0.00038629, 0.999993
+		};
+		for (int i = 0; i < 49; i++)
+			Mij[i] = Mij_inverted[i];
+	}
+	TRepMat MSum(Mij);
+	cout << " the starting condition is " << endl;
+	MSum.Print();
+/*
 	double Mij[49] = {
 			1		,0	,0		,0		,0		,0	,0		,
 			0		,1	,0		,0		,0		,0	,0		,
@@ -360,13 +582,17 @@ void trafo_matrix_fit(){
 		varname << "M" << i/7 << i%7;
 		if (i > 3) stepsize = 0.001;
 		else stepsize = 0.001;
+		//if (Mij[i] == 0) Mij[i] = 0.0001;
 		min->SetVariable(i, varname.str().c_str(), Mij[i], stepsize);
 	}
 	trafo_matrix_test(Mij, "residuals_before.pdf");
 	min->Minimize();
-	const double *Mij_min = min->X();
-	trafo_matrix_test(Mij_min, "residuals_after.pdf");
+	TRepMat Mij_min(min->X());
+	Mij_min.Print();
+
+	trafo_matrix_test(Mij_min.Get(), "residuals_after.pdf");
 	//min->PrintResults();
+	myapp.Run();
 }
 /*
 	Root_Appearance();
@@ -720,7 +946,7 @@ void trafo_matrix_fit(){
 	results.Write();
 	treefile.Close();
 	cout << " done " << endl;
-	myapp.Run();
+
 }*/
 
 void Root_Appearance(){
