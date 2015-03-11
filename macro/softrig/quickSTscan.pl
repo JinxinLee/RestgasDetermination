f54f4ee69d9ce6255ed55cc37c34481990eca639
Klaus Goetzen <K.Goetzen@gsi.de>
2015-03-11 10:30:04 +0000

    extended quickSTscan for job submission

--- macro/softrig/quickSTscan.pl
+++ macro/softrig/quickSTscan.pl
@@ -10,10 +10,12 @@ my $decfile  = $ARGV[4];
 my $nevt     = $ARGV[5];
 my $res      = $ARGV[6];
 my $mode     = $ARGV[7];
+my $njobs    = 0;
 
-if (!defined($nevt))  {$nevt  = 1000;}
-if (!defined($res))   {$res   = "pbp0";}
-if (!defined($mode))  {$mode  = 900;}
+if (!defined($nevt))   {$nevt  = 1000;}
+if (!defined($res))    {$res   = "pbp0";}
+if (!defined($mode))   {$mode  = 900;}
+if (defined($ARGV[8])) {$njobs = $ARGV[8];}
 
 if (!defined($ARGV[4])) 
 {
@@ -26,7 +28,8 @@ if (!defined($ARGV[4]))
     print "   <decfile> : decay file or 'DPM' or 'FTF'\n";
     print "   [nevt]    : events per scan point; default: 1000\n";
     print "   [res]     : resonance; default: 'pbp'\n";
-    print "   [mode]    : data mode; default: [3 digits Ecm]900\n\n";
+    print "   [mode]    : data mode; default: [3 digits Ecm]900\n";
+    print "   [njobs]   : if > 0, njobs jobs will be submitted per energy; default: 0\n\n";
    exit(0);
 }
 
@@ -40,8 +43,16 @@ $step *= 100;
 for (my $ecm = $min; $ecm<=$max; $ecm+=$step)
 {
 	my $ecmstr = int($ecm+0.0001);
-	my $command = "root -l -b -q '$macro(\"$pref$ecmstr\", \"$decfile\", ".(-$ecm/100.).", $nevt, \"$res\", $ecmstr$mode, $cnt, 1)'";
+	my $command = "";
+	if ($njobs==0) 
+	{
+		$command = "root -l -b -q '$macro(\"$pref$ecmstr\", \"$decfile\", ".(-$ecm/100.).", $nevt, \"$res\", $ecmstr$mode, $cnt, 1)'";
+	}
+	else
+	{
+		$command = "qsub -t 1-$njobs job_sof_fsim.sge $ecmstr$mode $nevt $decfile ".(-$ecm/100.)." $res $pref 1"; 
+	}
 	print $command."\n";
-	system($command);
+	#system($command);
 	$cnt++;
 }
