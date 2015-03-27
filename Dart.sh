90d9b4b45c1e0ce7a3e128fcde43bc94431eda11
Florian Uhlig <f.uhli@gsi.de>
2015-03-27 19:36:07 +0000

    Add new profiling mode to the Dart script.

--- Dart.sh
+++ Dart.sh
@@ -39,11 +39,11 @@ if [ "$#" -lt "2" ]; then
 fi
 
 # test if a ctest model is either Experimantal or Nightly
-if [ "$1" == "Experimental" -o "$1" == "Nightly" -o "$1" == "Continuous" ]; then
+if [ "$1" == "Experimental" -o "$1" == "Nightly" -o "$1" == "Continuous" -o "$1" == "Profile" ]; then
   echo ""
 else
   echo "-- Error -- This ctest model is not supported."
-  echo "-- Error -- Possible arguments are Nightly, Experimental or Continuous."
+  echo "-- Error -- Possible arguments are Nightly, Experimental, Continuous or Profile."
   exit 1
 fi 
 
@@ -74,7 +74,7 @@ else
   GCC_VERSION=$($CXX -dumpversion)
 fi
 
-export LABEL1=${LINUX_FLAVOUR}-$SYSTEM-$COMPILER$GCC_VERSION-fairsoft_$FAIRSOFT_VERSION
+export LABEL1=${LINUX_FLAVOUR}-$SYSTEM-$COMPILER$GCC_VERSION-fairsoft_$FAIRSOFT_VERSION-fairroot_$FAIRROOT_VERSION
 export LABEL=$(echo $LABEL1 | sed -e 's#/#_#g')
 
 # get the number of processors
@@ -88,6 +88,9 @@ then
     export number_of_processors=$(cat /proc/cpuinfo | grep processor | wc -l)
   fi
   export SITE=$(hostname -f)
+  if [ -z $SITE ]; then
+    export SITE=$(uname -n)
+  fi
 elif [ "$arch" = "darwin" ];
 then
   if [ "$NCPU" != "" ];
