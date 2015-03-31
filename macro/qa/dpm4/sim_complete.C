af73a1e5c735f5459be2ad1d23cb7ee982ae5d69
Stefano Spataro <spataro@to.infn.it>
2015-03-31 12:01:10 +0000

    g4

--- macro/qa/dpm4/sim_complete.C
+++ macro/qa/dpm4/sim_complete.C
@@ -5,7 +5,7 @@
 // to run with different options:(e.g more events, different momentum, Geant4)
 // root  sim_complete.C"(100, "TGeant4",2)"
 
-sim_complete(Int_t nEvents = 10, TString  SimEngine ="TGeant3", Float_t mom = 6.231552)
+sim_complete(Int_t nEvents = 10, TString  SimEngine ="TGeant4", Float_t mom = 6.231552)
 {
   //-----User Settings:-----------------------------------------------
   TString  OutputFile     ="sim_complete.root";
