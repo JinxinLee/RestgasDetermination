39e201ffb00f27ab9f26d36cca439e371fbd5f68
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2015-03-31 12:13:22 +0000

    Fix problem with MC propagation in PndSdsCluster

--- pnddata/SdsData/PndSdsCluster.h
+++ pnddata/SdsData/PndSdsCluster.h
@@ -3,7 +3,7 @@
 
 //#include "FairMultiLinkedData.h" 
 #include "PndDetectorList.h" // contains SensorSide enumeration
-#include "FairMultiLinkedData.h"
+#include "FairMultiLinkedData_Interface.h"
 #include "TObject.h"
 #include <vector>
 #include <iostream>
@@ -16,7 +16,7 @@
 //! This class holds the information which Digi belongs to the actual cluster.
 //! The information is stored in a vector<Int_t> which contains the
 //! position of the digi in the TClonesArray where it is stored.
-class PndSdsCluster : public FairMultiLinkedData
+class PndSdsCluster : public FairMultiLinkedData_Interface
 {
 
 friend std::ostream& operator<< (std::ostream& out, PndSdsCluster& cl){
