8b4c5c4b878cf2cd747c6eb3ad267374f91ebb67
Stefano Spataro <spataro@to.infn.it>
2015-04-04 21:10:13 +0000

    Removed obsolete classes

--- GenfitTools/recotasks/RecoTasksLinkDef.h
+++ GenfitTools/recotasks/RecoTasksLinkDef.h
@@ -9,6 +9,4 @@
 #pragma link C++ class PndRecoDafFit+;
 #pragma link C++ class PndRecoKalmanTask+;
 #pragma link C++ class PndRecoMultiKalmanTask+;
-#pragma link C++ class PndGFTrackToPndTrackConvertorTask+;
-#pragma link C++ class PrimSelector+;
 #endif
