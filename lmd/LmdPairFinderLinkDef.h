cfcc543aba3e0b09e1b01d0ea5271aa28c988942
rklasen <rklasen>
2015-11-02 12:03:27 +0000

    updated CMakeList for LmdSensorAlignment and added LinkDef files.

---
+++ lmd/LmdPairFinderLinkDef.h
@@ -0,0 +1,14 @@
+// $Id: LmdLinkDef.h,v 1.0 M. Michel $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//pair Finder
+#pragma link C++ class LmdPairFinderTask+;
+#pragma link C++ class PndLmdHitPair+;
+
+#endif
+
---
+++ lmd/LmdSensorAlignerLinkDef.h
@@ -0,0 +1,15 @@
+// $Id: LmdLinkDef.h,v 1.0 M. Michel $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//pair Finder Tools
+#pragma link C++ class PndLmdSensorAligner+;
+#pragma link C++ class PndLmdAlignManager+;
+#pragma link C++ class PndLmdAlignQA+;
+
+#endif
+
