0b2a98f343ca73120c997477b783c7887703605c
Konstantin I. Beloborodov <K.I.Beloborodov@inp.nsk.su>
2015-12-09 17:36:16 +0000

    cherenkov photon hit (digi)

---
+++ pnddata/RichData/PndRichPDHit.cxx
@@ -0,0 +1,51 @@
+// -------------------------------------------------------------------------
+// -----                       PndRichPDHit source file                -----
+// -----               Created 01/11/14  by Beloborodov Konstantin     -----
+// -----                                                               -----
+// -----                                                               -----
+// -------------------------------------------------------------------------
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#include "PndRichPDHit.h"
+
+// -----   Default constructor   -------------------------------------------
+PndRichPDHit::PndRichPDHit()
+   :FairHit(),
+   fTime(-999.),
+   fTimeThreshold(-999.)
+{  
+}
+// -------------------------------------------------------------------------
+
+
+// -----   Standard constructor   ------------------------------------------
+
+PndRichPDHit::PndRichPDHit(Int_t detID, 
+                           Int_t sensorId, 
+                           TVector3& pos, 
+                           TVector3& dpos,
+                           Double_t time,
+                           Double_t timeThreshold,
+                           Int_t index)
+:FairHit(detID, pos, dpos, index),  
+   fSensorId (sensorId), 
+   fTime       (time),
+   fTimeThreshold (timeThreshold)
+{    
+}
+
+// -----   Destructor   ----------------------------------------------------
+PndRichPDHit::~PndRichPDHit(){}
+// -------------------------------------------------------------------------
+
+
+// -----   Public method Print   -------------------------------------------
+void PndRichPDHit::Print(const Option_t* opt) const {
+  cout << "RICH Photon hit: Time = " << fTime << endl;
+}
+// -------------------------------------------------------------------------
+
+ClassImp(PndRichPDHit)
---
+++ pnddata/RichData/PndRichPDHit.h
@@ -0,0 +1,56 @@
+// -------------------------------------------------------------------------
+// -----                     PndRichPDHit header file                  -----
+// -----               Created 01/11/14 by Konstantin Beloborodov      -----
+// -----                                                               -----
+// -------------------------------------------------------------------------
+
+
+#ifndef PNDRICHPDHIT_H
+#define PNDRICHPDHIT_H
+
+#include "FairHit.h"
+
+class PndRichPDHit : public FairHit {
+
+public:    
+  
+  /** Default constructor **/
+  PndRichPDHit();
+
+  PndRichPDHit(Int_t detID, 
+               Int_t sensorId, 
+               TVector3& pos, 
+               TVector3& dpos,
+               Double_t time,
+               Double_t timeThreshold,
+               Int_t index); 
+   
+
+/** Copy constructor **/
+  PndRichPDHit(const PndRichPDHit& hit) { *this = hit; };
+
+  /** Destructor **/
+  virtual ~PndRichPDHit();    
+  
+  /** Output to screen **/
+  virtual void Print(const Option_t* opt ="") const;
+  
+  /** Modifiers **/
+  
+    
+  /** Accessors **/
+  virtual Double_t GetTime()  {return fTime;}
+  virtual Double_t GetTimeThreshold()  {return fTimeThreshold;}
+  virtual Int_t GetRefIndex()  {return fRefIndex;}
+  TVector3 GetPosition()	  const { return TVector3(fX, fY, fZ);	  }
+  Int_t GetSensorId()  {return fSensorId;}
+
+ protected:
+  
+  Int_t fSensorId;
+  Double_t fTime,  fTimeThreshold;
+   
+  ClassDef(PndRichPDHit,1)
+};
+
+#endif //PNDRICHPDHIT_H
