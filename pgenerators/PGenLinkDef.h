1fda1688f2a650eb1f6c06787cb603a4c1544278
Ralf Kliemt <r.kliemt@gsi.de>
2016-01-05 09:01:39 +0000

    moved PndFtfDirect from FtfEvtGen folder

--- pgenerators/PGenLinkDef.h
+++ pgenerators/PGenLinkDef.h
@@ -9,6 +9,7 @@
 #pragma link C++ class  PndFtfGenerator+;
 #pragma link C++ class  PndEvtGenGenerator+;
 #pragma link C++ class  PndDpmDirect+;
+#pragma link C++ class  PndFtfDirect+;
 #pragma link C++ class  PndBoxGenerator+;
 #pragma link C++ class  PndUrqmdSmmGenerator+;
 #pragma link C++ class  PndJRGenerator+;
