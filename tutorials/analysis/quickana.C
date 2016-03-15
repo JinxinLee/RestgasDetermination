8720be33fb4ea69d9590734643dfdb4b0089c4b4
Klaus Goetzen <K.Goetzen@gsi.de>
2016-03-15 06:44:36 +0000

    Updated quickana.C and README file

--- tutorials/analysis/quickana.C
+++ tutorials/analysis/quickana.C
@@ -18,7 +18,7 @@
 
 void quickana(TString Fname="", double Mom=0, TString anadecay="", int nevts=0, TString anaparms="", bool fastsim=false, bool runST=false, int run=0, int runmode=0)
 {
-	if (Fname=="" || anadecay=="") 
+	if (Fname=="" || Mom==0) 
 	{
 		cout << "USAGE:\n";
 		cout << "quickana.C( <input>, <mom>, <decay>, [nevt], [parms], [fastsim], [runST], [runnum] )\n\n";
@@ -26,13 +26,16 @@ void quickana(TString Fname="", double Mom=0, TString anadecay="", int nevts=0, 
 		cout << "   <mom>     : pbar momentum; negative values are interpreted as -E_cm\n";
 		cout << "   <decay>   : the decay pattern to be reconstructed, e.g. 'phi -> K+ K-; D_s+ -> phi pi-'\n";
 		cout << "   [nevt]    : number of events; default: 0 = all\n";
-		cout << "   [parms]   : parameters for the analysis, e.g. 'mwin=0.4:mwin(phi)=0.1:emin=0.1:pmin=0.1:qamc'\n";
+		cout << "   [parms]   : parameters for the analysis, e.g. 'mwin=0.4:mwin(phi)=0.1:emin=0.1:pmin=0.1:qamc'; 'qapart' runs particle QA task\n";
 		cout << "   [fastsim] : set true, if running fast sim (sets the PID algos properly); default: false'\n";
 		cout << "   [runST]   : if 'true' runs Software Trigger (default: false)\n";
 		cout << "   [runnum]  : integer run number (default: 0)\n";
 		cout << "   [mode]    : arbitrary mode number (default: 0)\n\n";
 		return;
 	}
+	
+	// do particle QA?
+	bool partQA  = (anaparms.Contains("qapart"));
 	
 	// if Mom<0, interprete as -E_cm
 	double mp = 0.938272;
@@ -56,8 +59,6 @@ void quickana(TString Fname="", double Mom=0, TString anadecay="", int nevts=0, 
 	//TLorentzVector fIni(0,0,Mom,0.938272+sqrt(Mom*Mom+0.938272*0.938272));
 	TDatabasePDG::Instance()->AddParticle("pbarpSystem","pbarpSystem",3,kFALSE,0.1,0, "",88888);
 	TDatabasePDG::Instance()->AddParticle("pbarpSystem0","pbarpSystem0",3,kFALSE,0.1,0, "",88880);
-	TDatabasePDG::Instance()->AddParticle("Z(3900)+","Z+",3.900,kFALSE,0.1,0, "",90000);
-	TDatabasePDG::Instance()->AddParticle("Z(3900)-","Z-",3.900,kFALSE,0.1,0, "",-90000);
 	
 	// *** set this to your output path
 	TString OutFile = Fname(Fname.Last('/')+1,Fname.Length()); // cut away input path
@@ -107,6 +108,18 @@ void quickana(TString Fname="", double Mom=0, TString anadecay="", int nevts=0, 
 	PndSimpleCombinerTask *scTask = new PndSimpleCombinerTask(anadecay, anaparms, Mom, run, runmode);
 	scTask->SetPidAlgo(pidalgo);
 	fRun->AddTask(scTask);
+	
+	
+	// *****************************
+	// *** PndParticleQATask ***
+	// *****************************
+	
+	if (partQA)
+	{
+		PndParticleQATask *partQaTask = new PndParticleQATask(fastsim); // particle QA task
+		fRun->AddTask(partQaTask);
+	}
+		
 
 	// *** and run analysis
 	fRun->Init(); 
