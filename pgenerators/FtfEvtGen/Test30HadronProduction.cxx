718ab1186840561b0a984f81d3798ca36293cbe9
Aida Galoyan <galoyan@lxmx00.jinr.ru>
2016-03-28 12:10:07 +0000

    improved FtfEvtGen/Test30HadronProduction.cxx

--- pgenerators/FtfEvtGen/Test30HadronProduction.cxx
+++ pgenerators/FtfEvtGen/Test30HadronProduction.cxx
@@ -1,3 +1,40 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// -------------------------------------------------------------
+//      GEANT 4 class 
+//
+//      ---------- Test30HadronProduction -------
+//                by Vladimir Ivanchenko, 12 March 2002 
+// 
+//    Modified:
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
 
 #include "Test30HadronProduction.hh"
 #include "Test30VSecondaryGenerator.hh"
@@ -18,7 +55,7 @@
 
 Test30HadronProduction::Test30HadronProduction(const G4String& aName)
  :G4VDiscreteProcess(aName),
-  theGenerator(0), theChange()
+  theGenerator(0)
 {
   InitializeMe();
 }
@@ -68,14 +105,14 @@ G4VParticleChange* Test30HadronProduction::PostStepDoIt(
   ClearNumberOfInteractionLengthLeft();
   theChange.Initialize(track);
 
-  G4int nos = result->GetNumberOfSecondaries();
-  G4int nb = nos;
+  G4int ns = result->GetNumberOfSecondaries();
+  G4int nb = ns;
   if(result->GetStatusChange() == isAlive) nb++;
 
   theChange.ProposeTrackStatus(fStopAndKill);
   theChange.SetNumberOfSecondaries(nb);
 
-  for(G4int i=0; i<nos; i++) {
+  for(G4int i=0; i<ns; i++) {
     G4Track* tr = new G4Track(result->GetSecondary(i)->GetParticle(),
                               track.GetGlobalTime(),
 	                      track.GetPosition());
