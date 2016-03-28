e96d48856b46f45122d698fdbdb5adb83b1553b6
Aida Galoyan <galoyan@lxmx00.jinr.ru>
2016-03-28 13:55:26 +0000

    improved FtfEvtGen/Test30VSecondaryGenerator.cxx

--- pgenerators/FtfEvtGen/Test30VSecondaryGenerator.cxx
+++ pgenerators/FtfEvtGen/Test30VSecondaryGenerator.cxx
@@ -25,11 +25,11 @@
 //
 //
 // -------------------------------------------------------------
-//      GEANT 4 class
+//      GEANT 4 class 
 //
 //      ---------- Test30VSecondaryGenerator -------
-//                by Vladimir Ivanchenko, 12 March 2002
-//
+//                by Vladimir Ivanchenko, 12 March 2002 
+// 
 //    Modified:
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -50,22 +50,18 @@
 
 Test30VSecondaryGenerator::Test30VSecondaryGenerator(
     G4HadronicInteraction* hadi, const G4Material* mat):
-   generatorName(""),
-   hInteraction(hadi),
-   material(mat),
-   elm(0),
-   targetNucleus(), ///FIXME should we use the default constructor or pass the mat pointer here? Is targetNucleus relevant at all?
-   result(0),
-   targetZ(0),
-   targetA(0)
+  hInteraction(hadi),
+  material(mat),
+  targetA(0)
 {
   elm = material->GetElement(0);
   targetZ = G4lrint(elm->GetZ());
-  G4cout << "New generator for material " << material->GetName()
-	 << " Nelm= " <<  material->GetNumberOfElements()
-	 << " Nmat= " <<  material->GetNumberOfMaterials()
-	 << " Target element: " << elm->GetName()
+  G4cout << "New generator for material " << material->GetName() 
+	 << " Nelm= " <<  material->GetNumberOfElements() 
+	 << " Nmat= " <<  material->GetNumberOfMaterials() 
+	 << " Target element: " << elm->GetName() 
 	 << G4endl;
+  generatorName = "";
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -75,14 +71,14 @@ Test30VSecondaryGenerator::~Test30VSecondaryGenerator()
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
-void Test30VSecondaryGenerator::SetA(G4int A)
+void Test30VSecondaryGenerator::SetA(G4int A) 
 {
   targetA = A;
-  G4cout << "Nucleus with A= " << targetA << "  Z= " << targetZ
+  G4cout << "Nucleus with A= " << targetA << "  Z= " << targetZ 
 	 << "  A(amu)= " << elm->GetN();
   if(targetA < targetZ) {
-    targetA = 0;
-    G4cout << "  Natural abundances";
+    targetA = 0; 
+    G4cout << "  Natural abundances"; 
   } else {
     G4double mass = G4NucleiProperties::GetNuclearMass(targetA, targetZ);
     G4cout << "Mass from G4NucleiProperties(GeV)= " << mass/GeV << G4endl;
