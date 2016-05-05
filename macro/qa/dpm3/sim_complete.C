b1f943571fcdcd023d908aa3815f5645d65ab596
Stefano Spataro <spataro@to.infn.it>
2016-05-05 15:37:50 +0000

    g4->g3

--- macro/qa/dpm3/sim_complete.C
+++ macro/qa/dpm3/sim_complete.C
@@ -5,7 +5,7 @@
 // to run with different options:(e.g more events, different momentum, Geant4)
 // root  sim_complete.C"(100, "TGeant4",2)"
 
-sim_complete(Int_t nEvents = 10, TString  SimEngine ="TGeant4", Double_t BeamMomentum = 6.231552)
+sim_complete(Int_t nEvents = 10, TString  SimEngine ="TGeant3", Double_t BeamMomentum = 6.231552)
 {
   //-----User Settings:------------------------------------------------------
   TString  parAsciiFile   = "all.par";
