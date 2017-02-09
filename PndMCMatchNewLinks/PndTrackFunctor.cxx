11448485e8a157338d29a60292b5edd85d0399cb
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-02-09 15:51:19 +0000

    Add missing PndTrackFunctor

---
+++ PndMCMatchNewLinks/PndTrackFunctor.cxx
@@ -0,0 +1,24 @@
+/*
+ * PndTrackFunctor.cxx
+ *
+ *  Created on: 06.02.2017
+ *      Author: Stockmanns
+ */
+#include "PndTrackFunctor.h"
+
+PndTrackFunctor* PndTrackFunctor::make_PndTrackFunctor(std::string functorName)
+{
+	if (functorName.compare("StandardTrackFunctor") == 0)
+		return new StandardTrackFunctor();
+	if (functorName.compare("OnlySttFunctor") == 0)
+		return new OnlySttFunctor();
+	if (functorName.compare("RiemannMvdSttGemFunctor") == 0)
+		return new RiemannMvdSttGemFunctor();
+	if (functorName.compare("CircleHoughTrackFunctor") == 0)
+		return new CircleHoughTrackFunctor();
+	if (functorName.compare("FtsTrackFunctor") == 0)
+		return new FtsTrackFunctor();
+	else
+		return new AllTracksFunctor();
+}
+
