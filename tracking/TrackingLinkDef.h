d143ae2b56f1b88128cef47d7424051dfaeb1114
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-03-04 07:03:59 +0000

    Disable obsolete PndTrkQATask in TrackingLinkDef.h

--- tracking/TrackingLinkDef.h
+++ tracking/TrackingLinkDef.h
@@ -21,7 +21,7 @@
 #pragma link C++ class PndTrkSttClusterFinder+;
 #pragma link C++ class PndTrkTracking2+;
 #pragma link C++ class PndTrkSttAdjacencies+;
-#pragma link C++ class PndTrkQATask+;
+//#pragma link C++ class PndTrkQATask+;
 
 #pragma link C++ class PndTrkTools+;
 #pragma link C++ class PndTrkHitList+; 
