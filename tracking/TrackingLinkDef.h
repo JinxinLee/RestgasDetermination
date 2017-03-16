106805d836894ab8e62497eee1f7a7cc1cf29305
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-03-16 14:20:26 +0000

    Moving (parts of)tracking to PndTrackers/CentralTracking

--- tracking/TrackingLinkDef.h
+++ tracking/TrackingLinkDef.h
@@ -3,25 +3,6 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class PndTrkBoundaryParStraws+;
-#pragma link C++ class PndTrkBoundaryParStraws2+;
-#pragma link C++ class PndTrkCategorizeStt+;
-#pragma link C++ class PndTrkChi2Fits+;
-#pragma link C++ class PndTrkCleanup+;
-#pragma link C++ class PndTrkComparisonMCtruth+;
-#pragma link C++ class PndTrkCTFindTrackInXY+;
-#pragma link C++ class PndTrkCTFindTrackInXY2+;
-#pragma link C++ class PndTrkCTGeometryCalculations+;
-#pragma link C++ class PndTrkLegendreFits+;
-#pragma link C++ class PndTrkMergeSort+;
-#pragma link C++ class PndTrkPlotMacros+;
-#pragma link C++ class PndTrkPlotMacros2+;
-#pragma link C++ class PndTrkPrintouts+;
-#pragma link C++ class PndTrkSttConformalFilling+;
-#pragma link C++ class PndTrkSttClusterFinder+;
-#pragma link C++ class PndTrkTracking2+;
-#pragma link C++ class PndTrkSttAdjacencies+;
-//#pragma link C++ class PndTrkQATask+;
 
 #pragma link C++ class PndTrkTools+;
 #pragma link C++ class PndTrkHitList+; 
@@ -46,13 +27,9 @@
 #pragma link C++ class PndTrkConformalHit+;
 #pragma link C++ class PndTrkConformalHitList+;
 #pragma link C++ class PndTrkSkewHitList+;
-//#pragma link C++ class PndTrkQATask+;
-//#pragma link C++ class PndTrkQualityAssuranceTask+;
 #pragma link C++ class PndTrkClean+;
 #pragma link C++ class PndTrkFitter+;
 #pragma link C++ class PndTrkNeighboringMap+;
-//#pragma link C++ class PndTrkMCTrackInfo+;
-//#pragma link C++ class PndTrkRecoTrackInfo+;
-//#pragma link C++ class PndTrkAnaTask+;
+
 #endif
 
