bec9e8a199e7a75ccd59daebe812318b77ab21e0
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-03-16 14:20:39 +0000

    Moving (parts of)tracking to PndTrackers/CentralTracking

---
+++ PndTrackers/CentralTracking/PndTrkCategorizeStt.h
@@ -0,0 +1,44 @@
+#ifndef PndTrkCategorizeStt_H
+#define PndTrkCategorizeStt_H 1
+
+#include "TClonesArray.h"
+
+
+class PndTrkCategorizeStt
+{
+
+ public:
+
+ /** Default constructor **/
+ PndTrkCategorizeStt();
+
+ /** Destructor **/
+ ~PndTrkCategorizeStt();
+
+ void CategorizeStt(
+	// in this geametry there are 4542 Stt Straws (axial and skew);
+		const Short_t NUMBER_STRAWS,  // number of Stt Straws in total;
+		TClonesArray *SttTubeArray, // input; array of the Stt tubes;
+		Short_t &nAxialOuterRight,  // output; number of axial Stt, outer, on the right (looking into the beam);
+		Short_t &nAxialInnerRight,  // output; number of axial Stt, inner, on the right (looking into the beam);
+		Short_t &nAxialOuterLeft,  // output; number of axial Stt, outer, on the left (looking into the beam);
+		Short_t &nAxialInnerLeft,  // output; number of axial Stt, inner, on the left (looking into the beam);
+
+		Short_t *ListAxialOuterRight,  // output; list of axial Stt, outer, on the right (looking into the beam);
+		Short_t *ListAxialInnerRight,  // output; list of axial Stt, inner, on the lright (looking into the beam);
+		Short_t *ListAxialOuterLeft,  // output; list of axial Stt, outer, on the left (looking into the beam);
+		Short_t *ListAxialInnerLeft,  // output; list of axial Stt, inner, on the left (looking into the beam);
+
+		Short_t &nSkewRight,  // output; number of skew Stt, on the right (looking into the beam);
+		Short_t &nSkewLeft,  // output; number of skew Stt, on the right (looking into the beam);
+		Short_t *ListSkewRight,  // output; list of axial Stt, inner, on the lright (looking into the beam);
+		Short_t *ListSkewLeft  // output; list of axial Stt, outer, on the left (looking into the beam);
+			);
+
+
+
+  ClassDef(PndTrkCategorizeStt,1);
+
+};
+
+#endif
