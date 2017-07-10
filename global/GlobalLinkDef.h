eeed48d6e7cbc83af345996bea2f41bf995f2972
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-07-10 09:51:54 +0000

    Removing outdated QA stuff from build process

--- global/GlobalLinkDef.h
+++ global/GlobalLinkDef.h
@@ -10,12 +10,12 @@
 //data
 
 //#pragma link C++ class PndBarrelTrackFinder+;
-#pragma link C++ class PndBarrelTrackFinderQA+;
+//#pragma link C++ class PndBarrelTrackFinderQA+;
 //#pragma link C++ class PndEventBuilderManager+;
 //#pragma link C++ class PndEventBuilderOnMCEvents+;
 //#pragma link C++ class PndGlobalIdealTrackMerger+;
-#pragma link C++ class PndGlobalTrackFitQA+;
-#pragma link C++ class PndGlobalTrackMergerQA+;
+//#pragma link C++ class PndGlobalTrackFitQA+;
+//#pragma link C++ class PndGlobalTrackMergerQA+;
 #pragma link C++ class PndMvdGemTrackFinderOnHits+;
 //#pragma link C++ class PndFitMCCircle+;
 
