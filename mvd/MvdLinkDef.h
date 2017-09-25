ab7fed3fca861d54881ba4cc3f4f48dc001a8071
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2017-09-25 09:00:55 +0000

    Adding CRC Calculator to build process

--- mvd/MvdLinkDef.h
+++ mvd/MvdLinkDef.h
@@ -63,6 +63,9 @@
 #pragma link C++ class PndMvdConvertApvTask+;
 #pragma link C++ class PndMvdCreateDefaultApvMap;
 
+//#pragma link C++ class PndHammingDecoder;
+#pragma link C++ class PndCRCCalculator;
+
 //#pragma link C++ class PndMvdReadInTBData;
 //#pragma link C++ class PndMvdReadInTBDataTask;
 //#pragma link C++ class PndMapSorterTask;
