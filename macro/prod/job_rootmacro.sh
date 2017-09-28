fb917167f7276fd372e54d7df54ec8ffdebfffb9
Klaus Goetzen <K.Goetzen@gsi.de>
2017-09-28 07:17:49 +0000

    Updated job script to submit arbitrary root macro

--- macro/prod/job_rootmacro.sh
+++ macro/prod/job_rootmacro.sh
@@ -9,69 +9,37 @@ if [ $# -lt 1 ]; then
   echo -e "\nJob script for submission of arbitrary ROOT macro (with options -l -b -q) on KRONOS. *The macro needs to configured beforehand!*\n"
   echo -e "USAGE: sbatch job_rootmacro.sh '<macro>'\n"
   echo -e " <macro>   : Complete call of a ROOT macro."
-  echo -e " <logf>    : logfile name.\n"
-  echo -e "Example : sbatch job_rootmacro.sh 'mymacro.C+(1,2,3)' mylog.log\n\n"
+  echo -e " <logf>    : logfile name. If not given, a default is name is created and stored in subdir log/..\n"
+  echo -e "Example 1: sbatch job_rootmacro.sh 'mymacro.C+(1,2,3)' mylog.log\n"
+  echo -e "Example 2: sbatch job_rootmacro.sh 'mymacro.C+(1,2,3)' # --> creates logfile log_mymacro__1_2_3.log\n\n"
   
   exit 1
 fi
 
 nyx=$PWD
 
-#run=$SLURM_ARRAY_TASK_ID
-
-## create tmp directory to write stuff in
-#tmpdir="/tmp/"$USER"_"$SLURM_JOB_ID"_"$run"/"
-#mkdir $tmpdir
-#cd $tmpdir
-
-## check input parameters
-#if test "$1" != ""; then
-  #prefix=$1
-#fi
-logname=$nyx"/log_"$USER"_"$SLURM_JOB_ID"_"$run".log"
+# macro call is parameter 1
 macro=$1
 
+# prepare default logfile name from macro call
+logname=$(echo $1 | tr -d '["()+]'| sed 's/.C/__/'| tr '[," ()]' '_')
+logname="log__"$logname".log"
+
+# or take the given one
 if test "$2" != ""; then
   logname=$2
 fi
 
+# some log output
+echo "Macro    : "$macro
+echo "Log file : "$logname
+echo "ROOTSYS  : "$ROOTSYS
+
+
+# prepend absolute nyx path
 logname=$nyx"/"$logname
-# variable to hold prefix with run number
-#prefrun=$prefix"_"$run
-
-## replace place-holders PREFIX and RUN with actual values, in particular add absolute paths
-#macro=${macro/PREFIX/$prefix}
-#macro=${macro/RUN/$run}
-
-## if there is a dec file given with relative path, add the absolute path
-#decname=`expr "$macro" : '.*\"\(.*.dec\)\".*'`
-
-#if test "$decname" != ""; then
-  #if [[ $decname != \/* ]] ; then
-    #macro=${macro/$decname/$nyx"/"$decname}
-	#decname=$nyx"/"$decname
-  #fi
-#fi
-
-#echo "decay file : \"$decname\""
-
-# this is the command to be run
-echo "root -l -b -q $nyx/$macro &> $logname"
-echo $ROOTSYS
-
-## output and log files
-#outname=$prefrun"_ana.root"
-#logname=$prefrun"_quickfa.log"
+echo "Command  : root -l -b -q $nyx/$macro &> $logname"
 
 ## the actual command
 root -l -b -q "$nyx/$macro" &> $logname
-
-## check whether everything is there (in slurmlog visible)
-#ls -lh $tmpdir
-
-## move output and remove tmp dir
-#rm gphysi.dat
-#mv $tmpdir/* $_target
-#rm -rf $tmpdir
-
 
