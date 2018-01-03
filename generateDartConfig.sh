3d7227cac194f07b30ab06bfa7e8ef141668f2e3
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2018-01-03 15:38:02 +0100

    Modify generateDartConfig.sh to automatically call configSettings.sh in PandaRoot_Settings folder

--- generateDartConfig.sh
+++ generateDartConfig.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 unamestr=`uname`
 unameversion=`uname -r`
-source ~/.bashrc
+source $PANDAROOT_SETTINGS/configSettings.sh
 echo "#!/bin/bash" > Dart.cfg
 echo "export LINUX_FLAVOR=$unamestr$unameversion" >> Dart.cfg
 echo "export BUILDDIR=$PWD/../build" >> Dart.cfg
 echo "export SOURCEDIR=$PWD" >> Dart.cfg
 echo "export SIMPATH=$SIMPATH" >> Dart.cfg
 echo "export FAIRROOTPATH=$FAIRROOTPATH" >> Dart.cfg
-echo "export FAIRSOFT_VERSION=may16p1" >> Dart.cfg
-echo "export FAIRROOT_VERSION=v-17.10c" >> Dart.cfg
+echo "export FAIRSOFT_VERSION=$FAIRSOFT_VERSION" >> Dart.cfg
+echo "export FAIRROOT_VERSION=$FAIRROOT_VERSION" >> Dart.cfg
