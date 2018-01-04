e877c5db3378ecd0ec3207152815c06387921324
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2018-01-04 11:47:17 +0100

    Call .bashrc to set PANDAROOT_SETTINGS variable

--- generateDartConfig.sh
+++ generateDartConfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 unamestr=`uname`
 unameversion=`uname -r`
+source ~/.bashrc
 source $PANDAROOT_SETTINGS/configSettings.sh
 echo "#!/bin/bash" > Dart.cfg
 echo "export LINUX_FLAVOR=$unamestr$unameversion" >> Dart.cfg
