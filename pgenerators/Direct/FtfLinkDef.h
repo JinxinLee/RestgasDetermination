dabffcb14500ccd3f6960c17aa17f9558d1ec818
ralfk <r.kliemt@gsi.de>
2018-02-16 11:36:58 +0100

    pgenerators linkdef issue solved

--- pgenerators/Direct/FtfLinkDef.h
+++ pgenerators/Direct/FtfLinkDef.h
@@ -4,6 +4,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class  PndDpmDirect+;
 #pragma link C++ class  PndFtfDirect+;
 #pragma link C++ class  PndG4DummyPhysicsList+;
 #pragma link C++ class  PndG4DummyDetectorConstruction+;
--- pgenerators/PGenLinkDef.h
+++ pgenerators/PGenLinkDef.h
@@ -8,8 +8,6 @@
 #pragma link C++ class  PndDpmGenerator+;
 #pragma link C++ class  PndFtfGenerator+;
 #pragma link C++ class  PndEvtGenGenerator+;
-#pragma link C++ class  PndDpmDirect+;
-//#pragma link C++ class  PndFtfDirect+;
 #pragma link C++ class  PndBoxGenerator+;
 #pragma link C++ class  PndUrqmdSmmGenerator+;
 #pragma link C++ class  PndJRGenerator+;
