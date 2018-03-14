d56ad719a72e2caacfe5eccff464b75f654566e9
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2018-03-14 17:58:50 +0100

    New macros for track finders

---
+++ macro/tracking/trackingTasks/barrelTrackFinder.C
@@ -0,0 +1,37 @@
+// Macro for running Panda digitization tasks
+// to run the macro:
+// root  digi_complete.C  or in root session root>.x  digi_complete.C
+int barrelTrackFinder(Int_t nEvents = 0)
+{
+  //-----User Settings:------------------------------------------------------
+  TString  parAsciiFile   = "all.par";
+  TString  prefix         = "evtcomplete_1G5";
+  TString  input          = "psi2s_Jpsi2pi_Jpsi_mumu.dec";
+  TString  output         = "barreltracking";
+  TString  friend1        = "digi";
+  TString  friend2        = "digionly";
+  TString  friend3        = "";
+  TString  friend4        = "";
+  TString  fOptions       = "";
+
+  // -----   Initial Settings   --------------------------------------------
+  PndMasterRunAna *fRun= new PndMasterRunAna();
+  fRun->SetInput(input);
+  fRun->SetOutput(output);
+  fRun->SetFriend1(friend1);
+  fRun->SetFriend2(friend2);
+  fRun->SetFriend3(friend3);
+  fRun->SetFriend4(friend4);
+  fRun->SetParamAsciiFile(parAsciiFile);
+  fRun->Setup(prefix);
+
+  PndBarrelTrackFinder *tracking = new PndBarrelTrackFinder();
+  tracking->UseMvdSttGem(kTRUE, kTRUE, kTRUE);
+  fRun->AddTask(tracking);
+
+  // -----   Intialise and run   --------------------------------------------
+  fRun->Init();
+  fRun->Run(0, nEvents);
+  fRun->Finish();
+  return 0;
+}
