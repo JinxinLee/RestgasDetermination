0077e559ebe4a8e272e59a5ae0c4849a765e179e
Klaus Goetzen <johndoe@example.com>
2018-07-30 10:47:55 +0200

    delete scrips

--- macro/production/scrips/cntAnaEvts.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-nfiles=`ls $1*ana.log|wc -l`
-nsum=`tail -n 1 $1*ana.log| grep "PndSimpleCombinerTask"|cut -c 28- | awk '{ sum+=$1} END {print sum}'`
-
-echo "Found "$nfiles" files with pattern "$1
-echo "Sum of events="$nsum
