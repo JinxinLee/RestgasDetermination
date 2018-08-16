0305a08c0e82f48d745d160930a103149582c7ff
Stefan <stpflueg@uni-mainz.de>
2018-08-16 14:25:36 +0200

    - merged in changes from origin (in this case deleted all of the incoming code changes)

--- macro/detectors/lmd/runLumiPixel0SimDPM.C
+++ macro/detectors/lmd/runLumiPixel0SimDPM.C
@@ -174,38 +174,6 @@ int runLumiPixel0SimDPM(const int nEvents = 10, const int startEvent = 0,
 		}
 	}
 
-
-	bool misalignedGeometry = true;
-		if (misalignedGeometry) {
-			string misMatricesFilePath = "geo/misalignMatrices-SensorsOnly-100u-himster2.root";
-			// check if file exists, if true, try to read it
-			TFile *misalignmentMatrixRootfile = new TFile(misMatricesFilePath.c_str(), "READ");
-			if (misalignmentMatrixRootfile->IsOpen()) {
-				printf("File opened successfully\n");
-				std::map < std::string, TGeoHMatrix > *matrices;
-				gDirectory->GetObject("PndLmdMisalignMatrices", matrices);
-				misalignmentMatrixRootfile->Close();
-				cout << matrices->size() << " matrices successfully read from file.";
-				Lum->SetMisalignmentMatrices(*matrices);
-				cout << "matrix set!\n";
-				if (true) {
-					// checking for good measure
-					for (auto &i : *matrices) {
-						cout << "\n---\n" << i.first << "\n";
-						i.second.Print();
-					}
-				}
-			}
-			// if not, fail violently
-			else {
-				cerr << "WARNING. I was instructed to use misaligned geometry,\n";
-				cerr << "but no misaligned matrices could be found in " << misMatricesFilePath << "\n";
-				return 1;
-			}
-		}
-
-
-
 	fRun->Init();
 	((TGeant4*)gMC)->ProcessGeantCommand("/mcVerbose/eventAction 0");
 	((TGeant4*)gMC)->ProcessGeantCommand("/mcTracking/loopVerbose 0");
