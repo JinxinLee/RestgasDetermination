b850a157d792acc771b53827362c896c1e83f9ef
Roman Klasen <klasen@kph.uni-mainz.de>
2018-08-20 15:23:08 +0200

    Added ThreadPool.h

---
+++ detectors/lmd/LmdSensorAlignment/PndLmdThreadPool.h
@@ -0,0 +1,71 @@
+/*
+ * Thread Pool implementation using boost::threads.
+ *
+ * taken from this StackOverflow answer:
+ * https://stackoverflow.com/questions/22685176/boost-group-threads-maximal-number-of-parallel-thread
+ *
+ */
+
+#include <boost/thread.hpp>
+#include <boost/phoenix.hpp>
+#include <boost/optional.hpp>
+
+using namespace boost;
+using namespace boost::phoenix::arg_names;
+
+boost::atomic_size_t counter(0ul);
+
+class thread_pool {
+private:
+	mutex mx;
+	condition_variable cv;
+
+	typedef function<void()> job_t;
+	std::deque<job_t> _queue;
+
+	thread_group pool;
+
+	boost::atomic_bool shutdown;
+	static void worker_thread(thread_pool& q) {
+		while (optional<job_t> job = q.dequeue())
+			(*job)();
+	}
+
+public:
+	thread_pool() :
+			shutdown(false) {
+		for (unsigned i = 0; i < boost::thread::hardware_concurrency(); ++i)
+			pool.create_thread(bind(worker_thread, ref(*this)));
+	}
+
+	void enqueue(job_t job) {
+		lock_guard<mutex> lk(mx);
+		_queue.push_back(job);
+
+		cv.notify_one();
+	}
+
+	optional<job_t> dequeue() {
+		unique_lock<mutex> lk(mx);
+		namespace phx = boost::phoenix;
+
+		cv.wait(lk, phx::ref(shutdown) || !phx::empty(phx::ref(_queue)));
+
+		if (_queue.empty()) return none;
+
+		job_t job = _queue.front();
+		_queue.pop_front();
+
+		return job;
+	}
+
+	~thread_pool() {
+		shutdown = true;
+		{
+			lock_guard<mutex> lk(mx);
+			cv.notify_all();
+		}
+
+		pool.join_all();
+	}
+};
