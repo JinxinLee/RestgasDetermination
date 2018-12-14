6a57844a7291ecc583eb06bda105df4bc08a9717
Roman Klasen <klasen@kph.uni-mainz.de>
2018-12-14 14:28:49 +0100

    Further rename

--- detectors/lmd/LmdDigi/PndLmdHybridHitProducer.cxx
+++ detectors/lmd/LmdDigi/PndLmdHybridHitProducer.cxx
@@ -59,7 +59,7 @@ void PndLmdHybridHitProducer::SetParContainers()
   FairRun* ana = FairRun::Instance();
   FairRuntimeDb* rtdb=ana->GetRuntimeDb();
   fDigiPar = (PndSdsPixelDigiPar*)(rtdb->getContainer("LMDPixelDigiPar"));
-  // fTotDigiPar = (PndSdsTotDigiPar*)(rtdb->getContainer("SDSPixelTotDigiPar"));
+  fTotDigiPar = (PndSdsTotDigiPar*)(rtdb->getContainer("LMDPixelTotDigiPar"));
   PndSdsHybridHitProducer::SetParContainers();
 }
 
